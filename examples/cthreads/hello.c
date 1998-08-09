91617082947f18e2d76edc78f41f65bf8defa6d5
sameer <sameer@a8e31b63-e6ca-409b-a01b-2eec1b506579>
1998-08-09 21:19:15 +0000

    Added an example program that illustrates use of C with the new TAU C API. It also demonstrates pthreads support.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@121 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ examples/cthreads/hello.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <Profile/Profiler.h>
+
+int fourth(void)
+{
+  TAU_PROFILE_TIMER(tautimer,"fourth()", "int ()", TAU_DEFAULT);
+  TAU_PROFILE_START(tautimer);
+  printf("Reached fourth\n");
+  TAU_PROFILE_STOP(tautimer);
+  return 0;
+}
+
+int third(void)
+{
+  TAU_PROFILE_TIMER(tautimer,"third()", "int ()", TAU_DEFAULT);
+  TAU_PROFILE_START(tautimer);
+  printf("third calling fourth()\n");
+  fourth();
+  TAU_PROFILE_STOP(tautimer);
+  return 0;
+}
+
+int second(void)
+{
+  TAU_PROFILE_TIMER(tautimer,"second()", "int ()", TAU_DEFAULT);
+  TAU_PROFILE_START(tautimer);
+  printf("second calling third\n");
+  third();
+  TAU_PROFILE_STOP(tautimer);
+  return 0;
+}
+
+int first(void)
+{ 
+  TAU_PROFILE_TIMER(tautimer,"first()", "int ()", TAU_DEFAULT);
+  TAU_PROFILE_START(tautimer);
+  printf("first.. calling second \n");
+  second();
+  TAU_PROFILE_STOP(tautimer);
+  return 0;
+}
+
+int work (void)
+{
+  TAU_PROFILE_TIMER(tautimer, "work()", "int ()", TAU_DEFAULT);
+  TAU_PROFILE_START(tautimer);
+  printf("Hello. This is thread work calling first\n");
+  sleep(5);
+  first(); 
+  TAU_PROFILE_STOP(tautimer);
+  return 0;
+}
+void * threaded_func(void *data)
+{
+  TAU_REGISTER_THREAD();
+  { /**** NOTE WE START ANOTHER BLOCK IN THREAD */
+    TAU_PROFILE_TIMER(tautimer, "threaded_func()", "int ()", TAU_DEFAULT);
+    TAU_PROFILE_START(tautimer);
+    work(); // work done by this thread 
+    TAU_PROFILE_STOP(tautimer);
+  }
+  return NULL;
+}
+
+int main (int argc, char **argv)
+{
+  TAU_PROFILE_TIMER(tautimer,"main()", "int (int, char **)", TAU_DEFAULT);
+  TAU_PROFILE_START(tautimer);
+  TAU_PROFILE_INIT(argc, argv);
+  TAU_PROFILE_SET_NODE(0);
+  int ret;
+  pthread_attr_t  attr;
+  pthread_t	  tid;
+
+  pthread_attr_init(&attr);
+  
+  printf("Started Main...\n");
+
+  if (ret = pthread_create(&tid, NULL, threaded_func, NULL) ) 
+  { 
+    printf(" pthread_create fails ret = %d\n", ret); 
+    TAU_PROFILE_EXIT("pthread_create");
+    exit(1);
+  }
+
+  //first();
+
+  if (ret = pthread_join(tid, NULL) ) 
+  {
+    printf(" pthread_join error  ret = %d\n", ret);
+    TAU_PROFILE_EXIT("pthread_join");
+    exit(1); 
+  }
+
+  printf("Exiting main...\n");
+  TAU_PROFILE_STOP(tautimer);
+  return 0;
+}
