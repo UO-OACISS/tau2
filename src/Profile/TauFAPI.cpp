c1cd52b77e51717e77e728a1d5cd4f47656f6b84
sameer <sameer@a8e31b63-e6ca-409b-a01b-2eec1b506579>
1998-09-17 15:24:54 +0000

    Added support for Fortran 77/Fortran 90 in the form of a thin layer that sits atop the TauCAPI. It provides the same type of interface as C/C++. e.g., call TAU_PROFILE('main', 'void', TAU_DEFAULT) call TAU_PROFILE_SET_NODE(0)
    etc.
    
    
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@177 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ src/Profile/TauFAPI.cpp
@@ -0,0 +1,88 @@
+/* Fortran Wrapper layer for TAU Portable Profiling */
+#include <stdio.h>
+
+/* 
+#define DEBUG_PROF
+*/
+extern "C" {
+void * tau_get_profiler(char *, char *, unsigned int);
+void tau_start_timer(void *);
+void tau_stop_timer(void *);
+void tau_exit(char *);
+void tau_init(int, char **);
+void tau_set_node(int);
+void tau_set_context(int);
+void tau_register_thread(void);
+
+/*****************************************************************************
+* The following routines are called by the Fortran program and they in turn
+* invoke the corresponding C routines. 
+*****************************************************************************/
+void tau_profile_timer_(void **ptr, char *fname, char *type, unsigned int *group)
+{
+#ifdef DEBUG_PROF
+  printf("Inside tau_profile_timer_ fname=%s, type=%s *ptr = %x\n", 
+    fname, type, *ptr);
+#endif /* DEBUG_PROF */
+
+  if (*ptr == 0) 
+  { 
+    *ptr = tau_get_profiler(fname, type, *group);
+  }
+
+#ifdef DEBUG_PROF 
+  printf("get_profiler returns %x\n", *ptr);
+#endif /* DEBUG_PROF */
+
+  return;
+}
+
+
+void tau_profile_start_(void **profiler)
+{ 
+#ifdef DEBUG_PROF
+  printf("start_timer gets %x\n", *profiler);
+#endif /* DEBUG_PROF */
+
+  tau_start_timer(*profiler);
+  return;
+}
+
+void tau_profile_stop_(void **profiler)
+{
+  tau_stop_timer(*profiler);
+  return;
+}
+
+void tau_profile_exit_(char *msg)
+{
+  tau_exit(msg);
+  return;
+}
+
+void tau_profile_init_(int *argc, char **argv)
+{
+  tau_init(*argc, argv);
+  return;
+}
+
+void tau_profile_set_node_(int *node)
+{
+  tau_set_node(*node);
+  return;
+} 
+
+void tau_profile_set_context_(int *context)
+{
+  tau_set_context(*context);
+  return;
+}
+
+#if (defined (PTHREADS) || defined (TULIPTHREADS))
+void tau_register_thread_(void)
+{
+  tau_register_thread();
+  return;
+}
+#endif 
+} /* extern "C" */
