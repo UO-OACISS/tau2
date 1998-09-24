9facfb3782715fc5de89b01ec18427bdc89e370c
sameer <sameer@a8e31b63-e6ca-409b-a01b-2eec1b506579>
1998-09-24 17:06:36 +0000

    Added an example of using TAU user defined events.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@199 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- examples/threads/hello.cpp
+++ examples/threads/hello.cpp
@@ -5,9 +5,13 @@
 #include <unistd.h>
 #include <Profile/Profiler.h>
 
+TAU_REGISTER_EVENT(iters, "Number of Iterates");
+TAU_REGISTER_EVENT(mem, "Memory allocated by arrays");
+
 int fourth(void)
 {
   TAU_PROFILE("fourth()", "int ()", TAU_DEFAULT);
+  TAU_EVENT(iters, 1);
   cout <<"Reached fourth " << endl;
   return 0;
 }
@@ -16,6 +20,8 @@ int third(void)
 {
   TAU_PROFILE("third()", "int ()", TAU_DEFAULT);
   cout <<"third calling fourth " <<endl;
+  TAU_EVENT(mem, 1024);
+  TAU_EVENT(iters, 1);
   fourth();
   return 0;
 }
@@ -24,6 +30,8 @@ int second(void)
 {
   TAU_PROFILE("second()", "int ()", TAU_DEFAULT);
   cout <<"second calling third " <<endl;
+  TAU_EVENT(mem, 16*1024);
+  TAU_EVENT(iters, 1);
   third();
   return 0;
 }
@@ -32,6 +40,7 @@ int first(void)
 { 
   TAU_PROFILE("first()", "int ()", TAU_DEFAULT);
   cout << "first.. calling second. " << endl;
+  TAU_EVENT(iters, 1);
   second();
   return 0;
 }
@@ -40,6 +49,8 @@ int work (void)
 {
   TAU_PROFILE("work()", "int ()", TAU_DEFAULT);
   cout << " Hello this is thread "<< endl;
+  TAU_EVENT(iters, 1);
+  TAU_EVENT(mem, 4096);
   sleep(5);
   first(); 
   return 0;
@@ -48,6 +59,7 @@ void * threaded_func(void *data)
 {
   TAU_REGISTER_THREAD();
   TAU_PROFILE("threaded_func()", "int ()", TAU_DEFAULT);
+  TAU_EVENT(iters, 1);
   work(); // work done by this thread 
   return NULL;
 }
@@ -70,7 +82,9 @@ int main (int argc, char **argv)
     cerr << " pthread_create fails ret = " << ret <<endl;
     exit(1);
   }
+  TAU_EVENT(iters, 1);
 
+  TAU_EVENT(mem, 2048);
   //first();
 
   if (ret = pthread_join(tid, NULL) ) 
@@ -78,6 +92,8 @@ int main (int argc, char **argv)
     cerr << " pthread_join error  ret = "<< ret << endl;
     exit(1); 
   }
+  //TAU_REPORT_THREAD_STATISTICS();
+  TAU_REPORT_STATISTICS();
 
   cout <<"Exiting main ..."<<endl;
   return 0;
