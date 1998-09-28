80494cd81eadd69aef5e88a30e326b653a66c540
sameer <sameer@a8e31b63-e6ca-409b-a01b-2eec1b506579>
1998-09-28 14:15:39 +0000

    Added the length parameters to the strings fname and type in the two tau_profile_timer routine calls.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@211 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- examples/fortran/hello.f
+++ examples/fortran/hello.f
@@ -6,14 +6,14 @@ cc-----------------------------------------------------------------------------
 
       program main
       include 'Profile/TauFAPI.h'
-        integer group(2), error, x
+        integer group(2), error, x, i
         integer profiler(2)
 	save profiler
 cc Remember to 'save' profiler object to ensure static like behavior. This
 cc allows name registration overhead to be restricted to the first time 
 cc the function is called.
 
-        call TAU_PROFILE_TIMER(profiler, 'main()', '',TAU_DEFAULT)
+        call TAU_PROFILE_TIMER(profiler, 'main()', 6,'', 0, TAU_DEFAULT)
         call TAU_PROFILE_START(profiler)
         call TAU_PROFILE_SET_NODE(0)
 
@@ -32,7 +32,8 @@ cc the function is called.
         integer profiler(2)
         save    profiler
 
-        call TAU_PROFILE_TIMER(profiler,'HelloWorld()', ' ',TAU_DEFAULT)
+        call TAU_PROFILE_TIMER(profiler,'HelloWorld()', 12, ' ', 1, 
+     c TAU_DEFAULT)
         call TAU_PROFILE_START(profiler)
 cc Do something here...
  	print *, "Iteration = ", iVal
