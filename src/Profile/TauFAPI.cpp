dc8af940bde0eed9d05a1e82f533d49daa762912
sameer <sameer@a8e31b63-e6ca-409b-a01b-2eec1b506579>
1999-04-22 23:12:32 +0000

    Changes for Cray T3E Fortran 90 support.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@286 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- src/Profile/TauFAPI.cpp
+++ src/Profile/TauFAPI.cpp
@@ -1,9 +1,11 @@
 /* Fortran Wrapper layer for TAU Portable Profiling */
 #include <stdio.h>
+#include "Profile/ProfileGroups.h"
 
 /* 
 #define DEBUG_PROF
 */
+
 extern "C" {
 void * tau_get_profiler(char *, char *, unsigned int);
 void tau_start_timer(void *);
@@ -86,5 +88,66 @@ void tau_register_thread_(void)
   tau_register_thread();
   return;
 }
+
+/* Cray F90 specific extensions */
+void TAU_REGISTER_THREAD(void)
+{
+  tau_register_thread();
+}
 #endif 
+/* Cray F90 specific extensions */
+void _main();
+void TAU_PROFILE_TIMER(void **ptr, char *fname, int *flen, char *type, int *tlen, unsigned int *group)
+{
+#ifdef DEBUG_PROF
+  printf("Inside tau_profile_timer_ fname=%s, type=%s *ptr = %x\n", 
+    fname, type, *ptr);
+#endif /* DEBUG_PROF */
+
+
+  if (*ptr == 0) 
+  { 
+#ifdef DEBUG_PROF
+    printf("tau_get_profiler() \n");
+#endif /* DEBUG_PROF */
+    *ptr = tau_get_profiler(fname, " ", TAU_DEFAULT);
+  }
+
+#ifdef DEBUG_PROF 
+  printf("get_profiler returns %x\n", *ptr);
+#endif /* DEBUG_PROF */
+
+  return;
+}
+
+void TAU_PROFILE_START(void **profiler)
+{
+  tau_profile_start_(profiler);
+}
+
+void TAU_PROFILE_STOP(void **profiler)
+{
+  tau_profile_stop_(profiler);
+}
+
+void TAU_PROFILE_EXIT(char *msg)
+{
+  tau_profile_exit_(msg);
+}
+
+void TAU_PROFILE_INIT()
+{
+  _main();
+  //tau_profile_init_(argc, argv);
+}
+
+void TAU_PROFILE_SET_NODE(int *node)
+{
+  tau_profile_set_node_(node);
+}
+
+void TAU_PROFILE_SET_CONTEXT(int *context)
+{
+  tau_profile_set_context_(context);
+}
 } /* extern "C" */
