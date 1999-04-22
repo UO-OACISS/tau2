4ca577b2cad9ed4a44cadbd9e359ca1dda0b1947
sameer <sameer@a8e31b63-e6ca-409b-a01b-2eec1b506579>
1999-04-22 23:06:55 +0000

    Changes for compiling with F90 on Cray T3E.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@283 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- examples/fortran/hello.f
+++ examples/fortran/hello.f
@@ -4,15 +4,27 @@ cc This file contains code for testing the Fortran interface to just the
 cc HPC++ init and exit functions.
 cc-----------------------------------------------------------------------------
 
+      subroutine HELLOWORLD(iVal)
+      include 'Profile/TauFAPI.h'
+        integer iVal
+        integer profiler(2)
+        save    profiler
+
+        call TAU_PROFILE_TIMER(profiler,'HelloWorld()', 12, '', 0, 
+     c TAU_DEFAULT)
+        call TAU_PROFILE_START(profiler)
+cc Do something here...
+ 	print *, "Iteration = ", iVal
+        call TAU_PROFILE_STOP(profiler)
+cc       HelloWorld = iVal
+      end
+
       program main
       include 'Profile/TauFAPI.h'
         integer group(2), error, x, i
         integer profiler(2)
 	save profiler
-cc Remember to 'save' profiler object to ensure static like behavior. This
-cc allows name registration overhead to be restricted to the first time 
-cc the function is called.
-
+        call TAU_PROFILE_INIT()
         call TAU_PROFILE_TIMER(profiler, 'main()', 6,'', 0, TAU_DEFAULT)
         call TAU_PROFILE_START(profiler)
         call TAU_PROFILE_SET_NODE(0)
@@ -20,23 +32,8 @@ cc the function is called.
       write(6,*) "hello world"
 
         do 10, i = 1, 10
-        x = HelloWorld(i)
+        call HELLOWORLD(i)
 10      continue
         call TAU_PROFILE_STOP(profiler)
-      stop
       end
 
-      integer function HelloWorld(iVal)
-      include 'Profile/TauFAPI.h'
-        integer iVal
-        integer profiler(2)
-        save    profiler
-
-        call TAU_PROFILE_TIMER(profiler,'HelloWorld()', 12, ' ', 1, 
-     c TAU_DEFAULT)
-        call TAU_PROFILE_START(profiler)
-cc Do something here...
- 	print *, "Iteration = ", iVal
-        call TAU_PROFILE_STOP(profiler)
-       HelloWorld = iVal
-      end
