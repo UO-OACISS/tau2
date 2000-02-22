abaf899ebea2fe070ca21073f41db2b74e66008d
bertie <bertie@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2000-02-22 22:50:26 +0000

    Support for Papi.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@557 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ include/Profile/PapiLayer.h
@@ -0,0 +1,47 @@
+/////////////////////////////////////////////////
+//Class definintion file for the PCL_Layer class.
+//
+//Author:   Robert Ansell-Bell
+//Created:  February 2000
+//
+/////////////////////////////////////////////////
+
+#ifndef _PAPI_LAYER_H_
+#define _PAPI_LAYER_H_
+
+#ifdef TAU_PAPI
+#include "papiStdEventDefs.h"
+#include "papi.h"
+#include "papi_internal.h"
+
+
+  struct ThreadValue{
+  int ThreadID;
+  long long CounterValue;
+  };
+
+
+
+class PapiLayer
+{
+
+  //No need to define constructors and destructors.
+  //The default ones will do.
+
+  public:
+    //Default getCounters ... without sychronization of resources.
+    static long long getCounters(int tid);
+};
+
+#endif /* TAU_PAPI */
+#endif /* _PAPI_LAYER_H_ */
+
+/////////////////////////////////////////////////
+//
+//End PCL_Layer class definition.
+//
+/////////////////////////////////////////////////
+
+
+
+
