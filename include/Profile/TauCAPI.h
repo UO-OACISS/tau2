a2be17f77769e4c385a8f9774c833e7ae83d5481
sameer <sameer@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2002-01-15 22:27:42 +0000

    Changed the C API to have an uppercase first letter as in Tau_set_node instead of the old tau_set_node. This prevents name clashes between C and Fortran. This idea is not new. It is used in naming MPI routines with a capital first letter as in MPI_Send_recv().
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@977 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- include/Profile/TauCAPI.h
+++ include/Profile/TauCAPI.h
@@ -43,94 +43,94 @@
 #define TAU_PROFILE_TIMER(var,name, type, group) static int taufirst##var = 1;\
                                  static void *var; \
                                  if (taufirst##var == 1) { \
-                                   var = tau_get_profiler(name, type, group, #group); \
+                                   var = Tau_get_profiler(name, type, group, #group); \
                                    taufirst##var = 0; }
 */
 
-#define TAU_PROFILE_TIMER(var,name, type, group) static void *var=NULL; tau_profile_c_timer(&var, name, type, group, #group);
+#define TAU_PROFILE_TIMER(var,name, type, group) static void *var=NULL; Tau_profile_c_timer(&var, name, type, group, #group);
 
 #define TAU_PROFILE_DECLARE_TIMER(var) static void *var=NULL;
-#define TAU_PROFILE_CREATE_TIMER(var,name,type,group) tau_profile_c_timer(&var, name, type, group, #group);
+#define TAU_PROFILE_CREATE_TIMER(var,name,type,group) Tau_profile_c_timer(&var, name, type, group, #group);
 
 
-#define TAU_PROFILE_START(var) 			tau_start_timer(var);
-#define TAU_PROFILE_STOP(var) 			tau_stop_timer(var);
+#define TAU_PROFILE_START(var) 			Tau_start_timer(var);
+#define TAU_PROFILE_STOP(var) 			Tau_stop_timer(var);
 #define TAU_PROFILE_STMT(stmt) 			stmt;
-#define TAU_PROFILE_EXIT(msg)  			tau_exit(msg);
-#define TAU_PROFILE_INIT(argc, argv)		tau_init(argc, argv);
-#define TAU_INIT(argc, argv)			tau_init_ref(argc, argv);
-#define TAU_PROFILE_SET_NODE(node) 		tau_set_node(node);
-#define TAU_PROFILE_SET_CONTEXT(context)	tau_set_context(context);
-#define TAU_PROFILE_CALLSTACK()			tau_callstack();
-#define TAU_DB_DUMP()                           tau_dump();
-#define TAU_DB_PURGE()                          tau_purge();
+#define TAU_PROFILE_EXIT(msg)  			Tau_exit(msg);
+#define TAU_PROFILE_INIT(argc, argv)		Tau_init(argc, argv);
+#define TAU_INIT(argc, argv)			Tau_init_ref(argc, argv);
+#define TAU_PROFILE_SET_NODE(node) 		Tau_set_node(node);
+#define TAU_PROFILE_SET_CONTEXT(context)	Tau_set_context(context);
+#define TAU_PROFILE_CALLSTACK()			Tau_callstack();
+#define TAU_DB_DUMP()                           Tau_dump();
+#define TAU_DB_PURGE()                          Tau_purge();
 
 /*
 #define TAU_REGISTER_EVENT(event, name)	static int tauuser##event = 1;\
 				        static void *event; \ 
 					if (tauuser##event == 1) { \ 
-					  event = tau_get_userevent(name); \
+					  event = Tau_get_userevent(name); \
 					  tauuser##event = 0; }
 */
 #define TAU_REGISTER_EVENT(event, name)	static int taufirst##event = 1;\
                                  static void *event; \
                                  if (taufirst##event == 1) { \
-                                   event = tau_get_userevent(name); \
+                                   event = Tau_get_userevent(name); \
                                    taufirst##event = 0; }
 				
-#define TAU_EVENT(event, data)			tau_userevent(event, data);
-#define TAU_REPORT_STATISTICS()		tau_report_statistics();
-#define TAU_REPORT_THREAD_STATISTICS()  tau_report_thread_statistics();
-#define TAU_EVENT_DISABLE_MIN(event) 	tau_event_disable_min(event);
-#define TAU_EVENT_DISABLE_MAX(event)	tau_event_disable_max(event);
-#define TAU_EVENT_DISABLE_MEAN(event)	tau_event_disable_mean(event);
-#define TAU_EVENT_DISABLE_STDDEV(event) tau_event_disable_stddev(event);
+#define TAU_EVENT(event, data)			Tau_userevent(event, data);
+#define TAU_REPORT_STATISTICS()		Tau_report_statistics();
+#define TAU_REPORT_THREAD_STATISTICS()  Tau_report_thread_statistics();
+#define TAU_EVENT_DISABLE_MIN(event) 	Tau_event_disable_min(event);
+#define TAU_EVENT_DISABLE_MAX(event)	Tau_event_disable_max(event);
+#define TAU_EVENT_DISABLE_MEAN(event)	Tau_event_disable_mean(event);
+#define TAU_EVENT_DISABLE_STDDEV(event) Tau_event_disable_stddev(event);
 #define TAU_STORE_ALL_EVENTS
 #define TYPE_STRING(profileString, str)
 #define PROFILED_BLOCK(name, type)
 /* C doesn't support runtime type information */
 #define CT(obj)
 
-#define TAU_REGISTER_THREAD()			tau_register_thread();	
-#define TAU_REGISTER_FORK(nodeid, op) 		tau_register_fork(nodeid, op);
-#define TAU_ENABLE_INSTRUMENTATION()		tau_enable_instrumentation();
-#define TAU_DISABLE_INSTRUMENTATION()		tau_disable_instrumentation();
-#define TAU_ENABLE_GROUP(group)			tau_enable_group(group);
-#define TAU_DISABLE_GROUP(group)		tau_disable_group(group);
-#define TAU_ENABLE_GROUP_NAME(group)            tau_enable_group_name(group) 
-#define TAU_DISABLE_GROUP_NAME(group)           tau_disable_group_name(group)
-#define TAU_ENABLE_ALL_GROUPS()            	tau_enable_all_groups() 
-#define TAU_DISABLE_ALL_GROUPS()            	tau_disable_all_groups() 
-#define TAU_DISABLE_GROUP_NAME(group)           tau_disable_group_name(group)
-#define TAU_GET_PROFILE_GROUP(group)            tau_get_profile_group(group)
+#define TAU_REGISTER_THREAD()			Tau_register_thread();	
+#define TAU_REGISTER_FORK(nodeid, op) 		Tau_register_fork(nodeid, op);
+#define TAU_ENABLE_INSTRUMENTATION()		Tau_enable_instrumentation();
+#define TAU_DISABLE_INSTRUMENTATION()		Tau_disable_instrumentation();
+#define TAU_ENABLE_GROUP(group)			Tau_enable_group(group);
+#define TAU_DISABLE_GROUP(group)		Tau_disable_group(group);
+#define TAU_ENABLE_GROUP_NAME(group)            Tau_enable_group_name(group) 
+#define TAU_DISABLE_GROUP_NAME(group)           Tau_disable_group_name(group)
+#define TAU_ENABLE_ALL_GROUPS()            	Tau_enable_all_groups() 
+#define TAU_DISABLE_ALL_GROUPS()            	Tau_disable_all_groups() 
+#define TAU_DISABLE_GROUP_NAME(group)           Tau_disable_group_name(group)
+#define TAU_GET_PROFILE_GROUP(group)            Tau_get_profile_group(group)
 
 
-extern void * tau_get_profiler(char *fname, char *type, TauGroup_t  group, char *gr_name);
-extern void tau_start_timer(void *profiler);
-extern void tau_stop_timer(void *profiler);
-extern void tau_exit(char *msg);
-extern void tau_init(int argc, char **argv);
-extern void tau_init_ref(int* argc, char ***argv);
-extern void tau_set_node(int node);
-extern void tau_set_context(int context);
-extern void tau_callstack(void);
-extern int tau_dump(void);
-extern void tau_purge(void);
-extern void tau_register_thread();
-extern void tau_register_fork(int nodeid, enum TauFork_t opcode);
-extern void * tau_get_userevent(char *name);
-extern void tau_userevent(void *event, double data);
-extern void tau_report_statistics(void);
-extern void tau_report_thread_statistics(void);
-extern void tau_event_disable_min(void *event);
-extern void tau_event_disable_max(void *event);
-extern void tau_event_disable_mean(void *event);
-extern void tau_event_disable_stddev(void *event);
-extern void tau_trace_sendmsg(int type, int destination, int length);
-extern void tau_trace_recvmsg(int type, int source, int length);
-extern TauGroup_t tau_enable_group_name(char *group);
-extern TauGroup_t tau_disable_group_name(char *group);
-extern TauGroup_t tau_get_profile_group(char *group);
+extern void * Tau_get_profiler(char *fname, char *type, TauGroup_t  group, char *gr_name);
+extern void Tau_start_timer(void *profiler);
+extern void Tau_stop_timer(void *profiler);
+extern void Tau_exit(char *msg);
+extern void Tau_init(int argc, char **argv);
+extern void Tau_init_ref(int* argc, char ***argv);
+extern void Tau_set_node(int node);
+extern void Tau_set_context(int context);
+extern void Tau_callstack(void);
+extern int Tau_dump(void);
+extern void Tau_purge(void);
+extern void Tau_register_thread();
+extern void Tau_register_fork(int nodeid, enum TauFork_t opcode);
+extern void * Tau_get_userevent(char *name);
+extern void Tau_userevent(void *event, double data);
+extern void Tau_report_statistics(void);
+extern void Tau_report_thread_statistics(void);
+extern void Tau_event_disable_min(void *event);
+extern void Tau_event_disable_max(void *event);
+extern void Tau_event_disable_mean(void *event);
+extern void Tau_event_disable_stddev(void *event);
+extern void Tau_trace_sendmsg(int type, int destination, int length);
+extern void Tau_trace_recvmsg(int type, int source, int length);
+extern TauGroup_t Tau_enable_group_name(char *group);
+extern TauGroup_t Tau_disable_group_name(char *group);
+extern TauGroup_t Tau_get_profile_group(char *group);
 
 
 
@@ -178,9 +178,9 @@ extern TauGroup_t tau_get_profile_group(char *group);
 
 #ifdef TRACING_ON
 #define TAU_TRACE_SENDMSG(type, destination, length) \
-        tau_trace_sendmsg(type, destination, length);
+        Tau_trace_sendmsg(type, destination, length);
 #define TAU_TRACE_RECVMSG(type, source, length) \
-        tau_trace_recvmsg(type, source, length);
+        Tau_trace_recvmsg(type, source, length);
 
 #else /* TRACING_ON */
 #define TAU_TRACE_SENDMSG(type, destination, length)
@@ -193,7 +193,7 @@ extern TauGroup_t tau_get_profile_group(char *group);
 
 /***************************************************************************
  * $RCSfile: TauCAPI.h,v $   $Author: sameer $
- * $Revision: 1.15 $   $Date: 2002/01/15 21:30:12 $
- * POOMA_VERSION_ID: $Id: TauCAPI.h,v 1.15 2002/01/15 21:30:12 sameer Exp $
+ * $Revision: 1.16 $   $Date: 2002/01/15 22:27:42 $
+ * POOMA_VERSION_ID: $Id: TauCAPI.h,v 1.16 2002/01/15 22:27:42 sameer Exp $
  ***************************************************************************/
 
