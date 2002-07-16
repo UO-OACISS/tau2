0b77544a6ef038010d336add600b1281fd116c76
sameer <sameer@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2002-07-16 21:57:04 +0000

    Added support for IA-64 TSC (-LINUXTIMERS).
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@1208 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- src/Profile/TauLinuxTimers.c
+++ src/Profile/TauLinuxTimers.c
@@ -1,11 +1,21 @@
 
 ///////////////////////////////////////////////////////////////////////////
-// High resolution timers. Compile with gcc under Linux on IA-32 systems. 
+// High resolution timers. Compile with gcc under Linux on IA-32/IA-64 systems. 
 ///////////////////////////////////////////////////////////////////////////
+#ifdef __ia64__
+inline unsigned long long getLinuxHighResolutionTscCounter(void)
+{
+  unsigned long long tmp;
+  __asm__ __volatile__("mov %0=ar.itc" : "=r"(tmp) :: "memory");
+  return tmp;
+}
+#else /* IA64 */
 inline unsigned long long getLinuxHighResolutionTscCounter(void)
 {
    unsigned long high, low;
    __asm__ __volatile__(".byte 0x0f,0x31" : "=a" (low), "=d" (high));
    return ((unsigned long long) high << 32) + low;
 }
+#endif /* IA64 */
 
+  
