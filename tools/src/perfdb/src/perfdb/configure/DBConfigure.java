7916668c7fedb95834af380179c56fb325129ec2
bertie <bertie@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2003-05-28 19:39:12 +0000

    Almost done. Added the buttons and fields.  Just need them to do something now. :-)
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@1469 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- tools/src/perfdb/src/perfdb/configure/DBConfigure.java
+++ tools/src/perfdb/src/perfdb/configure/DBConfigure.java
@@ -66,46 +66,274 @@ public class DBConfigure extends JFrame implements ActionListener
 	//******************************
 
 	
-    //******************************
-    //Code to generate the menus.
-    //******************************
-    
-    JMenuBar mainMenu = new JMenuBar();
-    
-    //******************************
-    //File menu.
-    //******************************
-    JMenu fileMenu = new JMenu("File");
-    
-    //Add a menu item.
-    JMenuItem saveItem = new JMenuItem("Save");
-    saveItem.addActionListener(this);
-    fileMenu.add(saveItem);
-    
-    //Add a menu item.
-    JMenuItem loadItem = new JMenuItem("Load");
-    loadItem.addActionListener(this);
-    fileMenu.add(loadItem);
-     
-    //Add a menu item.
-    JMenuItem exitItem = new JMenuItem("Exit DBConfigure!");
-    exitItem.addActionListener(this);
-    fileMenu.add(exitItem);
-    //******************************
-    //End - File menu.
-    //******************************
-     
-    //Now, add all the menus to the main menu.
-    mainMenu.add(fileMenu);
-     
-    setJMenuBar(mainMenu);
-    
-    //******************************
-    //End - Code to generate the menus.
-    //******************************
+	//******************************
+	//Code to generate the menus.
+	//******************************
+	
+	JMenuBar mainMenu = new JMenuBar();
+	
+	//******************************
+	//File menu.
+	//******************************
+	JMenu fileMenu = new JMenu("File");
+	
+	//Add a menu item.
+	JMenuItem saveItem = new JMenuItem("Save");
+	saveItem.addActionListener(this);
+	fileMenu.add(saveItem);
+	
+	//Add a menu item.
+	JMenuItem loadItem = new JMenuItem("Load");
+	loadItem.addActionListener(this);
+	fileMenu.add(loadItem);
+	
+	//Add a menu item.
+	JMenuItem exitItem = new JMenuItem("Exit DBConfigure!");
+	exitItem.addActionListener(this);
+	fileMenu.add(exitItem);
+	//******************************
+	//End - File menu.
+	//******************************
+	
+	//Now, add all the menus to the main menu.
+	mainMenu.add(fileMenu);
+	
+	setJMenuBar(mainMenu);
+	
+	//******************************
+	//End - Code to generate the menus.
+	//******************************
+	
+	
+	//******************************
+	//Setting up the layout system.
+	//******************************
+	Container contentPane = getContentPane();
+	GridBagLayout gbl = new GridBagLayout();
+	contentPane.setLayout(gbl);
+	GridBagConstraints gbc = new GridBagConstraints();
+	gbc.insets = new Insets(5, 5, 5, 5);
+	//******************************
+	//End - Setting up the layout system.
+	//******************************
+	
+	
+	//******************************
+	//Create and add the componants.
+	//******************************
+	
+	
+	JButton button = null;
+	JLabel label = null;
+	
+	button = new JButton("PerfDB Home");
+	button.addActionListener(this);
+	perfDBHome = new JTextField("unset", 30);
+	//Add button.
+	gbc.fill = GridBagConstraints.NONE;
+	gbc.anchor = GridBagConstraints.EAST;
+	gbc.weightx = 0;
+	gbc.weighty = 0;
+	addCompItem(button, gbc, 0, 0, 1, 1);
+	//Add field.
+	gbc.fill = GridBagConstraints.BOTH;
+	gbc.anchor = GridBagConstraints.CENTER;
+	gbc.weightx = 100;
+	gbc.weighty = 0;
+	addCompItem(perfDBHome, gbc, 1, 0, 1, 1);
+	
+	button = new JButton("JDBC Jar File");
+	button.addActionListener(this);
+	jDBCJarfile = new JTextField("unset", 30);
+	//Add button.
+	gbc.fill = GridBagConstraints.NONE;
+	gbc.anchor = GridBagConstraints.EAST;
+	gbc.weightx = 0;
+	gbc.weighty = 0;
+	addCompItem(button, gbc, 0, 1, 1, 1 );
+	//Add field.
+	gbc.fill = GridBagConstraints.BOTH;
+	gbc.anchor = GridBagConstraints.CENTER;
+	gbc.weightx = 100;
+	gbc.weighty = 0;
+	addCompItem(jDBCJarfile, gbc, 1, 1, 1, 1);
+	
+	label = new JLabel("JDBC Driver");
+	jDBCDriver = new JTextField("unset", 30);
+	//Add label.
+	gbc.fill = GridBagConstraints.NONE;
+	gbc.anchor = GridBagConstraints.EAST;
+	gbc.weightx = 0;
+	gbc.weighty = 0;
+	addCompItem(label, gbc, 0, 2, 1, 1);
+	//Add field.
+	gbc.fill = GridBagConstraints.BOTH;
+	gbc.anchor = GridBagConstraints.CENTER;
+	gbc.weightx = 100;
+	gbc.weighty = 0;
+	addCompItem(jDBCDriver, gbc, 1, 2, 1, 1);
+	
+	label = new JLabel("JDBC Type");
+	jDBCType = new JTextField("unset", 30);
+	//Add label.
+	gbc.fill = GridBagConstraints.NONE;
+	gbc.anchor = GridBagConstraints.EAST;
+	gbc.weightx = 0;
+	gbc.weighty = 0;
+	addCompItem(label, gbc, 0, 3, 1, 1);
+	//Add field.
+	gbc.fill = GridBagConstraints.BOTH;
+	gbc.anchor = GridBagConstraints.CENTER;
+	gbc.weightx = 100;
+	gbc.weighty = 0;
+	addCompItem(jDBCType, gbc, 1, 3, 1, 1);
+	
+	label = new JLabel("Hostname");
+	dBHostname = new JTextField("unset", 30);
+	//Add label.
+	gbc.fill = GridBagConstraints.NONE;
+	gbc.anchor = GridBagConstraints.EAST;
+	gbc.weightx = 0;
+	gbc.weighty = 0;
+	addCompItem(label, gbc, 0, 4, 1, 1);
+	//Add field.
+	gbc.fill = GridBagConstraints.BOTH;
+	gbc.anchor = GridBagConstraints.CENTER;
+	gbc.weightx = 100;
+	gbc.weighty = 0;
+	addCompItem(dBHostname, gbc, 1, 4, 1, 1);
+	
+	label = new JLabel("Port Number");
+	dBPortNum = new JTextField("unset", 30);
+	//Add label.
+	gbc.fill = GridBagConstraints.NONE;
+	gbc.anchor = GridBagConstraints.EAST;
+	gbc.weightx = 0;
+	gbc.weighty = 0;
+	addCompItem(label, gbc, 0, 5, 1, 1);
+	//Add field.
+	gbc.fill = GridBagConstraints.BOTH;
+	gbc.anchor = GridBagConstraints.CENTER;
+	gbc.weightx = 100;
+	gbc.weighty = 0;
+	addCompItem(dBPortNum, gbc, 1, 5, 1, 1);
+	
+	label = new JLabel("DB Name");
+	dBName = new JTextField("unset", 30);
+	//Add label.
+	gbc.fill = GridBagConstraints.NONE;
+	gbc.anchor = GridBagConstraints.EAST;
+	gbc.weightx = 0;
+	gbc.weighty = 0;
+	addCompItem(label, gbc, 0, 6, 1, 1);
+	//Add field.
+	gbc.fill = GridBagConstraints.BOTH;
+	gbc.anchor = GridBagConstraints.CENTER;
+	gbc.weightx = 100;
+	gbc.weighty = 0;
+	addCompItem(dBName, gbc, 1, 6, 1, 1);
+	
+	label = new JLabel("Username");
+	dBUsername = new JTextField("unset", 30);
+	//Add label.
+	gbc.fill = GridBagConstraints.NONE;
+	gbc.anchor = GridBagConstraints.EAST;
+	gbc.weightx = 0;
+	gbc.weighty = 0;
+	addCompItem(label, gbc, 0, 7, 1, 1);
+	//Add field.
+	gbc.fill = GridBagConstraints.BOTH;
+	gbc.anchor = GridBagConstraints.CENTER;
+	gbc.weightx = 100;
+	gbc.weighty = 0;
+	addCompItem(dBUsername, gbc, 1, 7, 1, 1);
+	
+	label = new JLabel("Password");
+	dBPassword = new JPasswordField("unset", 30);
+	//Add label.
+	gbc.fill = GridBagConstraints.NONE;
+	gbc.anchor = GridBagConstraints.EAST;
+	gbc.weightx = 0;
+	gbc.weighty = 0;
+	addCompItem(label, gbc, 0, 8, 1, 1);
+	//Add field.
+	gbc.fill = GridBagConstraints.BOTH;
+	gbc.anchor = GridBagConstraints.CENTER;
+	gbc.weightx = 100;
+	gbc.weighty = 0;
+	addCompItem(dBPassword, gbc, 1, 8, 1, 1);
 
+	button = new JButton("Schema File");
+	button.addActionListener(this);
+	//Add button.
+	dBSchemaFile = new JTextField("unset", 30);
+	gbc.fill = GridBagConstraints.NONE;
+	gbc.anchor = GridBagConstraints.EAST;
+	gbc.weightx = 0;
+	gbc.weighty = 0;
+	addCompItem(button, gbc, 0, 9, 1, 1);
+	//Add field.
+	gbc.fill = GridBagConstraints.BOTH;
+	gbc.anchor = GridBagConstraints.CENTER;
+	gbc.weightx = 100;
+	gbc.weighty = 0;
+	addCompItem(dBSchemaFile, gbc, 1, 9, 1, 1);
+	
+	button = new JButton("XML Parser");
+	button.addActionListener(this);
+	//Add button.
+	xMLPaser = new JTextField("unset", 30);
+	gbc.fill = GridBagConstraints.NONE;
+	gbc.anchor = GridBagConstraints.EAST;
+	gbc.weightx = 0;
+	gbc.weighty = 0;
+	addCompItem(button, gbc, 0,10, 1, 1);
+	//Add field.
+	gbc.fill = GridBagConstraints.BOTH;
+	gbc.anchor = GridBagConstraints.CENTER;
+	gbc.weightx = 100;
+	gbc.weighty = 0;
+	addCompItem(xMLPaser, gbc, 1, 10, 1, 1);
+	
+	button = new JButton("Config File");
+	button.addActionListener(this);
+	//Add button.
+	configFileName = new JTextField("unset", 30);
+	gbc.fill = GridBagConstraints.NONE;
+	gbc.anchor = GridBagConstraints.EAST;
+	gbc.weightx = 0;
+	gbc.weighty = 0;
+	addCompItem(button, gbc, 0, 11, 1, 1);
+	//Add field.
+	gbc.fill = GridBagConstraints.BOTH;
+	gbc.anchor = GridBagConstraints.CENTER;
+	gbc.weightx = 100;
+	gbc.weighty = 0;
+	addCompItem(configFileName, gbc, 1, 11, 1, 1);
 
+	//Create buttons to click for saving and adding.
+	//Can be done from the menu, but it is nice to put it here too.
+	button = new JButton("Load");
+	button.addActionListener(this);
+	//Add button.
+	gbc.fill = GridBagConstraints.NONE;
+	gbc.anchor = GridBagConstraints.EAST;
+	gbc.weightx = 0;
+	gbc.weighty = 0;
+	addCompItem(button, gbc, 0, 12, 1, 1);
 
+	button = new JButton("Save");
+	button.addActionListener(this);
+	//Add button.
+	gbc.fill = GridBagConstraints.NONE;
+	gbc.anchor = GridBagConstraints.WEST;
+	gbc.weightx = 0;
+	gbc.weighty = 0;
+	addCompItem(button, gbc, 1, 12, 1, 1);
+
+	//******************************
+	//End - Create and add the componants.
+	//******************************
     }
     
     public void actionPerformed(ActionEvent evt){
@@ -125,6 +353,15 @@ public class DBConfigure extends JFrame implements ActionListener
 		System.exit(0);
 	    }
 	}
+    }
+
+    //Adds the components.
+    private void addCompItem(Component c, GridBagConstraints gbc, int x, int y, int w, int h){
+	gbc.gridx = x;
+	gbc.gridy = y;
+	gbc.gridwidth = w;
+	gbc.gridheight = h;	
+	this.getContentPane().add(c, gbc);
     }
     
     
@@ -154,5 +391,18 @@ public class DBConfigure extends JFrame implements ActionListener
     //Instance data.
     //This object does all the actual non-gui work.
     //Configure = new Configure();
+    private JTextField  perfDBHome = null;
+    private JTextField  jDBCJarfile = null;
+    private JTextField  jDBCDriver = null;
+    private JTextField  jDBCType = null;
+    private JTextField  dBHostname = null;
+    private JTextField  dBPortNum = null;
+    private JTextField  dBName = null;
+    private JTextField  dBUsername = null;
+    private JPasswordField dBPassword = null;
+    private JTextField  dBSchemaFile = null;
+    private JTextField  xMLPaser = null;
+    private JTextField  configFileName = null;
 
+    private String unsetString = new String("unset");
 }
