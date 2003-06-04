285915a9c4ef8c564754cd08d8ebac4ed99fc4c1
bertie <bertie@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2003-06-04 04:09:37 +0000

    Writes various image types to file.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@1475 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ tools/src/paraprof/src/ParaProfImageOutput.java
@@ -0,0 +1,26 @@
+/* 
+  
+  ParaProfImageOutput.java
+  
+  Title:       ParaProfImageOutput.java
+  Author:      Robert Bell
+  Description: Handles the output of the various panels to image files.
+*/
+
+package paraprof;
+
+//import java.util.*;
+//import java.awt.*;
+//import java.awt.event.*;
+import javax.swing.*;
+//import javax.swing.border.*;
+import javax.swing.event.*;
+//import java.awt.print.*;
+//import java.awt.geom.*;
+
+public class ParaProfImageOutput{
+
+    public ParaProfImageOutput(){
+    }
+
+    public 
