dc316ee9c8955d4abbb1d457389165c15ea8328b
sameer <sameer@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2003-09-02 18:36:30 +0000

    Added support for the Cray SV1 platform. It uses the CRAYX1CC macros. For src/Profile/Makefile, we added $(TAU_NULL)$(TAU_SHLIB): 	... so that Cray make works. For the T3E we now use -DTAU_DOT_H_LESS_HEADERS as well.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@1634 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- utils/archcray.h
+++ utils/archcray.h
@@ -11,12 +11,17 @@ ARCHt3e
 #     define _havehosttype_
 ARCHcrayx1
 #else
+#   ifdef cray                /* CRAYTEST */
+#     define _havehosttype_   /* CRAYTEST */
+ARCHcraysv1                   /* CRAYTEST */
+#   else                      /* CRAYTEST */
 #   ifdef CRAY-C90            /* CRAYTEST */
 #     define _havehosttype_   /* CRAYTEST */
 ARCHc90                       /* CRAYTEST */
 #   else                      /* CRAYTEST */
 #     define _havehosttype_   /* CRAYTEST */
 ARCHcray                      /* CRAYTEST */
+#   endif /* CRAYMPP */       /* CRAYTEST */
 #   endif /* CRAY-C90 */      /* CRAYTEST */
 #   endif /* cray x1  */
 #   endif
