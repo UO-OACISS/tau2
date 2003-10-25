af077c104d0ea14b838e921582c1e20808368ed5
sameer <sameer@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2003-10-25 00:33:30 +0000

    Implemented the package.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@1760 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- include/Profile/TauMuse.h
+++ include/Profile/TauMuse.h
@@ -13,7 +13,7 @@
 * This is for handlers stuff
 * *************************************************************************************/
 //=====================================
-// Tau_count handler
+// TAU_count handler
 //=====================================
 /*********************
  * Description	: Struct for input data for count.
@@ -57,28 +57,23 @@ struct process_scheduling_handler_return_data{
   unsigned int cpu_counter_in[NUMOFCPU];
   unsigned int cpu_counter_out[NUMOFCPU];
   unsigned long long cpu_busy_time[NUMOFCPU];
+  unsigned long long cpu_speed;
 
 };
 
 int CreateProcessSchedulingEncode(char *ascii_command, int size, char *binary_command);
-int QueryProcessSchedulingDecode(const char *binary_command, 
+double QueryProcessSchedulingDecode(const char *binary_command, 
 		const char *binary_reply, int size, char *ascii_reply);
 
 /***************************************************************************************
 * This is for filters stuff
 * *************************************************************************************/
-struct filter_arguments{
-	unsigned int pid;
-	char filter_name[50];
-	unsigned int event;
-};
-
 int AddFilterProcessFilterEncode(char *ascii_command, int size, char *binary_command);
 
 /***************************************************************************************
 * This is for TauMuse stuff
 * *************************************************************************************/
-int TauMuseInit(char *handler_name, int filter_argc,struct filter_arguments *args[]);
+int TauMuseInit(char *handler_name, int filter_argc,char *args[]);
 double TauMuseQuery(void);
 void TauMuseDestroy(void);
 #endif /* TAU_MUSE */
