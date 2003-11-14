5d8eb940f0df76459d3b174eea4bb41549e936ae
khuck <khuck@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2003-11-14 00:44:39 +0000

    Broke the Main.java file into individual classes.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@1839 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ tools/src/perfdb/src/perfdb/loadxml/LoadApplication.java
@@ -0,0 +1,116 @@
+package perfdb.loadxml;
+
+import jargs.gnu.CmdLineParser;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.sql.SQLException;
+
+import perfdb.util.dbinterface.DB;
+
+public class LoadApplication {
+    private Load load = null;
+    private DB db = null;
+    
+    private static String APP_USAGE = 
+        "USAGE: perfdb_loadapp [{-h,--help}] [{-x,--xmlfile} filename] \n";
+
+    private perfdb.ConnectionManager connector;
+
+    public LoadApplication(String configFileName) {
+		super();
+		connector = new perfdb.ConnectionManager(configFileName);
+    }
+
+    public perfdb.ConnectionManager getConnector(){
+		return connector;
+    }
+
+    public Load getLoad() {
+		if (load == null) {
+	    	if (connector.getDB() == null) {
+			load = new Load(connector.getParserClass());
+	    	} else {
+			load = new Load(connector.getDB(), connector.getParserClass());
+	    	}
+		}
+		return load;
+    }
+
+    /*** Parse and load an application. ***/   
+
+    public String storeApp(String appFile) {
+		String appid = null;
+
+		try {	
+	    	appid = getLoad().parseApp(appFile);
+		} catch (Throwable ex) {
+	    	System.out.println("Error: " + ex.getMessage());
+	    	return null;
+		}
+
+		if ((appid==null) || (appid.trim().length()==0)) {
+	    	System.out.println("Loadding application failed");
+	    	return null;
+		}
+		return appid;
+    }
+
+    /*** Beginning of main program. ***/
+
+    public static void main(java.lang.String[] args) {
+        CmdLineParser parser = new CmdLineParser();
+        CmdLineParser.Option helpOpt = parser.addBooleanOption('h', "help");
+        CmdLineParser.Option configfileOpt = parser.addStringOption('g', "configfile");
+        CmdLineParser.Option xmlfileOpt = parser.addStringOption('x', "xmlfile");
+
+        try {
+            parser.parse(args);
+        }
+        catch ( CmdLineParser.OptionException e ) {
+            System.err.println(e.getMessage());
+	    	System.err.println(APP_USAGE);
+	    	System.exit(-1);
+        }
+
+        Boolean help = (Boolean)parser.getOptionValue(helpOpt);
+        String configFile = (String)parser.getOptionValue(configfileOpt);
+        String xmlFile = (String)parser.getOptionValue(xmlfileOpt);
+
+    	if (help != null && help.booleanValue()) {
+	    	System.err.println(APP_USAGE);
+	    	System.exit(-1);
+    	}
+
+		if (configFile == null) {
+            System.err.println("Please enter a valid config file.");
+	    	System.err.println(APP_USAGE);
+	    	System.exit(-1);
+		}
+
+		// validate the command line options...
+		if (xmlFile == null) {
+           	System.err.println("Please enter a valid application XML file.");
+    		System.err.println(APP_USAGE);
+    		System.exit(-1);
+		}
+
+	// create a new LoadApplication object, pass in the configuration file name
+		LoadApplication loadApp = new LoadApplication(configFile);
+		loadApp.getConnector().connect();
+
+		int exitval = 0;
+	
+    	/***** Load appliation into PerfDB *********/
+			String appid = loadApp.storeApp(xmlFile);
+			if (appid != null)
+				exitval = Integer.parseInt(appid);
+
+		loadApp.getConnector().dbclose();
+		System.exit(exitval);
+    }
+
+}
+
