90b3cc1c2e0b9c64014d537660226b269687ef1c
bertie <bertie@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2003-11-15 21:35:33 +0000

    This window controls all the applied operation in ParaProf.  This takes over from everything being in ParaProfManager.  The window was getting too large and bulky to manage well.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@1870 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ tools/src/paraprof/src/PPMLWindow.java
@@ -0,0 +1,203 @@
+/*  
+    PPMLWindow.java
+
+    Title:      ParaProf
+    Author:     Robert Bell
+    Description:  
+*/
+
+package paraprof;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class  PPMLWindow extends JFrame implements ActionListener{ 
+    
+    public PPMLWindow(ParaProfManager paraProfManager){
+	this.paraProfManager = paraProfManager;
+
+	String password = paraProfManager.getDatabasePassword();
+	if(password == null)
+	   passwordField = new JPasswordField(password, 20);
+	else
+	   passwordField = new JPasswordField(20);
+
+	String configFile = paraProfManager.getDatabaseConfigurationFile();
+	if(configFile == null)
+	    configFileField = new JTextField(System.getProperty("user.dir"), 30);
+	else
+	    configFileField = new JTextField(configFile, 30);
+	
+	//####################################
+	//Window Stuff.
+	//####################################
+	int windowWidth = 400;
+	int windowHeight = 200;
+
+	//Grab the screen size.
+	Toolkit tk = Toolkit.getDefaultToolkit();
+	Dimension screenDimension = tk.getScreenSize();
+	int screenHeight = screenDimension.height;
+	int screenWidth = screenDimension.width;
+	
+	
+	//Set the window to come up in the center of the screen.
+	int xPosition = (screenWidth - windowWidth) / 2;
+	int yPosition = (screenHeight - windowHeight) / 2;
+
+	this.setLocation(xPosition, yPosition);
+	setSize(new java.awt.Dimension(windowWidth, windowHeight));
+	setTitle("Database Configuration");
+	//####################################
+	//End -Window Stuff.
+	//####################################
+	
+	//Add some window listener code
+	addWindowListener(new java.awt.event.WindowAdapter() {
+		public void windowClosing(java.awt.event.WindowEvent evt) {
+		    thisWindowClosing(evt);
+		}
+	    });
+	
+	//####################################
+	//Create and add the components.
+	//####################################
+	//Setting up the layout system for the main window.
+	Container contentPane = getContentPane();
+	GridBagLayout gbl = new GridBagLayout();
+	contentPane.setLayout(gbl);
+	GridBagConstraints gbc = new GridBagConstraints();
+	gbc.insets = new Insets(5, 5, 5, 5);
+
+ 	gbc.fill = GridBagConstraints.NONE;
+	gbc.anchor = GridBagConstraints.WEST;
+	gbc.weightx = 0;
+	gbc.weighty = 0;
+	addCompItem(new JLabel("Password:"), gbc, 0, 0, 1, 1);
+
+	gbc.fill = GridBagConstraints.BOTH;
+	gbc.anchor = GridBagConstraints.WEST;
+	gbc.weightx = 100;
+	gbc.weighty = 0;
+	addCompItem(passwordField, gbc, 1, 0, 1, 1);
+
+	JButton jButton = new JButton("Config File");
+	jButton.addActionListener(this);
+	gbc.fill = GridBagConstraints.NONE;
+	gbc.anchor = GridBagConstraints.WEST;
+	gbc.weightx = 0;
+	gbc.weighty = 0;
+	addCompItem(jButton, gbc, 0, 1, 1, 1);
+	    
+	gbc.fill = GridBagConstraints.BOTH;
+	gbc.anchor = GridBagConstraints.WEST;
+	gbc.weightx = 100;
+	gbc.weighty = 0;
+	addCompItem(configFileField, gbc, 1, 1, 2, 1);
+
+	jButton = new JButton("Ok");
+	jButton.addActionListener(this);
+	gbc.fill = GridBagConstraints.NONE;
+	gbc.anchor = GridBagConstraints.EAST;
+	gbc.weightx = 0;
+	gbc.weighty = 0;
+	addCompItem(jButton, gbc, 2, 2, 1, 1);
+
+	jButton = new JButton("Cancel");
+	jButton.addActionListener(this);
+	gbc.fill = GridBagConstraints.NONE;
+	gbc.anchor = GridBagConstraints.EAST;
+	gbc.weightx = 0;
+	gbc.weighty = 0;
+	addCompItem(jButton, gbc, 0, 2, 1, 1);
+	//####################################
+	//End - Create and add the components.
+	//####################################
+    }
+
+     //####################################
+    //Interface code.
+    //####################################
+    
+    //######
+    //ActionListener.
+    //######
+    public void actionPerformed(ActionEvent evt){
+	try{
+	    Object EventSrc = evt.getSource();
+	    String arg = evt.getActionCommand();
+	    if(arg.equals("Config File")){
+		JFileChooser jFileChooser = new JFileChooser(System.getProperty("user.dir"));
+		jFileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		jFileChooser.setMultiSelectionEnabled(false);
+		if((jFileChooser.showDialog(this, "Select")) != JFileChooser.APPROVE_OPTION){
+		    System.out.println("File selection cancelled by user!");
+		    return;
+		}
+		//User clicked the approve option.
+		configFileField.setText((jFileChooser.getSelectedFile()).getCanonicalPath());
+	    }
+	    else if(arg.equals("Cancel")){
+		closeThisWindow();}
+	    else if(arg.equals("Ok")){
+		paraProfManager.setDatabasePassword((new String(passwordField.getPassword())).trim());
+		paraProfManager.setDatabaseConfigurationFile(configFileField.getText().trim());
+		closeThisWindow();
+	    }
+	}
+	catch(Exception e){
+	    ParaProf.systemError(e, null, "DBC02");
+	}
+    }
+    //######
+    //End - ActionListener.
+    //######
+
+    //####################################
+    //End - Interface code.
+    //####################################
+
+    //####################################
+    //Pivate Section.
+    //####################################
+    private void addCompItem(Component c, GridBagConstraints gbc, int x, int y, int w, int h){
+	try{
+	    gbc.gridx = x;
+	    gbc.gridy = y;
+	    gbc.gridwidth = w;
+	    gbc.gridheight = h;
+	    
+	    getContentPane().add(c, gbc);
+	}
+	catch(Exception e){
+	    ParaProf.systemError(e, null, "DBC03");
+	}
+    }
+    
+    //Close the window when the close box is clicked
+    private void thisWindowClosing(java.awt.event.WindowEvent e){
+	closeThisWindow();}
+    
+    void closeThisWindow(){ 
+	this.setVisible(false);
+	dispose();
+    }
+    //####################################
+    //End - Pivate Section.
+    //####################################
+    
+    //####################################
+    //Instance data.
+    //####################################
+    ParaProfManager paraProfManager = null;
+    JPasswordField passwordField = null;
+    JTextField configFileField = null;
+    //####################################
+    //End - Instance data.
+    //#################################### 
+}
