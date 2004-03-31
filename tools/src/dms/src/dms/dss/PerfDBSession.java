6b5f8c4deddc535ea47f08fc960da4337d29a8b5
khuck <khuck@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2004-03-31 01:33:35 +0000

    Added support for saving a ParaProfTrial to the database!
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@2177 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- tools/src/dms/src/dms/dss/PerfDBSession.java
+++ tools/src/dms/src/dms/dss/PerfDBSession.java
@@ -4,11 +4,12 @@ import dms.perfdb.*;
 import java.util.*;
 import java.sql.*;
 import java.util.Date;
+import paraprof.*;
 
 /**
  * This is the top level class for the Database implementation of the API.
  *
- * <P>CVS $Id: PerfDBSession.java,v 1.5 2004/03/30 19:57:20 khuck Exp $</P>
+ * <P>CVS $Id: PerfDBSession.java,v 1.6 2004/03/31 01:33:35 khuck Exp $</P>
  * @author	Kevin Huck, Robert Bell
  * @version	0.1
  */
@@ -535,5 +536,141 @@ public class PerfDBSession extends DataSession {
 		return;
 	}
 
+/**
+ * Saves the ParaProfTrial object to the database
+ * 
+ * @param paraProfTrial
+ * @return the database index ID of the saved trial record
+ */
+
+	public int saveParaProfTrial(ParaProfTrial paraProfTrial) {
+		GlobalMapping mapping = paraProfTrial.getGlobalMapping();
+	
+		//Build an array of group names.  This speeds lookup of group names.
+		Vector groups = mapping.getMapping(1);
+		String[] groupNames = new String[groups.size()];
+		int position = 0;
+		for(Enumeration e = groups.elements(); e.hasMoreElements() ;){
+	    	GlobalMappingElement group = (GlobalMappingElement) e.nextElement();
+	    	groupNames[position++] = group.getMappingName();
+		}
+
+		//Get max node,context, and thread numbers.
+		int[] maxNCT = paraProfTrial.getMaxNCTNumbers();
+	
+		// fix up some trial data
+		paraProfTrial.setNodeCount(maxNCT[0]+1);
+		paraProfTrial.setNumContextsPerNode(maxNCT[1]+1);
+		paraProfTrial.setNumThreadsPerContext(maxNCT[2]+1);
+
+		// output the trial data
+		int newTrialID = paraProfTrial.saveTrial(db);
+
+		functions = new Vector();
+
+		// create the functions
+		for(Enumeration e = mapping.getMapping(0).elements(); e.hasMoreElements() ;) {
+			GlobalMappingElement element = (GlobalMappingElement) e.nextElement();
+			if(element!=null) {
+				// create a function
+				Function function = new Function(this);
+				function.setName(element.getMappingName());
+				function.setFunctionID(element.getMappingID());
+				function.setTrialID(newTrialID);
+				// build the group name
+				int[] groupIDs = element.getGroups();
+				StringBuffer buf = new StringBuffer();
+				for (int i = 0; i < element.getNumberOfGroups() ; i++) {
+					if (i > 0) buf.append ("|");
+					buf.append(groupNames[groupIDs[i]]);
+				}
+				function.setGroup(buf.toString());
+				// put the function in the vector
+				functions.add(function);
+	    	}
+	    }
+	    
+		/* 
+	    //Write out userevent name to id mapping.
+	    writeBeginObject(xwriter,21);
+	    for(Enumeration e = globalMapping.getMapping(2).elements(); e.hasMoreElements() ;){
+		GlobalMappingElement globalMappingElement = (GlobalMappingElement) e.nextElement();
+		if(globalMappingElement!=null)
+		    writeNameIDMap(xwriter,globalMappingElement.getMappingName(),globalMappingElement.getMappingID());
+	    }
+	    writeEndObject(xwriter,21);
+	    
+	    StringBuffer groupsStringBuffer = new StringBuffer(10);
+	    Vector nodes = paraProfTrial.getNCT().getNodes();
+	    for(Enumeration e1 = nodes.elements(); e1.hasMoreElements() ;){
+		Node node = (Node) e1.nextElement();
+		Vector contexts = node.getContexts();
+		for(Enumeration e2 = contexts.elements(); e2.hasMoreElements() ;){
+		    Context context = (Context) e2.nextElement();
+		    Vector threads = context.getThreads();
+		    for(Enumeration e3 = threads.elements(); e3.hasMoreElements() ;){
+			Thread thread = (Thread) e3.nextElement();
+			Vector functions = thread.getFunctionList();
+			Vector userevents = thread.getUsereventList();
+			//Write out the node,context and thread ids.
+			writeIDs(xwriter,thread.getNodeID(),thread.getContextID(),thread.getThreadID());
+			//Write out function data for this thread.
+			for(Enumeration e4 = functions.elements(); e4.hasMoreElements() ;){
+			    GlobalThreadDataElement function = (GlobalThreadDataElement) e4.nextElement();
+			    if (function!=null){
+				writeBeginObject(xwriter,16);
+				// @@@ Commented out as is questionable functionality.  Add back in for legacy support.
+				// writeFunctionName(xwriter,function.getMappingName());
+				//
+				writeInt(xwriter,11,function.getMappingID());
+				//Build group string.
+				groupsStringBuffer.delete(0,groupsStringBuffer.length());
+				int[] groupIDs = function.getGroups();
+				for(int i=0;i<groupIDs.length;i++){
+				    if(i==0)
+					groupsStringBuffer.append(groupNames[groupIDs[i]]);
+				    else
+					groupsStringBuffer.append(":"+groupNames[groupIDs[i]]);
+				}
+				writeString(xwriter,14,groupsStringBuffer.toString());
+				writeDouble(xwriter,0,function.getInclusivePercentValue(metricID));
+				writeDouble(xwriter,1,function.getInclusiveValue(metricID));
+				writeDouble(xwriter,2,function.getExclusivePercentValue(metricID));
+				writeDouble(xwriter,3,function.getExclusiveValue(metricID));
+				writeDouble(xwriter,5,function.getNumberOfCalls());
+				writeDouble(xwriter,6,function.getNumberOfSubRoutines());
+				writeDouble(xwriter,4,function.getUserSecPerCall(metricID));
+				writeEndObject(xwriter,16);
+			    }
+			}
+			//Write out user event data for this thread.
+			if(userevents!=null){
+			    for(Enumeration e4 = userevents.elements(); e4.hasMoreElements() ;){
+				GlobalThreadDataElement userevent = (GlobalThreadDataElement) e4.nextElement();
+				if (userevent!=null){
+				    writeBeginObject(xwriter,17);
+				    // @@@ Commented out as is questionable functionality.  Add back in for legacy support.
+				    // writeString(xwriter,15,userevent.getUserEventName());
+				    //
+				    writeInt(xwriter,12,userevent.getMappingID());
+				    writeInt(xwriter,13,userevent.getUserEventNumberValue());
+				    writeDouble(xwriter,7,userevent.getUserEventMaxValue());
+				    writeDouble(xwriter,8,userevent.getUserEventMinValue());
+				    writeDouble(xwriter,9,userevent.getUserEventMeanValue());
+				    //writeDouble(xwriter,10,userevent.getUserEventStdDevValue());@@@Commented out due to lack of ParaProf data support. Should probably add methods which
+				    //generate this data. @@@
+				    writeBeginObject(xwriter,17);
+				}
+			    }
+			}
+		    }
+		}    
+	    }
+
+		*/
+
+		return newTrialID;
+	    
+    }
 };
 
