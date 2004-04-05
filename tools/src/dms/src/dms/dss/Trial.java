a3e1e03b96a784af8377b71cc12add109104698b
bertie <bertie@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2004-04-05 16:10:43 +0000

    Made dataSession protected so that decendants can access it.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@2199 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- tools/src/dms/src/dms/dss/Trial.java
+++ tools/src/dms/src/dms/dss/Trial.java
@@ -19,7 +19,7 @@ import java.lang.String;
  * the number of contexts per node, the number of threads per context
  * and the metrics collected during the run.
  *
- * <P>CVS $Id: Trial.java,v 1.4 2004/04/02 23:28:17 khuck Exp $</P>
+ * <P>CVS $Id: Trial.java,v 1.5 2004/04/05 16:10:43 bertie Exp $</P>
  * @author	Kevin Huck, Robert Bell
  * @version	0.1
  * @since	0.1
@@ -42,7 +42,7 @@ public class Trial {
     private Vector metric;
     private String userData;
     private String problemDefinition;
-    private DataSession dataSession = null;
+    protected DataSession dataSession = null;
 
     /**
      * Gets the unique identifier of the current trial object.
