6519eaa4c13a00c84ab9a3e00e2abafad59836ae
sameer <sameer@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2004-05-13 00:40:47 +0000

    Extended wildcardCompare (char *s1, char *s2) to wildcardCompare (char *string1, char *string2, char kleenestar) to take an additional argument for the "*" operator. While we can use ? on both file names and function names for a single character match, we use '+' as the kleene star operator for function names and '*' operator for file names. This is because function names frequently have * in the prototype names (char *, int *...) and we didn't want to use ... as varargs uses this. So, + was chosen. This is to assist ASAP CACR code instrumentation which involves template instantiations with numerical values and our instrumentation needs to work on multiple platforms (e.g., 32,64 parameters are passed to void foo<32>::myfunc(); for instance for 32 bit systems. The exclude list can now say either void foo<??>::myfunc() or void foo<+>::myfunc().
    [Julian Cummings, ASAP]
    
    
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@2276 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- utils/tau_selective.cpp
+++ utils/tau_selective.cpp
@@ -37,6 +37,8 @@ list<string> includelist;
 list<string> fileincludelist;
 list<string> fileexcludelist;
 
+/* prototypes used in this file */
+bool wildcardCompare(char *wild, char *string, char kleenestar);
 /* -------------------------------------------------------------------------- */
 /* -- Read the instrumentation requests file. ------------------------------- */
 
@@ -176,7 +178,10 @@ bool matchName(const string& str1, const string& str2)
 
   size_to_compare = (length1 < length2) ? length1 : length2;
  
-  if (strncmp(str1.c_str(), str2.c_str(), size_to_compare) == 0)
+  // OLD 
+  //if (strncmp(str1.c_str(), str2.c_str(), size_to_compare) == 0)
+  /* Use '#' as the wildcard kleene star operator character */
+  if (wildcardCompare((char *)(str1.c_str()), (char *)(str2.c_str()), '+'))
     return true;
   else 
     return false;
@@ -188,6 +193,9 @@ bool matchName(const string& str1, const string& str2)
 bool instrumentEntity(const string& function_name)
 {
 
+#ifdef DEBUG
+  cout <<"instrument "<<function_name<<" ?"<<endl;
+#endif /* DEBUG */
   if (!excludelist.empty())
   { /* if exclude list has entries */
     for (list<string>::iterator it = excludelist.begin();
@@ -246,14 +254,14 @@ bool instrumentEntity(const string& function_name)
 }
 
 /* -------------------------------------------------------------------------- */
-/* -- Compares the file name and the wildcard. Returns 0 or 1   ------------- */
+/* -- Compares the file name and the wildcard. Returns true or false -------- */
 /* -------------------------------------------------------------------------- */
-bool wildcardCompare(char *wild, char *string) 
+bool wildcardCompare(char *wild, char *string, char kleenestar) 
 {
   char *cp, *mp;
 
   /* check if it is not a ? */
-  while ((*string) && (*wild != '*')) {
+  while ((*string) && (*wild != kleenestar)) {
     if ((*wild != *string) && (*wild != '?')) {
       return false;
     }
@@ -263,7 +271,7 @@ bool wildcardCompare(char *wild, char *string)
 		
   /* next check for '*' */
   while (*string) {
-    if (*wild == '*') {
+    if (*wild == kleenestar) {
       if (!*++wild) {
         return true;
       }
@@ -282,7 +290,7 @@ bool wildcardCompare(char *wild, char *string)
     
    } /* string is not null */
   
-   while (*wild == '*') {
+   while (*wild == kleenestar) {
      wild++;
    }
    /* is there anything left in wild? */
@@ -302,8 +310,9 @@ bool processFileForInstrumentation(const string& file_name)
   { /* if exclude list has entries */
     for (list<string>::iterator it = fileexcludelist.begin();
         it != fileexcludelist.end(); it++)
-    { /* iterate through the entries and see if the names match */
-      if (wildcardCompare((char *)((*it).c_str()), (char *) (file_name.c_str()))) 
+    { /* iterate through the entries and see if the names match. Use '*' as 
+       the kleene star operator */
+      if (wildcardCompare((char *)((*it).c_str()), (char *) (file_name.c_str()), '*'))
       {
         /* names match! This routine should not be instrumented */
 #ifdef DEBUG
@@ -332,7 +341,7 @@ bool processFileForInstrumentation(const string& file_name)
     for (list<string>::iterator it = fileincludelist.begin();
         it != fileincludelist.end(); it++)
     { /* iterate through the entries and see if the names match */
-      if (wildcardCompare((char *)((*it).c_str()), (char *)(file_name.c_str()))) 
+      if (wildcardCompare((char *)((*it).c_str()), (char *)(file_name.c_str()), '*')) 
       {
         /* names match! This routine should be instrumented */
 #ifdef DEBUG
@@ -358,6 +367,6 @@ bool processFileForInstrumentation(const string& file_name)
 
 /***************************************************************************
  * $RCSfile: tau_selective.cpp,v $   $Author: sameer $
- * $Revision: 1.4 $   $Date: 2003/07/15 18:04:53 $
- * VERSION_ID: $Id: tau_selective.cpp,v 1.4 2003/07/15 18:04:53 sameer Exp $
+ * $Revision: 1.5 $   $Date: 2004/05/13 00:40:47 $
+ * VERSION_ID: $Id: tau_selective.cpp,v 1.5 2004/05/13 00:40:47 sameer Exp $
  ***************************************************************************/
