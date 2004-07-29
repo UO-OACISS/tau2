b4bf015bd60da2a4fae2f960a007f0ba110f313a
khuck <khuck@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2004-07-29 23:19:14 +0000

    another test
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@2460 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ tools/src/dms/src/examples/testing/DrawImage.java
@@ -0,0 +1,88 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import javax.swing.*;
+import java.util.Vector;
+import edu.uoregon.tau.dms.dss.*;
+import edu.uoregon.tau.dms.analysis.*;
+import java.lang.StrictMath;
+import java.lang.reflect.Array;
+
+public class DrawImage extends JPanel {
+   
+   private Image img = null;
+   
+   public DrawImage(String config) {
+
+      try {
+         // Create a PerfDMFSession object
+         DataSession session = new PerfDMFSession();
+         session.initialize(config);
+         System.out.println ("API loaded...");
+
+         // test out the analysis!
+         // Trial trial = session.setTrial(55); // sppm, 256 threads on blue
+         // Trial trial = session.setTrial(61); // sppm, 256 threads on frost
+         // Trial trial = session.setTrial(67); // sphot, 32 nodes, 2 threads
+         // Trial trial = session.setTrial(68); // pprof.dat example
+         // Trial trial = session.setTrial(69); // sphot, 4 nodes 2 threads
+         // Trial trial = session.setTrial(72); // sppm, openmp, 8 threads
+         Trial trial = session.setTrial(77); // sppm, 32 threads on mcr
+         Vector metrics = session.getMetrics();
+         // Metric metric = (Metric)(metrics.elementAt(0));
+         Metric metric = (Metric)(metrics.elementAt(7));
+
+         DistanceAnalysis distance = 
+			new ThreadDistance((PerfDMFSession)session, trial, metric);  
+
+         distance.getManhattanDistance();
+
+         int[] pixels = distance.toImage(true, true);
+		 int size = (int)(java.lang.StrictMath.sqrt(Array.getLength(pixels)));
+         MemoryImageSource purpleMIS = new MemoryImageSource(size, size, pixels, 0, size); 
+         // toolkit is an interface to the environment
+         Toolkit toolkit = getToolkit();
+         // create the image using the toolkit
+         img = toolkit.createImage(purpleMIS);
+         session.terminate();
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+   }
+   
+   public void paint(Graphics g) {
+      super.paint(g);
+      // the size of the component
+      Dimension d = getSize();
+      // the internal margins of the component
+      Insets i = getInsets();
+      // draw to fill the entire component
+      g.drawImage(img, i.left, i.top, d.width - i.left - i.right, d.height - i.top - i.bottom, this );
+      // g.drawImage(img, i.left, i.top, 512, 512, this );
+   }
+   
+   // the entry point for the application
+   public static void main(String[] args) {
+      try {
+         // create the panel
+         DrawImage dimg = new DrawImage(args[0]);
+         // create the window
+         final JFrame f = new PlotDrawingWindow("TEST");
+         // create and add an event handler for window closing event
+         f.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+               System.exit(0);
+            }
+         });
+
+         //Add the image pane to this frame. 
+         f.getContentPane().add(dimg, BorderLayout.CENTER);
+
+         f.setVisible(true);
+      } catch (Exception r) {
+	     System.out.println("Usage: DrawImage <configfile>");
+         System.exit(0);
+      }
+   }
+}
+
