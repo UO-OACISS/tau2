735af387ef993c3d4ca304f63371f99ec752842c
bertie <bertie@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2004-08-10 21:18:21 +0000

    Added to allow better error tracking.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@2539 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ tools/src/dms/src/edu/uoregon/tau/dms/dss/Debug.java
@@ -0,0 +1,83 @@
+
+/* 
+   Name:        ParaProfDataSession.java
+   Author:      Robert Bell
+   Description:  
+*/
+
+package edu.uoregon.tau.dms.dss;
+
+import java.io.*;
+
+public class Debug{
+
+    public Debug(){
+	super();
+	try{
+	    out = new PrintWriter(new FileWriter(new File("ParaProf.debug.out")));
+	}
+	catch(IOException exception){
+	    System.out.println("An error occured trying whilst trying to create ParaProf.debug.out!");
+	    System.out.println("Debugging information will be printed to the standard out instead.");
+	    System.out.println("Details about this error are below:");
+	    exception.printStackTrace();
+	}
+	
+    }
+
+    //####################################
+    //Public Section.
+    //####################################
+    public void outputToFile(String s){
+	if(out!=null)
+	    out.println(s);
+	else
+	    System.out.println(s);
+    }
+    
+    public void flushDebugFileBuffer(){
+	if(out!=null)
+	    out.flush();
+    }
+    
+    public void closeDebugFile(){
+	if(out!=null)
+	    out.close();
+    }
+    //####################################
+    //End - Public Section.
+    //####################################
+
+    //####################################
+    //Protected Section.
+    //####################################
+    //####################################
+    //End - Protected Section.
+    //####################################
+
+    //####################################
+    //Private Section.
+    //####################################
+    //####################################
+    //End - Private Section.
+    //####################################
+
+    //####################################
+    //Instance data.
+    //####################################
+
+    //######
+    //Private Section.
+    //######
+    public static boolean debug = false;
+    //When in debugging mode, this class can print a lot of data.
+    //Initialized in this.setDebug(...).
+    private PrintWriter out = null;
+    //######
+    //End - Private Section.
+    //######
+
+    //####################################
+    //End - Instance data.
+    //####################################
+}    
