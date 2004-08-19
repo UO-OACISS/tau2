e33ff98581a11f2e002b6322b3f8f72e28c101a6
sameer <sameer@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2004-08-19 19:16:33 +0000

    Changed TraceEvInit return type from void to int (it now returns the number of records that it creates).
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@2601 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- include/Profile/pcxx_events.h
+++ include/Profile/pcxx_events.h
@@ -116,7 +116,7 @@ extern int aa_poll_count; /* For Ariadne - defined in tulip/mpi/src/KernelCom.c 
         void pcxx_EvClose ();
         void pcxx_EvFlush ();
 	/* New tracing interface */
-	void TraceEvInit(int tid);
+	int TraceEvInit(int tid);
 	void TraceUnInitialize(int tid);
  	void TraceReinitialize(int oldid, int newid, int tid);
         void TraceEventOnly(long int ev,long long par, int tid);
