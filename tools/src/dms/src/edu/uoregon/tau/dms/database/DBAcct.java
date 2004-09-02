931601cdcc116e2da51a7fba10641efa8447f946
bertie <bertie@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2004-09-02 23:57:02 +0000

    Don't appear to be used.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@2665 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- tools/src/dms/src/edu/uoregon/tau/dms/database/DBAcct.java
+++
@@ -1,9 +0,0 @@
-package edu.uoregon.tau.dms.database;
-
-/*** Account access for DBMS. ***/
-
-public interface DBAcct {
-    String getAcct();
-    void setAcct(String acct);
-    String toString();
-}
