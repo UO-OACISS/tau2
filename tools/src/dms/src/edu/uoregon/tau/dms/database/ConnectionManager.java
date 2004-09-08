40797fae309f66b59d2f6acfa637787bedb8aa44
bertie <bertie@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2004-09-08 21:57:15 +0000

    Removed some code that was not being used. Cleaned up code.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@2669 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- tools/src/dms/src/edu/uoregon/tau/dms/database/ConnectionManager.java
+++ tools/src/dms/src/edu/uoregon/tau/dms/database/ConnectionManager.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 
 /*** Some default setups for system ***/
 
-public class ConnectionManager {
+public class ConnectionManager{
 	
     //perfdmfAcct is the DBMS account 
     private String perfdmfUser;
@@ -19,119 +19,106 @@ public class ConnectionManager {
     // database schema file name. default one should be "~/PerfDMF/db/dbschema.txt".
     private String dbschema;
 
-	private ParseConfig parser = null;
+    private ParseConfig parser = null;
 
     private DB db = null;
 
     public ConnectionManager(String configFileName){
-	super();
 	parser = new ParseConfig(configFileName);
 	String password = getPassword();
 	initialize(password);
-	//System.out.println("\r\n");
     }
 	
     public ConnectionManager(String configFileName, String password) {
-	super();
 	parser = new ParseConfig(configFileName);
 	initialize(password);
     }
     
     public ConnectionManager(String configFileName, boolean prompt){
-	super();
 	parser = new ParseConfig(configFileName);
 	String password = getPassword(prompt);
 	initialize(password);
-	//System.out.println("\r\n");
     }
 
     public void initialize(String password) {
-		perfdmfUser = parser.getDBUserName();
-		perfdmfPass = password;
-		dbschema = parser.getDBSchema();
+	perfdmfUser = parser.getDBUserName();
+	perfdmfPass = password;
+	dbschema = parser.getDBSchema();
     }
     
-    public ParseConfig getParseConfig () {
-	return parser;
-    }
+    public ParseConfig getParseConfig(){
+	return parser;}
 
-    public void connect() throws SQLException {
-	    setDB(new DBConnector(perfdmfUser, perfdmfPass, parser));
-    }
+    public void connect() throws SQLException{
+	setDB(new DBConnector(perfdmfUser, perfdmfPass, parser));}
 
-    public String getParserClass() {
-	return parserClass;
-    }
+    public String getParserClass(){
+	return parserClass;}
 
-    public String getSchemafile() {
-	return dbschema;
-    }
+    public String getSchemafile(){
+	return dbschema;}
 
     public void dbclose(){
 	try{
-	    if (db != null) {
+	    if(db != null){
 		db.close();
 	    }
-	}catch (Throwable e) {
+	}
+	catch (Throwable e) {
 	    e.printStackTrace();
 	}
     }
 
-    public void setParserClass(String newValue) {
-	parserClass = newValue;
-    }
+    public void setParserClass(String parserClass){
+	this.parserClass = parserClass;}
 
-    public void setDB(DB newValue) {
-        db = newValue;
-    }
+    public void setDB(DB db) {
+        this.db = db;}
 
     public DB getDB(){
-	return db; 
-    }
+	return db;}
 
-    public void setSchemafile(String filename) {
-        dbschema = filename;
-    }
+    public void setSchemafile(String dbschema){
+        this.dbschema = dbschema;}
 
     /*** This method loads database schema. Be sure to load it ONLY once. ***/
 
     public void genParentSchema(String filename){
-		File readSchema = new File(filename);
-		String inputString;
-		StringBuffer buf = new StringBuffer();
-
-		if (readSchema.exists()){
+	File readSchema = new File(filename);
+	String inputString;
+	StringBuffer buf = new StringBuffer();
+	
+	if (readSchema.exists()){
             System.out.println("Found " + filename + "  ... Loading");
         }
         else System.out.println("Did not find " +  filename);
-
-		try{	
-	    	BufferedReader preader = new BufferedReader(new FileReader(readSchema));	
-
-	    	while ((inputString = preader.readLine())!= null){
-				inputString = inputString.replaceAll("@DATABASE_NAME@", parser.getDBName());
-				buf.append(inputString);
-				if (isEnd(inputString)) {
-		    		try {
-						getDB().executeUpdate(buf.toString());
-						buf = buf.delete(0,buf.length());
-		    		} catch (SQLException ex) {
-						ex.printStackTrace();
-		    		}				
-				}		
-	    	}
-		}catch (Exception e){
-	    	e.printStackTrace();
-		}
+	
+	try{	
+	    BufferedReader preader = new BufferedReader(new FileReader(readSchema));	
+	    
+	    while ((inputString = preader.readLine())!= null){
+		inputString = inputString.replaceAll("@DATABASE_NAME@", parser.getDBName());
+		buf.append(inputString);
+		if (isEnd(inputString)) {
+		    try {
+			getDB().executeUpdate(buf.toString());
+			buf = buf.delete(0,buf.length());
+		    } catch (SQLException ex) {
+			ex.printStackTrace();
+		    }				
+		}		
+	    }
+	}
+	catch (Exception e){
+	    e.printStackTrace();
+	}
     }
 
-	public void genParentSchema() {
-		genParentSchema(parser.getDBSchema());
-	}
+    public void genParentSchema(){
+	genParentSchema(parser.getDBSchema());}
 
     public static boolean isEnd(String st){
-	return st.trim().endsWith(";");
-    }
+	return st.trim().endsWith(";");}
 
     //For backward compatibility, simulate default parameter.
     public String getPassword (){
