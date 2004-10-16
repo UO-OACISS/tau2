d8140984b0eda96b2a0d403b408d5920b8f634f7
bertie <bertie@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2004-10-16 00:08:53 +0000

    Added a display properties interface to make setting display properties easier!
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@2738 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ tools/src/paraprof/src/edu/uoregon/tau/paraprof/DisplayProperties.java
@@ -0,0 +1,35 @@
+/* 
+   Name:        DisplayProperties.java
+   Author:      Robert Bell
+   Description: 
+*/
+
+package edu.uoregon.tau.paraprof;
+
+public interface DisplayProperties{
+
+    public void setValueType(int valueType);
+    public int getValueType();
+    public void setOrder(int order);
+    public int getOrder();
+    public void setUnits(int units);
+    public int getUnits();
+
+    //####################################
+    //Instance data.
+    //####################################
+    public static final int EXCLUSIVE = 2;
+    public static final int INCLUSIVE = 4;
+    public static final int NUMBER_OF_CALLS = 6;
+    public static final int NUMBER_OF_SUBROUTINES = 8;
+    public static final int PAR_CALL_VALUE = 10;
+    public static final int DESCENDING_ORDER = 0;
+    public static final int ASCENDING_ORDER = 1;
+    public static final int MICROSECONDS = 0;
+    public static final int MILLISECONDS = 1;
+    public static final int SECONDS = 2;
+    //####################################
+    //End - Instance data.
+    //####################################
+
+}
