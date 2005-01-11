9a9b49d48e25393cdaeeb1d005c95b31902caa20
sameer <sameer@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2005-01-11 00:37:33 +0000

    Added an example to illustrate phase based profiling.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@2959 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ examples/phase/simple.cpp
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <TAU.h>
+
+int f1();
+int f2();
+int f3();
+int f4();
+int f5();
+
+int f1(void)
+{
+  TAU_PROFILE("f1()", "(sleeps 1 sec, calls f2, f4)", TAU_USER);
+  printf("Inside f1: sleeps 1 sec, calls f2, f4\n");
+  sleep(1);
+  f2();
+  f4();
+  return 0;
+}
+
+int f2(void)
+{
+  static int x = 0;
+  x++;
+  char buf[32];
+  sprintf(buf, "%d", x);
+  TAU_PHASE_CREATE_DYNAMIC(t,"f2() (sleeps 2 sec, calls f3)", buf, TAU_USER);
+  TAU_PHASE_START(t);
+  printf("Inside f2: sleeps 2 sec, calls f3\n");
+  sleep(2);
+  f3();
+  TAU_PHASE_STOP(t);
+  return 0;
+}
+
+int f3(void)
+{
+  TAU_PROFILE("f3()", "(sleeps 3 sec)", TAU_USER);
+  printf("Inside f3: sleeps 3 sec\n");
+  sleep(3);
+  return 0;
+}
+
+int f4(void)
+{
+  TAU_PROFILE("f4()", "(sleeps 4 sec, calls f2)", TAU_USER);
+  printf("Inside f4: sleeps 4 sec, calls f2\n");
+  sleep(4);
+  f2();
+  return 0;
+}
+
+int f5(void)
+{
+  TAU_PROFILE("f5()", "(sleeps 5 sec)", TAU_USER);
+  printf("Inside f5: sleeps 5 sec\n");
+  sleep(5);
+  return 0;
+}
+
+int main(int argc, char **argv)
+{
+  TAU_PROFILE("main()", "(calls f1, f5)", TAU_DEFAULT);
+  TAU_PROFILE_SET_NODE(0);
+  printf("Inside main: calls f1, f5\n");
+
+  f1();
+  f5();
+}
