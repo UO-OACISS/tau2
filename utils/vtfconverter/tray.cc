0cd5ac621bf2929317ee084dea703bc4186b951f
sameer <sameer@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2005-01-18 23:26:30 +0000

    Wyatt Spear added support for multiple metrics to vtf2profile. Old log: RCS file: /research/paraducks2/cvs-src/master/VTFtoTAU/tray.cc,v Working file: tray.cc head: 1.3 branch: locks: strict access list: symbolic names:         start: 1.1.1.1         rharris1: 1.1.1 keyword substitution: kv total revisions: 4;     selected revisions: 4 description: ---------------------------- revision 1.3 date: 2003/12/12 22:49:29;  author: ryan;  state: Exp;  lines: +2 -0 fixed templatedFunctions bug, works w/Paraprof again, more comment cleanup ---------------------------- revision 1.2 date: 2003/10/20 00:53:26;  author: ryan;  state: Exp;  lines: +25 -0 implemented interval selection and blank profiles ---------------------------- revision 1.1 date: 2003/08/27 22:58:58;  author: rharris1;  state: Exp; branches:  1.1.1; Initial revision ---------------------------- revision 1.1.1.1 date: 2003/08/27 22:58:58;  author: rharris1;  state: Exp;  lines: +0 -0 initial import into CVS =============================================================================
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@2997 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ utils/vtfconverter/tray.cc
@@ -0,0 +1,229 @@
+/************************************************************************
+************************************************************************/
+
+/************************************************************************
+	tray.cc
+	Author: Ryan K. Harris
+ 
+	Source file for tray class.
+*************************************************************************
+ 
+	Member functions:
+		getFunctionID -- returns functionID
+		getTimeStamp -- returns time stamp
+		getSamples -- returns samples
+		getExchangeType -- returns exchange type
+		setFunctionID -- sets functionID
+		setTimeStamp -- sets timeStamp
+		setSamples -- sets the sample data (samples) array
+		setExchangeType -- sets exchangeType
+ 
+		getPrevious -- gets tray that came previous to the current one
+		getNext -- gets tray that comes after this one
+		setPrevious -- set previousTray pointer
+		setNext -- set nextTray pointer
+		getLastEntrance -- return the 'lastEntrance' var.
+		setLastEntrance -- set time of 'lastEntrance' var.
+		getLastSample -- return last observed sample data
+		setLastSample -- set last observed sample data
+************************************************************************/
+
+#include "tray.h"
+
+
+/************************************************************************
+	tray::getFunctionID
+		Returns the functionID
+ 
+	Parameters: none
+************************************************************************/
+int tray::getFunctionID(void)
+{
+	return(functionID);
+}//getFunctionID
+
+/************************************************************************
+	tray::getTimeStamp
+		Returns the timeStamp
+ 
+	Parameters: none
+************************************************************************/
+double tray::getTimeStamp(void)
+{
+	return(timeStamp);
+}//getTimeStamp
+
+/************************************************************************
+	tray::getExchangeType
+		Returns exchangeType
+ 
+	Parameters: none
+************************************************************************/
+int tray::getExchangeType(void)
+{
+	return(exchangeType);
+}//getExchangeType
+
+/************************************************************************
+	tray::setFunctionID
+		set the functionID
+ 
+	Parameters: int (pass by value)
+************************************************************************/
+void tray::setFunctionID(int n_functionID)
+{
+	functionID = n_functionID;
+	return;
+}//setFunctionID
+
+/************************************************************************
+	tray::setTimeStamp
+		set the timeStamp
+ 
+	Parameters: int (pass by value)
+************************************************************************/
+void tray::setTimeStamp(double n_timeStamp)
+{
+	timeStamp = n_timeStamp;
+	return;
+}//setTimeStamp
+
+/************************************************************************
+	tray::setExchangeType
+		set the exchangeType
+ 
+	Parameters: int (pass by value)
+************************************************************************/
+void tray::setExchangeType(int n_exchangeType)
+{
+	exchangeType = n_exchangeType;
+	return;
+}//setExchangeType
+
+/************************************************************************
+	tray::getPrevious
+		get the tray that came previous to this one on the stack
+ 
+	Parameters: none
+************************************************************************/
+tray * tray::getPrevious(void)
+{
+	return(previousTray);
+}//getPrevious
+
+/************************************************************************
+	tray::getNext
+		returns the tray that comes after this one on the stack
+ 
+	Parameters: none
+************************************************************************/
+tray * tray::getNext(void)
+{
+	return(nextTray);
+}//getNext
+
+/************************************************************************
+	tray::setPrevious
+		set the previousTray pointer
+ 
+	Parameters: reference to a tray (pass by address)
+************************************************************************/
+void tray::setPrevious(tray * n_previousTray)
+{
+	delete previousTray;
+	previousTray = n_previousTray;
+	return;
+}//setPrevious
+
+/************************************************************************
+	tray::setNext
+		set the nextTray pointer
+ 
+	Parameters: reference to a tray pointer ( pass by address)
+************************************************************************/
+void tray::setNext(tray * n_nextTray)
+{
+	delete nextTray;
+	nextTray = n_nextTray;
+	return;
+}//setNext
+
+/************************************************************************
+	tray::getLastEntrance
+		returns time this function was last entered/returned to, or
+		began generating exclusive time again
+ 
+	Parameters: none
+************************************************************************/
+double tray::getLastEntrance(void)
+{
+	return(lastEntrance);
+}//getLastEntrance
+
+/************************************************************************
+	tray::setLastEntrance
+		set last time this function was entered/returned to, or
+		began generating exclusive time
+ 
+	Parameters: double: time to set (pass by value)
+************************************************************************/
+void tray::setLastEntrance(double n_lastEntrance)
+{
+	lastEntrance = n_lastEntrance;
+	return;
+}//setLastEntrance
+
+
+/************************************************************************
+	tray::setLastSamples
+		set last values of this  function's samples
+ 
+	Parameters: int*: values to set (pass by value)
+************************************************************************/
+void tray::setLastSamples(unsigned long long * a_lastsamples)
+{
+	for(int i = 0; i<samplesdim; i++)
+	{
+		lastsamples[i] = a_lastsamples[i];
+	}
+	return;
+}//setLastSamples
+
+/************************************************************************
+	tray::getLastSamples
+		get last values of this function's samples
+ 
+	Parameters: int*: values to get (pass by value)
+************************************************************************/
+unsigned long long * tray::getLastSamples(void)
+{
+	return lastsamples;
+}//getLastSamples
+
+
+/************************************************************************
+	tray::getSamples
+		get last values of this function's samples
+ 
+	Parameters: int*: values to get (pass by value)
+************************************************************************/
+unsigned long long * tray::getSamples(void)
+{
+	return samples;
+}//getSamples
+
+
+/************************************************************************
+	tray::setSamples
+		set values of this  function's samples
+ 
+	Parameters: int*: values to set (pass by value)
+************************************************************************/
+void tray::setSamples(unsigned long long * a_samples)
+{
+	for(int i = 0; i<samplesdim; i++)
+	{
+		samples[i] = a_samples[i];
+	}
+	return;
+}//setLastSample
