e7cb66e02acbd170d20ef498c665040314dd3deb
sameer <sameer@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2005-01-18 23:29:22 +0000

    RCS file: /research/paraducks2/cvs-src/master/VTFtoTAU/stack.h,v Working file: stack.h head: 1.3 branch: locks: strict access list: symbolic names:         start: 1.1.1.1         rharris1: 1.1.1 keyword substitution: kv total revisions: 4;     selected revisions: 4 description: ---------------------------- revision 1.3 date: 2003/12/12 22:49:28;  author: ryan;  state: Exp;  lines: +1 -0 fixed templatedFunctions bug, works w/Paraprof again, more comment cleanup ---------------------------- revision 1.2 date: 2003/10/20 00:53:25;  author: ryan;  state: Exp;  lines: +16 -0 implemented interval selection and blank profiles ---------------------------- revision 1.1 date: 2003/08/27 22:58:58;  author: rharris1;  state: Exp; branches:  1.1.1; Initial revision ---------------------------- revision 1.1.1.1 date: 2003/08/27 22:58:58;  author: rharris1;  state: Exp;  lines: +0 -0 initial import into CVS =============================================================================
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@3002 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ utils/vtfconverter/stack.h
@@ -0,0 +1,105 @@
+/************************************************************************
+************************************************************************/
+
+/************************************************************************
+	stack.h
+	Author: Ryan K. Harris
+ 
+	Definitions for the stack class.
+	A stack is filled with trays.
+ 
+	Notice that the stack constructor takes a reference to
+	a nodeC (pass by address). What we're doing is passing in a
+	reference to the parent nodeC ( 'cpu' node) of this instantiated
+	stack. The reason I chose this approach is: we want every data
+	structure within the parent 'cpu' node to be able to directly
+	interact with every other data structure; namely, we want the
+	internal stack to be able to interact directly with the internal
+	'funcTree' (binarytree data structure holding the 'function' nodes
+	associated to this cpuID).
+*************************************************************************
+	class stack
+ 
+	Member functions:
+		push -- push a tray onto the stack
+				-- It also handles the time counters for each tray.
+************************************************************************/
+#ifndef __STACK_H
+#define __STACK_H
+
+#include "tray.h"
+#include "nodeC.h"
+#include "node.h"
+
+class nodeC;
+
+class stack
+{
+private:
+	tray * topTray;		// Points to the tray on top
+	tray * lastEntry;	// Used to track most recent timeStamp
+	tray * tempTray;	//For memory control
+	nodeC * parent;		// 'cpu' node which owns this stack
+	double startTime;	// Holds starting interval time if provided.
+	double endTime;		// Holds ending interval time if provided.
+	unsigned long long * startMets;//As startTime but for metrics
+	unsigned long long * endMets;//As endTime but for metrics
+	int startset;
+	int endset;
+
+public:
+	/* Constructor: pass in one parameter, the cpuID
+		of the 'cpu' node which owns this stack. */
+	stack(nodeC * n_parent)
+	{
+		parent = n_parent;
+		topTray = 0;
+		lastEntry = 0;
+		tempTray = 0;
+
+		startMets = 0;
+		endMets = 0;
+		startset = 0;
+		endset = 0;
+
+		startTime = -1;
+		endTime = -1;
+	}
+
+	stack(nodeC * n_parent, double n_startTime, double n_endTime)
+	{
+		parent = n_parent;
+		topTray = 0;
+		lastEntry = 0;
+
+		startMets = 0;
+		endMets = 0;
+		startset = 0;
+		endset = 0;
+
+		startTime = n_startTime;
+		endTime = n_endTime;
+	}
+
+
+	~stack()
+	{
+		//delete topTray;
+		//delete lastEntry;
+	}
+
+	void push(int n_functionID, double n_timeStamp,
+			  unsigned long long * a_samples, int n_samplesdim, int n_exchangeType);
+	void newtray(int n_functionID, double n_timeStamp,
+		unsigned long long * a_samples, int n_samplesdim);
+	void routine(int n_functionID, double n_timeStamp,
+		unsigned long long * a_samples, int n_samplesdim);
+	void returnfrom(int n_functionID, double n_timeStamp,
+		unsigned long long * a_samples, int n_samplesdim);
+	void stack::lastreturn(int n_functionID, double n_timeStamp, 
+		unsigned long long * a_samples, int n_samplesdim);
+	void stack::norec(node * funcNode, int n_samplesdim, tray * tempTray);
+}
+;//stack
+
+#endif
