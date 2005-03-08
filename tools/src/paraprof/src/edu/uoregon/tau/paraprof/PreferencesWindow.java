61ff69a0d06f5fab22363817107d2dda10462f40
amorris <amorris@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2005-03-08 01:17:41 +0000

    Added Preferences Window.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@3088 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ tools/src/paraprof/src/edu/uoregon/tau/paraprof/PreferencesWindow.java
@@ -0,0 +1,494 @@
+/*
+ * Preferences.java
+ * 
+ * Title: ParaProf 
+ * Author: Robert Bell 
+ * Description:
+ * 
+ * There is one "Preferences" object associated with each trial (not anymore!)
+ */
+
+package edu.uoregon.tau.paraprof;
+
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import edu.uoregon.tau.dms.dss.*;
+
+public class PreferencesWindow extends JFrame implements ActionListener, Observer {
+
+    private boolean mShown = false;
+
+    //References for some of the components for this frame.
+    private PrefSpacingPanel prefSpacingPanel;
+    private JCheckBox bold;
+    private JCheckBox italic;
+
+    private JLabel fontLabel = new JLabel("Font");
+    private JComboBox fontComboBox;
+    private JLabel barHeightLabel = new JLabel("Size");
+    private JSlider barHeightSlider = new JSlider(SwingConstants.HORIZONTAL, 0, 40, 0);
+
+    private Preferences preferences;
+
+    // computed on each execution
+    private int barSpacing = 0;
+    private int barHeight = 0;
+    private boolean barDetailsSet = false;
+
+    private String paraProfFont = "SansSerif";
+    private int fontStyle = Font.PLAIN;
+    private int fontSize = 12;
+
+    void setControls() {
+        int tmpInt = fontComboBox.getItemCount();
+        int counter = 0;
+        //We should always have some fonts available, so this should be safe.
+        String tmpString = (String) fontComboBox.getItemAt(counter);
+        while ((counter < tmpInt) && (!(paraProfFont.equals(tmpString)))) {
+            counter++;
+            tmpString = (String) fontComboBox.getItemAt(counter);
+        }
+
+        if (counter == tmpInt) {
+            //The default font was not available. Indicate an error.
+            System.out.println("The default font was not found!  This is not a good thing as it is a default Java font!");
+        } else {
+            fontComboBox.setSelectedIndex(counter);
+        }
+
+        bold.setSelected((fontStyle == Font.BOLD) || (fontStyle == (Font.BOLD | Font.ITALIC)));
+        italic.setSelected((fontStyle == (Font.PLAIN | Font.ITALIC))
+                || (fontStyle == (Font.BOLD | Font.ITALIC)));
+        barHeightSlider.setValue(fontSize);
+    }
+
+    public PreferencesWindow(Preferences preferences) {
+        this.preferences = preferences;
+
+        if (preferences.getLoaded()) {
+            // Set preferences based on saved values.
+            paraProfFont = preferences.getParaProfFont();
+            fontStyle = preferences.getFontStyle();
+            fontSize = preferences.getFontSize();
+        }
+
+        //Add some window listener code
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowClosing(java.awt.event.WindowEvent evt) {
+                thisWindowClosing(evt);
+            }
+        });
+
+        //Get available fonts and initialize the fontComboBox..
+        GraphicsEnvironment gE = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        String[] fontFamilyNames = gE.getAvailableFontFamilyNames();
+        fontComboBox = new JComboBox(fontFamilyNames);
+
+        fontComboBox.addActionListener(this);
+
+        //Now initialize the panels.
+        prefSpacingPanel = new PrefSpacingPanel();
+
+        //Window Stuff.
+        setTitle("ParaProf Preferences");
+
+        int windowWidth = 550;
+        int windowHeight = 300;
+        setSize(new java.awt.Dimension(windowWidth, windowHeight));
+
+        //There is really no need to resize this window.
+        setResizable(true);
+
+        //Grab the screen size.
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Dimension screenDimension = tk.getScreenSize();
+        int screenHeight = screenDimension.height;
+        int screenWidth = screenDimension.width;
+
+        //Set the window to come up in the center of the screen.
+        int xPosition = 0;
+        int yPosition = 0;
+
+        setLocation(xPosition, yPosition);
+
+        //End - Window Stuff.
+
+        setupMenus();
+
+        //####################################
+        //Create and add the components
+        //####################################
+
+        //Setup the layout system for the main window.
+        Container contentPane = getContentPane();
+        GridBagLayout gbl = new GridBagLayout();
+        contentPane.setLayout(gbl);
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+
+        JScrollPane jScrollPane = new JScrollPane(prefSpacingPanel);
+        jScrollPane.setPreferredSize(new Dimension(300, 200));
+
+        bold = new JCheckBox("Bold Font");
+        bold.addActionListener(this);
+        italic = new JCheckBox("Italic Font");
+        italic.addActionListener(this);
+
+        setControls();
+
+        barHeightSlider.setPaintTicks(true);
+        barHeightSlider.setMajorTickSpacing(10);
+        barHeightSlider.setMinorTickSpacing(5);
+        barHeightSlider.setPaintLabels(true);
+        barHeightSlider.addChangeListener(prefSpacingPanel);
+
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.anchor = GridBagConstraints.EAST;
+        gbc.weightx = 0;
+        gbc.weighty = 0;
+        addCompItem(fontLabel, gbc, 0, 0, 1, 1);
+
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.weightx = 0;
+        gbc.weighty = 0;
+        addCompItem(fontComboBox, gbc, 1, 0, 1, 1);
+
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.weightx = 0;
+        gbc.weighty = 0;
+        addCompItem(bold, gbc, 0, 1, 2, 1);
+
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.weightx = 0;
+        gbc.weighty = 0;
+        addCompItem(italic, gbc, 0, 2, 2, 1);
+
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.weightx = 0;
+        gbc.weighty = 0;
+        addCompItem(barHeightLabel, gbc, 0, 3, 2, 1);
+
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.weightx = 0.25;
+        gbc.weighty = 0.25;
+        addCompItem(barHeightSlider, gbc, 0, 4, 2, 1);
+
+        JButton defaultButton = new JButton("Restore Defaults");
+        defaultButton.addActionListener(this);
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.weightx = 0;
+        gbc.weighty = 0;
+        addCompItem(defaultButton, gbc, 0, 5, 2, 1);
+
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.anchor = GridBagConstraints.EAST;
+        gbc.weightx = 0.5;
+        gbc.weighty = 0.5;
+        addCompItem(jScrollPane, gbc, 2, 0, 2, 5);
+
+        JPanel applyCancelPanel = new JPanel();
+
+        JButton applyButton = new JButton("Apply");
+        applyButton.addActionListener(this);
+        JButton cancelButton = new JButton("Cancel");
+        cancelButton.addActionListener(this);
+
+        applyCancelPanel.add(applyButton);
+        applyCancelPanel.add(cancelButton);
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.anchor = GridBagConstraints.EAST;
+        gbc.weightx = 0;
+        gbc.weighty = 0;
+        addCompItem(applyCancelPanel, gbc, 3, 5, 1, 1);
+
+        setSavedPreferences();
+
+    }
+
+    private void setupMenus() {
+        JMenuBar mainMenu = new JMenuBar();
+
+        JMenu fileMenu = new JMenu("File");
+
+        JMenuItem jmenuItem;
+
+        jmenuItem = new JMenuItem("Load Preferences...");
+        jmenuItem.addActionListener(this);
+        fileMenu.add(jmenuItem);
+        jmenuItem = new JMenuItem("Save Preferences...");
+        jmenuItem.addActionListener(this);
+        fileMenu.add(jmenuItem);
+
+        JMenuItem editColorItem = new JMenuItem("Edit Default Colors");
+        editColorItem.addActionListener(this);
+        fileMenu.add(editColorItem);
+
+        //        JMenuItem saveColorItem = new JMenuItem("Save Color Map");
+        //        saveColorItem.addActionListener(this);
+        //        fileMenu.add(saveColorItem);
+        //
+        //        JMenuItem loadColorItem = new JMenuItem("Load Color Map");
+        //        loadColorItem.addActionListener(this);
+        //        fileMenu.add(loadColorItem);
+
+        JMenuItem newItem = new JMenuItem("Show Color Map");
+        newItem.addActionListener(this);
+        fileMenu.add(newItem);
+
+        JMenuItem closeItem = new JMenuItem("Apply and Close Window");
+        closeItem.addActionListener(this);
+        fileMenu.add(closeItem);
+
+        JMenuItem exitItem = new JMenuItem("Exit ParaProf!");
+        exitItem.addActionListener(this);
+        fileMenu.add(exitItem);
+
+        mainMenu.add(fileMenu);
+
+        setJMenuBar(mainMenu);
+    }
+
+    public void showPreferencesWindow() {
+        //The path to data might have changed, therefore, reset the title.
+        this.setTitle("ParaProf Preferences");
+        this.show();
+    }
+
+    public void setSavedPreferences() {
+        ParaProf.preferences.setParaProfFont(paraProfFont);
+        ParaProf.preferences.setFontStyle(fontStyle);
+        ParaProf.preferences.setFontSize(fontSize);
+    }
+
+    public boolean areBarDetailsSet() {
+        return barDetailsSet;
+    }
+
+    public String getParaProfFont() {
+        return paraProfFont;
+    }
+
+    public int getFontStyle() {
+        return fontStyle;
+    }
+
+    public int getFontSize() {
+        return fontSize;
+    }
+
+    public void setBarDetails(Graphics2D g2D) {
+        if (!barDetailsSet) {
+            Font font = new Font(paraProfFont, fontStyle, fontSize);
+            g2D.setFont(font);
+            FontMetrics fmFont = g2D.getFontMetrics(font);
+            int maxFontAscent = fmFont.getAscent();
+            int maxFontDescent = fmFont.getMaxDescent();
+            this.barHeight = maxFontAscent;
+            this.barSpacing = maxFontAscent + maxFontDescent + 2;
+            barDetailsSet = true;
+        }
+    }
+
+    public void setFontSize(int fontSize) {
+        this.fontSize = fontSize;
+        barDetailsSet = false;
+    }
+
+    public void updateFontSize() {
+        fontSize = Math.max(1, barHeightSlider.getValue());
+        barDetailsSet = false;
+    }
+
+    public int getBarSpacing() {
+        return barSpacing;
+    }
+
+    public int getBarHeight() {
+        return barHeight;
+    }
+
+    //####################################
+    //Interface code.
+    //####################################
+
+    //######
+    //ActionListener.
+    //######
+    public void actionPerformed(ActionEvent evt) {
+        try {
+            Object EventSrc = evt.getSource();
+            String arg = evt.getActionCommand();
+            if (EventSrc instanceof JMenuItem) {
+
+                if (arg.equals("Show Color Map")) {
+                    ParaProf.colorMap.showColorMap();
+                } else if (arg.equals("Load Preferences...")) {
+
+                    JFileChooser fileChooser = new JFileChooser();
+
+                    //Set the directory to the current directory.
+                    fileChooser.setCurrentDirectory(new File("."));
+
+                    //Bring up the file chooser.
+                    int resultValue = fileChooser.showOpenDialog(this);
+
+                    if (resultValue == JFileChooser.APPROVE_OPTION) {
+                        File file = fileChooser.getSelectedFile();
+
+                        try {
+                            ParaProf.loadPreferences(file);
+                        } catch (Exception e) {
+                            JOptionPane.showMessageDialog(this, "Error loading preferences!", "ParaProf Preferences", JOptionPane.ERROR_MESSAGE);
+
+                        }
+                        this.preferences = ParaProf.preferences;
+                        setControls();
+                    }
+
+                } else if (arg.equals("Save Preferences...")) {
+
+                    JFileChooser fileChooser = new JFileChooser();
+                    fileChooser.setCurrentDirectory(new File("."));
+                    int resultValue = fileChooser.showSaveDialog(this);
+
+                    if (resultValue == JFileChooser.APPROVE_OPTION) {
+
+                        File file = fileChooser.getSelectedFile();
+
+                        ParaProf.colorChooser.setSavedColors();
+                        ParaProf.preferences.setManagerWindowPosition(ParaProf.paraProfManager.getLocation());
+
+                        //System.out.println ("saving manager position = " + preferences.getManagerWindowPosition());
+
+                        try {
+                            ObjectOutputStream prefsOut = new ObjectOutputStream(new FileOutputStream(file));
+                            prefsOut.writeObject(ParaProf.preferences);
+                            prefsOut.close();
+                        } catch (Exception e) {
+                            System.err.println("An error occured while trying to save ParaProf preferences.");
+                            //e.printStackTrace();
+                        }
+                    }
+
+                } else if (arg.equals("Edit Default Colors")) {
+                    ParaProf.colorChooser.showColorChooser();
+                } else if (arg.equals("Exit ParaProf!")) {
+                    setVisible(false);
+                    dispose();
+                    ParaProf.exitParaProf(0);
+                } else if (arg.equals("Apply and Close Window")) {
+                    setVisible(false);
+                    setSavedPreferences();
+                    Vector trials = ParaProf.paraProfManager.getLoadedTrials();
+                    for (Iterator it = trials.iterator(); it.hasNext();) {
+                        ParaProfTrial ppTrial = (ParaProfTrial) it.next();
+                        ppTrial.getSystemEvents().updateRegisteredObjects("prefEvent");
+                    }
+                }
+
+            } else if (EventSrc instanceof JCheckBox) {
+                if (arg.equals("Bold Font")) {
+                    if (italic.isSelected())
+                        fontStyle = Font.BOLD | Font.ITALIC;
+                    else
+                        fontStyle = Font.BOLD;
+
+                    prefSpacingPanel.repaint();
+                } else if (arg.equals("Italic Font")) {
+                    if (italic.isSelected()) {
+                        if (bold.isSelected())
+                            fontStyle = Font.BOLD | Font.ITALIC;
+                        else
+                            fontStyle = Font.PLAIN | Font.ITALIC;
+                    } else {
+                        if (bold.isSelected())
+                            fontStyle = Font.BOLD;
+                        else
+                            fontStyle = Font.PLAIN;
+                    }
+
+                    prefSpacingPanel.repaint();
+                }
+            } else if (EventSrc == fontComboBox) {
+                paraProfFont = (String) fontComboBox.getSelectedItem();
+                prefSpacingPanel.repaint();
+            } else if (EventSrc instanceof JButton) {
+                if (arg.equals("Apply")) {
+                    setSavedPreferences();
+                    Vector trials = ParaProf.paraProfManager.getLoadedTrials();
+                    for (Iterator it = trials.iterator(); it.hasNext();) {
+                        ParaProfTrial ppTrial = (ParaProfTrial) it.next();
+                        ppTrial.getSystemEvents().updateRegisteredObjects("prefEvent");
+                    }
+                } else if (arg.equals("Cancel")) {
+                    setVisible(false);
+                    paraProfFont = preferences.getParaProfFont();
+                    fontStyle = preferences.getFontStyle();
+                    fontSize = preferences.getFontSize();
+                    setControls();
+                } else if (arg.equals("Restore Defaults")) {
+                    paraProfFont = "SansSerif";
+                    fontStyle = Font.PLAIN;
+                    fontSize = 12;
+                    setControls();
+                }
+
+            }
+        } catch (Exception e) {
+            ParaProfUtils.handleException(e);
+        }
+    }
+
+    public void update(Observable o, Object arg) {
+        String tmpString = (String) arg;
+        if (tmpString.equals("colorEvent")) {
+            //Just need to call a repaint.
+            prefSpacingPanel.repaint();
+        }
+    }
+
+    private void addCompItem(Component c, GridBagConstraints gbc, int x, int y, int w, int h) {
+        gbc.gridx = x;
+        gbc.gridy = y;
+        gbc.gridwidth = w;
+        gbc.gridheight = h;
+        getContentPane().add(c, gbc);
+    }
+
+    public void addNotify() {
+        super.addNotify();
+
+        if (mShown)
+            return;
+
+        // resize frame to account for menubar
+        JMenuBar jMenuBar = getJMenuBar();
+        if (jMenuBar != null) {
+            int jMenuBarHeight = jMenuBar.getPreferredSize().height;
+            Dimension dimension = getSize();
+            dimension.height += jMenuBarHeight;
+            setSize(dimension);
+        }
+
+        mShown = true;
+    }
+
+    // Close the window when the close box is clicked
+    void thisWindowClosing(java.awt.event.WindowEvent e) {
+        setVisible(false);
+        paraProfFont = preferences.getParaProfFont();
+        fontStyle = preferences.getFontStyle();
+        fontSize = preferences.getFontSize();
+        setControls();
+    }
+
+}
