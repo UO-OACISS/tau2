a3d147f89489e67f5bd737ed3ef269388bf636de
amorris <amorris@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2005-05-31 23:27:33 +0000

    Changes for java 1.3
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@3268 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- tools/src/paraprof/src/edu/uoregon/tau/paraprof/wrapper/ParaProfImageOutput.java
+++ tools/src/paraprof/src/edu/uoregon/tau/paraprof/wrapper/ParaProfImageOutput.java
@@ -16,19 +16,19 @@ import java.awt.event.*;
 import java.io.*;
 import javax.swing.*;
 import edu.uoregon.tau.dms.dss.*;
+import edu.uoregon.tau.paraprof.interfaces.*;
 
 public class ParaProfImageOutput{
 
     public ParaProfImageOutput(){
     }
 
-    public static void saveImage(ParaProfImageInterface ref){
+    public static void saveImage(ImageExport ref){
 	try{
 	    JOptionPane.showMessageDialog(null, "Jar compiled for jdk-ver < 1.4. Image support not available.",
-						      "Image Error!",
-						      JOptionPane.ERROR_MESSAGE);
-	}
-	catch(Exception e){
+					  "Image Error!",
+					  JOptionPane.ERROR_MESSAGE);
+	} catch(Exception e){
 	}
     }
 
