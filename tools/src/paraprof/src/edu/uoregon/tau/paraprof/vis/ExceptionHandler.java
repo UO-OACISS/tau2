301cb2d16d256e2846344aa26c4c931236c08e2b
amorris <amorris@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2005-07-11 23:03:54 +0000

    Forgot to add file.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@3393 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ tools/src/paraprof/src/edu/uoregon/tau/paraprof/vis/ExceptionHandler.java
@@ -0,0 +1,12 @@
+package edu.uoregon.tau.paraprof.vis;
+
+/**
+ * Defines a class that will handle exceptions
+ * 
+ * @author amorris
+ *
+ * @see VisTools
+ */
+public interface ExceptionHandler {
+    void handleException(Exception e);
+}
