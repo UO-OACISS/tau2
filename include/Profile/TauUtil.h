5ad4204194d126d3fbe09ae93b14fdde9e9092fa
amorris <amorris@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2005-07-19 00:54:41 +0000

    Fixed status conversion for IBM MPI on AIX and SGI on ia64.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@3427 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- include/Profile/TauUtil.h
+++ include/Profile/TauUtil.h
@@ -37,14 +37,15 @@
 #define TAU_ALLOC_LOCAL(mtype, l, size) l = (mtype *) malloc(sizeof(mtype) * size)
 #define TAU_DECL_ALLOC_LOCAL(mtype, l, size) TAU_DECL_LOCAL(mtype, l) = TAU_ALLOC_LOCAL(mtype, l, size) 
 #define TAU_ASSIGN_VALUES(dest, src, size, func) { int i; for (i = 0; i < size; i++) dest[i] = func(src[i]); }
-#define TAU_ASSIGN_STATUS(dest, src, size, func) { int i; for (i = 0; i < size; i++) func(&src[i], &dest[i]); }
+#define TAU_ASSIGN_STATUS(dest, src, size, func) { int i; for (i = 0; i < size; i++) func(&((MPI_Status*)src)[i], &((MPI_Status*)dest)[i]); }
+
 #define TAU_FREE_LOCAL(l) free(l)
 #endif /* sgi || TAU_MPI_NEEDS_STATUS */
 
 
 /******************************************************/
 #if (defined(sgi) || defined(TAU_MPI_NEEDS_STATUS))
-#define MPI_Status_c2f(c,f) *f=*(MPI_Fint *)c 
+#define MPI_Status_c2f(c,f) *(MPI_Status *)f=*c 
 #define MPI_Status_f2c(f,c) *c=*(MPI_Status *)f
 #endif /* sgi || TAU_MPI_NEEDS_STATUS */
 
