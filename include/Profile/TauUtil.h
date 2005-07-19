609f4d151e852354b239a9bc939a53c861fea9a1
amorris <amorris@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2005-07-19 19:38:11 +0000

    Split TAU_ASSIGN_STATUS into two macros to satiate Cray's C compiler.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@3429 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- include/Profile/TauUtil.h
+++ include/Profile/TauUtil.h
@@ -37,7 +37,11 @@
 #define TAU_ALLOC_LOCAL(mtype, l, size) l = (mtype *) malloc(sizeof(mtype) * size)
 #define TAU_DECL_ALLOC_LOCAL(mtype, l, size) TAU_DECL_LOCAL(mtype, l) = TAU_ALLOC_LOCAL(mtype, l, size) 
 #define TAU_ASSIGN_VALUES(dest, src, size, func) { int i; for (i = 0; i < size; i++) dest[i] = func(src[i]); }
-#define TAU_ASSIGN_STATUS(dest, src, size, func) { int i; for (i = 0; i < size; i++) func(&((MPI_Status*)src)[i], &((MPI_Status*)dest)[i]); }
+
+#define TAU_ASSIGN_STATUS_F2C(dest, src, size, func) { int i; for (i = 0; i < size; i++) func((MPI_Fint*)&((MPI_Status*)src)[i], &((MPI_Status*)dest)[i]); }
+#define TAU_ASSIGN_STATUS_C2F(dest, src, size, func) { int i; for (i = 0; i < size; i++) func(&((MPI_Status*)src)[i], (MPI_Fint*)&((MPI_Status*)dest)[i]); }
+
+
 
 #define TAU_FREE_LOCAL(l) free(l)
 #endif /* sgi || TAU_MPI_NEEDS_STATUS */
@@ -45,8 +49,8 @@
 
 /******************************************************/
 #if (defined(sgi) || defined(TAU_MPI_NEEDS_STATUS))
-#define MPI_Status_c2f(c,f) *(MPI_Status *)f=*c 
-#define MPI_Status_f2c(f,c) *c=*(MPI_Status *)f
+#define MPI_Status_c2f(c,f) *(MPI_Status *)f=*(MPI_Status *)c 
+#define MPI_Status_f2c(f,c) *(MPI_Status *)c=*(MPI_Status *)f
 #endif /* sgi || TAU_MPI_NEEDS_STATUS */
 
 #endif /* _TAU_UTIL_H_ */
