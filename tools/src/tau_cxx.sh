6cc5625248e8034add82599f4e73ce1e53d89bae
sameer <sameer@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2005-08-06 00:59:00 +0000

    Finally! We have scripts that can invoke TAU_COMPILER from the command line. You can specify the name of the stub makefile using commandline or an environment variable. The same goes for the options to TAU_COMPILER. Use TAU_MAKEFILE and TAU_OPTIONS variables. Bonus: it works with -MM options for make depend. Output of: % tau_cc.sh -c f1.c -MM -DSOMETHING f1.o: f1.c
    % tau_f90.sh -c foo.f90
    will make foo.o with TAU instrumentation in it.
    
    Usage tau_cxx.sh [-tau_makefile=<tau_stub_makefile>] [-tau_options=<tau_compiler_opts>] <opts> <file>
    If -tau_makefile option is not used,
    TAU uses the file specified in TAU_MAKEFILE environment variable
    e.g.,
    % tau_cxx.sh -tau_makefile=/usr/local/tau-2.x/ia64/lib/Makefile.tau-mpi-pdt  -tau_options=-optVerbose -c foo.cpp
            or
    % setenv TAU_MAKEFILE /usr/local/tau-2.x/include/Makefile
    % setenv TAU_OPTIONS -optVerbose -optSelectTauFile=select.tau
    % tau_cxx.sh -c foo.cpp
    
    
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@3478 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ tools/src/tau_cxx.sh
@@ -0,0 +1,80 @@
+#!/bin/sh
+
+# Define variables 
+makefile_specified=no
+options_specified=no
+makedepend_specified=no
+
+if [ $# = 0 ]
+then
+  echo "Usage $0 [-tau_makefile=<tau_stub_makefile>] [-tau_options=<tau_compiler_opts>] <opts> <file>"
+  echo "If -makefile option is not used, "
+  echo "TAU uses the file specified in TAU_MAKEFILE environment variable"
+  echo "e.g., "
+  echo "% tau_cxx.sh -tau_makefile=/usr/local/tau-2.x/ia64/lib/Makefile.tau-mpi-pdt  -tau_options=-optVerbose -c foo.cpp"
+  echo " 	or"
+  echo "% setenv TAU_MAKEFILE /usr/local/tau-2.x/include/Makefile"
+  echo "% setenv TAU_OPTIONS -optVerbose -optSelectTauFile=select.tau"
+  echo "% tau_cxx.sh -c foo.cpp"
+  exit 1
+fi
+
+for arg in "$@"
+do
+  case $arg in 
+    -tau_makefile=*)
+      MAKEFILE=`echo $arg | sed -e 's/-tau_makefile=//'`
+      makefile_specified=yes
+      shift
+      ;;
+    -tau_options=*)
+      TAUCOMPILER_OPTIONS=`echo $arg | sed -e 's/-tau_options=//'`
+      options_specified=yes
+      shift
+      ;;
+    -MM)
+      makedepend_specified=yes	
+# hack to get proper .d generation support for eclipse
+      shift
+      ;;
+    *)
+       ;;
+  esac
+done
+if [ $makefile_specified = no ]
+then
+     MAKEFILE=$TAU_MAKEFILE
+     if [ ! -r $MAKEFILE ]
+     then
+	echo "ERROR: TAU_MAKEFILE environment variable not set or file not readable"
+	exit 1
+     fi
+fi
+if [ $options_specified = no ]
+then
+     TAUCOMPILER_OPTIONS=$TAU_OPTIONS
+     if [ "x$TAUCOMPILER_OPTIONS" = "x" ]
+     then
+       TAUCOMPILER_OPTIONS=-optVerbose 
+     fi
+fi
+
+if [ $makedepend_specified = yes ]
+then
+cat <<EOF > /tmp/makefile.tau$$
+  include $MAKEFILE
+  all:
+	@\$(TAU_CXX) $*
+EOF
+else
+cat <<EOF > /tmp/makefile.tau$$
+  include $MAKEFILE
+  all:
+	\$(TAU_COMPILER) $TAUCOMPILER_OPTIONS \$(TAU_CXX) $* -g
+
+EOF
+fi
+
+make -f /tmp/makefile.tau$$
+#/bin/rm -f /tmp/makefile.tau$$
+
