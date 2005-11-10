8a06038a57d15576711853f6d80e76f26f851c1b
wspear <wspear@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2005-11-10 18:41:16 +0000

    Slightly modified to suit earlier versions of Java.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@3821 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- tools/src/tauconf/Build.sh
+++ tools/src/tauconf/Build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cp -r src/tau_conf .
-javac -cp ../contrib/swing-layout-0.7.1.jar tau_conf/TAU_Conf.java
+javac -classpath ../contrib/swing-layout-0.7.1.jar tau_conf/TAU_Conf.java
 jar -cf TAU_Conf.jar tau_conf
 mv TAU_Conf.jar bin
 rm -rf tau_conf
