6c7e6b71d3dc30518370aaab3a12de3c45dbe58f
amorris <amorris@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2005-11-14 17:47:26 +0000

    unsigned was mapped to signed for windows.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@3854 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- include/Profile/tau_types.h
+++ include/Profile/tau_types.h
@@ -19,10 +19,10 @@
 #define _TAU_TYPES_H_
 
 #ifdef TAU_WINDOWS
-typedef char x_uint8;
-typedef short x_uint16;
-typedef int x_uint32;
-typedef __int64 x_uint64;
+typedef char x_int8;
+typedef short x_int16;
+typedef int x_int32;
+typedef __int64 x_int64;
 
 typedef unsigned char x_uint8;
 typedef unsigned short x_uint16;
