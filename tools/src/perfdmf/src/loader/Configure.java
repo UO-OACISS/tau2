ceee78c8b55df2b581ad7ab12d22356f87d513e6
khuck <khuck@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2006-04-06 18:28:40 +0000

    Allow derby configuration for perfdmf
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@4023 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- tools/src/perfdmf/src/loader/Configure.java
+++ tools/src/perfdmf/src/loader/Configure.java
@@ -24,7 +24,7 @@ public class Configure {
     private String arch = "";
     private String jdbc_db_jarfile = "derby.jar";
     private String jdbc_db_driver = "org.apache.derby.jdbc.EmbeddedDriver";
-    private String jdbc_db_type = "postgresql";
+    private String jdbc_db_type = "derby";
     private String db_hostname = "";
     private String db_portnum = "";
     private String db_dbname = "perfdmf";
@@ -148,13 +148,12 @@ public class Configure {
 
             while (!valid) {
                 // Prompt for database type
-                //System.out.print("Please enter the database vendor (oracle, postgresql, mysql or derby).\n(" 
-                System.out.print("Please enter the database vendor (oracle, postgresql or mysql).\n(" + jdbc_db_type + "):");
+                System.out.print("Please enter the database vendor (oracle, postgresql, mysql or derby).\n(" + jdbc_db_type + "):");
+                //System.out.print("Please enter the database vendor (oracle, postgresql or mysql).\n(" + jdbc_db_type + "):");
                 tmpString = reader.readLine();
                 if (tmpString.compareTo("oracle") == 0 || tmpString.compareTo("postgresql") == 0
-                //|| tmpString.compareTo("mysql") == 0 || tmpString.compareTo("derby") == 0 
-                        || tmpString.compareTo("mysql") == 0 || tmpString.length() == 0) {
-
+                || tmpString.compareTo("mysql") == 0 || tmpString.compareTo("derby") == 0 
+                || tmpString.length() == 0) {
                     if (tmpString.length() > 0)
                         jdbc_db_type = tmpString;
                     valid = true;
