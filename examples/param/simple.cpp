6ec89400b7c7561fcf61842e3e2a40973e10ae7f
sameer <sameer@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2006-04-14 04:56:51 +0000

    Added an example to show how to use -PROFILEPARAM. Alan, would you try this?
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@4052 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ examples/param/simple.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <TAU.h>
+
+int f2(int y)
+{
+  TAU_PROFILE("f2()", "", TAU_USER);
+  static string s("y");
+  TAU_PROFILE_PARAM1L((long) y, s);
+  printf("Inside f2: sleeping for %d seconds\n", y/2);
+  sleep(y/2);
+}
+
+int f1(int x)
+{
+  TAU_PROFILE("f1()", "", TAU_USER);
+  static string s("x");
+  TAU_PROFILE_PARAM1L((long) x, s);
+  printf("Inside f1: sleeping for %d seconds, calling f2\n", x);
+  sleep(x);
+  f2(x);
+  return 0;
+}
+
+int main(int argc, char **argv)
+{
+  TAU_PROFILE("main()", "(calls f1, f5)", TAU_DEFAULT);
+  TAU_PROFILE_SET_NODE(0);
+  printf("Inside main: calling f1\n");
+
+  f1(2);
+  f1(4);
+  f1(3);
+  f1(2);
+  f1(2);
+}
