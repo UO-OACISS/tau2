fbb6cdcb0db5e379c3b894b1fb555b1fb57d5cb4
wspear <wspear@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2006-04-27 23:01:33 +0000

    Modified time values for exact output equivalence with the C/C++ example.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@4113 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- examples/javatracewriter/jtau_writer.java
+++ examples/javatracewriter/jtau_writer.java
@@ -24,37 +24,37 @@ public static void main(String[] args) {
 
 		TAU_tf_writer.Ttf_DefUserEvent(file, USER_EVENT_1, "User Event 1", 1);
 
-		//double s = 1e6;
-		int s=1;
+		long s = (long)1e6;
+		//int s=1;
 		TAU_tf_writer.Ttf_EnterState(file, 1*s, 0, 0, MAIN);
 		TAU_tf_writer.Ttf_EnterState(file, 2*s, 0, 0, FOO);
 		TAU_tf_writer.Ttf_EnterState(file, 3*s, 0, 0, BAR);
-		TAU_tf_writer.Ttf_EventTrigger(file, 4*s, 0, 0, USER_EVENT_1, 500);
-		TAU_tf_writer.Ttf_EventTrigger(file, 5*s, 0, 0, USER_EVENT_1, 1000);
+		TAU_tf_writer.Ttf_EventTrigger(file, (long)(3.1*s), 0, 0, USER_EVENT_1, 500);
+		TAU_tf_writer.Ttf_EventTrigger(file, (long)(3.9*s), 0, 0, USER_EVENT_1, 1000);
 
 		TAU_tf_writer.Ttf_EnterState(file, 4*s, 1, 0, MAIN);
-		TAU_tf_writer.Ttf_SendMessage(file, 4*s,
+		TAU_tf_writer.Ttf_SendMessage(file, (long)(4.5*s),
 				1, 0, // from 1,0
 				0, 0, // to 0,0
 				500,  // length
 				42,   // tag
 				0);   // communicator
-		TAU_tf_writer.Ttf_EnterState(file, 6*s, 1, 0, FOO);
-		TAU_tf_writer.Ttf_EnterState(file, 7*s, 1, 0, BAR);
-		TAU_tf_writer.Ttf_LeaveState(file, 8*s, 1, 0, BAR);
-		TAU_tf_writer.Ttf_LeaveState(file, 9*s, 1, 0, FOO);
-		TAU_tf_writer.Ttf_LeaveState(file, 10*s, 1, 0, MAIN);
+		TAU_tf_writer.Ttf_EnterState(file, 5*s, 1, 0, FOO);
+		TAU_tf_writer.Ttf_EnterState(file, 6*s, 1, 0, BAR);
+		TAU_tf_writer.Ttf_LeaveState(file, 7*s, 1, 0, BAR);
+		TAU_tf_writer.Ttf_LeaveState(file, 8*s, 1, 0, FOO);
+		TAU_tf_writer.Ttf_LeaveState(file, 9*s, 1, 0, MAIN);
 
 
-		TAU_tf_writer.Ttf_LeaveState(file, 11*s, 0, 0, BAR);
-		TAU_tf_writer.Ttf_RecvMessage(file, 12*s,
+		TAU_tf_writer.Ttf_LeaveState(file, 10*s, 0, 0, BAR);
+		TAU_tf_writer.Ttf_RecvMessage(file, (long)(10.5*s),
 				1, 0, // from 1,0
 				0, 0, // to 0,0
 				500,  // length
 				42,   // tag
 				0);   // communicator
-		TAU_tf_writer.Ttf_LeaveState(file, 13*s, 0, 0, FOO);
-		TAU_tf_writer.Ttf_LeaveState(file, 14*s, 0, 0, MAIN);
+		TAU_tf_writer.Ttf_LeaveState(file, 11*s, 0, 0, FOO);
+		TAU_tf_writer.Ttf_LeaveState(file, 12*s, 0, 0, MAIN);
 
 		TAU_tf_writer.Ttf_CloseOutputFile(file);
 		return ;
