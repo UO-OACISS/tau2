95daa623253676309b436ec7c78ebd2426130f78
sameer <sameer@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2006-06-17 04:25:29 +0000

    Added prototype for memory leak detection routine that is invoked in Profiler.cpp.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@4215 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ include/Profile/TauMemory.h
@@ -0,0 +1,33 @@
+/****************************************************************************
+**			TAU Portable Profiling Package			   **
+**			http://www.cs.uoregon.edu/research/tau	           **
+*****************************************************************************
+**    Copyright 2006  						   	   **
+**    Department of Computer and Information Science, University of Oregon **
+**    Advanced Computing Laboratory, Los Alamos National Laboratory        **
+****************************************************************************/
+/***************************************************************************
+**	File 		: TauMemory.h					  **
+**	Description 	: TAU Profiling Package				  **
+**	Author		: Sameer Shende					  **
+**	Contact		: sameer@cs.uoregon.edu sameer@paratools.com      **
+**	Flags		: Compile with				          **
+**	Documentation	: See http://www.cs.uoregon.edu/research/tau      **
+***************************************************************************/
+
+//////////////////////////////////////////////////////////////////////
+// Include Files 
+//////////////////////////////////////////////////////////////////////
+
+#ifndef _TAU_MEMORY_H_
+#define _TAU_MEMORY_H_
+
+int TauDetectMemoryLeaks(void);
+
+#endif /* _TAU_MEMORY_H_ */
+
+/***************************************************************************
+ * $RCSfile: TauMemory.h,v $   $Author: sameer $
+ * $Revision: 1.1 $   $Date: 2006/06/17 04:25:29 $
+ * TAU_VERSION_ID: $Id: TauMemory.h,v 1.1 2006/06/17 04:25:29 sameer Exp $ 
+ ***************************************************************************/
