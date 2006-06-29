0772c40f1436081835c86ba349caf7cd093a41ba
amorris <amorris@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2006-06-29 20:52:08 +0000

    exit and cycle statements are only in gfortran pdb files, commenting out for now.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@4261 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- utils/tau_instrument.cpp
+++ utils/tau_instrument.cpp
@@ -783,8 +783,8 @@ int processBlock(const pdbStmt *s, const pdbRoutine *ro, vector<itemRef *>& item
         break;
       case pdbStmt::ST_GOTO:
 #ifndef PDT_NOFSTMTS
-      case pdbStmt::ST_FCYCLE:
-      case pdbStmt::ST_FEXIT:
+//       case pdbStmt::ST_FCYCLE:
+//       case pdbStmt::ST_FEXIT:
       case pdbStmt::ST_FGOTO:
 #endif /* PDT_NOFSTMTS */
 	if (s->extraStmt())
@@ -1118,6 +1118,6 @@ bool addFileInstrumentationRequests(PDB& p, pdbFile *file, vector<itemRef *>& it
 
 /***************************************************************************
  * $RCSfile: tau_instrument.cpp,v $   $Author: amorris $
- * $Revision: 1.24 $   $Date: 2006/06/29 20:01:57 $
- * VERSION_ID: $Id: tau_instrument.cpp,v 1.24 2006/06/29 20:01:57 amorris Exp $
+ * $Revision: 1.25 $   $Date: 2006/06/29 20:52:08 $
+ * VERSION_ID: $Id: tau_instrument.cpp,v 1.25 2006/06/29 20:52:08 amorris Exp $
  ***************************************************************************/
