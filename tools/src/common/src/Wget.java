5dc2e5c6eda0c5a32c74203f07d7ce0224ac9490
khuck <khuck@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2006-06-30 18:27:21 +0000

    Added a status to the Wget class.  That way, when downloading weka, the user knows something good is happening.  Every 100k bytes, the status is updated
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@4268 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- tools/src/common/src/Wget.java
+++ tools/src/common/src/Wget.java
@@ -6,29 +6,41 @@ import java.net.URLConnection;
 
 public class Wget {
 
-    public static void wget(String URL, String file) throws IOException {
-
+    public static void wget(String URL, String file, boolean status) throws IOException {
         URLConnection url = new URL(URL).openConnection();
         DataInputStream in = new DataInputStream(url.getInputStream());
         OutputStream out = new FileOutputStream(file);
-
+		int i = 0;
         try {
             while (true) {
                 out.write(in.readUnsignedByte());
+				i++;
+				if (status && i % 100000 == 0)
+					System.out.print("\r" + i / 1000 + "k bytes...");
             }
         } catch (EOFException e) {
             out.close();
+			if (status)
+				System.out.println("\r" + i / 1000 + "k bytes... done.");
             return;
         }
     }
 
 	public static void main(String[] args) {
-		if (args.length != 2)
+		if (args.length == 2) {
+        	try {
+				Wget.wget(args[0], args[1], false);
+			} catch (IOException e) {
+				System.out.println("Failed getting " + args[0]);
+			}
+		} else if (args.length == 3) {
+        	try {
+				Wget.wget(args[0], args[1], args[2].equalsIgnoreCase("true"));
+			} catch (IOException e) {
+				System.out.println("Failed getting " + args[0]);
+			}
+		} else {
 			System.out.println("Usage: Wget <url> <local filename>");
-        try {
-			Wget.wget(args[0], args[1]);
-		} catch (IOException e) {
-			System.out.println("Failed getting " + args[0]);
 		}
 	}
 }
