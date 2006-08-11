1cac7ad95341c9736b20b42863117d8b2800529e
sameer <sameer@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2006-08-11 19:16:43 +0000

    To determine if a routine is pure, we use:   if (((pdbRoutine *)(*it)->item)->fprefix() == pdbItem::FP_PURE but sometimes itemRef item is null when it is an INSTRUMENTATION_POINT record. I added a check for this case and it works properly now. Alan, would you check this with a pure function to make sure pure instrumentation takes place correctly? This solves the error: tau_instrumentor is crashing when using a selective instrumentation file:
    % cat simple.f90
           PROGRAM SUM_OF_CUBES
           INTEGER :: H, T, U
    
           ! This program prints all 3-digit numbers that
           ! equal the sum of the cubes of their digits.
           DO H = 1, 9
             DO T = 0, 9
               DO U = 0, 9
               IF (100*H + 10*T + U == H**3 + T**3 + U**3) THEN
                  PRINT "(3I1)", H, T, U
               ENDIF
               END DO
             END DO
           END DO
           END PROGRAM SUM_OF_CUBES
    
    % cat select.tau
    BEGIN_INSTRUMENT_SECTION
    entry routine ="SUM_OF_CUBES" code = " call TAU_PROFILE_SET_NODE(0)"
    END_INSTRUMENT_SECTION
    
    % tau_instrumentor simple.pdb simple.f90 -o simple.inst.f90 -f select.tau
    Segmentation fault (core dumped)
    
    If I don't use the selective instrumentation file, it works.
    
    
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@4332 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- utils/tau_instrumentor.cpp
+++ utils/tau_instrumentor.cpp
@@ -1775,9 +1775,12 @@ bool instrumentFFile(PDB& pdb, pdbFile* f, string& outfile, string& group_name)
 
 	  int pure = 0;
 #ifdef PDT_PURE
-	  if (((pdbRoutine *)(*it)->item)->fprefix() == pdbItem::FP_PURE ||
-	      ((pdbRoutine *)(*it)->item)->fprefix() == pdbItem::FP_ELEM) {
-	    pure = 1;
+	  /* When INSTRUMENTATION_POINT is used, sometimes the item is null */
+	  if ( (*it)->item != (pdbItem *) NULL) { 
+             if (((pdbRoutine *)(*it)->item)->fprefix() == pdbItem::FP_PURE ||
+	        ((pdbRoutine *)(*it)->item)->fprefix() == pdbItem::FP_ELEM) {
+	       pure = 1;
+             }
 	  }
 #endif
 
@@ -2476,8 +2479,8 @@ int main(int argc, char **argv)
   
 /***************************************************************************
  * $RCSfile: tau_instrumentor.cpp,v $   $Author: sameer $
- * $Revision: 1.104 $   $Date: 2006/07/06 01:28:19 $
- * VERSION_ID: $Id: tau_instrumentor.cpp,v 1.104 2006/07/06 01:28:19 sameer Exp $
+ * $Revision: 1.105 $   $Date: 2006/08/11 19:16:43 $
+ * VERSION_ID: $Id: tau_instrumentor.cpp,v 1.105 2006/08/11 19:16:43 sameer Exp $
  ***************************************************************************/
 
 
