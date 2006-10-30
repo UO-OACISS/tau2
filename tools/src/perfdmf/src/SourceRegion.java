0d9055d44a63f46f90898ee0a900993b08771cfe
amorris <amorris@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2006-10-30 18:14:59 +0000

    Added SourceRegion class.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@4422 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ tools/src/perfdmf/src/SourceRegion.java
@@ -0,0 +1,51 @@
+package edu.uoregon.tau.perfdmf;
+
+public class SourceRegion {
+
+    private String filename;
+    private int startLine, endLine, startColumn, endColumn;
+
+    public SourceRegion() {
+    }
+    
+    public int getEndColumn() {
+        return endColumn;
+    }
+
+    public void setEndColumn(int endColumn) {
+        this.endColumn = endColumn;
+    }
+
+    public int getEndLine() {
+        return endLine;
+    }
+
+    public void setEndLine(int endLine) {
+        this.endLine = endLine;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public int getStartColumn() {
+        return startColumn;
+    }
+
+    public void setStartColumn(int startColumn) {
+        this.startColumn = startColumn;
+    }
+
+    public int getStartLine() {
+        return startLine;
+    }
+
+    public void setStartLine(int startLine) {
+        this.startLine = startLine;
+    }
+
+}
