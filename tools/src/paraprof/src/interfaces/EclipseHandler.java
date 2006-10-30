6150dfaa9db4ede0b35c2f6fdc40f7b38f9e1021
amorris <amorris@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2006-10-30 18:19:32 +0000

    Added EclipseHandler interface
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@4424 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ tools/src/paraprof/src/interfaces/EclipseHandler.java
@@ -0,0 +1,11 @@
+package edu.uoregon.tau.paraprof.interfaces;
+
+import edu.uoregon.tau.paraprof.ParaProfTrial;
+import edu.uoregon.tau.perfdmf.Function;
+
+public interface EclipseHandler {
+
+    
+    public boolean openSourceLocation(ParaProfTrial ppTrial, Function function);
+    
+}
