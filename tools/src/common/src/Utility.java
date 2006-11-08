c10d2cd1cd36b69b1bd7c5a2b796c2688412ba73
amorris <amorris@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2006-11-08 20:27:22 +0000

    Added medium sized logo, and accessor method.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@4461 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- tools/src/common/src/Utility.java
+++ tools/src/common/src/Utility.java
@@ -2,8 +2,19 @@ package edu.uoregon.tau.common;
 
 import java.net.URL;
 
+import javax.swing.ImageIcon;
+
 public class Utility {
 
+    public static ImageIcon getImageIconResource(String name) {
+        java.net.URL imgURL = getResource(name);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL);
+        } else {
+            return null;
+        }
+    }
+    
     public static URL getResource(String name) {
         URL url = null;
         url = Utility.class.getResource(name);
