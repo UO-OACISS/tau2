156854c38484c6799f58364447e55ecbda8ec877
wspear <wspear@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2006-11-11 00:19:32 +0000

    A -very- simple script to copy the plugins and required jar files to the specified Eclipse directory.  Copying the jar files instead of keeping duplicates in the plugin folder saves about 5 MB.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@4509 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ tools/src/eclipse/install_plugins.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+if [ $# != 1 ]
+then
+  echo "Please enter the location of your eclipse installation."
+  echo "Note that you must have the CDT and PTP plugins installed"
+  echo "for the TAU plugins to function properly."
+  exit 1
+fi
+
+cp ./plugins/*.jar $1/plugins/
+cp -r ./plugins/org.eclipse.ptp.tau.perfdmf_1.0.0/ $1/plugins/
+
+cp ../contrib/batik-combined.jar $1/plugins/org.eclipse.ptp.tau.perfdmf_1.0.0/
+cp ../contrib/jargs.jar $1/plugins/org.eclipse.ptp.tau.perfdmf_1.0.0/
+cp ../contrib/jcommon-0.9.6.jar $1/plugins/org.eclipse.ptp.tau.perfdmf_1.0.0/
+cp ../contrib/jfreechart-0.9.21.jar $1/plugins/org.eclipse.ptp.tau.perfdmf_1.0.0/
+cp ../contrib/jgraph.jar $1/plugins/org.eclipse.ptp.tau.perfdmf_1.0.0/
+cp ../contrib/jogl.jar $1/plugins/org.eclipse.ptp.tau.perfdmf_1.0.0/
+cp ../contrib/jython.jar $1/plugins/org.eclipse.ptp.tau.perfdmf_1.0.0/
+
+cp ../paraprof/bin/paraprof.jar $1/plugins/org.eclipse.ptp.tau.perfdmf_1.0.0/
+cp ../perfdmf/bin/perfdmf.jar $1/plugins/org.eclipse.ptp.tau.perfdmf_1.0.0/
+cp ../common/bin/tau-common.jar $1/plugins/org.eclipse.ptp.tau.perfdmf_1.0.0/
+cp ../vis/bin/vis.jar $1/plugins/org.eclipse.ptp.tau.perfdmf_1.0.0/
