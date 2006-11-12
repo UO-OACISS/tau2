1b79eebfb0a0b65bb6dad4cc3634ecabf29b6b35
amorris <amorris@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2006-11-12 00:12:48 +0000

    Added usage information and some sanity checking on arguments.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@4519 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- tools/src/eclipse/install_plugins.sh
+++ tools/src/eclipse/install_plugins.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
-if [ $# != 1 ]
-then
+if [ $# != 1 ] ; then
+  echo "Usage: $0 <path/to/eclipse/root>"
+  echo ""
   echo "Please enter the location of your eclipse installation."
   echo "Note that you must have the CDT and PTP plugins installed"
   echo "for the TAU plugins to function properly."
   exit 1
 fi
 
+if [ ! -d "$1" ] ; then
+    echo "Invalid eclipse directory"
+    exit 1
+fi
+
+echo "Installing to $1/plugins"
+echo "..."
 cp ./plugins/*.jar $1/plugins/
 cp -r ./plugins/org.eclipse.ptp.tau.perfdmf_1.0.0/ $1/plugins/
 
@@ -22,3 +30,5 @@ cp ../paraprof/bin/paraprof.jar $1/plugins/org.eclipse.ptp.tau.perfdmf_1.0.0/
 cp ../perfdmf/bin/perfdmf.jar $1/plugins/org.eclipse.ptp.tau.perfdmf_1.0.0/
 cp ../common/bin/tau-common.jar $1/plugins/org.eclipse.ptp.tau.perfdmf_1.0.0/
 cp ../vis/bin/vis.jar $1/plugins/org.eclipse.ptp.tau.perfdmf_1.0.0/
+
+echo "Eclipse plugins installed!"
