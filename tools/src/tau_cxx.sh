152826f427539eda7d892b724150c05b3fdaf572
amorris <amorris@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2007-01-25 02:08:47 +0000

    These scripts can now handle -MD, -MMD and some other options.  It now has the capability to invoke multiple compile stages.  For example, if -MD is given, it will invoke the original compiler with the -MD option (which will generate the .d file and the .o file) and then it will invoke TAU_COMPILER.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@4638 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- tools/src/tau_cxx.sh
+++ tools/src/tau_cxx.sh
@@ -3,10 +3,12 @@
 # Define variables 
 makefile_specified=no
 options_specified=no
-makedepend_specified=no
 
-if [ $# = 0 ]
-then
+# depending on the options, we might invoke the regular compiler, TAU_COMPILER, or both
+invoke_without_tau=no
+invoke_with_tau=yes
+
+if [ $# = 0 ] ; then
   echo "Usage $0 [-tau_makefile=<tau_stub_makefile>] [-tau_options=<tau_compiler_opts>] <opts> <file>"
   echo "If -tau_makefile option is not used, "
   echo "TAU uses the file specified in the TAU_MAKEFILE environment variable"
@@ -19,73 +21,84 @@ then
   exit 1
 fi
 
-THEARGS=
+TAUARGS=
+NON_TAUARGS=
 
-for arg in "$@"
-do
+for arg in "$@" ; do
+  # Thanks to Bernd Mohr for the following that handles quotes and spaces (see configure for explanation)
+  modarg=`echo "x$arg" | sed -e 's/^x//' -e 's/"/\\\"/g' -e s,\',%@%\',g -e 's/%@%/\\\/g' -e 's/ /\\\ /g'`
   case $arg in 
     -tau_makefile=*)
       MAKEFILE=`echo $arg | sed -e 's/-tau_makefile=//'`
       makefile_specified=yes
-      shift
       ;;
     -tau_options=*)
       TAUCOMPILER_OPTIONS=`echo $arg | sed -e 's/-tau_options=//'`
       options_specified=yes
-      shift
       ;;
-    -MM | -M | -MF | -MD | -V | -v | --version | -print-prog-name=ld | -print-search-dirs )
-      makedepend_specified=yes	
-# hack to get proper .d generation support for eclipse
+    -E)
+      invoke_without_tau=yes
+      invoke_with_tau=no
+      NON_TAUARGS="$NON_TAUARGS $modarg"
+      ;;
+    -MD | -MMD)
+      # if either of these are specified, we invoke the regular compiler
+      # and TAU_COMPILER, unless -E or another disabling option is specified
+      invoke_without_tau=yes
+      NON_TAUARGS="$NON_TAUARGS $modarg"
+      ;;
+    -M | -MM | -V | -v | --version | -print-prog-name=ld | -print-search-dirs)
+      # if any of these are specified, we invoke the regular compiler only
+      invoke_without_tau=yes
+      invoke_with_tau=no
+      NON_TAUARGS="$NON_TAUARGS $modarg"
       ;;
     *)
-      # Thanks to Bernd Mohr for the following that handles quotes and spaces (see configure for explanation)
-      modarg=`echo "x$arg" | sed -e 's/^x//' -e 's/"/\\\"/g' -e s,\',%@%\',g -e 's/%@%/\\\/g' -e 's/ /\\\ /g'`
-      THEARGS="$THEARGS $modarg"
+      TAUARGS="$TAUARGS $modarg"
+      NON_TAUARGS="$NON_TAUARGS $modarg"
       ;;
   esac
 done
-if [ $makefile_specified = no ]
-then
-     MAKEFILE=$TAU_MAKEFILE
-     if [ "x$MAKEFILE" != "x" ]
-     then
-	if [ ! -r $MAKEFILE ] 
-        then
-	  echo "ERROR: environment variable TAU_MAKEFILE is set but the file is not readable"
-	  exit 1
+
+if [ $makefile_specified = no ] ; then
+    MAKEFILE=$TAU_MAKEFILE
+    if [ "x$MAKEFILE" != "x" ] ; then
+	if [ ! -r $MAKEFILE ] ; then
+	    echo "ERROR: environment variable TAU_MAKEFILE is set but the file is not readable"
+	    exit 1
         fi
-     else
+    else
 	echo $0: "ERROR: please set the environment variable TAU_MAKEFILE"
 	exit 1
-     fi
+    fi
 fi
 
-if [ $options_specified = no ]
-then
-     TAUCOMPILER_OPTIONS=$TAU_OPTIONS
-     if [ "x$TAUCOMPILER_OPTIONS" = "x" ]
-     then
-       TAUCOMPILER_OPTIONS=-optVerbose 
-     fi
+if [ $options_specified = no ] ; then
+    TAUCOMPILER_OPTIONS=$TAU_OPTIONS
+    if [ "x$TAUCOMPILER_OPTIONS" = "x" ] ; then
+	TAUCOMPILER_OPTIONS=-optVerbose 
+    fi
 fi
 
-if [ $makedepend_specified = yes ]
-then
+if [ $invoke_without_tau = yes ] ; then
 cat <<EOF > /tmp/makefile.tau$$
   include $MAKEFILE
   all:
-	@\$(TAU_CXX) $THEARGS
+	@\$(TAU_CXX) $NON_TAUARGS
 EOF
-else
+make -s -f /tmp/makefile.tau$$
+/bin/rm -f /tmp/makefile.tau$$
+fi
+
+
+if [ $invoke_with_tau = yes ] ; then
 cat <<EOF > /tmp/makefile.tau$$
 include $MAKEFILE
 all:
-	@\$(TAU_COMPILER) $TAUCOMPILER_OPTIONS \$(TAU_CXX) $THEARGS
+	@\$(TAU_COMPILER) $TAUCOMPILER_OPTIONS \$(TAU_CXX) $TAUARGS
 
 EOF
-fi
-
 make -s -f /tmp/makefile.tau$$
 /bin/rm -f /tmp/makefile.tau$$
+fi
 
--- tools/src/tau_f90.sh
+++ tools/src/tau_f90.sh
@@ -3,79 +3,99 @@
 # Define variables 
 makefile_specified=no
 options_specified=no
-makedepend_specified=no
 
-if [ $# = 0 ]
-then
+# depending on the options, we might invoke the regular compiler, TAU_COMPILER, or both
+invoke_without_tau=no
+invoke_with_tau=yes
+
+if [ $# = 0 ] ; then
   echo "Usage $0 [-tau_makefile=<tau_stub_makefile>] [-tau_options=<tau_compiler_opts>] <opts> <file>"
   echo "If -tau_makefile option is not used, "
   echo "TAU uses the file specified in the TAU_MAKEFILE environment variable"
   echo "e.g., "
-  echo "% tau_cxx.sh -tau_makefile=/usr/local/tau-2.x/ia64/lib/Makefile.tau-mpi-pdt  -tau_options=-optVerbose -c foo.f90"
+  echo "% tau_f90.sh -tau_makefile=/usr/local/tau-2.x/ia64/lib/Makefile.tau-mpi-pdt  -tau_options=-optVerbose -c foo.f90"
   echo " 	or"
   echo "% setenv TAU_MAKEFILE /usr/local/tau-2.x/include/Makefile"
   echo "% setenv TAU_OPTIONS -optVerbose -optTauSelectFile=select.tau"
-  echo "% tau_cxx.sh -c foo.f90"
+  echo "% tau_f90.sh -c foo.f90"
   exit 1
 fi
 
-for arg in "$@"
-do
+TAUARGS=
+NON_TAUARGS=
+
+for arg in "$@" ; do
+  # Thanks to Bernd Mohr for the following that handles quotes and spaces (see configure for explanation)
+  modarg=`echo "x$arg" | sed -e 's/^x//' -e 's/"/\\\"/g' -e s,\',%@%\',g -e 's/%@%/\\\/g' -e 's/ /\\\ /g'`
   case $arg in 
     -tau_makefile=*)
       MAKEFILE=`echo $arg | sed -e 's/-tau_makefile=//'`
       makefile_specified=yes
-      shift
       ;;
     -tau_options=*)
       TAUCOMPILER_OPTIONS=`echo $arg | sed -e 's/-tau_options=//'`
       options_specified=yes
-      shift
       ;;
-    -MM | -M)
-      makedepend_specified=yes	
-# hack to get proper .d generation support for eclipse
+    -E)
+      invoke_without_tau=yes
+      invoke_with_tau=no
+      NON_TAUARGS="$NON_TAUARGS $modarg"
+      ;;
+    -MD | -MMD)
+      # if either of these are specified, we invoke the regular compiler
+      # and TAU_COMPILER, unless -E or another disabling option is specified
+      invoke_without_tau=yes
+      NON_TAUARGS="$NON_TAUARGS $modarg"
+      ;;
+    -M | -MM | -V | -v | --version | -print-prog-name=ld | -print-search-dirs)
+      # if any of these are specified, we invoke the regular compiler only
+      invoke_without_tau=yes
+      invoke_with_tau=no
+      NON_TAUARGS="$NON_TAUARGS $modarg"
       ;;
     *)
-      # Thanks to Bernd Mohr for the following that handles quotes and spaces (see configure for explanation)
-      modarg=`echo "x$arg" | sed -e 's/^x//' -e 's/"/\\\"/g' -e s,\',%@%\',g -e 's/%@%/\\\/g' -e 's/ /\\\ /g'`
-      THEARGS="$THEARGS $modarg"
+      TAUARGS="$TAUARGS $modarg"
+      NON_TAUARGS="$NON_TAUARGS $modarg"
       ;;
   esac
 done
-if [ $makefile_specified = no ]
-then
-     MAKEFILE=$TAU_MAKEFILE
-     if [ "x$MAKEFILE" != "x" ]
-     then
-	if [ ! -r $MAKEFILE ] 
-        then
-	  echo "ERROR: environment variable TAU_MAKEFILE is set but the file is not readable"
-	  exit 1
+
+if [ $makefile_specified = no ] ; then
+    MAKEFILE=$TAU_MAKEFILE
+    if [ "x$MAKEFILE" != "x" ] ; then
+	if [ ! -r $MAKEFILE ] ; then
+	    echo "ERROR: environment variable TAU_MAKEFILE is set but the file is not readable"
+	    exit 1
         fi
-     else
+    else
 	echo $0: "ERROR: please set the environment variable TAU_MAKEFILE"
 	exit 1
-     fi
+    fi
 fi
 
-if [ $options_specified = no ]
-then
-     TAUCOMPILER_OPTIONS=$TAU_OPTIONS
-     if [ "x$TAUCOMPILER_OPTIONS" = "x" ]
-     then
-       TAUCOMPILER_OPTIONS=-optVerbose 
-     fi
+if [ $options_specified = no ] ; then
+    TAUCOMPILER_OPTIONS=$TAU_OPTIONS
+    if [ "x$TAUCOMPILER_OPTIONS" = "x" ] ; then
+	TAUCOMPILER_OPTIONS=-optVerbose 
+    fi
 fi
 
-if [ $makedepend_specified = yes ]
-then
+if [ $invoke_without_tau = yes ] ; then
 cat <<EOF > /tmp/makefile.tau$$
   include $MAKEFILE
   all:
-	@\$(TAU_F90) $THEARGS
-EOF
+ifeq (\$(TAU_F90),)
+	echo "Error, no fortran compiler specified in TAU configure (use -fortran=<>)"
 else
+	@\$(TAU_F90) $NON_TAUARGS
+endif
+EOF
+make -s -f /tmp/makefile.tau$$
+/bin/rm -f /tmp/makefile.tau$$
+fi
+
+
+if [ $invoke_with_tau = yes ] ; then
 cat <<EOF > /tmp/makefile.tau$$
 include $MAKEFILE
 all:
@@ -86,8 +106,14 @@ else
 endif
 
 EOF
-fi
-
 make -s -f /tmp/makefile.tau$$
 /bin/rm -f /tmp/makefile.tau$$
+fi
+
+
+
+
+
+
+
 
