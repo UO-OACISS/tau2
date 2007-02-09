e0d9d7d21e6b6a3d4e586a148178be3372ffa386
sameer <sameer@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2007-02-09 21:00:49 +0000

    Darn! They didn't install vt_trc.h. So, we need to add prototypes here for TAU to use VampirTrace.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@4706 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ include/Profile/TauVampirTrace.h
@@ -0,0 +1,40 @@
+#ifndef __TAU_VAMPIRTRACE_H__
+#define __TAU_VAMPIRTRACE_H__
+
+#ifndef VT_NO_ID
+#define VT_NO_ID                  0xFFFFFFFF
+#endif /* VT_NO_ID */
+
+#ifndef VT_NO_LNO
+#define VT_NO_LNO                 0xFFFFFFFF
+#endif /* VT_NO_LNO */
+
+#ifndef VT_FUNCTION
+#define VT_FUNCTION               1
+#endif /* VT_FUNCTION */
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+void vt_open(void);
+void vt_close(void);
+
+uint32_t vt_def_region      ( const char* rname,
+                                     uint32_t fid,
+                                     uint32_t begln,
+                                     uint32_t endln,
+                                     const char* rdesc,
+                                     uint8_t rtype );
+
+void vt_enter(uint64_t* time, uint32_t rid);
+
+void vt_exit(uint64_t* time);
+uint64_t vt_pform_wtime(void);
+
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* __TAU_VAMPIRTRACE_H__ */
