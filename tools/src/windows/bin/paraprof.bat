904d05cf064579512c1193afc3d0f171c9c55230
amorris <amorris@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2007-02-13 19:09:58 +0000

    Added xerces.jar to the classpath.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@4729 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- tools/src/windows/bin/paraprof.bat
+++ tools/src/windows/bin/paraprof.bat
@@ -11,7 +11,7 @@ set PERFDMF_JAR=%JAR_ROOT%/perfdmf.jar
 set JARGS_JAR=%JAR_ROOT%/jargs.jar
 set JDBC_JAR=%JAR_ROOT%/postgresql.jar;%JAR_ROOT%/mysql.jar;%JAR_ROOT%/oracle.jar;%JAR_ROOT%/derby.jar
 set COMMON_JAR=%JAR_ROOT%/tau-common.jar
-set JARS=%JAR_ROOT%/paraprof.jar;%JAR_ROOT%/vis.jar;%PERFDMF_JAR%;%JAR_ROOT%/jogl.jar;%JAR_ROOT%/jgraph.jar;%JDBC_JAR%;%JAR_ROOT%/jargs.jar;%JAR_ROOT%/epsgraphics.jar;%JAR_ROOT%/batik-combined.jar;%JAR_ROOT%/tau-common.jar;%JAR_ROOT%/jfreechart-0.9.21.jar;%JAR_ROOT%/jcommon-0.9.6.jar
+set JARS=%JAR_ROOT%/paraprof.jar;%JAR_ROOT%/vis.jar;%PERFDMF_JAR%;%JAR_ROOT%/jogl.jar;%JAR_ROOT%/jgraph.jar;%JDBC_JAR%;%JAR_ROOT%/jargs.jar;%JAR_ROOT%/epsgraphics.jar;%JAR_ROOT%/batik-combined.jar;%JAR_ROOT%/tau-common.jar;%JAR_ROOT%/jfreechart-0.9.21.jar;%JAR_ROOT%/jcommon-0.9.6.jar;%JAR_ROOT%/xerces.jar
 
 java -Xmx500m -Djava.library.path=%JAR_ROOT% -cp %JARS% edu/uoregon/tau/paraprof/ParaProf %1 %2 %3 %4 %5
 
