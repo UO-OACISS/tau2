c0a0d13e37f8d8a5d9ba3f886ae1427777220bc3
khuck <khuck@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2007-04-14 00:59:04 +0000

    Added support for compressing strings and streams on the fly to the Gzip class.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@5001 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- tools/src/common/src/Gzip.java
+++ tools/src/common/src/Gzip.java
@@ -2,6 +2,7 @@ package edu.uoregon.tau.common;
 
 import java.io.*;
 import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 public class Gzip {
 
@@ -15,6 +16,70 @@ public class Gzip {
         gis.close();
         bos.close();
     }
+
+	public static final byte[] compress(String str) {
+		byte[] compressed = null;
+		try {
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			GZIPOutputStream zout = new GZIPOutputStream(out);
+			//zout.putNextEntry(new ZipEntry("0"));
+			zout.write(str.getBytes());
+			//zout.closeEntry();
+			zout.finish();
+			compressed = out.toByteArray();
+			zout.close();
+		} catch (IOException e) {
+			System.err.println("Couldn't compress string!");
+		}
+		return compressed;
+	}
+
+	public static final String decompress(byte[] compressed) {
+		String decompressed = null;
+		try {
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			ByteArrayInputStream in = new ByteArrayInputStream(compressed);
+			GZIPInputStream zin = new GZIPInputStream(in);
+			byte[] buffer = new byte[1024];
+			int offset = -1;
+			while((offset = zin.read(buffer)) != -1) {
+				out.write(buffer, 0, offset);
+			}
+			decompressed = out.toString();
+			out.close();
+			zin.close();
+		} catch (NullPointerException e) {
+			// nothing to decompress
+		} catch (IOException e) {
+			System.err.println("Couldn't decompress string!");
+			System.err.println(e.getMessage());
+			e.printStackTrace();
+		}
+		return decompressed;
+	}
+
+	public static final String decompress(InputStream in) {
+		String decompressed = null;
+		try {
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			GZIPInputStream zin = new GZIPInputStream(in);
+			byte[] buffer = new byte[1024];
+			int offset = -1;
+			while((offset = zin.read(buffer)) != -1) {
+				out.write(buffer, 0, offset);
+			}
+			decompressed = out.toString();
+			out.close();
+			zin.close();
+		} catch (NullPointerException e) {
+			// nothing to decompress
+		} catch (IOException e) {
+			System.err.println("Couldn't decompress string!");
+			System.err.println(e.getMessage());
+			e.printStackTrace();
+		}
+		return decompressed;
+	}
 
     
     
