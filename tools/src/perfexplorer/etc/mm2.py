1dc1fd0b0ddb8bab65f2acaaf3f15dcc367b9fa1
khuck <khuck@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2007-05-01 06:28:20 +0000

    Adding analysis script to CVS.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@5036 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ tools/src/perfexplorer/etc/mm2.py
@@ -0,0 +1,38 @@
+from client import ScriptFacade
+from common import TransformationType
+from common import AnalysisType
+from common import EngineType
+
+def TotalExecutionTime(pe):
+	pe.resetChartDefaults();
+	pe.setApplication("mm2")
+	pe.setMetricName("PAPI_L2_TCM")
+	pe.setChartTitle("Matrix Multiply Total Cache Misses per problem size")
+	pe.setChartSeriesName("experiment.name")
+	pe.setChartXAxisName("trial.name", "Matrix dimension")
+	pe.setChartYAxisName("avg(interval_mean_summary.inclusive)", "Total Cache misses (million)")
+	pe.setChartMainEventOnly(1);
+	pe.setChartEventNoCallPath(1)
+	pe.doGeneralChart()
+
+def TotalExecutionTimeByEvent(pe):
+	pe.resetChartDefaults();
+	pe.setApplication("mm2")
+	pe.setMetricName("PAPI_L2_TCM")
+	pe.setChartTitle("Matrix Multiply Total Cache Misses per problem size")
+	#pe.setDimensionReduction(TransformationType.OVER_X_PERCENT, "1")
+	pe.setChartSeriesName("interval_event.name")
+	pe.setChartXAxisName("trial.name", "Matrix dimension")
+	pe.setChartYAxisName("avg(interval_mean_summary.exclusive)", "Total Cache misses (million)")
+	pe.setChartEventNoCallPath(1)
+	pe.doGeneralChart()
+
+print "--------------- JPython test script start ------------"
+
+pe = ScriptFacade()
+TotalExecutionTime(pe)
+TotalExecutionTimeByEvent(pe)
+
+# pe.exit()
+
+print "---------------- JPython test script end -------------"
