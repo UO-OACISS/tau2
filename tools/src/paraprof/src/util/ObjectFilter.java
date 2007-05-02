fdf8d4acae283d6658010eb11bdfff3c30d9d312
amorris <amorris@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2007-05-02 19:44:25 +0000

    Object filter now has a frame that let's you turn items on and off with a list of checkboxes.  We should use this for groups and function filtering as well.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@5049 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- tools/src/paraprof/src/util/ObjectFilter.java
+++ tools/src/paraprof/src/util/ObjectFilter.java
@@ -1,25 +1,37 @@
 package edu.uoregon.tau.paraprof.util;
 
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Observable;
 
-public class ObjectFilter {
+import javax.swing.*;
+
+import edu.uoregon.tau.paraprof.ParaProfUtils;
+
+public class ObjectFilter extends Observable {
     private Object objects[];
-    
+
     private boolean filter[];
 
     private int numShown;
     private int numHidden;
-    
+
+    private JFrame frame;
+
     public ObjectFilter(List objects) {
         this.objects = objects.toArray();
         filter = new boolean[this.objects.length];
         showAll();
     }
-    
-    public List getObjects() {
+
+    public List getFilteredObjects() {
         List list = new ArrayList();
-        for (int i=0; i<objects.length; i++) {
+        for (int i = 0; i < objects.length; i++) {
             if (filter[i]) {
                 list.add(objects[i]);
             }
@@ -29,7 +41,7 @@ public class ObjectFilter {
 
     public void hide(Object object) {
         // maybe use a hash?
-        for (int i=0; i<objects.length; i++) {
+        for (int i = 0; i < objects.length; i++) {
             if (object.equals(objects[i])) {
                 filter[i] = false;
                 numShown--;
@@ -40,7 +52,7 @@ public class ObjectFilter {
 
     public void show(Object object) {
         // maybe use a hash?
-        for (int i=0; i<objects.length; i++) {
+        for (int i = 0; i < objects.length; i++) {
             if (object.equals(objects[i])) {
                 filter[i] = true;
                 numShown++;
@@ -48,14 +60,77 @@ public class ObjectFilter {
             }
         }
     }
-    
+
     public void showAll() {
-        for (int i=0; i<filter.length; i++) {
+        for (int i = 0; i < filter.length; i++) {
             filter[i] = true;
         }
         numShown = filter.length;
         numHidden = 0;
     }
 
+    public void showFrame(String title) {
+
+        if (frame == null) {
+            frame = new JFrame();
+
+            final JCheckBox[] boxes = new JCheckBox[objects.length];
+            for (int i = 0; i < objects.length; i++) {
+                boxes[i] = new JCheckBox(objects[i].toString());
+                boxes[i].setSelected(filter[i]);
+            }
+
+            CheckBoxList checkBoxList = new CheckBoxList(boxes);
+
+            JScrollPane scrollpane = new JScrollPane(checkBoxList);
+
+            frame.setLayout(new GridBagLayout());
+
+            GridBagConstraints gbc = new GridBagConstraints();
+
+            gbc.fill = GridBagConstraints.BOTH;
+            gbc.anchor = GridBagConstraints.NORTH;
+            gbc.weightx = 0.5;
+            gbc.weighty = 0.5;
+
+            JButton applyButton = new JButton("Apply");
+
+            applyButton.addActionListener(new ActionListener() {
+
+                public void actionPerformed(ActionEvent e) {
+                    for (int i = 0; i < objects.length; i++) {
+                        filter[i] = boxes[i].isSelected();
+                    }
+                    setChanged();
+                    notifyObservers();
+                }
+            });
+
+            JButton cancelButton = new JButton("Cancel");
+            cancelButton.addActionListener(new ActionListener() {
+
+                public void actionPerformed(ActionEvent e) {
+                    frame.setVisible(false);
+                    frame.dispose();
+                    frame = null;
+                }
+            });
+            ParaProfUtils.addCompItem(frame, scrollpane, gbc, 0, 0, 2, 1);
+            gbc.fill = GridBagConstraints.NONE;
+            gbc.anchor = GridBagConstraints.SOUTHEAST;
+            gbc.weightx = 0.0;
+            gbc.weighty = 0.0;
+
+            ParaProfUtils.addCompItem(frame, applyButton, gbc, 0, 1, 1, 1);
+            ParaProfUtils.addCompItem(frame, cancelButton, gbc, 1, 1, 1, 1);
+
+            frame.pack();
+            frame.setSize(new Dimension(300, 800));
+            ParaProfUtils.setFrameIcon(frame);
+        }
+
+        frame.setTitle(title);
+        frame.setVisible(true);
+    }
 
 }
