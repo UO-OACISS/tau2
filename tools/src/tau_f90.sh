b3159848edf1a42bbd514da7a774516bfac5af98
amorris <amorris@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2007-05-04 17:08:57 +0000

    Now properly handle -MF -MT -MQ if there is a space after them before their target.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@5066 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- tools/src/tau_f90.sh
+++ tools/src/tau_f90.sh
@@ -23,45 +23,58 @@ fi
 
 TAUARGS=
 NON_TAUARGS=
+EATNEXT=false
 
 for arg in "$@" ; do
   # Thanks to Bernd Mohr for the following that handles quotes and spaces (see configure for explanation)
   modarg=`echo "x$arg" | sed -e 's/^x//' -e 's/"/\\\"/g' -e s,\',%@%\',g -e 's/%@%/\\\/g' -e 's/ /\\\ /g'`
-  case $arg in 
-    -tau_makefile=*)
-      MAKEFILE=`echo $arg | sed -e 's/-tau_makefile=//'`
-      makefile_specified=yes
-      ;;
-    -tau_options=*)
-      TAUCOMPILER_OPTIONS=`echo $arg | sed -e 's/-tau_options=//'`
-      options_specified=yes
-      ;;
-    -E)
-      invoke_without_tau=yes
-      invoke_with_tau=no
-      NON_TAUARGS="$NON_TAUARGS $modarg"
-      ;;
-    -MD | -MMD)
-      # if either of these are specified, we invoke the regular compiler
-      # and TAU_COMPILER, unless -E or another disabling option is specified
-      invoke_without_tau=yes
-      NON_TAUARGS="$NON_TAUARGS $modarg"
-      ;;
-    -MF* | -MT* | -MQ* | -MP | -MG)
+  if [ $EATNEXT = true ] ; then
       # these arguments should only go to the non-tau invocation
       NON_TAUARGS="$NON_TAUARGS $modarg"
-      ;;
-    -M | -MM | -V | -v | --version | -print-prog-name=ld | -print-search-dirs | -dumpversion)
-      # if any of these are specified, we invoke the regular compiler only
-      invoke_without_tau=yes
-      invoke_with_tau=no
-      NON_TAUARGS="$NON_TAUARGS $modarg"
-      ;;
-    *)
-      TAUARGS="$TAUARGS $modarg"
-      NON_TAUARGS="$NON_TAUARGS $modarg"
-      ;;
-  esac
+      EATNEXT=false
+  else
+      case $arg in 
+	  -tau_makefile=*)
+	      MAKEFILE=`echo $arg | sed -e 's/-tau_makefile=//'`
+	      makefile_specified=yes
+	      ;;
+	  -tau_options=*)
+	      TAUCOMPILER_OPTIONS=`echo $arg | sed -e 's/-tau_options=//'`
+	      options_specified=yes
+	      ;;
+	  -E)
+	      invoke_without_tau=yes
+	      invoke_with_tau=no
+	      NON_TAUARGS="$NON_TAUARGS $modarg"
+	      ;;
+	  -MD | -MMD)
+              # if either of these are specified, we invoke the regular compiler
+              # and TAU_COMPILER, unless -E or another disabling option is specified
+	      invoke_without_tau=yes
+	      NON_TAUARGS="$NON_TAUARGS $modarg"
+	      ;;
+	  -MF | -MT | -MQ)
+              # these arguments should only go to the non-tau invocation
+	      NON_TAUARGS="$NON_TAUARGS $modarg"
+	      # we must eat the next argument as well and use it for the non-tau invocation only
+	      EATNEXT=true
+	      ;;
+	  -MF* | -MT* | -MQ* | -MP | -MG)
+              # these arguments should only go to the non-tau invocation
+	      NON_TAUARGS="$NON_TAUARGS $modarg"
+	      ;;
+	  -M | -MM | -V | -v | --version | -print-prog-name=ld | -print-search-dirs | -dumpversion)
+              # if any of these are specified, we invoke the regular compiler only
+	      invoke_without_tau=yes
+	      invoke_with_tau=no
+	      NON_TAUARGS="$NON_TAUARGS $modarg"
+	      ;;
+	  *)
+	      TAUARGS="$TAUARGS $modarg"
+	      NON_TAUARGS="$NON_TAUARGS $modarg"
+	      ;;
+      esac
+  fi
 done
 
 if [ $makefile_specified = no ] ; then
