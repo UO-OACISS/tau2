55aceaa78429bab8948500cf9e6a46f8d7538add
amorris <amorris@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2007-05-11 21:40:57 +0000

    changed to public public constructor.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@5085 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- tools/src/perfdmf/src/DatabaseException.java
+++ tools/src/perfdmf/src/DatabaseException.java
@@ -1,12 +1,26 @@
-package edu.uoregon.tau.perfdmf;
+/*
+ * DatabaseException.java
+ *
+ * Copyright 2005-2007                                
+ * Performance Research Laboratory, University of Oregon
+ */
 
+package edu.uoregon.tau.perfdmf;
 
 import java.sql.SQLException;
 
-
+/**
+ * Encapsulates an SQLException as a RuntimeException with an additional message
+ *
+ * <P>CVS $Id: DatabaseException.java,v 1.4 2007/05/11 21:40:57 amorris Exp $</P>
+ * @author  Alan Morris
+ * @version $Revision: 1.4 $
+ */
 public class DatabaseException extends RuntimeException {
+    private String message;
+    private SQLException exception;
 
-    DatabaseException(String s, SQLException sqlException) {
+    public DatabaseException(String s, SQLException sqlException) {
         this.message = s;
         this.exception = sqlException;
     }
@@ -18,10 +32,4 @@ public class DatabaseException extends RuntimeException {
     public Exception getException() {
         return exception;
     }
-    
-    private String message;
-    private SQLException exception;
-
 }
-
-
