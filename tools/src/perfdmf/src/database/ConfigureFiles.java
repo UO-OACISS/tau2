69023543abee54438038800e6a766d4e03f0340f
scottb <scottb@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2007-05-16 18:19:45 +0000

    A Simple abstract class that gets a list of configurations.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@5097 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ tools/src/perfdmf/src/database/ConfigureFiles.java
@@ -0,0 +1,70 @@
+package edu.uoregon.tau.perfdmf.database;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+
+/**
+ * Simple Class to retrieve database configurations stored on the file system.
+ * 
+ * @author scottb
+ *
+ */
+
+
+abstract public class ConfigureFiles {
+
+	/* Returns a List of ParseConfigs. */
+	public static List getConfigurations()
+	{
+		List files = getConfigurationFiles();
+		List configs = new Vector();
+		for (int i=0; i<files.size(); i++)
+		{
+			configs.add(new ParseConfig(((File) files.get(i)).getAbsolutePath()));
+		}
+		return configs;
+	}
+	/* Returns a List of Files. */
+	public static List getConfigurationFiles()
+	{
+		List names = getConfigurationNames();
+		List files = new Vector();
+		for (int i=0; i<names.size(); i++)
+		{
+			if (((String) names.get(i)).compareTo("Default") == 0)
+				files.add(new File(System.getProperty("user.home") + "/.ParaProf/perfdmf.cfg"));
+			else
+				files.add(new File(System.getProperty("user.home") + "/.ParaProf/perfdmf.cfg." + (String) names.get(i)));
+		}
+		return files;
+	}
+	/* Returns a List of Strings. */
+	public static List getConfigurationNames()
+	{
+		File paraprofDirectory = new File(System.getProperty("user.home") + "/.ParaProf");
+		String[] fileNames = paraprofDirectory.list();
+		List perfdmfConfigs  = new Vector();
+		for (int i = 0; i<fileNames.length; i++)
+		{
+			
+			if (fileNames[i].compareTo("perfdmf.cfg") == 0)
+			{
+				perfdmfConfigs.add("Default");
+			}
+			else if (fileNames[i].startsWith("perfdmf.cfg") && !fileNames[i].endsWith("~"))
+			{
+				String name = fileNames[i].substring(12);
+				perfdmfConfigs.add(name);
+			}	
+		}
+		return perfdmfConfigs;
+	}
+	/* testing only */
+	public static void main(String[] args)
+	{
+		System.out.println("Names: ");
+		System.out.println(getConfigurationFiles());	
+	}
+}
