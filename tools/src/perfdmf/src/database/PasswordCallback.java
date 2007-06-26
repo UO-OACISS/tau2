e53f242662fc28e1eb919fcf9f580dd2d1d9dcf0
scottb <scottb@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2007-06-26 17:33:57 +0000

    Sorry about that, this Password Callback should work independently of paraprof.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@5290 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- tools/src/perfdmf/src/database/PasswordCallback.java
+++ tools/src/perfdmf/src/database/PasswordCallback.java
@@ -1,6 +1,42 @@
 package edu.uoregon.tau.perfdmf.database;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+	
+
 public interface PasswordCallback {
 
     String getPassword(ParseConfig config);
+
+    public static PasswordCallback guiPasswordCallback = new PasswordCallback() {
+
+        public String getPassword(ParseConfig config) {
+            JPanel promptPanel = new JPanel();
+
+            promptPanel.setLayout(new GridBagLayout());
+            GridBagConstraints gbc = new GridBagConstraints();
+            gbc.insets = new Insets(5, 5, 5, 5);
+
+            JLabel label = new JLabel("<html>Enter password for user '" + config.getDBUserName() + "'<br> Database: '"
+                    + config.getDBName() + "' (" + config.getDBHost() + ":" + config.getDBPort() + ")</html>");
+
+            JPasswordField password = new JPasswordField(15);
+
+            promptPanel.add(label);
+            promptPanel.add(password);
+
+            if (JOptionPane.showConfirmDialog(null, promptPanel, "Enter Password", JOptionPane.OK_CANCEL_OPTION) == JOptionPane.OK_OPTION) {
+                return new String(password.getPassword());
+            } else {
+                return null;
+            }
+        }
+    };
 }
+
