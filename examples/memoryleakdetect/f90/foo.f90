b9320881aceaa510fdfb6301011eddeada293fa6
amorris <amorris@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2007-09-09 18:19:16 +0000

    Replaced fortran 90 memory leak example with one that really has a leak that isn't auto cleaned up by f95 compilers.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@5442 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- examples/memoryleakdetect/f90/foo.f90
+++ examples/memoryleakdetect/f90/foo.f90
@@ -1,47 +1,27 @@
-       subroutine bar(value)
-       integer:: value
-       integer, dimension(:), allocatable:: X
-   
-       if (value .gt. 3) then
-         print *, "bar called with value=", value
-         allocate(X(value))
-         X(2) = 2
-         ! Do not free X. Create a  memory leak unless value > 15 
-         if (value .gt. 15) deallocate(X)
-       else
-         print *, "bar called from foo!"
-         allocate(X(45))
-         X(23) = 2
-         deallocate(X)
-       endif
-       end subroutine bar
-        
-       subroutine g(value)
-       integer:: value
-     
-       print *, "Inside g: value = ", value
-       call bar(value)
-       end subroutine g
-        
-       subroutine foo(value)
-       integer:: value, ierr
-       
-       print *, "Inside foo: value = ", value
-       
-       ! Create two distinct paths to reach bar
-       if (value .gt. 5) then
-          call g(value)
-       else 
-          call bar(value)
-       endif
+module scratch
+   integer, parameter :: wp = selected_real_kind(15)
+   real(wp), allocatable :: A(:,:), B(:,:), C(:,:)   ! scratch arrays
+contains
+   subroutine allocate_scratch(n)
+      integer, intent(in) :: n
+      allocate( A(n,n), B(n,n), C(n,n) )
+      return
+   end subroutine allocate_scratch
+   subroutine use_scratch
+      ! use the module arrays A, B, and C.
+      call random_number( A ); call random_number( B )
+      C = matmul( A, B )
+      return
+   end subroutine use_scratch
+   subroutine deallocate_scratch
+      deallocate( A, B )  ! note that C is not included in this list.
+      return
+   end subroutine deallocate_scratch
+end module scratch
 
-       end subroutine foo
-
-       program main
-       call TAU_PROFILE_SET_NODE(0)
-       call foo(12) ! leak
-       call foo(20) ! no leak
-       call foo(2)  ! no leak
-       call foo(13) ! leak
-       call foo(4)  ! leak
-       end program main
+program memory_leak
+   use scratch
+   call allocate_scratch( 100 )
+   call use_scratch
+   call deallocate_scratch
+end program memory_leak
