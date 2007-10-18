438f4570b5a30f33c6cec19c048bf0710d862cf6
amorris <amorris@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2007-10-18 00:42:42 +0000

    updated windows example to have a ring, added comment stuff for changing profiling/tracing modes
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@5531 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- tools/src/windows/examples/mpi/mpi.cpp
+++ tools/src/windows/examples/mpi/mpi.cpp
@@ -3,6 +3,20 @@
 
 /*** For regular profiling: ***/
 #define PROFILING_ON 1
+#pragma comment(lib, "tau-profile-static.lib")
+
+/*** For callpath profiling: ***/
+//#define PROFILING_ON 1
+//#define TAU_CALLPATH 1
+//#pragma comment(lib, "tau-callpath-static.lib")
+
+/*** For tracing: ***/
+//#define TRACING_ON 1
+//#pragma comment(lib, "tau-trace-static.lib")
+ 
+#define CSIZE 10
+
+
 #define TAU_WINDOWS
 #define TAU_DOT_H_LESS_HEADERS
 #include <TAU.h>
@@ -18,6 +32,33 @@ void bar() {
 	TAU_PROFILE("bar", "", TAU_USER);
 	Sleep(2000);
 }
+
+void func(int me, int proc) {
+  int i;
+  int field[CSIZE];
+  MPI_Status status;
+
+  for (i=0; i<CSIZE; i++)
+    field[i] = i;
+
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  for (i=0; i<3; ++i) {
+    if (me==0) {
+      MPI_Send(&field, CSIZE, MPI_INT, 1, 4711, MPI_COMM_WORLD);
+      MPI_Recv(&field, CSIZE, MPI_INT, proc-1, 4711, MPI_COMM_WORLD, &status);
+    }
+    else {
+      MPI_Recv(&field, CSIZE, MPI_INT, me-1, 4711, MPI_COMM_WORLD, &status);
+      MPI_Send(&field, CSIZE, MPI_INT, (me+1)%proc, 4711, MPI_COMM_WORLD);
+    }
+  }
+  MPI_Bcast (&field, CSIZE, MPI_INT, 0, MPI_COMM_WORLD);
+  printf("%d done.\n", me);
+}
+
+
+
 
 
 int main(int argc, char **argv) {
@@ -36,5 +77,6 @@ int main(int argc, char **argv) {
   printf("mpi: [%d/%d] %s: hello world\n", rank+1,size, name);
   foo();
   bar();
+  func(rank, size);
   MPI_Finalize();
 }
