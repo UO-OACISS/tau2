9f3600d0c89767da80be3bad40c838024e7f0522
khuck <khuck@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2008-03-06 21:34:37 +0000

    minor fix to perfexplorer page.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@5813 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- tools/src/perfexplorer/doc/index.php
+++ tools/src/perfexplorer/doc/index.php
@@ -6,26 +6,41 @@
 
 <head>
 <title>University of Oregon - PerfExplorer</title>
-<!--<link rel="stylesheet" type="text/css" href="mine.css"/>-->
+<link rel="stylesheet" type="text/css" href="new.css"/>
 <link rel="icon" href="tau16x16.gif" type="image/gif"/>
 <?php include("utility_functions.php"); ?>
 </head>
 
 <body>
 
+<div style="float: left; min-width: 280px;">
 <p>
-<img src="tau-large.png" alt="TAU logo"/>
+<img src="tau-large.png" alt="TAU logo" height="250" width="250"/><br/>
+<a href="#intro">Introduction</a><br/>
+<a href="#features">New Features in PerfExplorer 2.0</a><br/>
+<a href="#prereqs">Prerequisites</a><br/>
+<a href="#running">Running PerfExplorer</a><br/>
+<a href="#using">Using PerfExplorer</a><br/>
+<a href="#bugs">Known Bugs</a><br/>
+<a href="#other">Other stuff...</a><br/>
 </p>
+</div>
 
-<h1>PerfExplorer</h1>
+<div>
 <p>
+<h1>PerfExplorer</h1>
 Thanks for your interest in PerfExplorer!  This simple page is hopefully only
 temporary - more useful content will come at a later time.
 </p>
 
+<p>
 <h2>For The Impatient</h2>
 <h3><a href="perfexplorer.jnlp">Click here to run PerfExplorer!</a></h3>
+</p>
+</div>
 
+<div>
+<a name="intro"/>
 <h2>Introduction</h2>
 <p>
 PerfExplorer is a framework for parallel performance data mining and knowledge
@@ -53,7 +68,10 @@ independent and dependent correlated variables in the data. In addition,
 comparative analysis extends these operations to compare results from different
 experiments, for instance, as part of a parametric study.
 </p>
+</div>
 
+<div>
+<a name="features"/>
 <h2>New Features in PerfExplorer 2.0</h2>
 <ul>
 <li><b>Analysis Process Automation</b>:  
@@ -91,6 +109,10 @@ results.
 time.</i></li>
 </ul>
 
+</div>
+
+<div>
+<a name="prereqs"/>
 <h2>Prerequisites</h2>
 <h3>Java Runtime Environment (JRE) 5</h3>
 <p>
@@ -110,6 +132,10 @@ hosting a PerfDMF data repository but rather wish to connect to a remote
 repository, then PerfExplorer will help you configure your connection.
 </p>
 
+</div>
+
+<div>
+<a name="running"/>
 <h2>Running PerfExplorer</h2>
 <p>
 <a href="perfexplorer.jnlp">Click here to run the PerfExplorer Java Web Start
@@ -131,6 +157,10 @@ File->Preferences, go to the Advanced Tab and select "Show Java Console".</i>
 </p>
 
 
+</div>
+
+<div>
+<a name="using"/>
 <h2>Using PerfExplorer</h2>
 <p>
 The full documentation for PerfExplorer 1.0 is located at the <a
@@ -160,6 +190,10 @@ examples:
 <li>...coming soon...</li>
 </ol>
 
+</div>
+
+<div>
+<a name="bugs"/>
 <h2>Known Bugs</h2>
 <p>
 There are several.  However, some of the more important ones:
@@ -175,6 +209,10 @@ please contact Kevin Huck at <?php echo
 javascript_ascii_email_string("khuck@cs.uoregon.edu", ""); ?>.</li>
 </ol>
 
+</div>
+
+<div>
+<a name="other"/>
 <h2>Other stuff...</h2>
 <p>
 For any questions, comments, problems, etc., please contact Kevin Huck at
@@ -182,6 +220,7 @@ For any questions, comments, problems, etc., please contact Kevin Huck at
 with more general TAU questions,
 <?php echo javascript_ascii_email_string("tau-bugs@cs.uoregon.edu", ""); ?>.
 </p>
+</div>
 
 <p>
 <a href="http://validator.w3.org/check?uri=referer">
