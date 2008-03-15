7bc68e6b4ddfba4cb286db17c50c721fd57bc6c2
sameer <sameer@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2008-03-15 02:10:20 +0000

    Added int as return values to mpi_init__ etc.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@5859 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- src/Profile/TauForceMPI.c
+++ src/Profile/TauForceMPI.c
@@ -1,6 +1,6 @@
 #include <mpi.h>
-extern mpi_init_(void*,void*);
-extern mpi_init__(void*,void*);
+extern int mpi_init_(void*,void*);
+extern int mpi_init__(void*,void*);
 static void unused() {
   MPI_Init(0,0);
   mpi_init_(0,0);
