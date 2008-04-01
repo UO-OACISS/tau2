712f1289b0f75290a7f71f185a8ec61eb11365e5
khuck <khuck@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2008-04-01 00:27:37 +0000

    Added class for scaling metrics by a scalar value.  It supports add, subtract, multiply and divide, just like the DeriveMetricOperation class.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@5907 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ tools/src/perfexplorer/src/glue/ScaleMetricOperation.java
@@ -0,0 +1,100 @@
+/**
+ * 
+ */
+package glue;
+
+import java.util.List;
+
+import edu.uoregon.tau.perfdmf.Trial;
+
+/**
+ * @author khuck
+ *
+ */
+public class ScaleMetricOperation extends AbstractPerformanceOperation {
+
+	public static final String ADD = "+";
+	public static final String SUBTRACT = "-";
+	public static final String MULTIPLY = "*";
+	public static final String DIVIDE = "/";
+
+	private String metric = null;
+	private Double value = 0.0;
+	private String operation = ADD;
+	
+	/**
+	 * @param input
+	 */
+	public ScaleMetricOperation(PerformanceResult input) {
+		super(input);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param trial
+	 */
+	public ScaleMetricOperation(Trial trial) {
+		super(trial);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param inputs
+	 */
+	public ScaleMetricOperation(List<PerformanceResult> inputs) {
+		super(inputs);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param input
+	 */
+	public ScaleMetricOperation(PerformanceResult input, String metric, Double value, String operation) {
+		super(input);
+		this.metric = metric;
+		this.value = value;
+		this.operation = operation;
+	}
+
+
+	/* (non-Javadoc)
+	 * @see glue.PerformanceAnalysisOperation#processData()
+	 */
+	public List<PerformanceResult> processData() {
+		String newName = "(" + metric + operation + value.toString() + ")";
+		for (PerformanceResult input : inputs) {
+			PerformanceResult output = new DefaultResult();
+			
+			for (String event : input.getEvents()) {
+				for (Integer thread : input.getThreads()) {
+					double value1 = 0.0;
+					double value2 = 0.0;
+					if (operation.equals(ADD)) {
+						value1 = input.getInclusive(thread, event, metric) + value;
+						value2 = input.getExclusive(thread, event, metric) + value;
+					} else if (operation.equals(SUBTRACT)) {
+						value1 = input.getInclusive(thread, event, metric) - value;
+						value2 = input.getExclusive(thread, event, metric) - value;
+					} else if (operation.equals(MULTIPLY)) {
+						value1 = input.getInclusive(thread, event, metric) * value;
+						value2 = input.getExclusive(thread, event, metric) * value;
+					} else if (operation.equals(DIVIDE)) {
+						if (value == 0.0) {
+							value1 = 0.0;
+						} else {
+							value1 = input.getInclusive(thread, event, metric) / value;
+							value2 = input.getExclusive(thread, event, metric) / value;
+						}
+					}
+					output.putInclusive(thread, event, newName, value1);
+					output.putExclusive(thread, event, newName, value2);
+					output.putCalls(thread, event, input.getCalls(thread, event));
+					output.putSubroutines(thread, event, input.getSubroutines(thread, event));
+				}
+			}
+			outputs.add(output);
+		}
+		return outputs;
+	}
+
+}
---
+++ tools/src/perfexplorer/src/glue/ScaleMetricOperationTest.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package glue;
+
+import edu.uoregon.tau.perfdmf.Trial;
+import junit.framework.TestCase;
+
+
+/**
+ * @author khuck
+ *
+ */
+public class ScaleMetricOperationTest extends TestCase {
+
+	/**
+	 * Test method for {@link glue.ScaleMetricOperation#processData()}.
+	 */
+	public final void testProcessData() {
+	    Utilities.setSession("perfdmf_test");
+		Trial trial = Utilities.getTrial("gtc_bench", "superscaling.jaguar", "64");
+		PerformanceResult result = new TrialResult(trial);
+		Double value = 10.0;
+	    ScaleMetricOperation derive = new ScaleMetricOperation(result, "P_WALL_CLOCK_TIME", 
+				value, ScaleMetricOperation.DIVIDE);
+	    PerformanceResult output = derive.processData().get(0);
+		for (String metric : output.getMetrics()) {
+			System.out.println(metric);
+		}
+		for (String event : result.getEvents()) {
+			for (Integer thread : result.getThreads()) {
+				double tmp = result.getInclusive(thread, event, "P_WALL_CLOCK_TIME");
+				assertEquals((tmp == 0.0 ? 0.0 : tmp / value),
+					output.getInclusive(thread, event, "(P_WALL_CLOCK_TIME/" + value.toString() + ")"));
+				tmp = result.getExclusive(thread, event, "P_WALL_CLOCK_TIME");
+				assertEquals((tmp == 0.0 ? 0.0 : tmp / value),
+					output.getExclusive(thread, event, "(P_WALL_CLOCK_TIME/" + value.toString() + ")"));
+			}
+		}
+	}
+
+}
