d9fcc77016103a39e8f83877183ed2b9582abde7
khuck <khuck@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2008-04-02 22:44:46 +0000

    Fixed the bug... power models now correctly computed.  Huzzah!
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@5913 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- tools/src/perfexplorer/openuh/powerModels.py
+++ tools/src/perfexplorer/openuh/powerModels.py
@@ -1,3 +1,4 @@
+import sys
 from edu.uoregon.tau.perfdmf import Trial
 from rules import RuleHarness
 from glue import PerformanceResult
@@ -18,6 +19,7 @@ from glue import MeanEventFact
 
 True = 1
 False = 0
+mainEvent = ""
 
 ###################################################################
 
@@ -51,7 +53,8 @@ def getPowerModel(input):
 	# set some metric names
 	totalCycles = "CPU_CYCLES"
 	totalInstructions = "IA64_INST_RETIRED_THIS"
-	L1references = "L1_REFERENCES"
+	#L1references = "L1_REFERENCES"
+	L1references = "PAPI_L1_TCA"
 	L2references = "L2_REFERENCES"
 	L3references = "L3_REFERENCES"
 	global mainEvent
@@ -95,98 +98,108 @@ def getEnergy(input, PowerPerProc):
 	time = "LINUX_TIMERS"
 
 	# convert time to seconds
+	print mainEvent, time, input.getInclusive(0, mainEvent, time)
 	input, seconds = scaleMetric(input, time, (1/1000000.0), ScaleMetricOperation.MULTIPLY)
+	print mainEvent, seconds, input.getInclusive(0, mainEvent, seconds)
 	input, joules = deriveMetric(input, PowerPerProc, seconds, DeriveMetricOperation.MULTIPLY)
+	print mainEvent, joules, input.getInclusive(0, mainEvent, joules)
 
 	# return the trial, and the new derived metric name
 	return input, joules
 
 ###################################################################
 
-print "--------------- JPython test script start ------------"
-print "--- Calculating Power Models --- "
+def main():
+	global mainEvent
+	global True
+	global False
+	
+	print "--------------- JPython test script start ------------"
+	print "--- Calculating Power Models --- "
 
-# create a rulebase for processing
-#print "Loading Rules..."
-#ruleHarness = RuleHarness.useGlobalRules("openuh/OpenUHRules.drl")
+	# create a rulebase for processing
+	#print "Loading Rules..."
+	#ruleHarness = RuleHarness.useGlobalRules("openuh/OpenUHRules.drl")
 
-# load the trial
-print "loading the data..."
+	# load the trial
+	print "loading the data..."
 
-# check to see if the user has selected a trial
-tmp = Utilities.getCurrentTrial()
-if tmp != None:
-	trial = TrialResult(tmp)
-	print 
-else:
-	# remove these two lines to bypass this and use the default trial
-	print "No trial selected - script exiting"
-	# this line will throw an error - sys.exit() undefined?
-	sys.exit()
+	# check to see if the user has selected a trial
+	tmp = Utilities.getCurrentTrial()
+	if tmp != None:
+		trial = TrialResult(tmp)
+		print 
+	else:
+		# remove these two lines to bypass this and use the default trial
+		print "No trial selected - script exiting"
+		return
 
-	# choose the right database configuration - a string which matches the end of the jdbc connection,
-	# such as "perfdmf" to match "jdbc:derby:/Users/khuck/src/tau2/apple/lib/perfdmf"
-	Utilities.setSession("openuh")
+		# choose the right database configuration - a string which matches the end of the jdbc connection,
+		# such as "perfdmf" to match "jdbc:derby:/Users/khuck/src/tau2/apple/lib/perfdmf"
+		#Utilities.setSession("openuh")
 
-	# load just the average values across all threads, input: app_name, exp_name, trial_name
-	#trial = TrialResult(Utilities.getTrial("msap_parametric.optix.static", "size.400", "16.threads"))
-	trial = TrialResult(Utilities.getTrial("Fluid Dynamic - Unoptimized", "rib 45", "1_8"))
+		# load just the average values across all threads, input: app_name, exp_name, trial_name
+		#trial = TrialResult(Utilities.getTrial("msap_parametric.optix.static", "size.400", "16.threads"))
+		#trial = TrialResult(Utilities.getTrial("Fluid Dynamic - Unoptimized", "rib 45", "1_8"))
 
-# extract the non-callpath events from the trial
-print "extracting non-callpath..."
-extractor = ExtractNonCallpathEventOperation(trial)
-extracted = extractor.processData().get(0)
+	# extract the non-callpath events from the trial
+	print "extracting non-callpath..."
+	extractor = ExtractNonCallpathEventOperation(trial)
+	extracted = extractor.processData().get(0)
 
-# print "extracting event..."
-# extractor = ExtractEventOperation(trial, "MAIN__")
-# extracted = extractor.processData().get(0)
+	# print "extracting event..."
+	# extractor = ExtractEventOperation(trial, "MAIN__")
+	# extracted = extractor.processData().get(0)
 
-# get basic statistics
-print "computing mean..."
-statMaker = BasicStatisticsOperation(extracted, True)
-stats = statMaker.processData()
-means = stats.get(BasicStatisticsOperation.MEAN)
+	# get basic statistics
+	print "computing mean..."
+	statMaker = BasicStatisticsOperation(extracted, True)
+	stats = statMaker.processData()
+	means = stats.get(BasicStatisticsOperation.MEAN)
 
-# get main event
-mainEvent = means.getMainEvent()
-print "Main Event: ", mainEvent
+	# get main event
+	mainEvent = means.getMainEvent()
+	print "Main Event: ", mainEvent
 
-# calculate all derived metrics
-print "Deriving power metrics..."
-derived, PowerPerProc = getPowerModel(means)
+	# calculate all derived metrics
+	print "Deriving power metrics..."
+	derived, PowerPerProc = getPowerModel(means)
 
-# get the top 10 "power dense" events
-top10er = TopXEvents(derived, PowerPerProc, AbstractResult.EXCLUSIVE, 10)
-top10 = top10er.processData().get(0)
+	# get the top 10 "power dense" events
+	top10er = TopXEvents(derived, PowerPerProc, AbstractResult.EXCLUSIVE, 10)
+	top10 = top10er.processData().get(0)
 
-# just one thread
-thread = 0
+	# just one thread
+	thread = 0
 
-# iterate over events, output inefficiency derived metric
-print "Top 10 Average", PowerPerProc, "values per thread for this trial:"
-for event in top10.getEvents():
-	print event, top10.getExclusive(thread, event, PowerPerProc)
-print
-print mainEvent, "INCLUSIVE: ", top10.getInclusive(thread, event, PowerPerProc)
-print
+	# iterate over events, output inefficiency derived metric
+	print "Top 10 Average", PowerPerProc, "values per thread for this trial:"
+	for event in top10.getEvents():
+		print event, top10.getExclusive(thread, event, PowerPerProc)
+	print
+	print mainEvent, "INCLUSIVE: ", derived.getInclusive(thread, mainEvent, PowerPerProc)
+	print
 
-# compute the energy consumed by each event
-print "Computing joules consumed..."
-derived, EnergyPerProc = getEnergy(derived, PowerPerProc)
+	# compute the energy consumed by each event
+	print "Computing joules consumed..."
+	derived, EnergyPerProc = getEnergy(derived, PowerPerProc)
 
-# get the top 10 "power dense" events
-top10er = TopXEvents(derived, EnergyPerProc, AbstractResult.EXCLUSIVE, 10)
-top10 = top10er.processData().get(0)
+	# get the top 10 "power dense" events
+	top10er = TopXEvents(derived, EnergyPerProc, AbstractResult.EXCLUSIVE, 10)
+	top10 = top10er.processData().get(0)
 
-# iterate over events, output inefficiency derived metric
-print "Top 10 Average", EnergyPerProc, "values per thread for this trial:"
-for event in top10.getEvents():
-	print event, top10.getExclusive(thread, event, EnergyPerProc)
-print
-print mainEvent, "INCLUSIVE: ", top10.getInclusive(thread, event, EnergyPerProc)
-print
+	# iterate over events, output inefficiency derived metric
+	print "Top 10 Average", EnergyPerProc, "values per thread for this trial:"
+	for event in top10.getEvents():
+		print event, top10.getExclusive(thread, event, EnergyPerProc)
+	print
+	print mainEvent, "INCLUSIVE: ", derived.getInclusive(thread, mainEvent, EnergyPerProc)
+	print
 
-# process the rules
-#RuleHarness.getInstance().processRules()
+	# process the rules
+	#RuleHarness.getInstance().processRules()
 
-print "---------------- JPython test script end -------------"
+	print "---------------- JPython test script end -------------"
+
+if __name__ == "__main__":
+	main()
