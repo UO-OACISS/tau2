3f4956ade0b99e0817a42e79fd62e46633406c38
khuck <khuck@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2008-04-02 21:57:48 +0000

    Latest changes to the power model script.  The script will compute the watts for each event, and then compute the joules for each event.  Something is wrong, though - the inclusive value for main is too small, relative to the inclusive values for the events.  Must be something about the derived metrics...
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@5912 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- tools/src/perfexplorer/openuh/powerModels.py
+++ tools/src/perfexplorer/openuh/powerModels.py
@@ -91,6 +91,16 @@ def getPowerModel(input):
 	# return the trial, and the new derived metric name
 	return input, PowerPerProc
 
+def getEnergy(input, PowerPerProc):
+	time = "LINUX_TIMERS"
+
+	# convert time to seconds
+	input, seconds = scaleMetric(input, time, (1/1000000.0), ScaleMetricOperation.MULTIPLY)
+	input, joules = deriveMetric(input, PowerPerProc, seconds, DeriveMetricOperation.MULTIPLY)
+
+	# return the trial, and the new derived metric name
+	return input, joules
+
 ###################################################################
 
 print "--------------- JPython test script start ------------"
@@ -103,13 +113,24 @@ print "--- Calculating Power Models --- "
 # load the trial
 print "loading the data..."
 
-# choose the right database configuration - a string which matches the end of the jdbc connection,
-# such as "perfdmf" to match "jdbc:derby:/Users/khuck/src/tau2/apple/lib/perfdmf"
-Utilities.setSession("openuh")
+# check to see if the user has selected a trial
+tmp = Utilities.getCurrentTrial()
+if tmp != None:
+	trial = TrialResult(tmp)
+	print 
+else:
+	# remove these two lines to bypass this and use the default trial
+	print "No trial selected - script exiting"
+	# this line will throw an error - sys.exit() undefined?
+	sys.exit()
 
-# load just the average values across all threads, input: app_name, exp_name, trial_name
-#trial = TrialResult(Utilities.getTrial("msap_parametric.optix.static", "size.400", "16.threads"))
-trial = TrialResult(Utilities.getTrial("Fluid Dynamic", "rib 45", "1_8"))
+	# choose the right database configuration - a string which matches the end of the jdbc connection,
+	# such as "perfdmf" to match "jdbc:derby:/Users/khuck/src/tau2/apple/lib/perfdmf"
+	Utilities.setSession("openuh")
+
+	# load just the average values across all threads, input: app_name, exp_name, trial_name
+	#trial = TrialResult(Utilities.getTrial("msap_parametric.optix.static", "size.400", "16.threads"))
+	trial = TrialResult(Utilities.getTrial("Fluid Dynamic - Unoptimized", "rib 45", "1_8"))
 
 # extract the non-callpath events from the trial
 print "extracting non-callpath..."
@@ -134,13 +155,35 @@ print "Main Event: ", mainEvent
 print "Deriving power metrics..."
 derived, PowerPerProc = getPowerModel(means)
 
+# get the top 10 "power dense" events
+top10er = TopXEvents(derived, PowerPerProc, AbstractResult.EXCLUSIVE, 10)
+top10 = top10er.processData().get(0)
+
 # just one thread
 thread = 0
 
 # iterate over events, output inefficiency derived metric
-print "Average", PowerPerProc, "values for this trial:"
-for event in derived.getEvents():
-	print event, derived.getInclusive(thread, event, PowerPerProc)
+print "Top 10 Average", PowerPerProc, "values per thread for this trial:"
+for event in top10.getEvents():
+	print event, top10.getExclusive(thread, event, PowerPerProc)
+print
+print mainEvent, "INCLUSIVE: ", top10.getInclusive(thread, event, PowerPerProc)
+print
+
+# compute the energy consumed by each event
+print "Computing joules consumed..."
+derived, EnergyPerProc = getEnergy(derived, PowerPerProc)
+
+# get the top 10 "power dense" events
+top10er = TopXEvents(derived, EnergyPerProc, AbstractResult.EXCLUSIVE, 10)
+top10 = top10er.processData().get(0)
+
+# iterate over events, output inefficiency derived metric
+print "Top 10 Average", EnergyPerProc, "values per thread for this trial:"
+for event in top10.getEvents():
+	print event, top10.getExclusive(thread, event, EnergyPerProc)
+print
+print mainEvent, "INCLUSIVE: ", top10.getInclusive(thread, event, EnergyPerProc)
 print
 
 # process the rules
