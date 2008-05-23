2be827595705014c05f3b2aa68a8d798fa493649
amorris <amorris@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2008-05-23 00:48:26 +0000

    Added ProfileMerge.c which uses MPI to merge profiles online during MPI_Finalize
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@5982 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ src/Profile/ProfileMerge.c
@@ -0,0 +1,80 @@
+
+
+#ifdef TAU_MPI
+
+#include <TAU.h>
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <Profile/tau_types.h>
+
+
+int Tau_RtsLayer_myThread();
+char *getSnapshotBuffer();
+int getSnapshotBufferLength();
+
+
+int Tau_mergeProfiles() {
+  int rank, size, tid, i, buflen, flag;
+  FILE *f;
+  char *buf;
+  MPI_Status status;
+
+  tid = Tau_RtsLayer_myThread();
+
+  if (tid != 0) {
+    fprintf (stderr, "TAU: Merged file format does not support threads yet!\n");
+    return 0;
+  }
+
+
+  PMPI_Finalized(&flag);
+  if (flag) {
+    return 0;
+  }
+  
+  PMPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  PMPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  buf = getSnapshotBuffer();
+  buflen = getSnapshotBufferLength();
+
+  TAU_VERBOSE("TAU: Merging Profiles\n");
+
+  if (rank == 0) {
+    if ((f = fopen ("tauprofile.xml", "w+")) == NULL) {
+      char errormsg[4096];
+      sprintf(errormsg,"Error: Could not create tauprofile.xml");
+      perror(errormsg);
+    }
+    
+    fwrite (buf, buflen, 1, f);
+
+    for (i=1; i<size; i++) {
+      MPI_Recv(&buflen, 1, MPI_INT, MPI_ANY_SOURCE, 42, MPI_COMM_WORLD, &status);
+      buf = (char*) malloc (buflen);
+      MPI_Recv(buf, buflen, MPI_CHAR, status.MPI_SOURCE, 42, MPI_COMM_WORLD, &status);
+      fwrite (buf, buflen, 1, f);
+      free (buf);
+    }
+
+  } else {
+
+    // send length
+    MPI_Send(&buflen, 1, MPI_INT, 0, 42, MPI_COMM_WORLD);
+
+    // send data
+    MPI_Send(buf, buflen, MPI_CHAR, 0, 42, MPI_COMM_WORLD);
+
+  }
+
+  return 0;
+}
+
+
+#else
+int Tau_mergeProfiles() {
+  return 0;
+}
+
+#endif
