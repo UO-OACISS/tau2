93480202f3f35349a03e6ad71260f56c36f23605
amorris <amorris@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2008-07-22 18:50:05 +0000

    When execution of java fails on windows, we keep the console open and echo a message about making sure Java is installed.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@6128 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- tools/src/windows/bin/paraprof.bat
+++ tools/src/windows/bin/paraprof.bat
@@ -13,5 +13,15 @@ set JDBC_JAR=%JAR_ROOT%/postgresql.jar;%JAR_ROOT%/mysql.jar;%JAR_ROOT%/oracle.ja
 set COMMON_JAR=%JAR_ROOT%/tau-common.jar
 set JARS=%JAR_ROOT%/paraprof.jar;%JAR_ROOT%/vis.jar;%PERFDMF_JAR%;%JAR_ROOT%/jogl.jar;%JAR_ROOT%/jgraph.jar;%JDBC_JAR%;%JAR_ROOT%/jargs.jar;%JAR_ROOT%/epsgraphics.jar;%JAR_ROOT%/batik-combined.jar;%JAR_ROOT%/tau-common.jar;%JAR_ROOT%/jfreechart-0.9.21.jar;%JAR_ROOT%/jcommon-0.9.6.jar;%JAR_ROOT%/xerces.jar
 
+echo.
 java -Xmx500m -Djava.library.path=%JAR_ROOT% -cp %JARS% edu/uoregon/tau/paraprof/ParaProf %1 %2 %3 %4 %5
 
+:handleError
+if not errorlevel 1 goto finalActions
+echo.
+echo Failed to execute ParaProf... 
+echo Please check to make sure Sun Java is installed and is in the path
+echo. 
+
+pause
+:finalActions
