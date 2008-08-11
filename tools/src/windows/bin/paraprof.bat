9dc383d924b64503961242f310f78dd169899253
amorris <amorris@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2008-08-11 21:25:50 +0000

    Added -Dderby.system.home="%HOMEPATH%/.ParaProf"
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@6241 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- tools/src/windows/bin/paraprof.bat
+++ tools/src/windows/bin/paraprof.bat
@@ -6,7 +6,6 @@ set TAU_ROOT=..
 
 
 set JAR_ROOT=%TAU_ROOT%/bin
-set CONFIG_FILE=%HOME%/.ParaProf/perfdmf.cfg
 set PERFDMF_JAR=%JAR_ROOT%/perfdmf.jar
 set JARGS_JAR=%JAR_ROOT%/jargs.jar
 set JDBC_JAR=%JAR_ROOT%/postgresql.jar;%JAR_ROOT%/mysql.jar;%JAR_ROOT%/oracle.jar;%JAR_ROOT%/derby.jar
@@ -14,7 +13,7 @@ set COMMON_JAR=%JAR_ROOT%/tau-common.jar
 set JARS=%JAR_ROOT%/paraprof.jar;%JAR_ROOT%/vis.jar;%PERFDMF_JAR%;%JAR_ROOT%/jogl.jar;%JAR_ROOT%/jgraph.jar;%JDBC_JAR%;%JAR_ROOT%/jargs.jar;%JAR_ROOT%/epsgraphics.jar;%JAR_ROOT%/batik-combined.jar;%JAR_ROOT%/tau-common.jar;%JAR_ROOT%/jfreechart-0.9.21.jar;%JAR_ROOT%/jcommon-0.9.6.jar;%JAR_ROOT%/xerces.jar
 
 echo.
-java -Xmx500m -Djava.library.path=%JAR_ROOT% -cp %JARS% edu/uoregon/tau/paraprof/ParaProf %1 %2 %3 %4 %5
+java -Xmx500m -Djava.library.path=%JAR_ROOT% -Dderby.system.home="%HOMEPATH%/.ParaProf" -cp %JARS% edu/uoregon/tau/paraprof/ParaProf %1 %2 %3 %4 %5
 
 :handleError
 if not errorlevel 1 goto finalActions
