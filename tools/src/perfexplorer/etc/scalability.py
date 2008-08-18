4ab160606398e7e2fd04710276aa40e560b10532
khuck <khuck@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2008-08-18 16:40:13 +0000

    adding the script
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@6262 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ tools/src/perfexplorer/etc/scalability.py
@@ -0,0 +1,79 @@
+from glue import PerformanceResult
+from glue import PerformanceAnalysisOperation
+from glue import ExtractEventOperation
+from glue import Utilities
+from glue import BasicStatisticsOperation
+from glue import DeriveMetricOperation
+from glue import MergeTrialsOperation
+from glue import TrialMeanResult
+from glue import AbstractResult
+from glue import DrawGraph
+from client import PerfExplorerModel
+from edu.uoregon.tau.perfdmf import Trial
+from java.util import HashSet
+from java.util import ArrayList
+
+True = 1
+False = 0
+config = "proper"
+inApp = "sweep3d_gnu"
+inExp = "mpi-pdt_2008-08-15_15:34:07"
+inTrial = ""
+
+def load():
+	print "loading data..."
+	parameterMap = PerfExplorerModel.getModel().getScriptParameters()
+	keys = parameterMap.keySet()
+	for key in keys:
+		print key, parameterMap.get(key)
+	config = parameterMap.get("config")
+	inApp = parameterMap.get("app")
+	inExp = parameterMap.get("exp")
+	Utilities.setSession(config)
+	trials = Utilities.getTrialsForExperiment(inApp, inExp)
+	print "...done."
+	return trials
+
+def extractMain(inputs):
+	events = ArrayList()
+	events.add(inputs.get(0).getMainEvent())
+
+	print "extracting main event..."
+	extractor = ExtractEventOperation(inputs, events)
+	extracted = extractor.processData()
+	print "...done."
+
+	return extracted
+
+def drawGraph(results):
+	print "drawing charts..."
+	for metric in results.get(0).getMetrics():
+		grapher = DrawGraph(results)
+		metrics = HashSet()
+		metrics.add(metric)
+		grapher.set_metrics(metrics)
+		#grapher.setSortXAxis(False)
+		grapher.setLogYAxis(False)
+		grapher.setTitle(inApp + ": " + inExp + ": " + metric)
+		grapher.setSeriesType(DrawGraph.EVENTNAME)
+		grapher.setCategoryType(DrawGraph.PROCESSORCOUNT)
+		grapher.setValueType(AbstractResult.INCLUSIVE)
+		grapher.setXAxisLabel("Processor Count")
+		grapher.setYAxisLabel("Inclusive " + metric)
+		grapher.processData()
+	print "...done."
+
+	return
+
+print "--------------- JPython test script start ------------"
+
+trials = load()
+results = ArrayList()
+for trial in trials:
+	loaded = TrialMeanResult(trial)
+	results.add(loaded)
+
+extracted = extractMain(results)
+drawGraph(extracted)
+
+print "---------------- JPython test script end -------------"
