ed4cb588bf8fe6843ea3ca6380f7e12c067e2e63
khuck <khuck@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2008-10-07 03:17:52 +0000

    script for GAMESS cqos.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@6407 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ tools/src/perfexplorer/cqos/chemistry.py
@@ -0,0 +1,117 @@
+from glue import *
+from client import PerfExplorerModel
+from edu.uoregon.tau.perfdmf import Trial
+from edu.uoregon.tau.perfdmf import Experiment
+from java.util import *
+
+True = 1
+False = 0
+config = "local"
+inApp = "GAMESS"
+# inExp = "CQoS"
+inTrial = ""
+parameterMap = None
+fileName = "/tmp/classifier.serialized"
+
+def getParameters():
+	global parameterMap
+	global config
+	global inApp
+	global inExp
+	global fileName
+	print "getting parameters..."
+	parameterMap = PerfExplorerModel.getModel().getScriptParameters()
+	keys = parameterMap.keySet()
+	for key in keys:
+		print key, parameterMap.get(key)
+	config = parameterMap.get("config")
+	inApp = parameterMap.get("app")
+	inExp = parameterMap.get("exp")
+	fileName = parameterMap.get("fileName")
+	print "...done."
+
+def loadTrials():
+	print "loading data..."
+	Utilities.setSession(config)
+	trials = Utilities.getTrialsForExperiment(inApp, inExp)
+	print "...done."
+	return trials
+
+def loadExperiments():
+	print "loading data..."
+	Utilities.setSession(config)
+	experiments = Utilities.getExperimentsForApplication(inApp)
+	print "...done."
+	return experiments
+
+def buildClassifier(results):
+	print "building classifier..."
+	metadataFields = HashSet()
+	metadataFields.add("molecule name")
+	metadataFields.add("basis set")
+	metadataFields.add("run type")
+	metadataFields.add("scf type")
+	metadataFields.add("node count")
+	metadataFields.add("core count")
+	metadataFields.add("mplevl")
+	metadataFields.add("dirscf")
+	# metadataFields.add("CPU MHz") # i.e. 1995.002
+	# metadataFields.add("CPU Cores") # i.e. 2
+	# metadataFields.add("OS Machine") # i.e. Linux
+	# metadataFields.add("Cache Size") # i.e. 4096 KB
+	# for performance
+	classifier = CQoSClassifierOperation(results, "Time", metadataFields, "dirscf")
+	# for accuracy
+	# classifier = CQoSClassifierOperation(results, "accuracy", metadataFields, "basis set")
+	# classifier.setClassifierType(CQoSClassifierOperation.MULTILAYER_PERCEPTRON)
+	# classifier.setClassifierType(CQoSClassifierOperation.NAIVE_BAYES)
+	classifier.setClassifierType(CQoSClassifierOperation.SUPPORT_VECTOR_MACHINE)
+	classifier.processData()
+	classifier.writeClassifier(fileName)
+	print "...done."
+	return classifier
+
+
+print "--------------- JPython test script start ------------"
+
+getParameters()
+results = ArrayList()
+
+print "getting trials..."
+
+trials = loadTrials()
+for trial in trials:
+	loaded = TrialMeanResult(trial)
+	results.add(loaded)
+
+#experiments = loadExperiments()
+#for experiment in experiments:
+	#inExp = experiment.getName();
+	#print "processing experiment: ", inExp
+	#trials = loadTrials()
+	#for trial in trials:
+		#loaded = TrialMeanResult(trial)
+		#results.add(loaded)
+
+print "...done."
+print "Total Trials:", results.size()
+
+classifier = buildClassifier(results)
+
+# test the classifier
+mols = ['AT', 'bz', 'bz-dimer', 'C60', 'GC', 'np', 'np-dimer']
+mps = ['MP0', 'MP2']
+for m in mols:
+	for mp in mps:
+		inputFields = HashMap()
+		inputFields.put("molecule name", m)
+		inputFields.put("basis set", "CCD")
+		inputFields.put("run type", "ENERGY")
+		inputFields.put("scf type", "RHF")
+		inputFields.put("node count", "8")
+		inputFields.put("core count", "8")
+		inputFields.put("mplevl", mp)
+		print inputFields
+		print "Direct / Conventional: ", classifier.getClass(inputFields),  classifier.getConfidence()
+
+print "---------------- JPython test script end -------------"
