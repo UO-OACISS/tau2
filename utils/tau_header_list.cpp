619ee6ac52e6ca6ae856e355b6cc8b8471292345
amorris <amorris@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2008-10-08 22:05:18 +0000

    Added ductape based header lister.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@6412 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ utils/tau_header_list.cpp
@@ -0,0 +1,59 @@
+/****************************************************************************
+ **                      TAU Portable Profiling Package                     **
+ **                      http://www.cs.uoregon.edu/research/tau             **
+ *****************************************************************************
+ **    Copyright 2008                                                       **
+ **    Department of Computer and Information Science, University of Oregon **
+ **    Advanced Computing Laboratory, Los Alamos National Laboratory        **
+ ****************************************************************************/
+/****************************************************************************
+ **      File            : tau_headerlist.cpp                               **
+ **      Description     : TAU Profiling Package                            **
+ **      Author          : Alan Morris                                      **
+ **      Contact         : tau-bugs@cs.uoregon.edu                          **
+ **      Documentation   : See http://www.cs.uoregon.edu/research/tau       **
+ **                                                                         **
+ **      Description     : This program outputs the headers included in     **
+ **                        a PDB file, excluding system headers             **
+ **                                                                         **
+ ****************************************************************************/
+
+#include "pdb.h"
+#include "pdbRoutine.h"
+
+static void printIncludes(const pdbFile *f) {
+  pdbFile::incvec i = f->includes();
+  for (pdbFile::incvec::iterator it=i.begin(); it!=i.end(); ++it) {
+    if (!(*it)->isSystemFile()) { // exclude system files
+      // skip over "./"
+      const char *ptr = (*it)->name().c_str();
+      if (*ptr == '.') {
+	if (*(ptr+1) == '/') {
+	  ptr+=2;
+	}
+      }
+      // output the name
+      cout << ptr << endl;
+    }
+    printIncludes(*it);
+  }
+}
+
+int main(int argc, char *argv[]) {
+  bool errflag = argc < 2;
+  if ( errflag ) {
+    cerr << "Usage: " << argv[0] << " pdbfile" << endl;
+    return 1;
+  }
+
+  PDB pdb(argv[1]);
+
+  if (!pdb) {
+    cerr << "Unable to read PDB file: %s\n", argv[1];
+    return 1;
+  }
+
+  pdbFile *topFile = pdb.fileTree();
+  printIncludes(topFile);
+  return 0;
+}
