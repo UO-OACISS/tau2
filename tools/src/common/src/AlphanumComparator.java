6c300566fb74d4717c33db6934c736e420ba0265
amorris <amorris@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2009-01-23 23:07:49 +0000

    Can now compare any kind of object using .toString()
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@6773 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- tools/src/common/src/AlphanumComparator.java
+++ tools/src/common/src/AlphanumComparator.java
@@ -70,11 +70,9 @@ public class AlphanumComparator implements Comparator {
     }
 
     public int compare(Object o1, Object o2) {
-        if (!(o1 instanceof String) || !(o2 instanceof String)) {
-            return 0;
-        }
-        String s1 = (String) o1;
-        String s2 = (String) o2;
+
+        String s1 = o1.toString();
+        String s2 = o2.toString();
 
         int thisMarker = 0;
         int thatMarker = 0;
