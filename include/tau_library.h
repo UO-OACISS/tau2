703921eebb0a3d5f43e4ee42bce1c6e6e65c0c12
amorris <amorris@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2009-02-20 23:40:46 +0000

    Created tau_library.h, a place for library-only stuff (the apps won't include it).  A much better place than profiler.h for all this stuff.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@6864 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ include/tau_library.h
@@ -0,0 +1,53 @@
+/****************************************************************************
+**			TAU Portable Profiling Package                     **
+**			http://www.cs.uoregon.edu/research/tau             **
+*****************************************************************************
+**    Copyright 2008  						   	   **
+**    Department of Computer and Information Science, University of Oregon **
+**    Advanced Computing Laboratory, Los Alamos National Laboratory        **
+**    Forschungszentrum Juelich                                            **
+****************************************************************************/
+/****************************************************************************
+**	File 		: tau_library.h			        	   **
+**	Description 	: TAU Profiling Package				   **
+**	Contact		: tau-bugs@cs.uoregon.edu               	   **
+**	Documentation	: See http://www.cs.uoregon.edu/research/tau       **
+**                                                                         **
+**      Description     : Include this header in all TAU source files      **
+**                        But not user applications                        **
+**                                                                         **
+****************************************************************************/
+
+#ifndef _TAU_LIBRARY_H_
+#define _TAU_LIBRARY_H_
+
+
+#if (defined(TAU_WINDOWS))
+#define TAUDECL __cdecl
+#else
+#define TAUDECL
+#endif /* TAU_WINDOWS */
+
+
+#include <Profile/tau_types.h>
+
+
+#ifdef TAU_LARGEFILE
+  #define LARGEFILE_OPTION O_LARGEFILE
+#else
+  #define LARGEFILE_OPTION 0
+#endif
+
+
+
+#ifdef TAU_WINDOWS
+  #include <io.h>
+#else
+  #include <unistd.h>
+  #define O_BINARY 0
+#endif
+
+
+
+
+#endif /* _TAU_LIBRARY_H_ */
