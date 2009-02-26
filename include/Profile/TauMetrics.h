5ed8afefb4923bece0228bbaeecc0c4f81e24c83
amorris <amorris@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2009-02-26 18:05:48 +0000

    Adding TauMetrics.h
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@6946 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ include/Profile/TauMetrics.h
@@ -0,0 +1,28 @@
+/****************************************************************************
+**			TAU Portable Profiling Package			   **
+**			http://www.cs.uoregon.edu/research/tau	           **
+*****************************************************************************
+**    Copyright 2009  						   	   **
+**    Department of Computer and Information Science, University of Oregon **
+**    Advanced Computing Laboratory, Los Alamos National Laboratory        **
+**    Forschungszentrum Juelich                                            **
+****************************************************************************/
+/****************************************************************************
+**	File 		: TauMetrics.cpp		        	   **
+**	Description 	: TAU Profiling Package				   **
+**	Contact		: tau-bugs@cs.uoregon.edu               	   **
+**	Documentation	: See http://www.cs.uoregon.edu/research/tau       **
+**                                                                         **
+**      Description     : Handles Metrics                                  **
+**                                                                         **
+****************************************************************************/
+
+
+
+
+
+
+
+int TauMetrics_init();
+
+
