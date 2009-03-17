2f34f543668ff0d1afdd7016d00bc724adf7d569
khuck <khuck@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2009-03-17 20:46:07 +0000

    Minor javadoc changes.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@6993 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- tools/src/perfexplorer/src/rules/FactWrapper.java
+++ tools/src/perfexplorer/src/rules/FactWrapper.java
@@ -1,11 +1,16 @@
 /**
- * 
+ * This class is used as a simple wrapper to create simple facts.
+ * For more complex fact objects, implement the {@link SelfAsserting} interface.
  */
 package edu.uoregon.tau.perfexplorer.rules;
 
 /**
- * @author khuck
+ * This class is used as a simple wrapper to create simple facts.
+ * For more complex fact objects, implement the {@link SelfAsserting} interface.
  *
+ * @author khuck
+ * @since 2.0
+ * @see SelfAsserting
  */
 public class FactWrapper {
 
@@ -14,7 +19,11 @@ public class FactWrapper {
 	private Object factData = null;
 	
 	/**
-	 * 
+	 * Constructor method.
+	 *
+	 * @param factName The name of the fact
+	 * @param factType The type of the fact
+	 * @param factData The object that this fact wrapper will contain
 	 */
 	public FactWrapper(String factName, String factType, Object factData) {
 		this.factName = factName;
@@ -23,42 +32,60 @@ public class FactWrapper {
 	}
 
 	/**
+	 * Return the object stored in the FactWrapper object.
+	 *
 	 * @return the factData
+	 * @see #setFactData
 	 */
 	public Object getFactData() {
 		return factData;
 	}
 
 	/**
+	 * Set the object in the FactWrapper.
+	 *
 	 * @param factData the factData to set
+	 * @see #getFactData
 	 */
 	public void setFactData(Object factData) {
 		this.factData = factData;
 	}
 
 	/**
+	 * Return the name of the fact.
+	 *
 	 * @return the factName
+	 * @see #setFactName
 	 */
 	public String getFactName() {
 		return factName;
 	}
 
 	/**
+	 * Set the name of the Fact.
+	 *
 	 * @param factName the factName to set
+	 * @see #getFactName
 	 */
 	public void setFactName(String factName) {
 		this.factName = factName;
 	}
 
 	/**
+	 * Return the type of the fact.
+	 *
 	 * @return the factType
+	 * @see #setFactType
 	 */
 	public String getFactType() {
 		return factType;
 	}
 
 	/**
+	 * Set the type of the fact.
+	 *
 	 * @param factType the factType to set
+	 * @see #getFactType
 	 */
 	public void setFactType(String factType) {
 		this.factType = factType;
