8b5a7e8594cb781f62491105e6a19fd2dec410f6
khuck <khuck@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2009-03-19 00:52:38 +0000

    Sample script to test heatmap.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@7006 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ tools/src/perfexplorer/etc/heatmap.py
@@ -0,0 +1,17 @@
+from edu.uoregon.tau.perfexplorer.glue import *
+from edu.uoregon.tau.perfdmf import *
+from java.util import *
+from java.lang import *
+
+True = 1
+False = 0
+
+print "--------------- JPython test script start ------------"
+Utilities.setSession("test")
+trial = Utilities.getTrial("NPB_LU", "TAU_EACH_SEND", "64p")
+#trial = Utilities.getTrial("ring", "TAU_EACH_SEND", "64p")
+input = TrialResult(trial)
+messageHeatMap = BuildMessageHeatMap(input)
+messageHeatMap.processData()
+
+print "---------------- JPython test script end -------------"
