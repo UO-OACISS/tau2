9996327daa8e777d4976b15f4e0bdefd4322a830
sameer <sameer@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2009-03-26 19:10:09 +0000

    Added an example for the TASK API with profiler create.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@7029 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ examples/profilercreate/taskc++/bar.cpp
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <TAU.h>
+
+int bar(int x)
+{
+  printf("Inside bar\n");
+  sleep(x);
+  return 0;
+}
+
+int foo(int x)
+{
+  printf("Inside foo\n");
+  sleep(x-1);
+  bar(x-1);
+  return 0;
+}
+
+int main(int argc, char **argv)
+{
+   /* Initialize */
+  
+
+  void *ptr, *top;
+  long calls, childcalls;
+  double incl[MAX_TAU_COUNTERS], excl[MAX_TAU_COUNTERS];
+  const char **counters;
+  int numcounters, i, j, taskid; 
+  TAU_INIT(&argc, &argv);
+  TAU_PROFILE_SET_NODE(0);
+  TAU_CREATE_TASK(taskid);
+  TAU_PROFILER_CREATE(top, "Top-level-timer","", TAU_USER);
+  TAU_PROFILER_CREATE(ptr, "foo","", TAU_USER);
+
+  TAU_PROFILER_START_TASK(top, taskid);
+  TAU_PROFILER_START_TASK(ptr, taskid);
+  foo(2);
+  TAU_PROFILER_STOP_TASK(ptr, taskid);
+
+  TAU_PROFILER_CREATE(ptr, "bar", "", TAU_USER);
+
+  
+  for (i=0; i < 5; i++) {
+    TAU_PROFILER_START_TASK(ptr, taskid);
+      bar(1);
+    TAU_PROFILER_STOP_TASK(ptr, taskid);
+  }
+  TAU_PROFILER_STOP_TASK(top, taskid);
+  TAU_PROFILER_GET_CALLS_TASK(ptr, &calls, taskid);
+  TAU_PROFILER_GET_CHILD_CALLS_TASK(ptr, &childcalls, taskid);
+  TAU_PROFILER_GET_INCLUSIVE_VALUES_TASK(ptr, &incl, taskid);
+  TAU_PROFILER_GET_EXCLUSIVE_VALUES_TASK(ptr, &excl, taskid);
+
+  TAU_PROFILER_GET_COUNTER_INFO_TASK(&counters, &numcounters, taskid);
+  printf("Calls = %ld, child = %ld\n", calls, childcalls);
+  printf("numcounters = %d\n", numcounters);
+  for (j = 0; j < numcounters ; j++)
+  {
+    printf(">>>");
+    printf("counter [%d] = %s\n", j, counters[j]);
+    printf(" excl [%d] = %g, incl [%d] = %g\n", j, excl[j], j, incl[j]);
+  }
+  return 0;
+}
