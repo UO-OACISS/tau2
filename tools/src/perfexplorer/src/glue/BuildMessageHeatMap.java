8983769c972e88fad7fab42a3309fc6090b82198
khuck <khuck@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2009-03-29 23:26:12 +0000

    Heat map works for all nodes of the call path now, not just the leaves.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@7056 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- tools/src/perfexplorer/src/glue/BuildMessageHeatMap.java
+++ tools/src/perfexplorer/src/glue/BuildMessageHeatMap.java
@@ -24,6 +24,12 @@ public class BuildMessageHeatMap extends AbstractPerformanceOperation {
 	private Map<String, double[]> mins = new TreeMap<String, double[]>(); 
 	private int size = 0;
 	private final static String allPaths = "All Paths";
+	private static final int COUNT = 0;
+	private static final int MAX = 1;
+	private static final int MIN = 2;
+	private static final int MEAN = 3;
+	private static final int STDDEV = 4;
+	private static final int VOLUME = 5;
 
 	private JFrame window = null;
 	public BuildMessageHeatMap(PerformanceResult input) {
@@ -46,9 +52,9 @@ public class BuildMessageHeatMap extends AbstractPerformanceOperation {
 						StringTokenizer st = new StringTokenizer(event, ":");
 						String first = st.nextToken().trim();
 						String path = st.nextToken().trim();
-						extractData(input, thread, event, first, path);
+//						extractData(input, thread, event, first, path);
 						// now, split up the path, and handle each node 
-/*						StringTokenizer st2 = new StringTokenizer(path, "=>");
+						StringTokenizer st2 = new StringTokenizer(path, "=>");
 						StringBuilder sb = new StringBuilder();
 						while (st2.hasMoreTokens()) {
 							if (sb.length() > 0) {
@@ -57,9 +63,11 @@ public class BuildMessageHeatMap extends AbstractPerformanceOperation {
 							sb.append(st2.nextToken());
 							extractData(input, thread, event, first, sb.toString());
 						}
-*/					}
+					}
 				}
 			}
+			
+			massageData();
 
 			window = new HeatMapWindow("Message Size Heat Maps", maps, maxs, mins, size);
 	        URL url = Utility.getResource("tau32x32.gif");
@@ -74,60 +82,108 @@ public class BuildMessageHeatMap extends AbstractPerformanceOperation {
 		return this.outputs;
 	}
 
+	private void massageData() {
+		for (String key : maps.keySet()) {
+			double[][][] map = maps.get(key);
+			double[] max = {0,0,0,0,0,0}; 
+			double[] min = {0,0,0,0,0,0};
+			for (int sender = 0 ; sender < size ; sender++) {
+				for (int receiver = 0 ; receiver < size ; receiver++) {
+					
+					// count and volume are fine... we need to re-compute the mean
+					if (map[COUNT][sender][receiver] > 0) {
+						map[MEAN][sender][receiver] = map[VOLUME][sender][receiver] / map[COUNT][sender][receiver];
+					} else {
+						map[MEAN][sender][receiver] = 0;
+					}
+
+					// compute stddev
+					if (map[COUNT][sender][receiver] > 0)
+						map[STDDEV][sender][receiver] = Math.sqrt(Math.abs((map[STDDEV][sender][receiver]/map[COUNT][sender][receiver])-(map[MEAN][sender][receiver]*map[MEAN][sender][receiver])));
+					else
+						map[STDDEV][sender][receiver] = 0;
+
+					max[COUNT] = Math.max(max[COUNT], map[COUNT][sender][receiver]);
+					max[MAX] = Math.max(max[MAX], map[MAX][sender][receiver]);
+					max[MIN] = Math.max(max[MIN], map[MIN][sender][receiver]);
+					max[MEAN] = Math.max(max[MEAN], map[MEAN][sender][receiver]);
+					max[STDDEV] = Math.max(max[STDDEV], map[STDDEV][sender][receiver]);
+					max[VOLUME] = Math.max(max[VOLUME], map[VOLUME][sender][receiver]);
+
+					if (map[COUNT][sender][receiver] > 0.0) {
+						min[COUNT] = (min[COUNT] == 0.0) ? map[COUNT][sender][receiver] : Math.min(min[COUNT], map[COUNT][sender][receiver]);
+						min[MAX] = (min[MAX] == 0.0) ? map[MAX][sender][receiver] : Math.min(min[MAX], map[MAX][sender][receiver]);
+						min[MIN] = (min[MIN] == 0.0) ? map[MIN][sender][receiver] : Math.min(min[MIN], map[MIN][sender][receiver]);
+						min[MEAN] = (min[MEAN] == 0.0) ? map[MEAN][sender][receiver] : Math.min(min[MEAN], map[MEAN][sender][receiver]);
+						min[STDDEV] = (min[STDDEV] == 0.0) ? map[STDDEV][sender][receiver] : Math.min(min[STDDEV], map[STDDEV][sender][receiver]);
+						min[VOLUME] = (min[VOLUME] == 0.0) ? map[VOLUME][sender][receiver] : Math.min(min[VOLUME], map[VOLUME][sender][receiver]);
+					}
+				}
+			}
+			maps.put(key, map);
+			maxs.put(key, max);
+			mins.put(key, min);
+		}
+	}
+
 	private void extractData(PerformanceResult input, Integer thread, String event, String first, String path) {
 		double numEvents, eventMax, eventMin, eventMean, eventSumSqr, stdev, volume = 0;
 		double[][][] map = new double[6][size][size];
 		double[] max = {0,0,0,0,0,0}; 
-//		double[] min = {Double.MAX_VALUE,Double.MAX_VALUE,Double.MAX_VALUE,Double.MAX_VALUE,Double.MAX_VALUE}; 
 		double[] min = {0,0,0,0,0,0}; 
 		
 		if (maps.keySet().contains(path)) {
 			map = maps.get(path);
-			max = maxs.get(path);
-			min = mins.get(path);
+//			max = maxs.get(path);
+//			min = mins.get(path);
 		} else {
 			maps.put(path, map);
-			maxs.put(path, max);
-			mins.put(path, min);
+//			maxs.put(path, max);
+//			mins.put(path, min);
 		}
 
 		StringTokenizer st = new StringTokenizer(first, "Message size sent to node ");
 		if (st.hasMoreTokens()) {
 			int receiver = Integer.parseInt(st.nextToken());
-			
+
 			numEvents = input.getUsereventNumevents(thread, event);
-			map[0][thread][receiver] = numEvents;
-			max[0] = max[0] < numEvents ? numEvents : max[0];
-			min[0] = numEvents > 0 && min[0] > numEvents ? numEvents : min[0];
+			map[COUNT][thread][receiver] += numEvents;
+//			max[0] = max[0] < numEvents ? numEvents : max[0];
+//			min[0] = numEvents > 0 && min[0] > numEvents ? numEvents : min[0];
 			
 			eventMax = input.getUsereventMax(thread, event);
-			map[1][thread][receiver] = eventMax;
-			max[1] = max[1] < eventMax ? eventMax : max[1];
-			min[1] = eventMax > 0 && min[1] > eventMax ? eventMax : min[1];
+			map[MAX][thread][receiver] = Math.max(eventMax, map[1][thread][receiver]);
+//			max[1] = max[1] < eventMax ? eventMax : max[1];
+//			min[1] = eventMax > 0 && min[1] > eventMax ? eventMax : min[1];
 			
 			eventMin = input.getUsereventMin(thread, event);
-			map[2][thread][receiver] = eventMin;
-			max[2] = max[2] < eventMin ? eventMin : max[2];
-			min[2] = eventMin > 0 && min[2] > eventMin ? eventMin : min[2];
+			if (map[MIN][thread][receiver] > 0) {
+				map[MIN][thread][receiver] = Math.min(map[MIN][thread][receiver],eventMin);
+			} else {
+				map[MIN][thread][receiver] = eventMin;
+			}
+//			max[2] = max[2] < eventMin ? eventMin : max[2];
+//			min[2] = eventMin > 0 && min[2] > eventMin ? eventMin : min[2];
 			
+			// we'll recompute this later.
 			eventMean = input.getUsereventMean(thread, event);
-			map[3][thread][receiver] = eventMean;
-			max[3] = max[3] < eventMean ? eventMean : max[3];
-			min[3] = eventMean > 0 && min[3] > eventMean ? eventMean : min[3];
+			map[MIN][thread][receiver] += eventMean;
+//			max[3] = max[3] < eventMean ? eventMean : max[3];
+//			min[3] = eventMean > 0 && min[3] > eventMean ? eventMean : min[3];
 			
 			eventSumSqr = input.getUsereventSumsqr(thread, event);
-			if (numEvents > 0)
-				stdev = Math.sqrt(Math.abs((eventSumSqr/numEvents)-(eventMean*eventMean)));
-			else
-				stdev = 0;
-			map[4][thread][receiver] = stdev;
-			max[4] = max[4] < stdev ? stdev : max[4];
-			min[4] = stdev > 0 && min[4] > stdev ? stdev : min[4];
+//			if (numEvents > 0)
+//				stdev = Math.sqrt(Math.abs((eventSumSqr/numEvents)-(eventMean*eventMean)));
+//			else
+//				stdev = 0;
+			map[STDDEV][thread][receiver] += eventSumSqr;
+//			max[4] = max[4] < stdev ? stdev : max[4];
+//			min[4] = stdev > 0 && min[4] > stdev ? stdev : min[4];
 			
 			volume = numEvents * eventMean;
-			map[5][thread][receiver] += volume;
-			max[5] = max[5] < volume ? volume : max[5];
-			min[5] = volume > 0 && min[5] > volume ? volume : min[5];
+			map[VOLUME][thread][receiver] += volume;
+//			max[5] = max[5] < volume ? volume : max[5];
+//			min[5] = volume > 0 && min[5] > volume ? volume : min[5];
 		}
 	}
 
