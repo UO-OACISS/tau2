a5e33a7effa20d87f3dc3bc15b644cb6916bbb55
khuck <khuck@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2009-04-05 22:50:32 +0000

    forgot to check it in
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@7078 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ tools/src/perfexplorer/src/client/CommunicationMatrix.java
@@ -0,0 +1,29 @@
+
+package edu.uoregon.tau.perfexplorer.client;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import javax.swing.JFrame;
+import java.lang.Math;
+import java.text.DecimalFormat;
+import java.text.FieldPosition;
+import java.net.URL;
+import edu.uoregon.tau.common.Utility;
+import edu.uoregon.tau.perfexplorer.common.*;
+
+import java.awt.Toolkit;
+
+public class CommunicationMatrix {
+
+	public CommunicationMatrix() {
+		super();
+	}
+
+	public static JFrame doCommunicationMatrix() {
+		System.out.println("not implemented yet");
+		return null;
+    }
+
+}
