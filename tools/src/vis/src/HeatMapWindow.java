b4c28962dd2a9a5237e7f510e98e93e1cb6ea649
khuck <khuck@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2009-04-07 18:33:24 +0000

    Minor change - remove setvisible call.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@7094 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- tools/src/vis/src/HeatMapWindow.java
+++ tools/src/vis/src/HeatMapWindow.java
@@ -97,7 +97,6 @@ public class HeatMapWindow extends JFrame implements ActionListener{
         }
 
 		this.pack();
-		this.setVisible(true);
 	}
 
 	private Component buildOptionPanel(String label) {
