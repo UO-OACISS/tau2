9d110060c7b97063d85c79144432e1d4ee3151ce
khuck <khuck@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2009-04-14 20:24:00 +0000

    Updated to use new sparse matrix method.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@7129 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- tools/src/paraprof/src/CommunicationMatrixWindow.java
+++ tools/src/paraprof/src/CommunicationMatrixWindow.java
@@ -16,13 +16,12 @@ import edu.uoregon.tau.common.Utility;
 import edu.uoregon.tau.paraprof.interfaces.ParaProfWindow;
 import edu.uoregon.tau.perfdmf.DataSource;
 import edu.uoregon.tau.perfdmf.UserEventProfile;
+import edu.uoregon.tau.vis.HeatMapData;
 import edu.uoregon.tau.vis.HeatMapWindow;
 
 public class CommunicationMatrixWindow implements ParaProfWindow, Printable {
 
-    private Map/*<String, double[][][]>*/maps = new TreeMap/*<String, double[][][]>*/();
-    private Map/*<String, double[]>*/maxs = new TreeMap/*<String, double[]>*/();
-    private Map/*<String, double[]>*/mins = new TreeMap/*<String, double[]>*/();
+    private HeatMapData mapData = null;
     private int size = 0;
     private final static String allPaths = "All Paths";
     private static final int COUNT = 0;
@@ -67,6 +66,9 @@ public class CommunicationMatrixWindow implements ParaProfWindow, Printable {
         boolean foundData = false;
         int threadID = 0;
         size = dataSource.getAllThreads().size();
+	    // declare the heatmap data object
+	    mapData = new HeatMapData(size);
+
         for (Iterator it = dataSource.getAllThreads().iterator(); it.hasNext();) {
             edu.uoregon.tau.perfdmf.Thread thread = (edu.uoregon.tau.perfdmf.Thread) it.next();
             for (Iterator it2 = thread.getUserEventProfiles(); it2.hasNext();) {
@@ -84,18 +86,11 @@ public class CommunicationMatrixWindow implements ParaProfWindow, Printable {
                         String path = st.nextToken().trim();
                         // now, split up the path, and handle each node 
                         StringTokenizer st2 = new StringTokenizer(path, "=>");
-                        StringBuffer sb = new StringBuffer();
                         String tmp = null;
                         while (st2.hasMoreTokens()) {
-                            if (sb.length() > 0) {
-                                sb.append(" => ");
-                            }
                             tmp = st2.nextToken().trim();
-                            sb.append(tmp);
                             extractData(uep, threadID, event, first, tmp);
                         }
-                        // do this to get "* => MPI_Isend()" and all equivalents
-                        //					extractData(input, thread, event, first, "* => "+tmp);
                     }
                 }
             }
@@ -108,8 +103,8 @@ public class CommunicationMatrixWindow implements ParaProfWindow, Printable {
                     "No Communication Matrix Data", JOptionPane.ERROR_MESSAGE);
             return null;
         }
-        massageData();
-        window = new HeatMapWindow("Message Size Heat Maps", maps, maxs, mins, size);
+        mapData.massageData();
+		window = new HeatMapWindow("Message Size Heat Maps", mapData);
         URL url = Utility.getResource("tau32x32.gif");
         if (url != null) {
             window.setIconImage(Toolkit.getDefaultToolkit().getImage(url));
@@ -118,94 +113,44 @@ public class CommunicationMatrixWindow implements ParaProfWindow, Printable {
         return window;
     }
 
-    private void massageData() {
-        for (Iterator it = maps.keySet().iterator(); it.hasNext();) {
-            String key = (String) it.next();
-            double[][][] map = (double[][][]) maps.get(key);
-            double[] max = { 0, 0, 0, 0, 0, 0 };
-            double[] min = { 0, 0, 0, 0, 0, 0 };
-            for (int sender = 0; sender < size; sender++) {
-                for (int receiver = 0; receiver < size; receiver++) {
-
-                    // count and volume are fine... we need to re-compute the mean
-                    if (map[COUNT][sender][receiver] > 0) {
-                        map[MEAN][sender][receiver] = map[VOLUME][sender][receiver] / map[COUNT][sender][receiver];
-                    } else {
-                        map[MEAN][sender][receiver] = 0;
-                    }
-
-                    // compute stddev
-                    if (map[COUNT][sender][receiver] > 0)
-                        map[STDDEV][sender][receiver] = Math.sqrt(Math.abs((map[STDDEV][sender][receiver] / map[COUNT][sender][receiver])
-                                - (map[MEAN][sender][receiver] * map[MEAN][sender][receiver])));
-                    else
-                        map[STDDEV][sender][receiver] = 0;
-
-                    max[COUNT] = Math.max(max[COUNT], map[COUNT][sender][receiver]);
-                    max[MAX] = Math.max(max[MAX], map[MAX][sender][receiver]);
-                    max[MIN] = Math.max(max[MIN], map[MIN][sender][receiver]);
-                    max[MEAN] = Math.max(max[MEAN], map[MEAN][sender][receiver]);
-                    max[STDDEV] = Math.max(max[STDDEV], map[STDDEV][sender][receiver]);
-                    max[VOLUME] = Math.max(max[VOLUME], map[VOLUME][sender][receiver]);
-
-                    if (map[COUNT][sender][receiver] > 0.0) {
-                        min[COUNT] = (min[COUNT] == 0.0) ? map[COUNT][sender][receiver] : Math.min(min[COUNT],
-                                map[COUNT][sender][receiver]);
-                        min[MAX] = (min[MAX] == 0.0) ? map[MAX][sender][receiver]
-                                : Math.min(min[MAX], map[MAX][sender][receiver]);
-                        min[MIN] = (min[MIN] == 0.0) ? map[MIN][sender][receiver]
-                                : Math.min(min[MIN], map[MIN][sender][receiver]);
-                        min[MEAN] = (min[MEAN] == 0.0) ? map[MEAN][sender][receiver] : Math.min(min[MEAN],
-                                map[MEAN][sender][receiver]);
-                        min[STDDEV] = (min[STDDEV] == 0.0) ? map[STDDEV][sender][receiver] : Math.min(min[STDDEV],
-                                map[STDDEV][sender][receiver]);
-                        min[VOLUME] = (min[VOLUME] == 0.0) ? map[VOLUME][sender][receiver] : Math.min(min[VOLUME],
-                                map[VOLUME][sender][receiver]);
-                    }
-                }
-            }
-            maps.put(key, map);
-            maxs.put(key, max);
-            mins.put(key, min);
-        }
-    }
-
+    
     private void extractData(UserEventProfile uep, int thread, String event, String first, String path) {
         double numEvents, eventMax, eventMin, eventMean, eventSumSqr, stdev, volume = 0;
-        double[][][] map = new double[6][size][size];
+		double[] empty = {0,0,0,0,0,0};
 
-        if (maps.keySet().contains(path)) {
-            map = (double[][][]) maps.get(path);
-        } else {
-            maps.put(path, map);
-        }
-
-        StringTokenizer st = new StringTokenizer(first, "Message size sent to node ");
+		StringTokenizer st = new StringTokenizer(first, "Message size sent to node ");
         if (st.hasMoreTokens()) {
             int receiver = Integer.parseInt(st.nextToken());
 
+			double[] pointData = mapData.get(thread, receiver, path);
+			if (pointData == null) {
+				pointData = empty;
+			}
+
             numEvents = uep.getNumSamples();
-            map[COUNT][thread][receiver] += numEvents;
+			pointData[COUNT] += numEvents;
 
             eventMax = uep.getMaxValue();
-            map[MAX][thread][receiver] = Math.max(eventMax, map[1][thread][receiver]);
+			pointData[MAX] = Math.max(eventMax, pointData[MAX]);
 
             eventMin = uep.getMinValue();
-            if (map[MIN][thread][receiver] > 0) {
-                map[MIN][thread][receiver] = Math.min(map[MIN][thread][receiver], eventMin);
-            } else {
-                map[MIN][thread][receiver] = eventMin;
-            }
-
+			if (pointData[MIN] > 0) {
+				pointData[MIN] = Math.min(pointData[MIN],eventMin);
+			} else {
+				pointData[MIN] = eventMin;
+			}
+            
             // we'll recompute this later.
             eventMean = uep.getMeanValue();
-            map[MEAN][thread][receiver] += eventMean;
+			pointData[MEAN] += eventMean;
 
+            // we'll recompute this later.
             eventSumSqr = uep.getStdDev();
-            map[STDDEV][thread][receiver] += eventSumSqr;
+			pointData[STDDEV] += eventSumSqr;
 
             volume = numEvents * eventMean;
-            map[VOLUME][thread][receiver] += volume;
+			pointData[VOLUME] += volume;
+			mapData.put(thread, receiver, path, pointData);
         }
     }
 
