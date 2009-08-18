d0db48176123a2ef4eedd83a4e6a9c32a5b38622
khuck <khuck@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2009-08-18 21:05:47 +0000

    Code I forgot to check in...  these files are responsible for loading profiles into PerfExplorer.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@7369 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ tools/src/perfexplorer/src/client/LoadTrialProgressWindow.java
@@ -0,0 +1,265 @@
+/*
+ * LoadTrialWindow.java
+ * 
+ * Title: ParaProf 
+ * Author: Robert Bell 
+ * Description:
+ */
+
+package edu.uoregon.tau.perfexplorer.client;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URL;
+
+import javax.swing.*;
+import javax.swing.tree.TreeNode;
+import javax.swing.tree.TreePath;
+
+import edu.uoregon.tau.common.Utility;
+import edu.uoregon.tau.perfdmf.DataSource;
+import edu.uoregon.tau.perfdmf.DatabaseAPI;
+import edu.uoregon.tau.perfdmf.DatabaseException;
+import edu.uoregon.tau.perfdmf.Trial;
+import edu.uoregon.tau.perfexplorer.server.PerfExplorerServer;
+
+public class LoadTrialProgressWindow extends JFrame implements ActionListener {
+
+    private PerfExplorerClient mainWindow = null;
+    private JProgressBar progressBar = null;
+    private DataSource dataSource = null;
+    private Trial ppTrial = null;
+    private boolean aborted = false;
+    private JLabel label;
+    private boolean dbUpload = false;
+    private javax.swing.Timer jTimer;
+
+    public LoadTrialProgressWindow(PerfExplorerClient mainWindow, final DataSource dataSource,
+            final Trial ppTrial, boolean justDB) {
+
+        this.dataSource = dataSource;
+        this.ppTrial = ppTrial;
+        this.mainWindow = mainWindow;
+
+        //Window Stuff.
+        int windowWidth = 300;
+        int windowHeight = 120;
+
+        //Grab mainWindow position and size.
+        Point parentPosition = mainWindow.getLocationOnScreen();
+        Dimension parentSize = mainWindow.getSize();
+        int parentWidth = parentSize.width;
+        int parentHeight = parentSize.height;
+
+        //Set the window to come up in the center of the screen.
+        int xPosition = (parentWidth - windowWidth) / 2;
+        int yPosition = (parentHeight - windowHeight) / 2;
+
+        xPosition = (int) parentPosition.getX() + xPosition;
+        yPosition = (int) parentPosition.getY() + yPosition;
+
+        this.setLocation(xPosition, yPosition);
+        setSize(new java.awt.Dimension(windowWidth, windowHeight));
+        setTitle("TAU: ParaProf: Loading...");
+    	URL url = Utility.getResource("tau32x32.gif");
+    	if (url != null)
+    		setIconImage(Toolkit.getDefaultToolkit().getImage(url));
+
+        //Add some window listener code
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowClosing(java.awt.event.WindowEvent evt) {
+                thisWindowClosing(evt);
+            }
+        });
+
+        Container contentPane = getContentPane();
+        GridBagLayout gbl = new GridBagLayout();
+        contentPane.setLayout(gbl);
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(2, 2, 2, 2);
+
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.weightx = 0;
+        gbc.weighty = 0;
+        label = new JLabel("Loading Profile Data...");
+        addCompItem(label, gbc, 0, 0, 1, 1);
+
+        progressBar = new JProgressBar(0, 100);
+
+        progressBar.setPreferredSize(new Dimension(200, 14));
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+
+        addCompItem(progressBar, gbc, 0, 1, 1, 1);
+
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.anchor = GridBagConstraints.EAST;
+        gbc.weightx = 0;
+        gbc.weighty = 0;
+
+        JButton jButton = new JButton("Cancel");
+        jButton.addActionListener(this);
+        addCompItem(jButton, gbc, 0, 3, 1, 1);
+
+        jTimer = new javax.swing.Timer(200, this);
+        jTimer.start();
+
+        execute(justDB);
+    }
+
+    private synchronized void setDBUpload(boolean dbUpload) {
+        this.dbUpload = dbUpload;
+    }
+
+    private synchronized boolean getDBUpload() {
+        return dbUpload;
+    }
+
+    private void upload() throws DatabaseException {
+        label.setText("Uploading Trial");
+        progressBar.setValue(0);
+        setDBUpload(true);
+
+        DatabaseAPI dbAPI = PerfExplorerServer.getServer().getSession();
+        if (dbAPI != null) {
+            // this call will block until the entire thing is uploaded (could be a while)
+            ppTrial.setID(dbAPI.uploadTrial(ppTrial));
+            // get the root of the tree
+            PerfExplorerJTree.getTree().expandAll(true);
+        }
+    }
+
+    public synchronized void waitForLoad() {
+        try {
+            this.wait();
+        } catch (InterruptedException ie) {
+            // ?
+        }
+    }
+
+    public synchronized void finishLoad() {
+        this.notifyAll();
+    }
+
+    private synchronized void execute(final boolean justDB) {
+
+        java.lang.Thread thread = new java.lang.Thread(new Runnable() {
+
+            public void run() {
+                try {
+
+                    if (justDB) {
+                        upload();
+                        jTimer.stop();
+                        finishLoad();
+                        LoadTrialProgressWindow.this.dispose();
+                    } else {
+
+                        dataSource.load();
+                        if (!aborted) {
+//                            progressBar.setValue(100);
+                            //ppTrial.update(dataSource);
+
+                                upload();
+                        }
+                        ppTrial = null;
+                        jTimer.stop();
+                        LoadTrialProgressWindow.this.dispose();
+                    }
+                } catch (final Exception e) {
+                    LoadTrialProgressWindow.this.dispose();
+                    System.err.print(e.getMessage());
+                    e.printStackTrace(System.err);
+                }
+            }
+
+        });
+        thread.start();
+
+    }
+
+    public void updateProgress() {
+        if (getDBUpload()) {
+            // we are on the db upload phase
+            DatabaseAPI dbAPI = PerfExplorerServer.getServer().getSession();
+            if (dbAPI != null) { // it may not be set yet
+                int progress = dbAPI.getProgress();
+                progressBar.setValue(progress);
+            }
+
+        } else {
+            int progress = (int) dataSource.getProgress();
+            if (progress > 100)
+                progress = 100;
+            progressBar.setValue(progress);
+        }
+    }
+
+    public void actionPerformed(ActionEvent evt) {
+        try {
+            Object EventSrc = evt.getSource();
+            if (EventSrc instanceof javax.swing.Timer) {
+                // the timer has ticked, get progress and post
+
+                updateProgress();
+            } else {
+                String arg = evt.getActionCommand();
+
+                if (arg.equals("Cancel")) {
+                    aborted = true;
+                    if (dbUpload) {
+                        DatabaseAPI dbAPI = PerfExplorerServer.getServer().getSession();
+                        dbAPI.cancelUpload();
+                    } else {
+                        dataSource.cancelLoad();
+                    }
+                }
+            }
+        } catch (Exception e) {
+        	System.err.println(e.getMessage());
+        	e.printStackTrace();
+        }
+    }
+
+    public void finishDatabase(boolean success) {
+
+        if (success && !aborted) {
+            progressBar.setValue(100);
+        }
+        jTimer.stop();
+
+        this.dispose();
+    }
+
+    private void addCompItem(Component c, GridBagConstraints gbc, int x, int y, int w, int h) {
+        gbc.gridx = x;
+        gbc.gridy = y;
+        gbc.gridwidth = w;
+        gbc.gridheight = h;
+        getContentPane().add(c, gbc);
+    }
+
+    //Close the window when the close box is clicked
+    private void thisWindowClosing(java.awt.event.WindowEvent e) {
+        closeThisWindow();
+    }
+
+    void closeThisWindow() {
+        try {
+            aborted = true;
+            if (dbUpload) {
+                DatabaseAPI dbAPI = PerfExplorerServer.getServer().getSession();
+                dbAPI.cancelUpload();
+            } else {
+                dataSource.cancelLoad();
+            }
+            jTimer.stop();
+            this.setVisible(false);
+        } catch (Exception e) {
+            // do nothing
+        }
+        dispose();
+    }
+
+}
---
+++ tools/src/perfexplorer/src/client/LoadTrialWindow.java
@@ -0,0 +1,333 @@
+package edu.uoregon.tau.perfexplorer.client;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.net.URL;
+
+import javax.swing.*;
+
+import edu.uoregon.tau.common.Utility;
+import edu.uoregon.tau.perfdmf.*;
+
+/**
+ * A window that lets the user select a profile format and launch a JFileChooser
+ * 
+ * <P>CVS $Id: LoadTrialWindow.java,v 1.1 2009/08/18 21:05:47 khuck Exp $</P>
+ * @author  Robert Bell, Alan Morris
+ * @version $Revision: 1.1 $
+ */
+public class LoadTrialWindow extends JFrame implements ActionListener {
+
+    private static int defaultIndex;
+
+    static String lastDirectory;
+
+    private PerfExplorerClient mainWindow = null;
+    private Application application = null;
+    private Experiment experiment = null;
+    private boolean newExperiment;
+    private boolean newApplication;
+
+    private JTextField dirLocationField = new JTextField(lastDirectory, 30);
+    private JComboBox trialTypes = null;
+    private File selectedFiles[];
+    private JButton selectButton = null;
+    private PerfExplorerActionListener listener = null;
+
+    private JCheckBox monitorTrialCheckBox = new JCheckBox("Monitor Trial");
+
+    public LoadTrialWindow(PerfExplorerClient mainWindow, PerfExplorerActionListener listener, Application application, Experiment experiment,
+            boolean newApplication, boolean newExperiment) {
+        this.mainWindow = mainWindow;
+        this.listener = listener;
+        this.application = application;
+        this.experiment = experiment;
+        this.newApplication = newApplication;
+        this.newExperiment = newExperiment;
+
+        if (lastDirectory == null) {
+            lastDirectory = System.getProperty("user.dir");
+            dirLocationField.setText(lastDirectory);
+        }
+
+        //Window Stuff.
+        int windowWidth = 400;
+        int windowHeight = 200;
+
+        //Grab paraProfManager position and size.
+        Point parentPosition = mainWindow.getLocationOnScreen();
+        Dimension parentSize = mainWindow.getSize();
+        int parentWidth = parentSize.width;
+        int parentHeight = parentSize.height;
+
+        //Set the window to come up in the center of the screen.
+        int xPosition = (parentWidth - windowWidth) / 2;
+        int yPosition = (parentHeight - windowHeight) / 2;
+
+        xPosition = (int) parentPosition.getX() + xPosition;
+        yPosition = (int) parentPosition.getY() + yPosition;
+
+        this.setLocation(xPosition, yPosition);
+        setSize(new java.awt.Dimension(windowWidth, windowHeight));
+        setTitle("TAU: ParaProf: Load Trial");
+    	URL url = Utility.getResource("tau32x32.gif");
+    	if (url != null)
+    		setIconImage(Toolkit.getDefaultToolkit().getImage(url));
+
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowClosing(java.awt.event.WindowEvent evt) {
+                thisWindowClosing(evt);
+            }
+        });
+
+        selectButton = new JButton("Select Directory");
+        selectButton.addActionListener(this);
+
+        trialTypes = new JComboBox(DataSource.formatTypeStrings);
+        trialTypes.setMaximumRowCount(DataSource.formatTypeStrings.length);
+        trialTypes.addActionListener(this);
+        // must be after action listener
+        trialTypes.setSelectedIndex(defaultIndex);
+
+        Container contentPane = getContentPane();
+        GridBagLayout gbl = new GridBagLayout();
+        contentPane.setLayout(gbl);
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.anchor = GridBagConstraints.EAST;
+        gbc.weightx = 0;
+        gbc.weighty = 0;
+        addCompItem(new JLabel("Trial Type"), gbc, 0, 0, 1, 1);
+
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.weightx = 100;
+        gbc.weighty = 0;
+        addCompItem(trialTypes, gbc, 1, 0, 1, 1);
+
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.anchor = GridBagConstraints.EAST;
+        gbc.weightx = 0;
+        gbc.weighty = 0;
+        addCompItem(selectButton, gbc, 0, 1, 1, 1);
+
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.weightx = 100;
+        gbc.weighty = 0;
+        addCompItem(dirLocationField, gbc, 1, 1, 2, 1);
+
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.anchor = GridBagConstraints.EAST;
+        gbc.weightx = 0;
+        gbc.weighty = 0;
+
+/*        if (!experiment.dBExperiment()) {
+            monitorTrialCheckBox.addActionListener(this);
+            gbc.fill = GridBagConstraints.BOTH;
+            gbc.anchor = GridBagConstraints.CENTER;
+
+            addCompItem(monitorTrialCheckBox, gbc, 1, 2, 1, 1);
+
+            gbc.fill = GridBagConstraints.NONE;
+            gbc.anchor = GridBagConstraints.EAST;
+
+            JButton jButton = new JButton("Cancel");
+            jButton.addActionListener(this);
+            addCompItem(jButton, gbc, 0, 3, 1, 1);
+
+            jButton = new JButton("Ok");
+            jButton.addActionListener(this);
+            addCompItem(jButton, gbc, 2, 3, 1, 1);
+        } else {
+*/
+            JButton jButton = new JButton("Cancel");
+            jButton.addActionListener(this);
+            addCompItem(jButton, gbc, 0, 2, 1, 1);
+
+            jButton = new JButton("Ok");
+            jButton.addActionListener(this);
+            addCompItem(jButton, gbc, 2, 2, 1, 1);
+
+//        }
+
+    }
+
+    public void actionPerformed(ActionEvent evt) {
+        try {
+            String arg = evt.getActionCommand();
+            if (arg.equals("Select Directory")) {
+                JFileChooser jFileChooser = new JFileChooser(lastDirectory);
+                jFileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                jFileChooser.setMultiSelectionEnabled(false);
+                jFileChooser.setDialogTitle("Select Directory");
+                jFileChooser.setApproveButtonText("Select");
+                if ((jFileChooser.showOpenDialog(this)) != JFileChooser.APPROVE_OPTION) {
+                    return;
+                }
+                lastDirectory = jFileChooser.getSelectedFile().getParent();
+                dirLocationField.setText(jFileChooser.getSelectedFile().getCanonicalPath());
+
+            } else if (arg.equals("  Select File(s)  ")) {
+                JFileChooser jFileChooser = new JFileChooser(lastDirectory);
+                jFileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+
+                if (trialTypes.getSelectedIndex() == DataSource.PPK || trialTypes.getSelectedIndex() == DataSource.MPIP
+                        || trialTypes.getSelectedIndex() == DataSource.PPROF || trialTypes.getSelectedIndex() == DataSource.CUBE) {
+                    // These formats are in a single file only
+                    jFileChooser.setMultiSelectionEnabled(false);
+                } else {
+                    // others may have multiple files
+                    jFileChooser.setMultiSelectionEnabled(true);
+                }
+                if (trialTypes.getSelectedIndex() == DataSource.PPK) {
+                    jFileChooser.setFileFilter(new FileFilter(FileFilter.PPK));
+                }
+                jFileChooser.setDialogTitle("Select File(s)");
+                jFileChooser.setApproveButtonText("Select");
+                if ((jFileChooser.showOpenDialog(this)) != JFileChooser.APPROVE_OPTION) {
+                    return;
+                }
+
+                selectedFiles = jFileChooser.getSelectedFiles();
+                lastDirectory = jFileChooser.getSelectedFile().getParent();
+                if (!jFileChooser.isMultiSelectionEnabled()) {
+                    selectedFiles = new File[1];
+                    selectedFiles[0] = jFileChooser.getSelectedFile();
+                }
+
+                if (selectedFiles.length > 1) {
+                    dirLocationField.setText("<Multiple Files Selected>");
+                    dirLocationField.setEditable(false);
+                } else {
+                    dirLocationField.setText(selectedFiles[0].toString());
+                    dirLocationField.setEditable(true);
+                }
+            } else if (arg.equals("Cancel")) {
+                // note that these are null if they're not top level (so this won't delete an application that has other experiments)
+
+                if (newExperiment) {
+                    listener.handleDelete(experiment);
+                }
+                if (newApplication) {
+                    listener.handleDelete(application);
+                }
+                closeThisWindow();
+            } else if (arg.equals("Ok")) {
+                if (trialTypes.getSelectedIndex() == 0) {
+                    File files[] = new File[1];
+                    files[0] = new File(dirLocationField.getText().trim());
+                    if (!files[0].exists()) {
+                        JOptionPane.showMessageDialog(this, dirLocationField.getText().trim() + " does not exist");
+                        return;
+                    }
+                    addTrial(application, experiment, files, trialTypes.getSelectedIndex(), false,
+                            monitorTrialCheckBox.isSelected());
+                } else {
+                    if (selectedFiles == null) {
+                        selectedFiles = new File[1];
+                        selectedFiles[0] = new File(dirLocationField.getText().trim());
+                        if (!selectedFiles[0].exists()) {
+                            JOptionPane.showMessageDialog(this, dirLocationField.getText().trim() + " does not exist");
+                            return;
+                        }
+                    }
+                    addTrial(application, experiment, selectedFiles, trialTypes.getSelectedIndex(), false,
+                            monitorTrialCheckBox.isSelected());
+                }
+                closeThisWindow();
+            } else if (arg.equals("comboBoxChanged")) {
+                if (trialTypes.getSelectedIndex() == DataSource.TAUPROFILE) {
+                    selectButton.setText("Select Directory");
+                    dirLocationField.setEditable(true);
+                    monitorTrialCheckBox.setEnabled(true);
+                } else {
+                    selectButton.setText("  Select File(s)  ");
+                    monitorTrialCheckBox.setSelected(false);
+                    monitorTrialCheckBox.setEnabled(false);
+                }
+            }
+        } catch (Exception e) {
+        	System.err.print(e.getMessage());
+        	e.printStackTrace(System.err);
+        }
+    }
+
+    private void addTrial(Application application, Experiment experiment, File files[], int fileType,
+            boolean fixGprofNames, boolean monitorProfiles) {
+		// TODO Auto-generated method stub
+        Trial ppTrial = null;
+        DataSource dataSource = null;
+
+        try {
+            dataSource = UtilFncs.initializeDataSource(files, fileType, fixGprofNames);
+            if (dataSource == null) {
+                throw new RuntimeException("Error creating dataSource!");
+            }
+            dataSource.setGenerateIntermediateCallPathData(true);
+        } catch (DataSourceException e) {
+            if (files == null || files.length != 0) {
+            	System.err.print(e.getMessage());
+            	e.printStackTrace();
+            }
+            return;
+        }
+
+        ppTrial = new Trial();
+        // this must be done before setting the monitored flag
+        ppTrial.setDataSource(dataSource);
+        //ppTrial.setLoading(true);
+        dataSource.setMonitored(monitorProfiles);
+        //ppTrial.setMonitored(monitorProfiles);
+
+        ppTrial.setApplicationID(experiment.getApplicationID());
+        ppTrial.setExperimentID(experiment.getID());
+//        if (files.length != 0) {
+//            ppTrial.setPaths(files[0].getPath());
+//        } else {
+//            ppTrial.setPaths(System.getProperty("user.dir"));
+//        }
+        if (files.length == 1) {
+            ppTrial.setName(files[0].toString());
+//            ppTrial.setPaths(files[0].toString());
+//        } else {
+//            ppTrial.setName(ppTrial.getPathReverse());
+        }
+//        if (experiment.dBExperiment()) {
+//            loadedDBTrials.add(ppTrial);
+//            ppTrial.setUpload(true); // This trial is not set to a db trial until after it has finished loading.
+//        } else {
+//            experiment.addTrial(ppTrial);
+//        }
+
+        LoadTrialProgressWindow lpw = new LoadTrialProgressWindow(PerfExplorerClient.getMainFrame(), dataSource, ppTrial, false);
+        lpw.show();		
+	}
+
+	private void addCompItem(Component c, GridBagConstraints gbc, int x, int y, int w, int h) {
+        gbc.gridx = x;
+        gbc.gridy = y;
+        gbc.gridwidth = w;
+        gbc.gridheight = h;
+        getContentPane().add(c, gbc);
+    }
+
+    //Close the window when the close box is clicked
+    private void thisWindowClosing(java.awt.event.WindowEvent e) {
+        closeThisWindow();
+    }
+
+    private void closeThisWindow() {
+        this.setVisible(false);
+        dispose();
+    }
+
+    public static void setDefaultIndex(int index) {
+        defaultIndex = index;
+    }
+
+}
