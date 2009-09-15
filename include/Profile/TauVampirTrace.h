888fe9c377bb4a17dc5d51585b18dfd08856bbc5
amorris <amorris@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2009-09-15 01:16:15 +0000

    1) Added checks for 5.7+ VampirTrace 2) Now use vtcc -vt:show and assorted commands to divine -I's and -L/-l's
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@7442 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- include/Profile/TauVampirTrace.h
+++ include/Profile/TauVampirTrace.h
@@ -24,23 +24,33 @@ extern "C" {
 void vt_open(void);
 void vt_close(void);
 
-uint32_t vt_def_region      ( const char* rname,
-                                     uint32_t fid,
-                                     uint32_t begln,
-                                     uint32_t endln,
-                                     const char* rdesc,
-                                     uint8_t rtype );
+#ifdef TAU_VAMPIRTRACE_5_7_API
+uint32_t vt_def_region(const char* rname,
+		       const char* rdesc,
+		       uint32_t fid,
+		       uint32_t begln,
+		       uint32_t endln,
+		       uint8_t rtype );
+#define TAU_VT_DEF_REGION(rname,fid,begln,endln,rdesc,rtype) vt_def_region (rname, rdesc, fid, begln, endln, rtype);  
+#else
+uint32_t vt_def_region(const char* rname,
+		       uint32_t fid,
+		       uint32_t begln,
+		       uint32_t endln,
+		       const char* rdesc,
+		       uint8_t rtype );
+#define TAU_VT_DEF_REGION(rname,fid,begln,endln,rdesc,rtype) vt_def_region (rname, fid, begln, endln, rdesc, rtype);  
+#endif
 
 void vt_enter(uint64_t* time, uint32_t rid);
-
+  
 void vt_exit(uint64_t* time);
 uint64_t vt_pform_wtime(void);
 
-uint32_t vt_def_counter_group ( const char* gname );
+uint32_t vt_def_counter_group(const char* gname );
 void vt_count(uint64_t* time, uint32_t cid, uint64_t cval);
-uint32_t vt_def_counter       (const char* cname, uint32_t cprop, uint32_t gid,
-                                      const char* cunit);
-
+uint32_t vt_def_counter(const char* cname, uint32_t cprop, uint32_t gid,
+			const char* cunit);
 
 #ifdef __cplusplus
 }
