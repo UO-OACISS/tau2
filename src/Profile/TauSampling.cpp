22f500539685ccb334062658ada25ef9bd509922
amorris <amorris@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2009-09-21 21:44:57 +0000

    Cleanup and comments.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@7466 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- src/Profile/TauSampling.cpp
+++ src/Profile/TauSampling.cpp
@@ -2,64 +2,61 @@
 **			TAU Portable Profiling Package			   **
 **			http://www.cs.uoregon.edu/research/tau	           **
 *****************************************************************************
-**    Copyright 2009  						   	   **
+**    Copyright 2009                                                       **
 **    Department of Computer and Information Science, University of Oregon **
 **    Advanced Computing Laboratory, Los Alamos National Laboratory        **
 ****************************************************************************/
 /****************************************************************************
-**	File 		: TauSampling.cpp  				   **
-**	Description 	: TAU Profiling Package				   **
-**	Contact		: tau-bugs@cs.uoregon.edu               	   **
+**	File            : TauSampling.cpp                                  **
+**	Description     : TAU Profiling Package				   **
+**	Contact		: tau-bugs@cs.uoregon.edu                          **
 **	Documentation	: See http://www.cs.uoregon.edu/research/tau       **
 **                                                                         **
 **      Description     : This file contains all the sampling related code **
 **                                                                         **
 ****************************************************************************/
 
-
 /****************************************************************************
- *
- *                      University of Illinois/NCSA
- *                          Open Source License
- *
- *          Copyright(C) 2004-2006, The Board of Trustees of the
- *              University of Illinois. All rights reserved.
- *
- *                             Developed by:
- *
- *                        The PerfSuite Project
- *            National Center for Supercomputing Applications
- *              University of Illinois at Urbana-Champaign
- *
- *                   http://perfsuite.ncsa.uiuc.edu/
- *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the "Software"),
- * to deal with the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * + Redistributions of source code must retain the above copyright notice,
- *   this list of conditions and the following disclaimers.
- * + Redistributions in binary form must reproduce the above copyright
- *   notice, this list of conditions and the following disclaimers in
- *   the documentation and/or other materials provided with the distribution.
- * + Neither the names of The PerfSuite Project, NCSA/University of Illinois
- *   at Urbana-Champaign, nor the names of its contributors may be used to
- *   endorse or promote products derived from this Software without specific
- *   prior written permission.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS WITH THE SOFTWARE.
- ****************************************************************************/
-
-
+*
+*                      University of Illinois/NCSA
+*                          Open Source License
+*
+*          Copyright(C) 2004-2006, The Board of Trustees of the
+*              University of Illinois. All rights reserved.
+*
+*                             Developed by:
+*
+*                        The PerfSuite Project
+*            National Center for Supercomputing Applications
+*              University of Illinois at Urbana-Champaign
+*
+*                   http://perfsuite.ncsa.uiuc.edu/
+*
+* Permission is hereby granted, free of charge, to any person obtaining
+* a copy of this software and associated documentation files (the "Software"),
+* to deal with the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+*
+* + Redistributions of source code must retain the above copyright notice,
+*   this list of conditions and the following disclaimers.
+* + Redistributions in binary form must reproduce the above copyright
+*   notice, this list of conditions and the following disclaimers in
+*   the documentation and/or other materials provided with the distribution.
+* + Neither the names of The PerfSuite Project, NCSA/University of Illinois
+*   at Urbana-Champaign, nor the names of its contributors may be used to
+*   endorse or promote products derived from this Software without specific
+*   prior written permission.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+* OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+* DEALINGS WITH THE SOFTWARE.
+****************************************************************************/
 
 #ifdef TAU_EXP_SAMPLING
 
@@ -72,68 +69,68 @@
 #include <TAU.h>
 #include <Profile/TauMetrics.h>
 
-
+/*********************************************************************
+ * Tau Sampling Record Definition
+ ********************************************************************/
 typedef struct {
-  int valid;
   caddr_t pc;
   x_uint64 timestamp;
   double counters[TAU_MAX_COUNTERS];
   x_uint64 deltaStart;
   x_uint64 deltaStop;
-  int outputCallpath;
 } TauSamplingRecord;
 
+/*********************************************************************
+ * Global Variables
+ ********************************************************************/
 
-
+/* The trace for this node, mulithreaded execution currently not supported */
 FILE *ebsTrace;
-TauSamplingRecord theRecord;
 
-
-#define USE_SIGCONTEXT 1
-
+/*********************************************************************
+ * Get the architecture specific PC
+ ********************************************************************/
 static inline caddr_t get_pc(void *p) {
-  struct ucontext *uc = (struct ucontext *) p;
+  struct ucontext *uc = (struct ucontext *)p;
   caddr_t pc;
   struct sigcontext *sc;
-  sc = (struct sigcontext *) &uc->uc_mcontext;
+  sc = (struct sigcontext *)&uc->uc_mcontext;
 # ifdef __x86_64__
-  pc = (caddr_t) sc->rip;
+  pc = (caddr_t)sc->rip;
 # elif IA32
-  pc = (caddr_t) sc->eip;
+  pc = (caddr_t)sc->eip;
 # elif __ia64__
-  pc = (caddr_t) sc->sc_ip;
+  pc = (caddr_t)sc->sc_ip;
 # else
 #  error "profile handler not defined for this architecture"
 # endif
-
   return pc;
 }
 
-
-
-
+/*********************************************************************
+ * Write out the TAU callpath
+ ********************************************************************/
 void Tau_sampling_output_callpath() {
   TAU_QUERY_DECLARE_EVENT(event);
   const char *str;
   TAU_QUERY_GET_CURRENT_EVENT(event);
   TAU_QUERY_GET_EVENT_NAME(event, str);
-  
-  
+
   int depth = TauEnv_get_callpath_depth();
   if (depth < 1) {
     depth = 1;
   }
-  
+
   while (str && depth > 0) {
     //    printf ("inside %s\n", str);
-    
-    Profiler *p = (Profiler*)event;
-    fprintf (ebsTrace, "%d", p->ThisFunction->GetFunctionId());
+
+    Profiler *p = (Profiler *)event;
+    fprintf(ebsTrace, "%d", p->ThisFunction->GetFunctionId());
     TAU_QUERY_GET_PARENT_EVENT(event);
     TAU_QUERY_GET_EVENT_NAME(event, str);
     if (str) {
       //fprintf (ebsTrace, " : ", str);
-      fprintf (ebsTrace, " ", str);
+      fprintf(ebsTrace, " ", str);
     }
     depth--;
   }
@@ -143,20 +140,19 @@ void Tau_sampling_output_callpath() {
  * Write out a single event record
  ********************************************************************/
 void Tau_sampling_flush_record(TauSamplingRecord *record) {
+  fprintf(ebsTrace, "$ | %lld | ", record->timestamp);
+  fprintf(ebsTrace, "%lld | ", record->deltaStart);
+  fprintf(ebsTrace, "%lld | ", record->deltaStop);
+  fprintf(ebsTrace, "%x | ", record->pc);
 
-  fprintf (ebsTrace, "$ | %lld | ", record->timestamp);
-  fprintf (ebsTrace, "%lld | ", record->deltaStart);
-  fprintf (ebsTrace, "%lld | ", record->deltaStop);
-  fprintf (ebsTrace, "%x | ", record->pc);
-
-  for (int i=0; i<Tau_Global_numCounters; i++) {
-    fprintf (ebsTrace, "%.16G ", record->counters[i]);
+  for (int i = 0; i < Tau_Global_numCounters; i++) {
+    fprintf(ebsTrace, "%.16G ", record->counters[i]);
   }
 
-  fprintf (ebsTrace, "| ");
-  
+  fprintf(ebsTrace, "| ");
+
   Tau_sampling_output_callpath();
-  fprintf (ebsTrace, "\n");
+  fprintf(ebsTrace, "\n");
 }
 
 /*********************************************************************
@@ -172,14 +168,13 @@ int Tau_sampling_event_stop(double stopTime) {
 
   double startTime = profiler->StartTime[0]; // gtod must be counter 0
 
-  x_uint64 start = (x_uint64) startTime;
-  x_uint64 stop = (x_uint64) stopTime;
+  x_uint64 start = (x_uint64)startTime;
+  x_uint64 stop = (x_uint64)stopTime;
 
-  fprintf (ebsTrace, "%% | %lld | %lld | ", start, stop);
-  theRecord.deltaStart = (x_uint64) startTime;
+  fprintf(ebsTrace, "%% | %lld | %lld | ", start, stop);
 
   Tau_sampling_output_callpath();
-  fprintf (ebsTrace, "\n");
+  fprintf(ebsTrace, "\n");
 
   return 0;
 }
@@ -188,6 +183,7 @@ int Tau_sampling_event_stop(double stopTime) {
  * Handler for itimer interrupt
  ********************************************************************/
 void Tau_sampling_handler(int signum, siginfo_t *si, void *p) {
+  TauSamplingRecord theRecord;
   int tid = RtsLayer::myThread();
   Profiler *profiler = TauInternal_CurrentProfiler(tid);
 
@@ -196,24 +192,21 @@ void Tau_sampling_handler(int signum, siginfo_t *si, void *p) {
 //   printf ("sample on %x\n", pc);
 
   struct timeval tp;
-  gettimeofday (&tp, 0);
+  gettimeofday(&tp, 0);
   x_uint64 timestamp = ((x_uint64)tp.tv_sec * (x_uint64)1e6 + (x_uint64)tp.tv_usec);
-
 
   theRecord.timestamp = timestamp;
   theRecord.pc = pc;
   theRecord.deltaStart = 0;
   theRecord.deltaStop = 0;
-  theRecord.outputCallpath = 0;
-  theRecord.valid = 1;
 
   double startTime = profiler->StartTime[0]; // gtod must be counter 0
-  theRecord.deltaStart = (x_uint64) startTime;
+  theRecord.deltaStart = (x_uint64)startTime;
   theRecord.deltaStop = 0;
 
   double values[TAU_MAX_COUNTERS];
   TauMetrics_getMetrics(tid, values);
-  for (int i=0; i<Tau_Global_numCounters; i++) {
+  for (int i = 0; i < Tau_Global_numCounters; i++) {
     theRecord.counters[i] = values[i];
   }
 
@@ -221,24 +214,24 @@ void Tau_sampling_handler(int signum, siginfo_t *si, void *p) {
 
   /* set this to get the stop event */
   profiler->needToRecordStop = 1;
-  
 }
 
-
-
+/*********************************************************************
+ * Output Format Header
+ ********************************************************************/
 int Tau_sampling_outputHeader() {
-  fprintf (ebsTrace, "# Format:\n");
-  fprintf (ebsTrace, "# <timestamp> | <delta-begin> | <delta-end> | <pc> | <metric 1> ... <metric N> | <tau callpath>\n");
-  fprintf (ebsTrace, "# Metrics:");
-  for (int i=0; i<Tau_Global_numCounters; i++) {
+  fprintf(ebsTrace, "# Format:\n");
+  fprintf(ebsTrace, "# <timestamp> | <delta-begin> | <delta-end> | <pc> | <metric 1> ... <metric N> | <tau callpath>\n");
+  fprintf(ebsTrace, "# Metrics:");
+  for (int i = 0; i < Tau_Global_numCounters; i++) {
     const char *name = TauMetrics_getMetricName(i);
-    fprintf (ebsTrace, " %s", name);
+    fprintf(ebsTrace, " %s", name);
   }
-  fprintf (ebsTrace, "\n");
+  fprintf(ebsTrace, "\n");
   char buffer[4096];
   bzero(buffer, 4096);
   int rc = readlink("/proc/self/exe", buffer, 4096);
-  fprintf (ebsTrace, "# exe: %s\n", buffer);
+  fprintf(ebsTrace, "# exe: %s\n", buffer);
 }
 
 /*********************************************************************
@@ -254,9 +247,6 @@ int Tau_sampling_init() {
   itval.it_interval.tv_usec = itval.it_value.tv_usec = 1000 % 1000000;
   itval.it_interval.tv_sec =  itval.it_value.tv_sec = 1000 / 1000000;
 
-  theRecord.valid = 0;
-  theRecord.outputCallpath = 0;
-
   const char *profiledir = TauEnv_get_profiledir();
 
   char filename[4096];
@@ -265,13 +255,11 @@ int Tau_sampling_init() {
   int node = RtsLayer::myNode();
 
   node = 0;
-  sprintf(filename,"%s/ebstrace.%d.%d.%d", profiledir, node, RtsLayer::myContext(), tid);
+  sprintf(filename, "%s/ebstrace.%d.%d.%d", profiledir, node, RtsLayer::myContext(), tid);
 
   ebsTrace = fopen(filename, "w");
-  
+
   Tau_sampling_outputHeader();
-
-
 
   struct sigaction act;
   memset(&act, 0, sizeof(struct sigaction));
@@ -281,11 +269,11 @@ int Tau_sampling_init() {
   ret = sigaction(SIGALRM, &act, NULL);
 
   ret = setitimer(ITIMER_REAL, &itval, 0);
-  if (ret != 0)  {
-    printf ("TAU: Sampling error: %s\n", strerror(ret));
+  if (ret != 0) {
+    printf("TAU: Sampling error: %s\n", strerror(ret));
     return 0;
   }
-  
+
   return 0;
 }
 
@@ -293,14 +281,13 @@ int Tau_sampling_init() {
  * Finalize the sampling trace system
  ********************************************************************/
 int Tau_sampling_finalize() {
-
   /* Disable sampling first */
   struct itimerval itval;
   int ret;
-  
+
   itval.it_interval.tv_usec = itval.it_value.tv_usec =
-    itval.it_interval.tv_sec = itval.it_value.tv_sec = 0;
-  
+                                itval.it_interval.tv_sec = itval.it_value.tv_sec = 0;
+
   ret = setitimer(ITIMER_REAL, &itval, 0);
   if (ret != 0) {
     /* ERROR */
@@ -311,22 +298,20 @@ int Tau_sampling_finalize() {
   int tid = RtsLayer::myThread();
   int node = RtsLayer::myNode();
   node = 0;
-  sprintf(filename,"%s/ebstracedef.%d.%d.%d", profiledir, node, RtsLayer::myContext(), tid);
+  sprintf(filename, "%s/ebstracedef.%d.%d.%d", profiledir, node, RtsLayer::myContext(), tid);
 
   FILE *def = fopen(filename, "w");
 
-  fprintf (def, "# Format:\n");
-  fprintf (def, "# <id> | <name>\n");
+  fprintf(def, "# Format:\n");
+  fprintf(def, "# <id> | <name>\n");
 
-  for (vector<FunctionInfo*>::iterator it = TheFunctionDB().begin(); it != TheFunctionDB().end(); it++) {
+  for (vector<FunctionInfo *>::iterator it = TheFunctionDB().begin(); it != TheFunctionDB().end(); it++) {
     FunctionInfo *fi = *it;
-    fprintf (def,"%d | %s %s\n", fi->GetFunctionId(), fi->GetName(), fi->GetType());
+    fprintf(def, "%d | %s %s\n", fi->GetFunctionId(), fi->GetName(), fi->GetType());
   }
   fclose(def);
 
   Tau_sampling_outputHeader();
-
 }
-
 
 #endif /* TAU_EXP_SAMPLING */
