6c657a0c59ba439b6f32f46316c8a4a06fcb43cf
amorris <amorris@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2009-09-22 22:42:36 +0000

    Restructured, now reads multiple files.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@7474 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- tools/src/tausamp_process.pl
+++ tools/src/tausamp_process.pl
@@ -4,100 +4,123 @@
 # This script reads an EBS trace and converts the PC's and TAU callpaths
 ########################################################################
 
+use strict;
+
 # Trim leading and trailing whitespace from a string
 sub trim($) {
-    my $string = shift;
-    $string =~ s/^\s+//;
-    $string =~ s/\s+$//;
-    return $string;
+  my $string = shift;
+  $string =~ s/^\s+//;
+  $string =~ s/\s+$//;
+  return $string;
 }
 
+sub process_trace {
+  my($def_file, $trace_file, $out_file) = @_;
 
-# Read event definitions
-my %eventmap;
-open (DEF, "<ebstracedef.0.0.0");
-while ($line = <DEF>) {
+  # Read event definitions
+  my %eventmap;
+  open (DEF, "<$def_file");
+  my ($line);
+  while ($line = <DEF>) {
     if ($line =~ /\#.*/) {
-	next;
+      next;
     }
-    ($id, $name) = split('\|',$line);
+    my ($id, $name) = split('\|',$line);
     $id = trim($id);
     $name = trim($name);
     $eventmap{$id} = $name;
-}
+  }
 
+  my ($lastCallpath);
+  my ($useDeltaStart);
+  my ($useDeltaStop);
 
-my ($lastCallpath);
-my ($useDeltaStart);
-my ($useDeltaStop);
+  my %startmap;
+  my %stopmap;
 
-my %startmap;
-my %stopmap;
-
-# Read the trace
-my ($exe);
-open (TRACE, "tac ebstrace.0.0.0 |");
-open (OUTPUT, "| tac > ebstrace.processed.0.0.0");
-while ($line = <TRACE>) {
+  # Read the trace
+  my ($junk, $exe, $node);
+  open (TRACE, "tac $trace_file |");
+  open (OUTPUT, "| tac > $out_file");
+  while ($line = <TRACE>) {
     if ($line =~ /\#.*/) {
-	if ($line =~ /\# exe:.*/) {
-	    ($junk, $exe) = split("exe:",$line);
-	    $exe = trim($exe);
-	} else {
-	    print (OUTPUT "$line");
-	}
-	next;
+      if ($line =~ /\# exe:.*/) {
+	($junk, $exe) = split("exe:",$line);
+	$exe = trim($exe);
+      } elsif ($line =~ /\# node:.*/) {
+	($junk, $node) = split("node:",$line);
+	$node = trim($node);
+      } else {
+	print (OUTPUT "$line");
+      }
+      next;
     } elsif ($line =~ /\%.*/) {
-	# process stop lines
+      # process stop lines
 
-	($type,$start,$stop,$callpath) = split('\|',$line);
-	$start = trim($start);
-	$stop = trim($stop);
-	$callpath = trim($callpath);
-	$callpath = reverse($callpath);
-	$startmap{$callpath} = $start;
-	$stopmap{$callpath} = $stop;
-    } else  {
-	# process sample lines
+      my ($type,$start,$stop,$callpath) = split('\|',$line);
+      $start = trim($start);
+      $stop = trim($stop);
+      $callpath = trim($callpath);
+      $startmap{$callpath} = $start;
+      $stopmap{$callpath} = $stop;
+    } else {
+      # process sample lines
 
-	($type,$timestamp,$deltaStart,$deltaStop,$pc,$metrics,$callpath) = split('\|',$line);
-	$timestamp = trim($timestamp);
-	$pc = trim($pc);
-	$metrics = trim($metrics);
-	$callpath = trim($callpath);
-	
-	# Process the callpath
-	$callpath = reverse($callpath);
-	@events = split(" ",$callpath);
-	$newCallpath = "";
-	my (@processedEvents);
-	foreach my $e (@events) {
-	    $newCallpath = "$newCallpath => $eventmap{$e}";
-	    push (@processedEvents, $eventmap{$e});
-	}
-	$newCallpath = join(" => ", @processedEvents);
-	$lastCallpath = $newCallpath;
+      my ($type,$timestamp,$deltaStart,$deltaStop,$pc,$metrics,$callpath) = split('\|',$line);
+      $timestamp = trim($timestamp);
+      $pc = trim($pc);
+      $metrics = trim($metrics);
+      $callpath = trim($callpath);
 
+      # Process the callpath
+      my @events = split(" ",$callpath);
+      @events = reverse (@events);
 
-	$check = $deltaStart;
-	$deltaStart = $timestamp - $startmap{$callpath};
-	$deltaStop = $stopmap{$callpath} - $timestamp;
+      my $newCallpath = "";
+      my (@processedEvents);
+      foreach my $e (@events) {
+	$newCallpath = "$newCallpath => $eventmap{$e}";
+	push (@processedEvents, $eventmap{$e});
+      }
+      $newCallpath = join(" => ", @processedEvents);
+      $lastCallpath = $newCallpath;
 
-	if ($check != $startmap{$callpath}) {
-	    die "inconsistent file, $check != $startmap{$callpath}\n";
-	}
+      my $check = $deltaStart;
+      $deltaStart = $timestamp - $startmap{$callpath};
+      $deltaStop = $stopmap{$callpath} - $timestamp;
 
-	# Process the PC
-	$out = `echo $pc | addr2line -e $exe`;
-	chomp($out);
-	$newpc = $out;
+#       if ($check != $startmap{$callpath}) {
+# 	die "inconsistent file, $check != $startmap{$callpath}\n";
+#       }
 
-	# Output the processed data
-	print OUTPUT "$timestamp | $deltaStart | $deltaStop | $newpc | $metrics | $newCallpath\n";
+      # Process the PC
+      my $out = `echo $pc | addr2line -e $exe`;
+      chomp($out);
+      my $newpc = $out;
+
+      # Output the processed data
+      print OUTPUT "$timestamp | $deltaStart | $deltaStop | $newpc | $metrics | $newCallpath\n";
     }
+  }
+  print OUTPUT "# node: $node\n";
 }
 
 
 
+sub main {
+
+  my $pattern = "ebstrace.raw.*.*.*.*";
+  while (defined(my $filename = glob($pattern))) {
+    my ($trace_file, $def_file, $out_file);
+    $trace_file = $filename;
+    my ($junk1, $junk2);
+    my ($junk1,$junk2,$pid,$nid,$cid,$tid) = split('\.',$filename);
+    $def_file = "ebstrace.def.$pid.$nid.$cid.$tid";
+    $out_file = "ebstrace.processed.$pid.$nid.$cid.$tid";
+    print "processing $filename ...\n";
+    process_trace($def_file, $trace_file, $out_file);
+  }
+}
 
 
+main
