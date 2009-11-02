7fbe8244b04e3c267e88240dc77e22e54cd05856
amorris <amorris@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2009-11-02 21:58:53 +0000

    Added script to attempt to determine the maximum allowed heap size.  Only works on Sun Java and probably only on some systems.  Where it doesn't work, we'll just revert to our previous behavior.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@7574 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ tools/src/tau_javamax.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# don't want the user to see glibc errors (on Franklin)
+export MALLOC_CHECK_=0
+
+# first, check that this is sun java
+
+ver=`java -version 2>&1 | tail -1 | awk '{print $1 $2}'`
+
+if [ "x$ver" != "xJavaHotSpot(TM)" ] ; then
+    echo "failed"
+    exit
+fi
+
+memtotal=`cat /proc/meminfo | head -1 | awk '{print $2}'`
+
+trymem=$(($memtotal/1000*7/8))
+
+while [ $trymem -gt 250 ] ; do
+
+    check=`java -Xmx${trymem}m foobar 2>&1 | head -2 | tail -1`
+    if [ "x$check" != "xCould not reserve enough space for object heap" ] ; then
+	echo "$trymem"
+	exit
+    fi
+
+    trymem=$(($trymem*3/4))
+done
+
+echo "failed"
