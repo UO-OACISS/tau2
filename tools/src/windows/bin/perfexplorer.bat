bc682c75bc201a3c299380a8e18285f980435d70
amorris <amorris@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2009-11-12 18:21:45 +0000

    Added the PE2 jars to the classpath for windows
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@7674 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- tools/src/windows/bin/perfexplorer.bat
+++ tools/src/windows/bin/perfexplorer.bat
@@ -6,6 +6,7 @@ set TAU_ROOT=..
 
 
 set JAR_ROOT=%TAU_ROOT%/bin
+set ETC=%TAU_ROOT%\etc
 set CONFIG_FILE=%USERPROFILE%/.ParaProf/perfdmf.cfg
 set PERFDMF_JAR=%JAR_ROOT%/perfdmf.jar
 set JARGS_JAR=%JAR_ROOT%/jargs.jar
@@ -13,6 +14,7 @@ set JDBC_JAR=%JAR_ROOT%/postgresql.jar;%JAR_ROOT%/mysql.jar;%JAR_ROOT%/oracle.ja
 set COMMON_JAR=%JAR_ROOT%/tau-common.jar
 set JARS=%JAR_ROOT%/paraprof.jar;%JAR_ROOT%/vis.jar;%PERFDMF_JAR%;%JAR_ROOT%/jogl.jar;%JAR_ROOT%/jgraph.jar;%JDBC_JAR%;%JAR_ROOT%/jargs.jar;%JAR_ROOT%/epsgraphics.jar;%JAR_ROOT%/batik-combined.jar;%JAR_ROOT%/tau-common.jar;%JAR_ROOT%/jfreechart-1.0.12.jar;%JAR_ROOT%/jcommon-1.0.15.jar
 
-set CLASSPATH=%JARS%;%JAR_ROOT%/perfexplorer.jar;%JAR_ROOT%/weka.jar
+set CLASSPATH=%JARS%;%JAR_ROOT%/perfexplorer.jar;%JAR_ROOT%/weka.jar;%JAR_ROOT%/antlr-2.7.6.jar;%JAR_ROOT%/commons-lang-2.1.jar;%JAR_ROOT%/drools-core-3.0.6.jar;%JAR_ROOT%/junit-3.8.1.jar;%JAR_ROOT%/antlr-3.0ea8.jar;%JAR_ROOT%/commons-logging-api-1.0.4.jar;%JAR_ROOT%/drools-decisiontables-3.0.6.jar;%JAR_ROOT%/jxl-2.4.2.jar;%JAR_ROOT%/commons-jci-core-1.0-406301.jar;%JAR_ROOT%/core-3.2.0.666.jar;%JAR_ROOT%/drools-jsr94-3.0.6.jar;%JAR_ROOT%/stringtemplate-2.3b6.jar;%JAR_ROOT%/commons-jci-eclipse-3.2.0.666.jar;%JAR_ROOT%/drools-compiler-3.0.6.jar;%JAR_ROOT%/jsr94-1.1.jar
 
-java -Xmx500m -classpath %CLASSPATH% edu.uoregon.tau.perfexplorer.client.PerfExplorerClient -w -s
+
+java -Xmx500m -classpath %CLASSPATH% edu.uoregon.tau.perfexplorer.client.PerfExplorerClient -t %JAR_ROOT% -a %ETC% -w -s
