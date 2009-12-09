ef4b0a3406f468b295bae92fe21439e704b1c517
wspear <wspear@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2009-12-09 22:00:06 +0000

    Updated jars to latest version.  Updated build scripts.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@7731 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- tools/src/jtau_tf/Build.sh
+++ tools/src/jtau_tf/Build.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 cp -r src/edu/ .
+rm -rf edu/CVS/
+rm -rf edu/uoregon/CVS/
+rm -rf edu/uoregon/tau/CVS/
+rm -rf edu/uoregon/tau/trace/CVS/
 javac edu/uoregon/tau/trace/*.java
+rm edu/uoregon/tau/trace/*.java
 jar -cf TAU_tf.jar edu/
 mv TAU_tf.jar bin/
 rm -rf edu/
--- tools/src/tau2slog2/Build.sh
+++ tools/src/tau2slog2/Build.sh
@@ -1,4 +1,8 @@
+#!/bin/sh
 cp -r src/edu/ .
+rm -rf edu/CVS/
+rm -rf edu/uoregon/CVS/
+rm -rf edu/uoregon/tau/CVS/
 javac -classpath ../jtau_tf/bin/TAU_tf.jar:../contrib/slog2sdk/lib/traceTOslog2.jar edu/uoregon/tau/*.java
 jar -cf tau2slog2.jar edu/
 mv tau2slog2.jar bin/
