46f497db2e87d9f73e05a6ab0f004beec189061c
amorris <amorris@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2009-12-12 07:18:25 +0000

    Added capability for uploading trials (TAU format so far):
    # <profile location> <database id> <app name> <exp name> <trial name>
    < control upload /home/amorris/foo 0 appname expname trialname
    > control return 42
    > control endreturn
    
    Added capability to open the main data window for a database trial:
    
    # <database id> <trial id>
    < control load 0 2
    
    
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@7742 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- tools/src/paraprof/src/ExternalController.java
+++ tools/src/paraprof/src/ExternalController.java
@@ -1,6 +1,7 @@
 package edu.uoregon.tau.paraprof;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStreamReader;
 import java.sql.SQLException;
 import java.util.Iterator;
@@ -40,7 +41,7 @@ public class ExternalController {
 
     }
 
-    static public void processCommand(String command) throws SQLException {
+    static public void processCommand(String command) throws Exception {
         System.out.println("processing command: " + command);
         if (command.equals("open manager")) {
             ParaProf.paraProfManagerWindow.setVisible(true);
@@ -52,9 +53,61 @@ public class ExternalController {
             listExperiments(command.substring("list experiments".length() + 1));
         } else if (command.startsWith("list trials")) {
             listTrials(command.substring("list trials".length() + 1));
+        } else if (command.startsWith("load")) {
+            loadDBTrial(command.substring("load".length() + 1));
+        } else if (command.startsWith("upload")) {
+            uploadTauTrial(command.substring("upload".length() + 1));
         } else if (command.equals("exit")) {
             exitController();
         }
+    }
+
+    static public void loadDBTrial(String command) throws Exception {
+        StringTokenizer tokenizer = new StringTokenizer(command, " ");
+        int dbID = Integer.parseInt(tokenizer.nextToken());
+        int trialID = Integer.parseInt(tokenizer.nextToken());
+
+        DatabaseAPI databaseAPI = new DatabaseAPI();
+        databaseAPI.initialize((Database) Database.getDatabases().get(dbID));
+        databaseAPI.setTrial(trialID, false);
+        DBDataSource dbDataSource = new DBDataSource(databaseAPI);
+        dbDataSource.load();
+        
+        Trial trial = new Trial();
+        trial.setDataSource(dbDataSource);
+        
+        ParaProfTrial ppTrial = new ParaProfTrial(trial);
+        ppTrial.finishLoad();
+        ppTrial.showMainWindow();
+    }
+
+    static public void uploadTauTrial(String command) throws Exception {
+        StringTokenizer tokenizer = new StringTokenizer(command, " ");
+
+        String location = tokenizer.nextToken();
+        int dbID = Integer.parseInt(tokenizer.nextToken());
+        String appName = tokenizer.nextToken();
+        String expName = tokenizer.nextToken();
+        String trialName = tokenizer.nextToken();
+
+        File file = new File(location);
+        File[] files = new File[1];
+        files[0] = file;
+        DataSource dataSource = UtilFncs.initializeDataSource(files, DataSource.TAUPROFILE, false);
+        dataSource.load();
+
+        Trial trial = new Trial();
+        trial.setDataSource(dataSource);
+
+        DatabaseAPI databaseAPI = new DatabaseAPI();
+        databaseAPI.initialize((Database) Database.getDatabases().get(dbID));
+
+        Experiment exp = databaseAPI.getExperiment(appName, expName, true);
+        trial.setName(trialName);
+        trial.setExperimentID(exp.getID());
+        int trialID = databaseAPI.uploadTrial(trial, false);
+        outputCommand("return " + trialID);
+        outputCommand("endreturn");
 
     }
 
@@ -105,7 +158,7 @@ public class ExternalController {
         List trials = databaseAPI.getTrialList(false);
         for (Iterator it = trials.iterator(); it.hasNext();) {
             Trial trial = (Trial) it.next();
-            outputCommand("return " +trial.getID() + " " + trial.getName());
+            outputCommand("return " + trial.getID() + " " + trial.getName());
         }
         outputCommand("endreturn");
     }
