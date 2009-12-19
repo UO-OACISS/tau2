0ee79fbcd9c6a3920b46d4f451db4d12745a6202
amorris <amorris@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2009-12-19 04:39:35 +0000

    Forgot to add GroupListCellRenderer.java
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@7763 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ tools/src/paraprof/src/GroupListCellRenderer.java
@@ -0,0 +1,281 @@
+package edu.uoregon.tau.paraprof;
+
+/*
+ * @(#)DefaultListCellRenderer.java 1.34 08/05/22
+ *
+ * Copyright 2006 Sun Microsystems, Inc. All rights reserved.
+ * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Rectangle;
+import java.io.Serializable;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+
+import edu.uoregon.tau.paraprof.GroupChangerWindow.GroupListBlob;
+
+/**
+ * Renders an item in a list.
+ * <p>
+ * <strong><a name="override">Implementation Note:</a></strong>
+ * This class overrides
+ * <code>invalidate</code>,
+ * <code>validate</code>,
+ * <code>revalidate</code>,
+ * <code>repaint</code>,
+ * <code>isOpaque</code>,
+ * and
+ * <code>firePropertyChange</code>
+ * solely to improve performance.
+ * If not overridden, these frequently called methods would execute code paths
+ * that are unnecessary for the default list cell renderer.
+ * If you write your own renderer,
+ * take care to weigh the benefits and
+ * drawbacks of overriding these methods.
+ *
+ * <p>
+ * 
+ * <strong>Warning:</strong>
+ * Serialized objects of this class will not be compatible with
+ * future Swing releases. The current serialization support is
+ * appropriate for short term storage or RMI between applications running
+ * the same version of Swing.  As of 1.4, support for long term storage
+ * of all JavaBeans<sup><font size="-2">TM</font></sup>
+ * has been added to the <code>java.beans</code> package.
+ * Please see {@link java.beans.XMLEncoder}.
+ *
+ * @version 1.34 05/22/08
+ * @author Philip Milne
+ * @author Hans Muller
+ */
+public class GroupListCellRenderer extends JLabel implements ListCellRenderer, Serializable {
+
+    /**
+     * An empty <code>Border</code>. This field might not be used. To change the
+     * <code>Border</code> used by this renderer override the 
+     * <code>getListCellRendererComponent</code> method and set the border
+     * of the returned component directly.
+     */
+    private static final Border SAFE_NO_FOCUS_BORDER = new EmptyBorder(1, 1, 1, 1);
+    private static final Border DEFAULT_NO_FOCUS_BORDER = new EmptyBorder(1, 1, 1, 1);
+    protected static Border noFocusBorder = DEFAULT_NO_FOCUS_BORDER;
+
+    /**
+     * Constructs a default renderer object for an item
+     * in a list.
+     */
+    public GroupListCellRenderer() {
+        super();
+        setOpaque(true);
+        setBorder(getNoFocusBorder());
+        setName("List.cellRenderer");
+    }
+
+    private Border getNoFocusBorder() {
+        return noFocusBorder;
+    }
+
+    public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+        setComponentOrientation(list.getComponentOrientation());
+
+        Color bg = null;
+        Color fg = null;
+
+        if (isSelected) {
+            setBackground(list.getSelectionBackground());
+            setForeground(list.getSelectionForeground());
+        } else {
+            setBackground(list.getBackground());
+            setForeground(list.getForeground());
+        }
+
+        GroupListBlob blob = (GroupListBlob) value;
+        if (!blob.allMembers) {
+            setForeground(Color.gray);
+        }
+
+        if (value instanceof Icon) {
+            setIcon((Icon) value);
+            setText("");
+        } else {
+            setIcon(null);
+            setText((value == null) ? "" : value.toString());
+        }
+
+        setEnabled(list.isEnabled());
+        setFont(list.getFont());
+
+        Border border = null;
+//        if (cellHasFocus) {
+//            border = getNoFocusBorder();
+//        }
+        setBorder(border);
+
+        return this;
+    }
+
+    /**
+     * Overridden for performance reasons.
+     * See the <a href="#override">Implementation Note</a> 
+     * for more information.
+     *
+     * @since 1.5
+     * @return <code>true</code> if the background is completely opaque
+     *         and differs from the JList's background;
+     *         <code>false</code> otherwise
+     */
+    public boolean isOpaque() {
+        Color back = getBackground();
+        Component p = getParent();
+        if (p != null) {
+            p = p.getParent();
+        }
+        // p should now be the JList. 
+        boolean colorMatch = (back != null) && (p != null) && back.equals(p.getBackground()) && p.isOpaque();
+        return !colorMatch && super.isOpaque();
+    }
+
+    /**
+     * Overridden for performance reasons.
+     * See the <a href="#override">Implementation Note</a>
+     * for more information.
+     */
+    public void validate() {}
+
+    /**
+     * Overridden for performance reasons.
+     * See the <a href="#override">Implementation Note</a>
+     * for more information.
+     *
+     * @since 1.5
+     */
+    public void invalidate() {}
+
+    /**
+     * Overridden for performance reasons.
+     * See the <a href="#override">Implementation Note</a>
+     * for more information.
+     *
+     * @since 1.5
+     */
+    public void repaint() {}
+
+    /**
+     * Overridden for performance reasons.
+     * See the <a href="#override">Implementation Note</a>
+     * for more information.
+     */
+    public void revalidate() {}
+
+    /**
+     * Overridden for performance reasons.
+     * See the <a href="#override">Implementation Note</a>
+     * for more information.
+     */
+    public void repaint(long tm, int x, int y, int width, int height) {}
+
+    /**
+     * Overridden for performance reasons.
+     * See the <a href="#override">Implementation Note</a>
+     * for more information.
+     */
+    public void repaint(Rectangle r) {}
+
+    /**
+     * Overridden for performance reasons.
+     * See the <a href="#override">Implementation Note</a>
+     * for more information.
+     */
+    protected void firePropertyChange(String propertyName, Object oldValue, Object newValue) {
+        // Strings get interned...
+        if (propertyName == "text"
+                || ((propertyName == "font" || propertyName == "foreground") && oldValue != newValue && getClientProperty(javax.swing.plaf.basic.BasicHTML.propertyKey) != null)) {
+
+            super.firePropertyChange(propertyName, oldValue, newValue);
+        }
+    }
+
+    /**
+     * Overridden for performance reasons.
+     * See the <a href="#override">Implementation Note</a>
+     * for more information.
+     */
+    public void firePropertyChange(String propertyName, byte oldValue, byte newValue) {}
+
+    /**
+     * Overridden for performance reasons.
+     * See the <a href="#override">Implementation Note</a>
+     * for more information.
+     */
+
+    public void firePropertyChange(String propertyName, char oldValue, char newValue) {}
+
+    /**
+     * Overridden for performance reasons.
+     * See the <a href="#override">Implementation Note</a>
+     * for more information.
+     */
+
+    public void firePropertyChange(String propertyName, short oldValue, short newValue) {}
+
+    /**
+     * Overridden for performance reasons.
+     * See the <a href="#override">Implementation Note</a>
+     * for more information.
+     */
+
+    public void firePropertyChange(String propertyName, int oldValue, int newValue) {}
+
+    /**
+     * Overridden for performance reasons.
+     * See the <a href="#override">Implementation Note</a>
+     * for more information.
+     */
+
+    public void firePropertyChange(String propertyName, long oldValue, long newValue) {}
+
+    /**
+     * Overridden for performance reasons.
+     * See the <a href="#override">Implementation Note</a>
+     * for more information.
+     */
+
+    public void firePropertyChange(String propertyName, float oldValue, float newValue) {}
+
+    /**
+     * Overridden for performance reasons.
+     * See the <a href="#override">Implementation Note</a>
+     * for more information.
+     */
+
+    public void firePropertyChange(String propertyName, double oldValue, double newValue) {}
+
+    /**
+     * Overridden for performance reasons.
+     * See the <a href="#override">Implementation Note</a>
+     * for more information.
+     */
+
+    public void firePropertyChange(String propertyName, boolean oldValue, boolean newValue) {}
+
+    /**
+     * A subclass of DefaultListCellRenderer that implements UIResource.
+     * DefaultListCellRenderer doesn't implement UIResource
+     * directly so that applications can safely override the
+     * cellRenderer property with DefaultListCellRenderer subclasses.
+     * <p>
+     * <strong>Warning:</strong>
+     * Serialized objects of this class will not be compatible with
+     * future Swing releases. The current serialization support is
+     * appropriate for short term storage or RMI between applications running
+     * the same version of Swing.  As of 1.4, support for long term storage
+     * of all JavaBeans<sup><font size="-2">TM</font></sup>
+     * has been added to the <code>java.beans</code> package.
+     * Please see {@link java.beans.XMLEncoder}.
+     */
+    public static class UIResource extends DefaultListCellRenderer implements javax.swing.plaf.UIResource {}
+}
