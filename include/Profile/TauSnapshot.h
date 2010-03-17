db316d6a7eb5e3b6f4ffd7e36cd97499317a2225
amorris <amorris@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2010-03-17 23:40:18 +0000

    Adding TauSnapshot.h to house snapshot related routines.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@7941 a8e31b63-e6ca-409b-a01b-2eec1b506579

---
+++ include/Profile/TauSnapshot.h
@@ -0,0 +1,38 @@
+/****************************************************************************
+**			TAU Portable Profiling Package			   **
+**			http://www.cs.uoregon.edu/research/tau	           **
+*****************************************************************************
+**    Copyright 2009  						   	   **
+**    Department of Computer and Information Science, University of Oregon **
+**    Advanced Computing Laboratory, Los Alamos National Laboratory        **
+****************************************************************************/
+/****************************************************************************
+**	File 		: TauSnapshot.h  				   **
+**	Description 	: TAU Profiling Package				   **
+**	Contact		: tau-bugs@cs.uoregon.edu               	   **
+**	Documentation	: See http://www.cs.uoregon.edu/research/tau       **
+**                                                                         **
+**      Description     : This file contains all the snapshot stuff        **
+**                                                                         **
+****************************************************************************/
+
+
+#ifndef _TAU_SNAPSHOT_H_
+#define _TAU_SNAPSHOT_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+int   TAUDECL Tau_snapshot_merge(const char *name);
+char* TAUDECL Tau_snapshot_getBuffer();
+int   TAUDECL Tau_snapshot_getBufferLength();
+int   TAUDECL Tau_snapshot_write_final(const char *name);
+int   TAUDECL Tau_snapshot_write_intermediate(const char *name);
+
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* _TAU_SNAPSHOT_H_ */
