7c2fea3af25610802552e55f562fdfbe9426be19
scottb <scottb@2965d551-5d0a-4728-ba96-6ec4fb1e77a7>
2010-05-12 21:32:19 +0000

    Made TAUCUDA_TIME the default metric. It need to be the only metric right now when tracing.
    git-svn-id: svn+ssh://subversion.nic.uoregon.edu/svnroot/taucuda/branches/regression@52 2965d551-5d0a-4728-ba96-6ec4fb1e77a7
    
    Former-commit-id: 1951fb662ec381e225af0c3fb088facda9ed43de

--- code/bin/taucuda_mpirun.sh
+++ code/bin/taucuda_mpirun.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 echo $(dirname $(readlink -f $0))
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../lib/$(dirname $(readlink -f $0))
-LD_PRELOAD=$LD_PRELOAD:$(dirname $(readlink -f $0))/libtaunexus.so:libTAU.so
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(dirname $(readlink -f $0))/../lib
+export TAU_METRICS=TAUCUDA_TIME
+LD_PRELOAD=$LD_PRELOAD:libtaucuda.so:libTAU.so
+#export LD_TRACE_LOADED_OBJECTS=1
 mpirun -x LD_PRELOAD=$LD_PRELOAD $@
--- code/bin/taucuda_profiler.sh
+++ code/bin/taucuda_profiler.sh
@@ -3,7 +3,7 @@ LOCATION=$(dirname $(readlink -f $0))
 
 #export LD_TRACE_LOADED_OBJECTS=1
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$LOCATION/../lib
+export TAU_METRICS=TAUCUDA_TIME
 export LD_PRELOAD=$LD_PRELOAD:libtaucuda.so
-#export LD_PRELOAD=$LD_PRELOAD:libso_wrapper.so
 #export LD_PRELOAD=$LD_PRELOAD:libtaucuda.so:libcuda_runtime_api_wrap.so
 $@
