c25195e113324886d9b4c1109f5eaeda6bb09da9
amorris <amorris@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2010-05-17 16:02:23 +0000

    Added missing tau_types.h for x_uint64
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@8227 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- include/Profile/TauMemoryWrap.h
+++ include/Profile/TauMemoryWrap.h
@@ -24,6 +24,7 @@
 extern "C" {
 #endif /* __cplusplus */
 
+#include <Profile/tau_types.h>
 int Tau_memorywrap_checkPassThrough();
 void Tau_memorywrap_writeHook();
 void Tau_memorywrap_checkInit();
