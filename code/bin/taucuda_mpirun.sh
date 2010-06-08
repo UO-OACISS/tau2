6995dec0a980a782384f5a5ad4fadd29b4a899f0
Scott Biersdorff <scottb@adagio.(none)>
2010-06-08 15:01:11 -0700

    Some more cleanup.
    Former-commit-id: d32bf9c2feb03856f673128ac984fafb5a4f4525

--- code/bin/taucuda_mpirun.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-echo $(dirname $(readlink -f $0))
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(dirname $(readlink -f $0))/../lib
-export TAU_METRICS=TAUCUDA_TIME
-LD_PRELOAD=$LD_PRELOAD:libtaucuda.so:libTAU.so
-#export LD_TRACE_LOADED_OBJECTS=1
-mpirun -x LD_PRELOAD=$LD_PRELOAD $@
--- code/gpu.mk
+++ code/gpu.mk
@@ -5,7 +5,7 @@ CUDA_BASE=/opt/cuda
 
 # TAU makefile for building the wrapper library (I think we need a TAU
 # confguration build with pthread support).
-TAU_MAKEFILE=/home/users/scottb/tau2/x86_64/lib/Makefile.tau-mpi
+TAU_MAKEFILE=/home/scottb/tau2/x86_64/lib/Makefile.tau-thread
 
 # Location of the CUDA sdk directory (we need the libcutil library).
 CUDA_SDK=/home/users/scottb/NVIDIA_GPU_Computing_SDK/C
