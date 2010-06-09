94dfc06938fd3b3441dcc3dee433e809d7f978da
amorris <amorris@a8e31b63-e6ca-409b-a01b-2eec1b506579>
2010-06-09 20:21:01 +0000

    No longer need calloc call here.
    git-svn-id: file:///mnt/fast/tau2git/svnroot/trunk/tau2@8301 a8e31b63-e6ca-409b-a01b-2eec1b506579

--- src/wrappers/taupreload/taupreload.c
+++ src/wrappers/taupreload/taupreload.c
@@ -7,19 +7,13 @@ void __attribute__ ((destructor)) taupreload_fini(void);
 #include <stdlib.h>
 
 void taupreload_init() {
+  Tau_global_incr_insideTAU();
   Tau_create_top_level_timer_if_necessary();
-  if (Tau_get_tid() == 0) {
-    Tau_global_incr_insideTAU();
-    char *c = calloc(1,8); /* TAU calls calloc: memory wrapper */
-    Tau_global_decr_insideTAU();
-    free(c);
-  }
-  
+  Tau_global_decr_insideTAU();
+
   TAU_PROFILE_SET_NODE(0);
-  //  TAU_START(".TAU Application!");
 }
 
 void taupreload_fini() {
   Tau_stop_top_level_timer_if_necessary();
-  //TAU_STOP(".TAU Application!");
 }
