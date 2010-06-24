a53cd0852742369ee83955893119251b4b7d8d75
Scott Biersdorff <scottb@mist.cs.uoregon.edu>
2010-06-24 15:49:56 -0700

    Changed the API names in TauGpu.h to reflect new naming scheme.
    Former-commit-id: c2bfc47d4036bf95d220c74d97e3d2afc9d0f22d

--- include/Profile/TauGpu.h
+++ include/Profile/TauGpu.h
@@ -18,13 +18,16 @@ class gpuId {
 
 public:
 	virtual char * printId() = 0;
-	virtual double id_p1() = 0;
-	virtual double id_p2() = 0;
+	virtual x_uint64 id_p1() = 0;
+	virtual x_uint64 id_p2() = 0;
 };
 
 class eventId {
 
 };
+#else
+struct eventId;
+struct gpuId;
 #endif // __cplusplus
 
 /************************************************************************
@@ -33,33 +36,33 @@ class eventId {
  */
 
 /* Initialization to be executed at the start of the application */
-extern "C" int tau_gpu_init(void);
+ int Tau_gpu_init(void);
 
 /* Stuff to be performed at the end of the application */
-extern "C" void tau_gpu_exit(void);
+ void Tau_gpu_exit(void);
 
 /* Entry point for CPU routines */
-extern "C" void enter_event(const char *functionName, eventId *id);
+ void Tau_gpu_enter_event(const char *functionName, eventId *id);
 
 /* Entry point for CPU routines that initiate a memory copy to the GPU */
-extern "C" void enter_memcpy_event(const char *functionName, eventId *id,
+ void Tau_gpu_enter_memcpy_event(const char *functionName, eventId *id,
 gpuId *device, bool memcpyType);
 
 /* Exit point for CPU routines */
-extern "C" void exit_event(const char *functionName, eventId *id);
+ void Tau_gpu_exit_event(const char *functionName, eventId *id);
 
 /* Exit point for CPU routines that initiate a memory copy to the GPU */
-extern "C" void exit_memcpy_event(const char *functionName, eventId *id,
+ void Tau_gpu_exit_memcpy_event(const char *functionName, eventId *id,
 gpuId *device, bool memcpyType);
 
 /* Callback for a GPU event that occurred earlier in the execution of the
  * program. Times are pre-aligned to the CPU clock. */
-extern "C" void register_gpu_event(const char *functionName, eventId *id, double startTime, double
+ void Tau_gpu_register_gpu_event(const char *functionName, eventId *id, double startTime, double
 endTime);
 
 /* Callback for a Memcpy event that occurred earlier in the execution of the
  * program. Times are pre-aligned to the CPU clock. */
-extern "C" void register_memcpy_event(eventId *id, gpuId *device, double startTime, double
+ void Tau_gpu_register_memcpy_event(eventId *id, gpuId *device, double startTime, double
 endTime, double transferSize, bool memcpyType);
 
 #endif // _TAU_GPU_INTERFACE
