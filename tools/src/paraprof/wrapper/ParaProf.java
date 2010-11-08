9119db99b19bf92331932272053cebd215106a8b
Chee Wai Lee <cheelee@mist.cs.uoregon.edu>
2010-11-08 12:31:26 -0800

    Removed print notification when PerfDmf detects the use of the Unified format.

--- tools/src/paraprof/wrapper/ParaProf.java
+++ tools/src/paraprof/wrapper/ParaProf.java
@@ -54,7 +54,7 @@ public class ParaProf implements ActionListener {
 	}
     }
 
-    private final static String VERSION = "Thu Nov  4 10:23:30 PDT 2010";
+    private final static String VERSION = "Mon Nov  8 12:13:59 PST 2010";
 
     public static int defaultNumberPrecision = 6;
 
