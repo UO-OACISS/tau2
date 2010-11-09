57f5aa68e7508a50f5f8376df5687b06fa53c370
Wyatt Spear <wspear@cs.uoregon.edu>
2010-11-08 20:08:31 -0800

    Updated jars to include latest paraprof 3d-topology features

--- tools/src/paraprof/wrapper/ParaProf.java
+++ tools/src/paraprof/wrapper/ParaProf.java
@@ -54,7 +54,7 @@ public class ParaProf implements ActionListener {
 	}
     }
 
-    private final static String VERSION = "Mon Nov  8 12:13:59 PST 2010";
+    private final static String VERSION = "Mon Nov  8 20:06:32 PST 2010";
 
     public static int defaultNumberPrecision = 6;
 
