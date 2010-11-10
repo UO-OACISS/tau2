c483b3560f9d5f6c0e8b7b99f48770c6ca9de53c
Sameer Shende <sameer@cs.uoregon.edu>
2010-11-09 18:44:14 -0800

    Added TAU_PROFILE_SET_NODE(0); if MPI is defined.

--- examples/validate/pdtcpp/simple.cc
+++ examples/validate/pdtcpp/simple.cc
@@ -1,7 +1,11 @@
 
 #include <stdio.h>
+#include <TAU.h>
 
 int main (int argc, char **argv) {
+#ifdef TAU_MPI
+  TAU_PROFILE_SET_NODE(0);
+#endif /* TAU_MPI */
   printf ("Hello, world\n");
   return 0;
 }
