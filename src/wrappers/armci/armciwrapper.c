08e438d64d71bb3167c1468db9241b9a424ffcaf
Sameer Shende <sameer@paratools.com>
2011-03-16 16:34:09 -0700

    TAU_ARMCI is the group.

--- src/wrappers/armci/armciwrapper.c
+++ src/wrappers/armci/armciwrapper.c
@@ -3,12 +3,14 @@
 #include "parmci.h"
 #include <TAU.h>
 
+#define TAU_ARMCI TAU_USER
+
 
 int
 ARMCI_AccV (int op, void *scale, armci_giov_t * darr, int len, int proc)
 {
   int rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_AccV()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_AccV()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   {
     int i, bytes = 0;
@@ -25,7 +27,7 @@ ARMCI_AccV (int op, void *scale, armci_giov_t * darr, int len, int proc)
 void
 ARMCI_Barrier ()
 {
-  TAU_PROFILE_TIMER(t, "ARMCI_Barrier()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_Barrier()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   PARMCI_Barrier ();
   TAU_PROFILE_STOP(t);
@@ -38,7 +40,7 @@ ARMCI_AccS (int optype, void *scale, void *src_ptr, int *src_stride_arr,
 	    int proc)
 {
   int rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_AccS()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_AccS()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   {
     int i, bytes = 1;
@@ -57,7 +59,7 @@ ARMCI_AccS (int optype, void *scale, void *src_ptr, int *src_stride_arr,
 void
 ARMCI_Finalize ()
 {
-  TAU_PROFILE_TIMER(t, "ARMCI_Finalize()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_Finalize()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   PARMCI_Finalize ();
   TAU_PROFILE_STOP(t);
@@ -69,7 +71,7 @@ ARMCI_NbPut (void *src, void *dst, int bytes, int proc,
 	     armci_hdl_t * nb_handle)
 {
   int rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_NbPut()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_NbPut()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   TAU_TRACE_SENDMSG (1, proc, bytes);
   rval = PARMCI_NbPut (src, dst, bytes, proc, nb_handle);
@@ -82,7 +84,7 @@ int
 ARMCI_GetValueInt (void *src, int proc)
 {
   int rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_GetValueInt()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_GetValueInt()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   TAU_TRACE_SENDMSG (1, proc, 4);
   rval = PARMCI_GetValueInt (src, proc);
@@ -95,7 +97,7 @@ int
 ARMCI_Put_flag (void *src, void *dst, int bytes, int *f, int v, int proc)
 {
   int rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_Put_flag()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_Put_flag()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   TAU_TRACE_SENDMSG (1, proc, bytes);
   rval = PARMCI_Put_flag (src, dst, bytes, f, v, proc);
@@ -110,7 +112,7 @@ ARMCI_NbGetS (void *src_ptr, int *src_stride_arr, void *dst_ptr,
 	      armci_hdl_t * nb_handle)
 {
   int rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_NbGetS()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_NbGetS()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   {
     int i, bytes = 1;
@@ -130,7 +132,7 @@ void *
 ARMCI_Malloc_local (armci_size_t bytes)
 {
   void *rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_Malloc_local()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_Malloc_local()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   rval = PARMCI_Malloc_local (bytes);
   TAU_PROFILE_STOP(t);
@@ -142,7 +144,7 @@ int
 ARMCI_Free_local (void *ptr)
 {
   int rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_Free_local()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_Free_local()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   rval = PARMCI_Free_local (ptr);
   TAU_PROFILE_STOP(t);
@@ -154,7 +156,7 @@ int
 ARMCI_Get (void *src, void *dst, int bytes, int proc)
 {
   int rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_Get()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_Get()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   TAU_TRACE_SENDMSG (1, proc, bytes);
   rval = PARMCI_Get (src, dst, bytes, proc);
@@ -167,7 +169,7 @@ int
 ARMCI_Put (void *src, void *dst, int bytes, int proc)
 {
   int rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_Put()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_Put()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   TAU_TRACE_SENDMSG (1, proc, bytes);
   rval = PARMCI_Put (src, dst, bytes, proc);
@@ -180,7 +182,7 @@ int
 ARMCI_Destroy_mutexes ()
 {
   int rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_Destroy_mutexes()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_Destroy_mutexes()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   rval = PARMCI_Destroy_mutexes ();
   TAU_PROFILE_STOP(t);
@@ -193,7 +195,7 @@ ARMCI_GetS (void *src_ptr, int *src_stride_arr, void *dst_ptr,
 	    int *dst_stride_arr, int *count, int stride_levels, int proc)
 {
   int rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_GetS()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_GetS()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   {
     int i, bytes = 1;
@@ -214,7 +216,7 @@ ARMCI_NbAccV (int op, void *scale, armci_giov_t * darr, int len, int proc,
 	      armci_hdl_t * nb_handle)
 {
   int rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_NbAccV()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_NbAccV()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   {
     int i, bytes = 0;
@@ -232,7 +234,7 @@ float
 ARMCI_GetValueFloat (void *src, int proc)
 {
   float rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_GetValueFloat()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_GetValueFloat()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   TAU_TRACE_SENDMSG (1, proc, 4);
   rval = PARMCI_GetValueFloat (src, proc);
@@ -245,7 +247,7 @@ int
 ARMCI_Malloc (void **ptr_arr, armci_size_t bytes)
 {
   int rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_Malloc()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_Malloc()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   rval = PARMCI_Malloc (ptr_arr, bytes);
   TAU_PROFILE_STOP(t);
@@ -259,7 +261,7 @@ ARMCI_NbAccS (int optype, void *scale, void *src_ptr, int *src_stride_arr,
 	      int stride_levels, int proc, armci_hdl_t * nb_handle)
 {
   int rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_NbAccS()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_NbAccS()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   {
     int i, bytes = 1;
@@ -280,7 +282,7 @@ ARMCI_PutS (void *src_ptr, int *src_stride_arr, void *dst_ptr,
 	    int *dst_stride_arr, int *count, int stride_levels, int proc)
 {
   int rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_PutS()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_PutS()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   {
     int i, bytes = 1;
@@ -300,7 +302,7 @@ void *
 ARMCI_Memat (armci_meminfo_t * meminfo, int memflg)
 {
   void *rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_Memat()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_Memat()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   rval = PARMCI_Memat (meminfo, memflg);
   TAU_PROFILE_STOP(t);
@@ -312,7 +314,7 @@ int
 ARMCI_PutV (armci_giov_t * darr, int len, int proc)
 {
   int rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_PutV()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_PutV()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   {
     int i, bytes = 0;
@@ -330,7 +332,7 @@ int
 ARMCI_Free (void *ptr)
 {
   int rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_Free()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_Free()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   rval = PARMCI_Free (ptr);
   TAU_PROFILE_STOP(t);
@@ -342,7 +344,7 @@ int
 ARMCI_Init_args (int *argc, char ***argv)
 {
   int rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_Init_args()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_Init_args()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   rval = PARMCI_Init_args (argc, argv);
   TAU_PROFILE_STOP(t);
@@ -354,7 +356,7 @@ int
 ARMCI_PutValueInt (int src, void *dst, int proc)
 {
   int rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_PutValueInt()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_PutValueInt()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   TAU_TRACE_SENDMSG (1, proc, 4);
   rval = PARMCI_PutValueInt (src, dst, proc);
@@ -366,7 +368,7 @@ ARMCI_PutValueInt (int src, void *dst, int proc)
 void
 ARMCI_Memget (size_t bytes, armci_meminfo_t * meminfo, int memflg)
 {
-  TAU_PROFILE_TIMER(t, "ARMCI_Memget()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_Memget()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   PARMCI_Memget (bytes, meminfo, memflg);
   TAU_PROFILE_STOP(t);
@@ -376,7 +378,7 @@ ARMCI_Memget (size_t bytes, armci_meminfo_t * meminfo, int memflg)
 void
 ARMCI_AllFence ()
 {
-  TAU_PROFILE_TIMER(t, "ARMCI_AllFence()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_AllFence()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   PARMCI_AllFence ();
   TAU_PROFILE_STOP(t);
@@ -387,7 +389,7 @@ int
 ARMCI_NbPutV (armci_giov_t * darr, int len, int proc, armci_hdl_t * nb_handle)
 {
   int rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_NbPutV()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_NbPutV()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   {
     int i, bytes = 0;
@@ -405,7 +407,7 @@ int
 ARMCI_PutValueDouble (double src, void *dst, int proc)
 {
   int rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_PutValueDouble()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_PutValueDouble()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   TAU_TRACE_SENDMSG (1, proc, 8);
   rval = PARMCI_PutValueDouble (src, dst, proc);
@@ -418,7 +420,7 @@ int
 ARMCI_GetV (armci_giov_t * darr, int len, int proc)
 {
   int rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_GetV()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_GetV()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   {
     int i, bytes = 0;
@@ -436,7 +438,7 @@ int
 ARMCI_Test (armci_hdl_t * nb_handle)
 {
   int rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_Test()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_Test()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   rval = PARMCI_Test (nb_handle);
   TAU_PROFILE_STOP(t);
@@ -447,7 +449,7 @@ ARMCI_Test (armci_hdl_t * nb_handle)
 void
 ARMCI_Unlock (int mutex, int proc)
 {
-  TAU_PROFILE_TIMER(t, "ARMCI_Unlock()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_Unlock()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   PARMCI_Unlock (mutex, proc);
   TAU_PROFILE_STOP(t);
@@ -457,7 +459,7 @@ ARMCI_Unlock (int mutex, int proc)
 void
 ARMCI_Fence (int proc)
 {
-  TAU_PROFILE_TIMER(t, "ARMCI_Fence()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_Fence()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   PARMCI_Fence (proc);
   TAU_PROFILE_STOP(t);
@@ -468,7 +470,7 @@ int
 ARMCI_Create_mutexes (int num)
 {
   int rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_Create_mutexes()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_Create_mutexes()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   rval = PARMCI_Create_mutexes (num);
   TAU_PROFILE_STOP(t);
@@ -482,7 +484,7 @@ ARMCI_PutS_flag (void *src_ptr, int *src_stride_arr, void *dst_ptr,
 		 int *flag, int val, int proc)
 {
   int rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_PutS_flag()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_PutS_flag()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   {
     int i, bytes = 1;
@@ -502,7 +504,7 @@ int
 ARMCI_WaitProc (int proc)
 {
   int rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_WaitProc()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_WaitProc()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   rval = PARMCI_WaitProc (proc);
   TAU_PROFILE_STOP(t);
@@ -513,7 +515,7 @@ ARMCI_WaitProc (int proc)
 void
 ARMCI_Lock (int mutex, int proc)
 {
-  TAU_PROFILE_TIMER(t, "ARMCI_Lock()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_Lock()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   PARMCI_Lock (mutex, proc);
   TAU_PROFILE_STOP(t);
@@ -524,7 +526,7 @@ double
 ARMCI_GetValueDouble (void *src, int proc)
 {
   double rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_GetValueDouble()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_GetValueDouble()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   TAU_TRACE_SENDMSG (1, proc, 8);
   rval = PARMCI_GetValueDouble (src, proc);
@@ -537,7 +539,7 @@ int
 ARMCI_NbGetV (armci_giov_t * darr, int len, int proc, armci_hdl_t * nb_handle)
 {
   int rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_NbGetV()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_NbGetV()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   {
     int i, bytes = 0;
@@ -555,7 +557,7 @@ int
 ARMCI_Rmw (int op, void *ploc, void *prem, int extra, int proc)
 {
   int rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_Rmw()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_Rmw()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   TAU_TRACE_SENDMSG (1, proc, 4);
   rval = PARMCI_Rmw (op, ploc, prem, extra, proc);
@@ -568,7 +570,7 @@ int
 ARMCI_Init ()
 {
   int rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_Init()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_Init()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   rval = PARMCI_Init ();
   TAU_PROFILE_STOP(t);
@@ -580,7 +582,7 @@ int
 ARMCI_WaitAll ()
 {
   int rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_WaitAll()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_WaitAll()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   rval = PARMCI_WaitAll ();
   TAU_PROFILE_STOP(t);
@@ -593,7 +595,7 @@ ARMCI_NbGet (void *src, void *dst, int bytes, int proc,
 	     armci_hdl_t * nb_handle)
 {
   int rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_NbGet()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_NbGet()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   TAU_TRACE_SENDMSG (1, proc, bytes);
   rval = PARMCI_NbGet (src, dst, bytes, proc, nb_handle);
@@ -606,7 +608,7 @@ int
 ARMCI_PutValueFloat (float src, void *dst, int proc)
 {
   int rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_PutValueFloat()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_PutValueFloat()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   TAU_TRACE_SENDMSG (1, proc, 4);
   rval = PARMCI_PutValueFloat (src, dst, proc);
@@ -621,7 +623,7 @@ ARMCI_NbPutS (void *src_ptr, int *src_stride_arr, void *dst_ptr,
 	      armci_hdl_t * nb_handle)
 {
   int rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_NbPutS()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_NbPutS()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   {
     int i, bytes = 1;
@@ -643,7 +645,7 @@ ARMCI_PutS_flag_dir (void *src_ptr, int *src_stride_arr, void *dst_ptr,
 		     int *flag, int val, int proc)
 {
   int rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_PutS_flag_dir()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_PutS_flag_dir()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   {
     int i, bytes = 1;
@@ -663,7 +665,7 @@ int
 ARMCI_PutValueLong (long src, void *dst, int proc)
 {
   int rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_PutValueLong()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_PutValueLong()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   TAU_TRACE_SENDMSG (1, proc, 8);
   rval = PARMCI_PutValueLong (src, dst, proc);
@@ -676,7 +678,7 @@ int
 ARMCI_Wait (armci_hdl_t * nb_handle)
 {
   int rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_Wait()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_Wait()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   rval = PARMCI_Wait (nb_handle);
   TAU_PROFILE_STOP(t);
@@ -688,7 +690,7 @@ long
 ARMCI_GetValueLong (void *src, int proc)
 {
   long rval;
-  TAU_PROFILE_TIMER(t, "ARMCI_GetValueLong()", "", TAU_USER);
+  TAU_PROFILE_TIMER(t, "ARMCI_GetValueLong()", "", TAU_ARMCI);
   TAU_PROFILE_START(t);
   TAU_TRACE_SENDMSG (1, proc, 8);
   rval = PARMCI_GetValueLong (src, proc);
