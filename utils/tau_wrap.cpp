de8daa6f9bde9d295ea8538b38cc5ca509deffd0
Sameer Shende <sameer@cs.uoregon.edu>
2011-08-07 11:23:56 -0700

    Added --shmem flags.

--- utils/tau_wrap.cpp
+++ utils/tau_wrap.cpp
@@ -69,6 +69,8 @@ extern bool addFileInstrumentationRequests(PDB& p, pdbFile *file, vector        
 /* Globals */
 bool memory_flag = false;   /* by default, do not insert malloc.h in instrumented C/C++ files */
 bool strict_typing = false; /* by default unless --strict option is used. */
+bool shmem_wrapper = false; /* by default unless --shmem option is used. */
+bool pshmem_use_underscore_instead_of_p = false; /* by default unless --pshmem_use_underscore_instead_of_p option is used. */
 
 
 ///////////////////////////////////////////////////////////////////////////
@@ -220,6 +222,194 @@ bool isReturnTypeVoid(pdbRoutine *r)
      return false;
 }
 
+bool doesRoutineNameContainGet(const char *rname, int namelen) {
+  if ((strstr(rname, "get") != 0 ) || 
+     ((rname[namelen-2] == '_') && (rname[namelen-1] == 'g')))
+    return true;
+  else
+    return false;
+}
+
+bool doesRoutineNameContainPut(const char *rname, int namelen) {
+  if ((strstr(rname, "put") != 0 ) || 
+     ((rname[namelen-2] == '_') && (rname[namelen-1] == 'p')))
+    return true;
+  else
+    return false;
+}
+
+/* Fetch and operate operations include swap, fadd and finc */
+bool doesRoutineNameContainFetchOp(const char *rname, int namelen) {
+  if ((strstr(rname, "swap") != 0 ) || (strstr(rname, "fadd") != 0) ||
+      (strstr(rname, "finc") != 0 ))
+    return true;
+  else
+    return false;
+}
+
+/* Fetch and operate operations include swap, fadd and finc */
+bool doesRoutineNameContainCondFetchOp(const char *rname, int namelen) {
+  if (strstr(rname, "cswap") != 0 ) 
+    return true;
+  else
+    return false;
+}
+
+void getMultiplierString(const char *rname, string& multiplier_string) {
+  if (strstr(rname, "char") !=0) { // char is found 
+    multiplier_string=string("sizeof(char)*"); return;
+  }
+  if (strstr(rname, "short") !=0) { // short is found 
+    multiplier_string=string("sizeof(short)*"); return;
+  }
+  if (strstr(rname, "int") !=0) { // int is found 
+    multiplier_string=string("sizeof(int)*"); return;
+  }
+  if (strstr(rname, "longlong") !=0) { // long long is found 
+    multiplier_string=string("sizeof(long long)*"); return;
+  }
+  if (strstr(rname, "longdouble") !=0) { // float is found 
+    multiplier_string=string("sizeof(long double)*"); return;
+  }
+  if (strstr(rname, "long") !=0) { // long is found 
+    multiplier_string=string("sizeof(long)*"); return;
+  }
+  if (strstr(rname, "double") !=0) { // double is found 
+    multiplier_string=string("sizeof(double)*"); return;
+  }
+  if (strstr(rname, "float") !=0) { // float is found 
+    multiplier_string=string("sizeof(float)*"); return;
+  }
+  if (strstr(rname, "16") !=0) { 
+    multiplier_string=string("2*"); return;
+  }
+  if (strstr(rname, "32") !=0) {
+    multiplier_string=string("4*"); return;
+  }
+  if (strstr(rname, "64") !=0) {
+    multiplier_string=string("8*"); return;
+  }
+  if (strstr(rname, "128") !=0) {
+    multiplier_string=string("16*"); return;
+  }
+  if (strstr(rname, "4") !=0) { // INT4_SWAP uses 4 bytes not 4 bits. 
+    multiplier_string=string("4*"); return;
+  }
+  if (strstr(rname, "8") !=0) { // INT8_SWAP uses 8 bytes not 8 bits. = 64
+    multiplier_string=string("8*"); return;
+  }
+}
+
+void  printShmemMessageBeforeRoutine(pdbRoutine *r, ofstream& impl, int len_argument_no, int pe_argument_no, bool fortran_interface) {
+  const char *rname = r->name().c_str();
+  int routine_len = r->name().size();
+  string multiplier_string(""); 
+  char length_string[1024];
+  char processor_arg[256];
+
+  if (fortran_interface) {
+    sprintf(processor_arg, "(*a%d)", pe_argument_no);
+  } else {
+    sprintf(processor_arg, "a%d", pe_argument_no);
+  }
+
+  printf("Size = %d, name = %s\n", routine_len, rname);
+  getMultiplierString(rname, multiplier_string); 
+  printf("Multiplier string = %s\n", multiplier_string.c_str());
+  if (len_argument_no != 0) {
+    if (fortran_interface) {
+      sprintf(length_string, "%s (*a%d)", multiplier_string.c_str(), len_argument_no);
+    } else {
+      sprintf(length_string, "%sa%d", multiplier_string.c_str(), len_argument_no);
+    }
+  } else {
+    sprintf(length_string, "%s1", multiplier_string.c_str());
+  }
+  
+  if ((doesRoutineNameContainGet(rname, routine_len) == true) || 
+      (doesRoutineNameContainFetchOp(rname, routine_len) == true)) { /* Get */
+    printf("Routine name %s contains Get variant\n", rname);
+    impl <<"  TAU_TRACE_SENDMSG_REMOTE(TAU_SHMEM_TAGID_NEXT, Tau_get_node(), "<<length_string<<", "<<processor_arg<<");"<<endl;
+  }
+  if (doesRoutineNameContainPut(rname, routine_len) == true) {
+    printf("Routine name %s contains Put variant\n", rname);
+    impl <<"  TAU_TRACE_SENDMSG(TAU_SHMEM_TAGID_NEXT, "<<processor_arg<<", "<<length_string<<");"<<endl;
+  }
+   
+
+}
+
+void  printShmemMessageAfterRoutine(pdbRoutine *r, ofstream& impl, int len_argument_no, int pe_argument_no, int cond_argument_no, bool fortran_interface) {
+  const char *rname = r->name().c_str();
+  int routine_len = r->name().size();
+  string multiplier_string("");
+  char length_string[1024];
+  char processor_arg[256];
+  char cond_string[1024];
+  bool is_it_a_get = false;
+  bool is_it_a_fetchop = false;
+  bool is_it_a_cond_fetchop = false;
+  bool is_it_a_put = false;
+
+  if (fortran_interface) {
+    sprintf(processor_arg, "(*a%d)", pe_argument_no);
+  } else {
+    sprintf(processor_arg, "a%d", pe_argument_no);
+  }
+
+  printf("Size = %d, name = %s\n", routine_len, rname);
+  getMultiplierString(rname, multiplier_string);
+  printf("Multiplier string = %s\n", multiplier_string.c_str());
+  if (len_argument_no != 0) {
+    if (fortran_interface) {
+      sprintf(length_string, "%s (*a%d)", multiplier_string.c_str(), len_argument_no);
+    } else {
+      sprintf(length_string, "%sa%d", multiplier_string.c_str(), len_argument_no);
+    }
+  } else {
+    sprintf(length_string, "%s1", multiplier_string.c_str());
+  }
+  is_it_a_get = doesRoutineNameContainGet(rname, routine_len);
+  is_it_a_fetchop = doesRoutineNameContainFetchOp(rname, routine_len); 
+  is_it_a_cond_fetchop = doesRoutineNameContainCondFetchOp(rname, routine_len); 
+
+  if (strstr(rname, "start_pes") != 0) {
+     impl << "  tau_totalnodes(1,pshmem_n_pes());"<<endl;
+     impl << "  TAU_PROFILE_SET_NODE(pshmem_my_pe());"<<endl;
+  }
+
+  if (is_it_a_get || is_it_a_fetchop ) { /* Get */
+    printf("Routine name %s contains Get variant\n", rname);
+    impl <<"  TAU_TRACE_RECVMSG(TAU_SHMEM_TAGID, "<<processor_arg<<", "<<length_string<<");"<<endl;
+  }
+  if (is_it_a_cond_fetchop || is_it_a_fetchop) { /* add condition */
+    if (is_it_a_cond_fetchop && (cond_argument_no == 0)) {
+      printf("WARNING: in fetchop function %s, cond_argument_no is 0???\n", 
+	rname); 
+    }
+    string indent(""); /* no indent by default */
+    if (is_it_a_cond_fetchop) {
+      indent=string("  ");
+      if (fortran_interface) {
+        sprintf(cond_string, "  if (retval == (*a%d)) { ", cond_argument_no);
+      } else {
+        sprintf(cond_string, "  if (retval == a%d) { ", cond_argument_no);
+      }
+      impl <<cond_string<<endl;; 
+    }
+    impl <<indent<<"  TAU_TRACE_SENDMSG(TAU_SHMEM_TAGID_NEXT, "<<processor_arg<<", "<<length_string<<");"<<endl;
+    impl <<indent<<"  TAU_TRACE_RECVMSG_REMOTE(TAU_SHMEM_TAGID, Tau_get_node(), "<<length_string<<", "<<processor_arg<<");"<<endl;
+    if (is_it_a_cond_fetchop) {
+      impl<<indent<<"}"<<endl;
+    }
+  }
+  if (doesRoutineNameContainPut(rname, routine_len) == true) {
+    printf("Routine name %s contains Put variant\n", rname);
+    impl <<"  TAU_TRACE_RECVMSG_REMOTE(TAU_SHMEM_TAGID, Tau_get_node(), "<<length_string<<", "<<processor_arg<<");"<<endl;
+  }
+
+}
+
 void  printRoutineInOutputFile(pdbRoutine *r, ofstream& header, ofstream& impl, string& group_name, int runtime, string& runtime_libname)
 {
   string macro("#define ");
@@ -237,6 +427,7 @@ void  printRoutineInOutputFile(pdbRoutine *r, ofstream& header, ofstream& impl, 
   rcalledfunc.append("(");
   proto.append("(");
   protoname.append("_p");
+  bool fortran_interface = false; /* if *len or *pe appears in the arglist */
 
   if (r->signature()->hasEllipsis()) {
     // For a full discussion of why vararg functions are difficult to wrap
@@ -251,7 +442,17 @@ void  printRoutineInOutputFile(pdbRoutine *r, ofstream& header, ofstream& impl, 
   } else {
     returntypename = r->signature()->returnType()->name();
   }
-  switch (runtime) {
+
+  impl << endl; 
+  impl << "/**********************************************************"<<endl;
+  impl << "   "<<r->name()<< endl;
+  impl << " **********************************************************/"<<endl<<endl;
+
+  if (shmem_wrapper == true) {
+    impl << returntypename << " "; /* nothing else */
+  }
+  else {
+    switch (runtime) {
     case 1: /* for runtime interception, put a blank, the name stays the same*/
       impl<<returntypename<<" "; /* put in return type */
       break;
@@ -264,6 +465,7 @@ void  printRoutineInOutputFile(pdbRoutine *r, ofstream& header, ofstream& impl, 
     default: /* hmmm, what about any other case? Just use __wrap_bar */
       impl<<returntypename<<"  __wrap_"; /* put in return type */
       break;
+    }
   }
   impl<<func;
 #ifdef DEBUG
@@ -273,6 +475,9 @@ void  printRoutineInOutputFile(pdbRoutine *r, ofstream& header, ofstream& impl, 
   pdbType::argvec av = r->signature()->arguments();
   int argcount = 1;
   bool isVoid = isReturnTypeVoid(r);
+  int shmem_len_argcount = 0; 
+  int shmem_pe_argcount = 0; 
+  int shmem_cond_argcount = 0; 
   for(pdbType::argvec::const_iterator argsit = av.begin();
       argsit != av.end(); argsit++, argcount++)
   {
@@ -280,6 +485,29 @@ void  printRoutineInOutputFile(pdbRoutine *r, ofstream& header, ofstream& impl, 
 #ifdef DEBUG
     cout <<"Argument "<<(*argsit).name()<<" Type "<<(*argsit).type()->name()<<endl;
 #endif /* DEBUG */
+
+    if (shmem_wrapper) {
+      cout <<"Argument "<<(*argsit).name()<<" Type "<<(*argsit).type()->name()<<endl;
+      if (strcmp((*argsit).name().c_str(), "len") == 0) {
+        printf("Argcount = %d for len\n", argcount); 
+        shmem_len_argcount = argcount; 
+        if ((*argsit).type()->kind() == pdbItem::TY_PTR) {
+          fortran_interface = true;
+        }
+      }
+      if (strcmp((*argsit).name().c_str(), "pe") == 0) {
+        printf("Argcount = %d for pe\n", argcount); 
+        shmem_pe_argcount = argcount; 
+        if ((*argsit).type()->kind() == pdbItem::TY_PTR) {
+          fortran_interface = true;
+        }
+      }
+      if ((strcmp((*argsit).name().c_str(), "match") == 0) || 
+         (strcmp((*argsit).name().c_str(), "cond") == 0)) {
+        printf("Argcount = %d for match/cond\n", argcount); 
+        shmem_cond_argcount = argcount; 
+      }
+    }
     if (argcount != 1) { /* not a startup */
       func.append(", ");
       proto.append(", ");
@@ -393,18 +621,33 @@ void  printRoutineInOutputFile(pdbRoutine *r, ofstream& header, ofstream& impl, 
   if (runtime == 1)
     impl <<dltext;
   impl<<"  TAU_PROFILE_START(t);"<<endl;
-  if (!isVoid)
-  {
-    impl<<"  retval  =";
-  }
-  if (runtime == 1) {
-    impl<<"  "<<rcalledfunc<<";"<<endl;
+  if (shmem_wrapper) { /* generate pshmem calls here */
+    printShmemMessageBeforeRoutine(r, impl, shmem_len_argcount, shmem_pe_argcount, fortran_interface);
+    if (!isVoid)
+    {
+      impl<<"  retval  =";
+    }
+    if (pshmem_use_underscore_instead_of_p) {
+      impl <<"   _"<<func<<";"<<endl;
+    } else {
+      impl <<"   p"<<func<<";"<<endl;
+    }
+    printShmemMessageAfterRoutine(r, impl, shmem_len_argcount, shmem_pe_argcount, shmem_cond_argcount, fortran_interface);
   }
   else {
-    if (runtime == -1) { /* link time instrumentation using -Wl,-wrap,bar */
-      impl<<"  __real_"<<func<<";"<<endl;
-    } else { /* default case when we use redirection of bar -> tau_bar */
-      impl<<"  "<<func<<";"<<endl;
+    if (!isVoid)
+    {
+      impl<<"  retval  =";
+    }
+    if (runtime == 1) {
+      impl<<"  "<<rcalledfunc<<";"<<endl;
+    }
+    else {
+      if (runtime == -1) { /* link time instrumentation using -Wl,-wrap,bar */
+        impl<<"  __real_"<<func<<";"<<endl;
+      } else { /* default case when we use redirection of bar -> tau_bar */
+        impl<<"  "<<func<<";"<<endl;
+      }
     }
   }
   impl<<"  TAU_PROFILE_STOP(t);"<<endl;
@@ -621,6 +864,16 @@ int main(int argc, char **argv)
           outFileNameSpecified = true;
         }
 
+        if (strcmp(argv[i], "--shmem") == 0)
+        {
+	  shmem_wrapper = true;
+        } 
+
+        if (strcmp(argv[i], "--pshmem_use_underscore_instead_of_p") == 0)
+        {
+	  pshmem_use_underscore_instead_of_p = true;
+        } 
+
         if (strcmp(argv[i], "-r") == 0)
         {
           ++i;
@@ -707,6 +960,13 @@ int main(int argc, char **argv)
   //header <<"#include <"<<filename<<">"<<endl;
   impl <<"#include <"<<filename<<">"<<endl;
   impl <<"#include <"<<header_file<<">"<<endl; /* Profile/Profiler.h */
+  if (shmem_wrapper) {
+    impl <<"int TAUDECL tau_totalnodes(int set_or_get, int value);"<<endl;
+    impl <<"int tau_shmem_tagid=0 ; "<<endl;
+    impl <<"#define TAU_SHMEM_TAGID tau_shmem_tagid"<<endl;
+    impl <<"#define TAU_SHMEM_TAGID_NEXT (++tau_shmem_tagid) % 256 "<<endl;
+  }
+
 
   if (runtime == 1) {
   /* add the runtime library calls */
