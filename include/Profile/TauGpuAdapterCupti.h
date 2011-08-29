b09e7166c6904b230241dc6065b4736a48969b64
Scott Biersdorff <scottb@mist.cs.uoregon.edu>
2011-08-29 12:14:43 -0700

    Support added for mem-copies and kernel launches. Synchronization and exit only if we have not synchronizated yet.

--- include/Profile/TauGpuAdapterCupti.h
+++ include/Profile/TauGpuAdapterCupti.h
@@ -38,36 +38,49 @@ void Tau_cupti_record_activity(CUpti_Activity *record);
 void __attribute__ ((constructor)) Tau_cupti_onload(void);
 void __attribute__ ((destructor)) Tau_cupti_onunload(void);
 
+void get_values_from_memcpy(const CUpti_CallbackData *info, CUpti_CallbackId id, CUpti_CallbackDomain domain, int &kind, int &count);
+
 int getMemcpyType(int kind);
 const char* demangleName(const char *n);
+
+int getParentFunction(uint32_t id);
+
+bool function_is_sync(CUpti_CallbackId id);
+bool function_is_memcpy(CUpti_CallbackId id);
+bool function_is_launch(CUpti_CallbackId id);
+
+bool registered_sync = false;
+
+map<uint32_t, FunctionInfo*> functionInfoMap;
 
 class cuptiGpuId : public gpuId
 {
 	uint32_t streamId;
-
+	uint32_t correlationId;
 public:
 
-	cuptiGpuId(uint32_t s) { streamId = s; };
+	cuptiGpuId(uint32_t s, uint32_t c) { streamId = s; correlationId = c; };
 	cuptiGpuId *getCopy() { 
 		cuptiGpuId *c = new cuptiGpuId(*this);
 		return c; 
 	};
 	char* printId() {
 		char *rtn = (char*) malloc(50*sizeof(char));
-		sprintf(rtn, "%d", streamId);
+		sprintf(rtn, "%d/%d", streamId, correlationId);
 		return rtn;
 	};
 	x_uint64 id_p1() {
-		return streamId;
+		return correlationId;
 	};
 	x_uint64 id_p2() { return 0; };
 
 	bool equals(const gpuId *other) const
 	{
-		return streamId == ((cuptiGpuId *)other)->id_p1();
+		return streamId == ((cuptiGpuId *)other)->stream();
 	};
 
 	double syncOffset() { return 0; };
+	uint32_t stream() { return streamId; };
 };
 
 
@@ -81,6 +94,26 @@ public:
 	cuptiRecord(const char* n, cuptiGpuId *id, FunctionInfo *site) : eventId(n, id, site)
 	{
 	};
-	cuptiRecord(const char* n, uint32_t id) : eventId(n, &cuptiGpuId(id), NULL) {};
+	cuptiRecord(const char* n, uint32_t stream, uint32_t correlation) : eventId(n, &cuptiGpuId(stream, correlation), getParentFunction(correlation)) {};
+
+	FunctionInfo* getParentFunction(uint32_t id)
+	{
+		FunctionInfo *funcInfo = NULL;
+		map<uint32_t, FunctionInfo*>::iterator it = functionInfoMap.find(id);
+		if (it != functionInfoMap.end())
+		{
+			funcInfo = it->second;
+		}
+		return funcInfo;
+	};
+
 
 };
+
+#define CAST_TO_RUNTIME_MEMCPY_TYPE_AND_CALL(name, id, info, kind, count) \
+	if ((id) == CUPTI_RUNTIME_TRACE_CBID_##name##_v3020) \
+	{ \
+		kind = ((name##_v3020_params *) info->functionParams)->kind; \
+		count = ((name##_v3020_params *) info->functionParams)->count; \
+	}
+
