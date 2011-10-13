0d5188347c4b8d34685d8fd45ec08f96da1014b2
Sameer Shende <sameer@login3.intrepid.alcf.anl.gov>
2011-10-13 01:52:16 +0000

    Added extern "C" to Tau_Bg_hwp_counters_stop/start/output calls.
    Former-commit-id: 8a3d12c2cd70d6e77fc6f4cd661ea2d591015342

--- src/Profile/TauCAPI.cpp
+++ src/Profile/TauCAPI.cpp
@@ -1646,13 +1646,13 @@ int *tau_pomp_rd_table = 0;
 #endif
 
 #ifndef TAU_BGP
-void Tau_Bg_hwp_counters_start(int *error) {
+extern "C" void Tau_Bg_hwp_counters_start(int *error) {
 }
 
-void Tau_Bg_hwp_counters_stop(int* numCounters, uint64_t counters[], int* mode, int *error) {
+extern "C" void Tau_Bg_hwp_counters_stop(int* numCounters, uint64_t counters[], int* mode, int *error) {
 }
 
-void Tau_Bg_hwp_counters_output(int* numCounters, uint64_t counters[], int* mode, int* error) {
+extern "C" void Tau_Bg_hwp_counters_output(int* numCounters, uint64_t counters[], int* mode, int* error) {
 }
 #endif /* TAU_BGP */
                     
