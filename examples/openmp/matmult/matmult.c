10ce9a04e0217ff5aef29bf6b12c63053fc012f9
Suzanne Millstein <smillst@cs.uoregon.edu>
2011-12-05 11:15:27 -0800

    Added two openmp examples that can be used as a benchmark for PAPI_FP_INS. See the README
    Former-commit-id: a723f2c4c73106070be4db2714841329ef474053

---
+++ examples/openmp/matmult/matmult.c
@@ -0,0 +1,77 @@
+/******************************************************************************
+*   OpenMp Example - Matrix Multiply - C Version
+*   Demonstrates a matrix multiply using OpenMP. 
+*
+*   Modified from here:
+*   https://computing.llnl.gov/tutorials/openMP/samples/C/omp_mm.c
+*
+*   For  PAPI_FP_INS, the exclusive count for the event: 
+*   for (null) [OpenMP location: file:matmult.c <55, 61>]
+*   should be  2E+06 / Number of Threads 
+******************************************************************************/
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NRA 100                 /* number of rows in matrix A */
+#define NCA 100                 /* number of columns in matrix A */
+#define NCB 100                  /* number of columns in matrix B */
+
+int main (int argc, char *argv[]) 
+{
+int	tid, nthreads, i, j, k;
+double	a[NRA][NCA],           /* matrix A to be multiplied */
+	b[NCA][NCB],           /* matrix B to be multiplied */
+	c[NRA][NCB];           /* result matrix C */
+
+
+/*** Spawn a parallel region explicitly scoping all variables ***/
+#pragma omp parallel shared(a,b,c,nthreads) private(tid,i,j,k)
+  {
+  tid = omp_get_thread_num();
+  if (tid == 0)
+    {
+    nthreads = omp_get_num_threads();
+    printf("Starting matrix multiple example with %d threads\n",nthreads);
+    printf("Initializing matrices...\n");
+    }
+  /*** Initialize matrices ***/
+  #pragma omp for schedule (static ) 
+  for (i=0; i<NRA; i++)
+    for (j=0; j<NCA; j++)
+      a[i][j]= i+j;
+  #pragma omp for schedule (static )
+  for (i=0; i<NCA; i++)
+    for (j=0; j<NCB; j++)
+      b[i][j]= i*j;
+  #pragma omp for schedule (static)
+  for (i=0; i<NRA; i++)
+    for (j=0; j<NCB; j++)
+      c[i][j]= 0;
+
+  /*** Do matrix multiply sharing iterations on outer loop ***/
+  /*** Display who does which iterations for demonstration purposes ***/
+  printf("Thread %d starting matrix multiply...\n",tid);
+  #pragma omp for schedule (static )
+  for (i=0; i<NRA; i++)    
+    {
+    for(j=0; j<NCB; j++)       
+      for (k=0; k<NCA; k++)
+        c[i][j] += a[i][k] * b[k][j];
+    }
+  }   /*** End of parallel region ***/
+
+/*** Print results **
+printf("******************************************************\n");
+printf("Result Matrix:\n");
+for (i=0; i<NRA; i++)
+  {
+  for (j=0; j<NCB; j++) 
+    printf("%6.2f   ", c[i][j]);
+  printf("\n"); 
+  }
+printf("******************************************************\n");
+*/
+printf ("Done.\n");
+
+}
