bcc1ac5e18e843be7256388500d640d7da19213b
Scott Biersdorff <scottb@adagio.nic.uoregon.edu>
2011-12-22 17:37:19 -0800

    Added C interface to myThread().
    Former-commit-id: 2d5d9819de7d265f07a2a0bfadddeff386c4ebd8

--- src/Profile/RtsThread.cpp
+++ src/Profile/RtsThread.cpp
@@ -145,6 +145,10 @@ int RtsLayer::myThread(void)
 #endif // PTHREADS
 }
 
+extern "C" int Tau_RtsLayer_myThread(void) {
+	return RtsLayer::myThread();
+}
+
 int RtsLayer::setMyThread(int i) { 
 #ifdef PTHREADS
 	PthreadLayer::SetThreadId(i);
