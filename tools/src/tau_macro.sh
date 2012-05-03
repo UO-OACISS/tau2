6db0ead84d9ca753408c40bc8f7aa5e9aaa5a7f3
Sameer Shende <sameer@cs.uoregon.edu>
2012-05-03 13:36:58 -0700

    Generates base.pp.suffix file now. Integrates with tau_compiler.sh.
    Former-commit-id: da0209e5532a24245466fb50529c9566f3477601

--- tools/src/tau_macro.sh
+++ tools/src/tau_macro.sh
@@ -2,16 +2,20 @@
 if [ $# = 1 ]
 then
   echo "Usage: $0 <filename> -D<variables> "
-  echo "generates taucpp.<filename> as output where macros are expanded but header files are not"
+  echo "generates <base>.pp.<suffix> as output where macros are expanded but header files are not"
   exit 1
 fi
 
 filename=$1
+base=`echo $filename | sed -e 's/\.[^\.]*$//' -e's/.*\///'`
+# this transforms /this/file\ name/has/spaces/ver1.0.2/foo.pp.F90 to foo.pp for base
+suf=`echo $filename | sed -e 's/.*\./\./' `
+
 shift
 cpp $filename $* -dM > $filename.tau.inc
 sed -e 's@#include@//TAU_INCLUDE#include@g' $filename > $filename.tau.tmp
 #echo "cpp $filename.tau.tmp  $* -P -traditional-cpp -w -include $filename.tau.inc"
-cpp $filename.tau.tmp  $* -CC -P -traditional-cpp -w -include $filename.tau.inc  | sed -e 's@//TAU_INCLUDE#include@#include@g' > taucpp.$filename
+cpp $filename.tau.tmp  $* -CC -P -traditional-cpp -w -include $filename.tau.inc  | sed -e 's@//TAU_INCLUDE#include@#include@g' > $base.pp$suf
 
 # -CC preserves the comments
 # -P removes any line number cluter 
