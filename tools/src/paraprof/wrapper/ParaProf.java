1a11ed095449ff5144f71da9fc39157779af265a
Kevin Huck <khuck@cs.uoregon.edu>
2012-05-08 20:58:45 -0700

    Forgot to check in new classes. ParaProfView and ViewTableModel for displaying views in ParaProf.
    Former-commit-id: 0c79b8f7de7a58357cbfcfa640ca6fc104c9587f

--- tools/src/paraprof/wrapper/ParaProf.java
+++ tools/src/paraprof/wrapper/ParaProf.java
@@ -54,7 +54,7 @@ public class ParaProf implements ActionListener {
 	}
     }
 
-    private final static String VERSION = "Thu Apr 26 14:27:35 PDT 2012";
+    private final static String VERSION = "Tue May  8 20:02:54 PDT 2012";
 
     public static int defaultNumberPrecision = 6;
 
