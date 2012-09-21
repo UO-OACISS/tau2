83576d94dbe9d7876268e03da2d92ab5697cd227
Suzanne Millstein <smillst@cs.uoregon.edu>
2012-09-21 11:46:58 -0700

    First working verison of the view creator.
    Former-commit-id: 57b09bd56e49be58e6a5378b2f22bcd250a782c7

--- tools/src/perfdmf/src/viewcreator/RuleListener.java
+++ tools/src/perfdmf/src/viewcreator/RuleListener.java
@@ -8,6 +8,8 @@ import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.text.BadLocationException;
 
+import edu.uoregon.tau.perfdmf.taudb.TAUdbTrial;
+
 public class RuleListener implements DocumentListener, ActionListener {
 	private static final String WILDCARD = "%";
 	/*
@@ -36,13 +38,24 @@ VALUES (2, 'primary_metadata', 'Application', '=', 'application');
 		this.column_name = column_name;
 	}
 	public String getOperator() {
+		if(operator == ViewCreatorGUI.STRING_BEGINS || operator == ViewCreatorGUI.STRING_ENDS
+				||operator == ViewCreatorGUI.STRING_CONTAINS){
+			return "like";
+		}
 		return operator;
 	}
 	public void setOperator(String operator) {
 		this.operator = operator;
 	}
 	public String getValue() {
-		fixValue();
+		if(operator == ViewCreatorGUI.STRING_BEGINS){
+			return value+WILDCARD;
+		} else if(operator == ViewCreatorGUI.STRING_ENDS){
+			return WILDCARD + value;
+		}else if(operator == ViewCreatorGUI.STRING_CONTAINS){
+			return WILDCARD + value+WILDCARD;
+		}
+
 		return value;
 	}
 	public void setValue(String value) {
@@ -54,24 +67,16 @@ VALUES (2, 'primary_metadata', 'Application', '=', 'application');
 	public void setValue2(String value2) {
 		this.value2 = value2;
 	}
-	public String getCompare() {
-		return compare;
-	}
-	public void setCompare(String compare) {
-		this.compare = compare;
-	}
+	
 	String table_name="primary_metadata"; //primary or secondary metadata
 	String column_name=""; //Metadata name
 	String operator=""; //= > < 
 	String value=""; //value of field
 	String value2="";
 	String type = "";
-	private String compare="";
 
 	private void change (DocumentEvent e){
-		// TODO Auto-generated method stub
-		try {
-			
+		try {			
 			 
 			if(e.getDocument().getProperty(ViewCreatorGUI.NUMBER_RANGE) != null){
 				String range = (String)e.getDocument().getProperty(ViewCreatorGUI.NUMBER_RANGE) ;
@@ -104,64 +109,43 @@ VALUES (2, 'primary_metadata', 'Application', '=', 'application');
 	public void actionPerformed(ActionEvent arg) {
 		if( "comboBoxChanged".equals(arg.getActionCommand())){
 			JComboBox combo = (JComboBox) arg.getSource();
-			if(combo.getName() == ViewCreatorGUI.STRING || combo.getName() == ViewCreatorGUI.NUMBER){
-			 compare = combo.getSelectedItem().toString();
-			}else if(combo.getName() == ViewCreatorGUI.STRING_BEGINS){
-				operator = "like";
-			}else if(combo.getName() == ViewCreatorGUI.STRING_CONTAINS){
-				operator = "like";
-			}else if(combo.getName() == ViewCreatorGUI.STRING_ENDS){
-				operator = "like";
-			}else if(combo.getName() == ViewCreatorGUI.STRING_EXACTLY){
-				operator = "=";
-			}else if(combo.getName() == ViewCreatorGUI.NUMBER_EQUAL){
-				operator = "=";
-			}else if(combo.getName() == ViewCreatorGUI.NUMBER_GREATER){
-				operator = ">";
-			}else if(combo.getName() == ViewCreatorGUI.NUMBER_LESS){
-				operator = "<";
-			}else if(combo.getName() == ViewCreatorGUI.NUMBER_NOT){
-				operator = "!=";
-
-			}else if(combo.getName() == ViewCreatorGUI.NUMBER_RANGE){
+			if(combo.getName() == ViewCreatorGUI.NUMBER_RANGE){
 				operator = ViewCreatorGUI.NUMBER_RANGE;
-System.out.println(compare);
 			}else if(combo.getName() == ViewCreatorGUI.METADATA){
 				column_name = combo.getSelectedItem().toString();
+				if(isTrialCol(column_name)){
+					table_name="trial"; 
+				}else{
+					table_name="primary_metadata";
+				}
 				System.out.println("colname: "+column_name);
 			}else if(combo.getName() == ViewCreatorGUI.READ_TYPE){
 				type = combo.getSelectedItem().toString();
 			}else {
 				System.out.println("Op:  "+combo.getSelectedItem().toString());
 				operator = combo.getSelectedItem().toString();
+			
+				if(operator == ViewCreatorGUI.STRING_EXACTLY){
+					operator = "=";
+				}else if(operator == ViewCreatorGUI.NUMBER_EQUAL){
+					operator = "=";
+				}else if(operator == ViewCreatorGUI.NUMBER_NOT){
+					operator = "!=";
+				}else if(operator == ViewCreatorGUI.NUMBER_GREATER){
+					operator = ">";
+				}else if(operator == ViewCreatorGUI.NUMBER_LESS){
+					operator = "<";
+				}else if(operator == ViewCreatorGUI.NUMBER_RANGE){
+					//Need to create two rules in this case.
+					operator = ViewCreatorGUI.NUMBER_RANGE;
+				}
 			}
 		}
+ 	}
+	private boolean isTrialCol(String column) {
+		for(String s: TAUdbTrial.TRIAL_COLUMNS)
+			if(s.equals(column)) return true;
+		return false;
 	}
-	public void fixValue(){
-		if(value.contains(WILDCARD)) return;
-		if(compare == ViewCreatorGUI.STRING_BEGINS){
-			operator = "like";
-			value = value+WILDCARD;
-		} else if(compare == ViewCreatorGUI.STRING_ENDS){
-			operator = "like";
-			value = WILDCARD + value;
-		}else if(compare == ViewCreatorGUI.STRING_CONTAINS){
-			operator = "like";
-			value = WILDCARD + value+WILDCARD;
-		}else if(compare == ViewCreatorGUI.STRING_EXACTLY){
-			operator = "=";
-		}else if(compare == ViewCreatorGUI.NUMBER_EQUAL){
-			operator = "=";
-		}else if(compare == ViewCreatorGUI.NUMBER_NOT){
-			operator = "!=";
-		}else if(compare == ViewCreatorGUI.NUMBER_GREATER){
-			operator = ">";
-		}else if(compare == ViewCreatorGUI.NUMBER_LESS){
-			operator = "<";
-		}else if(compare == ViewCreatorGUI.NUMBER_RANGE){
-			//Need to create two rules in this case.
-			operator = ViewCreatorGUI.NUMBER_RANGE;
-		}
-		System.out.println("fix: "+operator);
-	}
+
 }
