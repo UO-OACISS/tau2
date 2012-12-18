c35fff714c3342a2fb164478816d10f593e1a7c8
Scott Biersdorff <scottb@werewolf.nic.uoregon.edu>
2012-12-17 17:17:00 -0800

    Added TAU_ASSERT macro which is enable for TAU configurations built with DEBUG_ASSERT macro set. Will print backtrace on failed assert if execinfo is available.
    Former-commit-id: 4d92d1b5686924fef57eb1f9978eb1a0870de110

--- src/Profile/RtsThread.cpp
+++ src/Profile/RtsThread.cpp
@@ -464,18 +464,9 @@ int RtsLayer::LockDB(void) {
   static bool init = initLocks();
   int tid=myThread();
 /* This block of code is helpful in debugging deadlocks... see the top of this file */
+	TAU_ASSERT(Tau_global_get_insideTAU_tid(tid) <= 0,
+		"Thread is trying for another DB lock. but it is not in TAU");
 #ifdef DEBUG_LOCK_PROBLEMS
-	if (Tau_global_get_insideTAU_tid(tid) <= 0) {
-		printf("ERROR! Thread %d is trying for another DB lock. but it is not in TAU!\n", tid);
-		void* callstack[128];
-		int i, frames = backtrace(callstack, 128);
-		char** strs = backtrace_symbols(callstack, frames);
-		for (i = 0; i < frames; ++i) {
-			 printf("%s\n", strs[i]);
-		}
-		free(strs);
-    exit(999);
-	}
   if (lockDBCount[tid] > 0) {
     printf("WARNING! Thread %d has DB lock, trying for another DB lock\n", tid);
      void* callstack[128];
@@ -575,19 +566,10 @@ int RtsLayer::getNumEnvLocks(void) {
 int RtsLayer::LockEnv(void) {
   static bool init = initEnvLocks();
   int tid=myThread();
+	TAU_ASSERT(Tau_global_get_insideTAU_tid(tid) <= 0,
+		"Thread is trying for another Env lock. but it is not in TAU");
 /* This block of code is helpful in debugging deadlocks... see the top of this file */
 #ifdef DEBUG_LOCK_PROBLEMS
-	if (Tau_global_get_insideTAU_tid(tid) <= 0) {
-		printf("ERROR! Thread %d is trying for another Env lock. but it is not in TAU!\n", tid);
-		void* callstack[128];
-		int i, frames = backtrace(callstack, 128);
-		char** strs = backtrace_symbols(callstack, frames);
-		for (i = 0; i < frames; ++i) {
-			 printf("%s\n", strs[i]);
-		}
-		free(strs);
-    exit(999);
-	}
   if (lockEnvCount[tid] > 0) {
     printf("WARNING! Thread %d has Env lock, trying for another Env lock\n", tid);
      void* callstack[128];
@@ -598,19 +580,10 @@ int RtsLayer::LockEnv(void) {
      }
      free(strs);
   }
-  if (lockDBCount[tid] > 0) {
-    printf("ERROR! Thread %d has DB lock, trying for Env lock\n", tid);
-     void* callstack[128];
-     int i, frames = backtrace(callstack, 128);
-     char** strs = backtrace_symbols(callstack, frames);
-     for (i = 0; i < frames; ++i) {
-         printf("%s\n", strs[i]);
-     }
-     free(strs);
-    exit(999);
-  }
 #endif
-  if (lockEnvCount[tid] == 0) {
+  TAU_ASSERT(lockDBCount[tid] > 0,
+		"Thread has DB lock, trying for Env lock");
+	if (lockEnvCount[tid] == 0) {
     threadLockEnv();
   }
   lockEnvCount[tid]++;
