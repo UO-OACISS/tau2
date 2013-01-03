35863e56e406e2ecc9c280ebac578c6fae7eac21
John C. Linford <jlinford@paratools.com>
2013-01-03 14:36:14 -0500

    Add more rbc examples.
    Former-commit-id: 99c16408d6aee3574cc2e3e3c10499cd47ab2cab

---
+++ examples/rbc/c++/rbc.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+#define DATA_COUNT 10
+#define OVERRUN    0
+
+int main(int argc, char ** argv)
+{
+  int i;
+  int * data;
+
+  double * x = new double;
+
+  data = new int[DATA_COUNT];
+
+  for(i=0; i<DATA_COUNT+OVERRUN; ++i) {
+    cout << "Setting data[" << i << "] to " << i << endl;
+    data[i] = i;
+    cout << "data[" << i << "] = " << i << endl;
+  }
+
+  delete x;
+
+  delete[] data;
+
+  cout << "All done!" << endl;
+
+  return 0;
+}
