536f61ec601ebab759895188658fe4cf041c0125
John C. Linford <jlinford@paratools.com>
2013-01-25 17:06:41 -0500

    Better portability in memory wrapper files.
    Former-commit-id: f96b98734fe96224f72c63b1db8ab1d7d5c8cc7d

--- src/wrappers/memory/memory_wrapper_static.c
+++ src/wrappers/memory/memory_wrapper_static.c
@@ -60,21 +60,26 @@ int Tau_memory_wrapper_passthrough(void)
       || Tau_global_get_insideTAU();
 }
 
-
+#ifdef HAVE_MALLOC
 malloc_t Tau_get_system_malloc()
 {
   return __real_malloc;
 }
+#endif
 
+#ifdef HAVE_CALLOC
 calloc_t Tau_get_system_calloc()
 {
   return __real_calloc;
 }
+#endif
 
+#ifdef HAVE_REALLOC
 realloc_t Tau_get_system_realloc()
 {
   return __real_realloc;
 }
+#endif
 
 #ifdef HAVE_MEMALIGN
 memalign_t Tau_get_system_memalign()
@@ -83,15 +88,19 @@ memalign_t Tau_get_system_memalign()
 }
 #endif
 
+#ifdef HAVE_POSIX_MEMALIGN
 posix_memalign_t Tau_get_system_posix_memalign()
 {
   return __real_posix_memalign;
 }
+#endif
 
+#ifdef HAVE_VALLOC
 valloc_t Tau_get_system_valloc()
 {
   return __real_valloc;
 }
+#endif
 
 #ifdef HAVE_PVALLOC
 pvalloc_t Tau_get_system_pvalloc()
@@ -100,26 +109,33 @@ pvalloc_t Tau_get_system_pvalloc()
 }
 #endif
 
+#ifdef HAVE_FREE
 free_t Tau_get_system_free()
 {
   return __real_free;
 }
+#endif
 
-
+#ifdef HAVE_MALLOC
 void * __wrap_malloc(size_t size)
 {
   return malloc_handle(size);
 }
+#endif
 
+#ifdef HAVE_CALLOC
 void * __wrap_calloc(size_t count, size_t size)
 {
   return calloc_handle(count, size);
 }
+#endif
 
+#ifdef HAVE_FREE
 void __wrap_free(void * ptr)
 {
   return free_handle(ptr);
 }
+#endif
 
 #ifdef HAVE_MEMALIGN
 void * __wrap_memalign(size_t alignment, size_t size)
@@ -128,20 +144,26 @@ void * __wrap_memalign(size_t alignment, size_t size)
 }
 #endif
 
+#ifdef HAVE_POSIX_MEMALIGN
 int __wrap_posix_memalign(void **ptr, size_t alignment, size_t size)
 {
   return posix_memalign_handle(ptr, alignment, size);
 }
+#endif
 
+#ifdef HAVE_REALLOC
 void * __wrap_realloc(void * ptr, size_t size)
 {
   return realloc_handle(ptr, size);
 }
+#endif
 
+#ifdef HAVE_VALLOC
 void * __wrap_valloc(size_t size)
 {
   return valloc_handle(size);
 }
+#endif
 
 #ifdef HAVE_PVALLOC
 void * __wrap_pvalloc(size_t size)
