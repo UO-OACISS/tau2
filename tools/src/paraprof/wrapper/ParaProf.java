0ff19945302f87c61e86aa9f4269072719307100
wspear <wspear@cs.uoregon.edu>
2013-03-25 13:01:00 -0700

    Rebuilt jars
    Former-commit-id: ae9877c4e14fa14b399ea029d5cf64ab652cf877

--- tools/src/paraprof/wrapper/ParaProf.java
+++ tools/src/paraprof/wrapper/ParaProf.java
@@ -54,7 +54,7 @@ public class ParaProf implements ActionListener {
 	}
     }
 
-    private final static String VERSION = "Fri Mar 22 18:14:45 PDT 2013";
+    private final static String VERSION = "Mon Mar 25 13:00:25 PDT 2013";
 
     public static int defaultNumberPrecision = 6;
 
