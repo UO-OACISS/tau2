a183fffddda25c3ea80e432f74010e8e04b6ad98
John C. Linford <jlinford@paratools.com>
2013-03-25 09:42:55 -0700

    Adding missing BFD interface stubs.
    Former-commit-id: 61dbbe020e2e930f526c7b8d65409755773835d7

--- src/Profile/TauBfdUnimplemented.cpp
+++ src/Profile/TauBfdUnimplemented.cpp
@@ -15,6 +15,7 @@
 #include <stdint.h>
 #include <string.h>
 #include <vector>
+
 using namespace std;
 
 #define TAU_BFD_UNIMPLEMENTED_HANDLE -1337
@@ -26,64 +27,78 @@ typedef int tau_bfd_module_handle_t;
 static vector<TauBfdAddrMap*> addressMaps;
 
 // Empty interface functions
-static void Tau_bfd_internal_issueBfdWarningIfNecessary() {
+static void Tau_bfd_internal_issueBfdWarningIfNecessary()
+{
   static bool warningIssued = false;
   if (!warningIssued) {
-    fprintf(stderr,"TAU Warning: BFD is not available in at least one part "
-    		"of this TAU-instrumented application! Please check to see if "
-    		"BFD is not shared or not present. Expect some missing BFD "
-    		"functionality.\n");
+    fprintf(stderr, "TAU Warning: BFD is not available in at least one part "
+        "of this TAU-instrumented application! Please check to see if "
+        "BFD is not shared or not present. Expect some missing BFD "
+        "functionality.\n");
     warningIssued = true;
   }
 }
 
-void Tau_bfd_initializeBfd() {
+void Tau_bfd_initializeBfd()
+{
   Tau_bfd_internal_issueBfdWarningIfNecessary();
 }
 
-tau_bfd_handle_t Tau_bfd_registerUnit() {
+tau_bfd_handle_t Tau_bfd_registerUnit()
+{
   Tau_bfd_internal_issueBfdWarningIfNecessary();
   return TAU_BFD_UNIMPLEMENTED_HANDLE;
 }
 
-bool Tau_bfd_checkHandle(tau_bfd_handle_t handle) {
+bool Tau_bfd_checkHandle(tau_bfd_handle_t handle)
+{
   Tau_bfd_internal_issueBfdWarningIfNecessary();
   return (handle == TAU_BFD_UNIMPLEMENTED_HANDLE);
 }
 
-void Tau_bfd_updateAddressMaps(tau_bfd_handle_t handle) {
+void Tau_bfd_updateAddressMaps(tau_bfd_handle_t handle)
+{
   Tau_bfd_internal_issueBfdWarningIfNecessary();
   Tau_bfd_checkHandle(handle);
 }
 
-std::vector<TauBfdAddrMap*> const &
-Tau_bfd_getAddressMaps(tau_bfd_handle_t handle) {
+bool Tau_bfd_resolveBfdInfo(tau_bfd_handle_t handle, unsigned long probeAddr, TauBfdInfo & info)
+{
+  Tau_bfd_internal_issueBfdWarningIfNecessary();
+  info.secure(probeAddr);
+  return false;
+}
+
+int Tau_bfd_processBfdExecInfo(tau_bfd_handle_t handle, TauBfdIterFn fn)
+{
+  Tau_bfd_internal_issueBfdWarningIfNecessary();
+  return TAU_BFD_SYMTAB_LOAD_FAILED;
+}
+
+int Tau_bfd_processBfdModuleInfo(tau_bfd_handle_t handle, tau_bfd_module_handle_t moduleHandle, TauBfdIterFn fn)
+{
+  Tau_bfd_internal_issueBfdWarningIfNecessary();
+  return TAU_BFD_SYMTAB_LOAD_FAILED;
+}
+
+std::vector<TauBfdAddrMap*> const & Tau_bfd_getAddressMaps(tau_bfd_handle_t handle)
+{
   Tau_bfd_internal_issueBfdWarningIfNecessary();
   return addressMaps;
 }
 
-TauBfdAddrMap const *
-Tau_bfd_getAddressMap(tau_bfd_handle_t handle, unsigned long probe_addr)
+TauBfdAddrMap const * Tau_bfd_getAddressMap(tau_bfd_handle_t handle, unsigned long probe_addr)
 {
-	Tau_bfd_internal_issueBfdWarningIfNecessary();
-	return NULL;
+  Tau_bfd_internal_issueBfdWarningIfNecessary();
+  return NULL;
 }
 
-tau_bfd_module_handle_t Tau_bfd_getModuleHandle(tau_bfd_handle_t handle,
-		unsigned long probeAddr)
+tau_bfd_module_handle_t Tau_bfd_getModuleHandle(tau_bfd_handle_t handle, unsigned long probeAddr)
 {
-	Tau_bfd_internal_issueBfdWarningIfNecessary();
-	if (!Tau_bfd_checkHandle(handle)) {
-		return TAU_BFD_NULL_MODULE_HANDLE;
-	}
-	return TAU_BFD_UNIMPLEMENTED_MODULE_HANDLE;
-}
-
-bool Tau_bfd_resolveBfdInfo(tau_bfd_handle_t handle,
-		unsigned long probeAddr, TauBfdInfo & info)
-{
-	Tau_bfd_internal_issueBfdWarningIfNecessary();
-	info.secure(probeAddr);
-	return false;
+  Tau_bfd_internal_issueBfdWarningIfNecessary();
+  if (!Tau_bfd_checkHandle(handle)) {
+    return TAU_BFD_NULL_MODULE_HANDLE;
+  }
+  return TAU_BFD_UNIMPLEMENTED_MODULE_HANDLE;
 }
 
