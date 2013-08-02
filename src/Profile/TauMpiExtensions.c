927f0c6ec73855cf792d6a30ef128414da8f05aa
Sameer Shende <sameer@cs.uoregon.edu>
2013-08-02 13:53:20 -0700

    Added #include <sys/types.h>
    Former-commit-id: d96cdb8d28b8f056c947ba7fb3279d89df7f6f96

--- src/Profile/TauMpiExtensions.c
+++ src/Profile/TauMpiExtensions.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 #include <Profile/TauUtil.h>
 /******************************************************/
 /******************************************************/
