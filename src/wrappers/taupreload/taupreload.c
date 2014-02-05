94ca0b9fb59ba6fbdde4b3ae93a325636a775cbd
Kevin Huck <khuck@cs.uoregon.edu>
2014-02-05 06:46:13 -0800

    On exit, terminate all timers and write all profiles.
    Former-commit-id: 12a77f10bec06522d6da18803cd756263cfd5552

--- src/wrappers/taupreload/taupreload.c
+++ src/wrappers/taupreload/taupreload.c
@@ -15,5 +15,6 @@ void taupreload_init() {
 }
 
 void taupreload_fini() {
-  Tau_stop_top_level_timer_if_necessary();
+  //Tau_stop_top_level_timer_if_necessary();
+  Tau_profile_exit_all_threads();
 }
