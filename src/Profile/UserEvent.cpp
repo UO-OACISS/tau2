UO-OACISS/tau2

commit dc2b3999a4266e1a17c90ae5b265dde3f896d13e

Added support for TAU_TRIGGER_EVENT("ename", data); similar to TAU_TRIGGER_CONTEXT_EVENT("ename", data);
Added support for markers in atomic events.

Markers show sudden variation in atomic events. They can be enabled by configuring TAU with
-useropt=-DTAU_USE_EVENT_THRESHOLDS. By default, when an atomic events value changes by 10%,
a marker context event is triggered with the value that caused this condition. To change the threshold to
something other than 0.1 (or 10%) to say 0.5 (or 50%), you may configure using:
-useropt=-DTAU_USE_EVENT_THRESHOLDS#-DTAU_EVENT_THRESHOLD=0.5

As a simple example consider a function triggering events such as:
300, 301, 304, 299, 2000, 21, 5, we see in foo.c:
% ./foo
Inside foo: x = 300
Inside foo: x = 301
Inside foo: x = 304
Inside foo: x = 299
Inside foo: x = 2000
Inside foo: x = 21
Inside foo: x = 5
% pprof
Reading Profile files in profile.*

NODE 0;CONTEXT 0;THREAD 0:
---------------------------------------------------------------------------------------
%Time    Exclusive    Inclusive       #Call      #Subrs  Inclusive Name
              msec   total msec                          usec/call
---------------------------------------------------------------------------------------
100.0        0.009        0.145           1           7        145 int main(int, char **) C
 93.8        0.136        0.136           7           0         19 int foo(int) C
---------------------------------------------------------------------------------------

USER EVENTS Profile :NODE 0, CONTEXT 0, THREAD 0
---------------------------------------------------------------------------------------
NumSamples   MaxValue   MinValue  MeanValue  Std. Dev.  Event Name
---------------------------------------------------------------------------------------
         1       2000       2000       2000          0  [GROUP=MAX_MARKER] foo counter

src/Profile/UserEvent.cpp
@@ -20,6 +20,14 @@
 #pragma mta instantiate used
 #endif /* TAU_CRAYXMT */
 
+#define TAU_USE_EVENT_THRESHOLDS 1 
+
+#ifdef TAU_USE_EVENT_THRESHOLDS
+#ifndef TAU_EVENT_THRESHOLD
+#define TAU_EVENT_THRESHOLD .1
+#endif /* TAU_EVENT_THRESHOLD */
+#endif 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -192,11 +200,43 @@ void TauUserEvent::TriggerEvent(TAU_EVENT_DATATYPE data, int tid, double timesta
   // Increment number of events
   ++d.nEvents;
 
+  std::size_t pos;
   // Compute relevant statistics for the data 
   if (minEnabled && data < d.minVal) {
+
+#ifdef TAU_USE_EVENT_THRESHOLDS
+    if (d.nEvents > 1 && data <= (1.0 - TAU_EVENT_THRESHOLD) * d.minVal) 
+    {
+      if (name.data()[0] != '[') { //re-entrant 
+        string ename(string("[GROUP=MIN_MARKER] ")+name);
+        pos = name.find("=>");
+        if (pos == std::string::npos) {
+          DEBUGPROFMSG("Marker: "<<ename<<"  d.minVal = "<<d.minVal<<" data = "<<data<<" d.nEvents = "<<d.nEvents<<endl;);
+          TAU_TRIGGER_CONTEXT_EVENT(ename.c_str(), data);
+        }
+      }
+    }
+   
+#endif /* TAU_USE_EVENT_THRESHOLDS */
     d.minVal = data;
+    
   }
   if (maxEnabled && data > d.maxVal) {
+#ifdef TAU_USE_EVENT_THRESHOLDS
+    if (d.nEvents > 1 && data >= (1.0 + TAU_EVENT_THRESHOLD) * d.maxVal) 
+    {
+      if (name.data()[0] != '[') { //re-entrant 
+        string ename(string("[GROUP=MAX_MARKER] ")+name);
+        pos = name.find("=>");
+        if (pos == std::string::npos) {
+          DEBUGPROFMSG("Marker: "<<ename<<"  d.maxVal = "<<d.maxVal<<" data = "<<data<<" d.nEvents = "<<d.nEvents<<endl;);
+          TAU_TRIGGER_CONTEXT_EVENT(ename.c_str(), data);
+        }
+      }
+      
+    }
+#endif /* TAU_USE_EVENT_THRESHOLDS */
+
     d.maxVal = data;
   }
   if (meanEnabled) {

include/Profile/TauAPI.h
@@ -169,6 +169,7 @@
                                  if (event == 0) Tau_get_context_userevent(&event, name);  
 
 #define TAU_TRIGGER_CONTEXT_EVENT(eventname, eventvalue) Tau_trigger_context_event(eventname, eventvalue)
+#define TAU_TRIGGER_EVENT(eventname, eventvalue) Tau_trigger_userevent(eventname, eventvalue)
 #define TAU_EVENT(event, data)			Tau_userevent(event, data);
 #define TAU_EVENT_THREAD(event, data, tid)				Tau_userevent_thread(event, data, tid)
 #define TAU_CONTEXT_EVENT(event, data)		Tau_context_userevent(event, data);
@@ -509,6 +510,7 @@ void Tau_register_fork(int nodeid, enum TauFork_t opcode);
 void* TAUDECL Tau_get_userevent(char const * name);
 void Tau_get_context_userevent(void **ptr, const char *name);
 void Tau_trigger_context_event(const char *name, double data);
+void Tau_trigger_userevent(const char *name, double data);
 void Tau_userevent(void *event, double data);
 void Tau_userevent_thread(void *event, double data, int tid);
 void Tau_context_userevent(void *event, double data);

src/Profile/TauCAPI.cpp
@@ -1387,6 +1387,12 @@ pure_atomic_map_t & ThePureAtomicMap() {
   return pureAtomicMap;
 }
 
+typedef TAU_HASH_MAP<string, TauUserEvent *> pure_userevent_atomic_map_t;
+pure_userevent_atomic_map_t & ThePureUserEventAtomicMap() {
+  static pure_userevent_atomic_map_t pureUserEventAtomicMap;
+  return pureUserEventAtomicMap;
+}
+
 extern "C" void Tau_pure_context_userevent(void **ptr, const char* name)
 {
   TauInternalFunctionGuard protects_this_function;
@@ -1403,6 +1409,22 @@ extern "C" void Tau_pure_context_userevent(void **ptr, const char* name)
   *ptr = (void *) ue;
 }
 
+extern "C" void Tau_pure_userevent(void **ptr, const char* name)
+{
+  TauInternalFunctionGuard protects_this_function;
+  TauUserEvent *ue = 0;
+  RtsLayer::LockEnv();
+  pure_userevent_atomic_map_t::iterator it = ThePureUserEventAtomicMap().find(string(name));
+  if (it == ThePureUserEventAtomicMap().end()) {
+    ue = new TauUserEvent(name); 
+    ThePureUserEventAtomicMap()[string(name)] = ue;
+  } else {
+    ue = (*it).second;
+  }
+  RtsLayer::UnLockEnv();
+  *ptr = (void *) ue;
+}
+
 
 
 ///////////////////////////////////////////////////////////////////////////
@@ -1412,12 +1434,21 @@ extern "C" void Tau_context_userevent(void *ue, double data) {
   t->TriggerEvent(data);
 } 
 
+
 ///////////////////////////////////////////////////////////////////////////
 extern "C" void Tau_trigger_context_event(const char *name, double data) {
   TauInternalFunctionGuard protects_this_function;
   void *ue;
   Tau_pure_context_userevent(&ue, name);
   Tau_context_userevent(ue, data);
+}
+
+///////////////////////////////////////////////////////////////////////////
+extern "C" void Tau_trigger_userevent(const char *name, double data) {
+  TauInternalFunctionGuard protects_this_function;
+  void *ue;
+  Tau_pure_userevent(&ue, name);
+  Tau_userevent(ue, data);
 }
 
 ///////////////////////////////////////////////////////////////////////////

commit 67f586dee1c9d6bbc197f3b236df6930abcfe06d

Bug fix: strings allocated in _do_global_detors
We must not allocate memory in global destructors.
Nearly every std::string operation eventually allocates.

This bug introduced in dc2b3999a4266e1a17c90ae5b265dde3f896d13e [formerly e465b7df4f65a95fd1a4bb76ce3b2015f440d252]


Former-commit-id: c35655284dc45af298e8f0df3733604d1584fed7

src/Profile/UserEvent.cpp
@@ -196,45 +196,39 @@ void TauUserEvent::TriggerEvent(TAU_EVENT_DATATYPE data, int tid, double timesta
   // Increment number of events
   ++d.nEvents;
 
-  std::size_t pos;
   // Compute relevant statistics for the data 
   if (minEnabled && data < d.minVal) {
-
-#ifdef TAU_USE_EVENT_THRESHOLDS
-    if (d.nEvents > 1 && data <= (1.0 - TauEnv_get_evt_threshold()) * d.minVal) 
-    {
-      if (name.data()[0] != '[') { //re-entrant 
-        string ename(string("[GROUP=MIN_MARKER] ")+name);
-        pos = name.find("=>");
-        if (pos == std::string::npos) {
-          DEBUGPROFMSG("Marker: "<<ename<<"  d.minVal = "<<d.minVal<<" data = "<<data<<" d.nEvents = "<<d.nEvents<<endl;);
-          TAU_TRIGGER_CONTEXT_EVENT_THREAD(ename.c_str(), data, tid);
-        }
-      }
-    }
-   
-#endif /* TAU_USE_EVENT_THRESHOLDS */
     d.minVal = data;
-    
-  }
-  if (maxEnabled && data > d.maxVal) {
 #ifdef TAU_USE_EVENT_THRESHOLDS
-    if (d.nEvents > 1 && data >= (1.0 + TauEnv_get_evt_threshold()) * d.maxVal) 
-    {
-      if (name.data()[0] != '[') { //re-entrant 
-        string ename(string("[GROUP=MAX_MARKER] ")+name);
-        pos = name.find("=>");
-        if (pos == std::string::npos) {
-          DEBUGPROFMSG("Marker: "<<ename<<"  d.maxVal = "<<d.maxVal<<" data = "<<data<<" d.nEvents = "<<d.nEvents<<endl;);
-          TAU_TRIGGER_CONTEXT_EVENT_THREAD(ename.c_str(), data, tid);
+    if (d.nEvents > 1 && data <= (1.0 - TauEnv_get_evt_threshold()) * d.minVal) {
+      if (name[0] != '[') { //re-entrant 
+        char ename[20 + name.length()];
+        sprintf(ename, "[GROUP=MIN_MARKER] %s", name.c_str());
+        if (name.find("=>") == std::string::npos) {
+          //DEBUGPROFMSG("Marker: "<<ename<<"  d.minVal = "<<d.minVal<<" data = "<<data<<" d.nEvents = "<<d.nEvents<<endl;);
+          TAU_TRIGGER_CONTEXT_EVENT_THREAD(ename, data, tid);
         }
       }
-      
     }
 #endif /* TAU_USE_EVENT_THRESHOLDS */
-
-    d.maxVal = data;
   }
+
+  if (maxEnabled && data > d.maxVal) {
+    d.maxVal = data;
+#ifdef TAU_USE_EVENT_THRESHOLDS
+    if (d.nEvents > 1 && data >= (1.0 + TauEnv_get_evt_threshold()) * d.maxVal) {
+      if (name[0] != '[') { //re-entrant 
+        char ename[20 + name.length()];
+        sprintf(ename, "[GROUP=MAX_MARKER] %s", name.c_str());
+        if (name.find("=>") == std::string::npos) {
+          //DEBUGPROFMSG("Marker: "<<ename<<"  d.maxVal = "<<d.maxVal<<" data = "<<data<<" d.nEvents = "<<d.nEvents<<endl;);
+          TAU_TRIGGER_CONTEXT_EVENT_THREAD(ename, data, tid);
+        }
+      }
+    }
+#endif /* TAU_USE_EVENT_THRESHOLDS */
+  }
+
   if (meanEnabled) {
     d.sumVal += data;
   }