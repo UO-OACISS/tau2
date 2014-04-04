UO-OACISS/tau2

commit d0f7c682f5f96b9c0df50f235940ab828cb4cf15

Several important changes:
1. Correction to the OMP nested code. Will now get the thread id for each nested region such that subsequent starts/stops in the same region will receive the same thread id. This method requires the compiler to implement OMP spec version 3 (or greater). PGI does not yet implement version 3 so a check was placed in the code to inform the user of the limitation.

2. The Tau_lite_start_timer and Tau_lite_stop_timer calls no longer require a thread id parameter. We can lower overhead by first checking for throttling before getting the thread id.

3. Cleanup of the Comp_pgi code, improved clarity and added a faster id check for OpenMP code (this thread id is only valid for the incrementing/decrementing the ignore flag).


Former-commit-id: 52aee6aa2ff22acdbbb978c107f72aa3021713a5

src/Profile/TauInit.cpp
@@ -492,6 +492,9 @@ extern "C" int Tau_init_initializeTAU() {
 	}
 #endif
 
+	//Initialize locks.
+	RtsLayer::Initialize();	
+
   return 0;
 }
 

include/Profile/OpenMPLayer.h
@@ -37,6 +37,7 @@ class OpenMPLayer
  	OpenMPLayer () { }  // defaults
 	~OpenMPLayer () { } 
 
+	static void Initialize(void);
 	static int RegisterThread(void); // called before any profiling code
 	static int numThreads(void); // max number of OpenMP threads.
         static int InitializeThreadData(void);     // init thread mutexes

include/Profile/RtsLayer.h
@@ -79,6 +79,7 @@ public:
     else return str;
   }
 
+	static void Initialize(void);
 
   static int 	SetEventCounter(void);
   static double GetEventCounter(void);
@@ -106,7 +107,12 @@ public:
   static int myThread(void);
 
   static int threadId(void);
-  
+ 
+ 	// Return the local thread id (ignoring tasks) This is a 
+	// low-overhead call but DO NOT use this call when
+	// accessing Profiler stack or the FunctionInfo DB.
+  static int localThreadId(void);
+
   static int getPid();
   static int getTid();
 

include/Profile/TauAPI.h
@@ -51,8 +51,8 @@ extern "C" {
 #define TAU_PROFILE_CREATE_TIMER(var,name,type,group) Tau_profile_c_timer(&var, name, type, group, #group);
 
 
-#define TAU_PROFILE_START(var) Tau_lite_start_timer(var, 0, Tau_get_tid());
-#define TAU_PROFILE_STOP(var) Tau_lite_stop_timer(var, Tau_get_tid());
+#define TAU_PROFILE_START(var) Tau_lite_start_timer(var, 0);
+#define TAU_PROFILE_STOP(var) Tau_lite_stop_timer(var);
 #define TAU_PROFILE_STMT(stmt) stmt;
 
 
@@ -385,6 +385,7 @@ void Tau_set_inclusive_values(void *handle, double* values, int tid);
 void Tau_get_exclusive_values(void *handle, double* values, int tid);
 void Tau_set_exclusive_values(void *handle, double* values, int tid);
 void Tau_get_counter_info(const char ***counterlist, int *numcounters);
+int TAUDECL Tau_get_local_tid(void);
 int TAUDECL Tau_get_tid(void);
 int TAUDECL Tau_create_tid(void);
 int TAUDECL Tau_get_node(void);
@@ -459,9 +460,9 @@ void TAUDECL Tau_reducescatter_data(int data);
 void TAUDECL Tau_scan_data(int data);
 void TAUDECL Tau_set_node(int node);
 void TAUDECL Tau_start_timer(void *profiler, int phase, int tid);
-void TAUDECL Tau_lite_start_timer(void *profiler, int phase, int tid);
+void TAUDECL Tau_lite_start_timer(void *profiler, int phase);
 int TAUDECL Tau_stop_timer(void *profiler, int tid); 
-int TAUDECL Tau_lite_stop_timer(void *profiler, int tid); 
+int TAUDECL Tau_lite_stop_timer(void *profiler); 
 void TAUDECL Tau_trace_sendmsg(int type, int destination, int length);
 void TAUDECL Tau_trace_recvmsg(int type, int source, int length);
 void TAUDECL Tau_trace_recvmsg_remote(int type, int source, int length, int remoteid);

include/Profile/TauCppAPI.h
@@ -45,13 +45,13 @@ public:
     phase = 0;
 #endif
     if (fi != 0) {
-      Tau_start_timer(fi, phase, Tau_get_tid());
+      Tau_lite_start_timer(fi, phase);
     }
   }
 
   inline ~Tau_Profile_Wrapper() {
     if (fInfo != 0) {
-      Tau_stop_timer(fInfo, Tau_get_tid());
+      Tau_lite_stop_timer(fInfo);
     }
   }
 };

src/Profile/Comp_gnu.cpp
@@ -193,6 +193,8 @@ void __cyg_profile_func_enter(void* func, void* callsite)
 		return;
 	}
 
+	int tid = Tau_get_tid();
+	Tau_global_incr_insideTAU_tid(tid);
 
 	if (gnu_init) {
 		gnu_init = false;
@@ -203,8 +205,6 @@ void __cyg_profile_func_enter(void* func, void* callsite)
 		}
 
 		Tau_init_initializeTAU();
-		int tid = Tau_get_tid();
-		Tau_global_incr_insideTAU_tid(tid);
 
 		//GNU has some internal routines that occur before main in entered. To
 		//ensure that a single top-level timer is present start the dummy '.TAU
@@ -240,9 +240,7 @@ void __cyg_profile_func_enter(void* func, void* callsite)
 		atexit(runOnExit);
 	}
 	
-	int tid = Tau_get_tid();
 	// prevent re-entry of this routine on a per thread basis
-	Tau_global_incr_insideTAU_tid(tid);
 	if (compInstDisabled[tid]) {
 		Tau_global_decr_insideTAU_tid(tid);
 		return;
@@ -348,15 +346,18 @@ void __cyg_profile_func_exit(void* func, void* callsite)
 
 	// prevent entry into cyg_profile functions while inside entry
 	if (compInstDisabled[tid]) {
+		Tau_global_decr_insideTAU_tid(tid);
 		return;
 	}
 
 	if (executionFinished) {
+		Tau_global_decr_insideTAU_tid(tid);
 		return;
 	}
 
 	//prevent entry into cyg_profile functions while still initializing TAU
 	if (Tau_init_initializingTAU()) {
+		Tau_global_decr_insideTAU_tid(tid);
 		return;
 	}
 

src/Profile/Comp_pgi.cpp
@@ -42,6 +42,8 @@
 #  include <omp.h>
 #endif
 
+enum RoutineState { ROUTINE_CREATED = 1, ROUTINE_THROTTLED = 2 };
+
 using namespace std;
 
 
@@ -50,7 +52,7 @@ struct s1 {
   long l2;
   double d1;
   double d2;
-  long isseen;
+  long isseen; //status of this routine.
   char *c;
   void *p1;
   long lineno;
@@ -88,16 +90,19 @@ extern "C" void __rouinit() {
 }
 
 int Tau_get_function_index_in_DB(FunctionInfo *fi) {
+	RtsLayer::LockDB();
   int i = TheFunctionDB().size();
+	int v = -1;
   for (vector<FunctionInfo*>::iterator it = TheFunctionDB().end();
       it != TheFunctionDB().begin(); it--, i--) {
     if (*it == fi)
     {
       dprintf("MATCH! i=%d, TheFunctionDB()[%d]->GetName() = %s, fi->GetName() = %s\n", i, i, TheFunctionDB()[i]->GetName(), fi->GetName());
-      return i;
+      v = i;
     }
   }
-  return -1;
+	RtsLayer::UnLockDB();
+  return v;
 }
 
 struct Tau_thread_ignore_flag {
@@ -115,31 +120,38 @@ static struct Tau_thread_ignore_flag Tau_ignore[TAU_MAX_THREADS] = {0};
 #endif
 #endif
 
+static int threadId = -1;
+#pragma omp threadprivate(threadId)
+static int threadCount = 0;
+
+int omp_thread() {
+#ifdef TAU_OPENMP
+#ifdef TAU_OPENMP_NESTED
+  if (threadId == -1) {
+#pragma omp critical(threadLock)
+    {
+      threadId = threadCount++;
+    }
+  }
+  return threadId;
+#else
+ 	return omp_get_thread_num();
+#endif /* TAU_OPENMP_NESTED */
+#endif /* TAU_OPENMP */
+}
+
 // called at the beginning of each profiled routine
 #pragma save_all_regs
 extern "C" void ___rouent2(struct s1 *p) {
-  char routine[2048];
-//  int isseen_local = p->isseen;
 
-  int tid = Tau_get_tid();
-  if (p->isseen == -1) {
-    Tau_ignore[tid].count ++;  // the rouent2 shouldn't call stop
-    return;
-  }
-  if ((!Tau_init_check_initialized()) || (Tau_global_get_insideTAU_tid(tid) > 0 )) { 
-    //dprintf("TAU not initialized /inside TAU in __rouent2. Going to ignore this one!name = p->rout %s\n", p->rout);
-    Tau_ignore[tid].count++;  // the rouent2 shouldn't call stop
-    return;
-  }
-
-  //p->isseen = -1; 
-  //p->isseen = isseen_local; 
-
+  int tid = Tau_get_local_tid();
+  
   if (!p->isseen) {
+		RtsLayer::LockEnv();
 		/* Some routines like length__Q2_3std20char_traits__tm__2_cSFPCc are called
 			 before main and get called repeatedly when <iostream> and cout are used
 			 in a C++ application. We need to create a top level timer if necessary */
-		Tau_create_top_level_timer_if_necessary_task(tid); 
+  	char routine[2048];
     sprintf (routine, "%s [{%s} {%d,0}]", p->rout, p->file, p->lineno);
     char* modpos;
     
@@ -147,80 +159,53 @@ extern "C" void ___rouent2(struct s1 *p) {
     if ( (modpos = strstr(p->file, ".mod.")) != NULL ) {
       strcpy(modpos, modpos+4);
     }
-    int isseen_local = p->isseen;
-      
-#ifdef TAU_OPENMP
-    
-    if (omp_in_parallel()) {
-      int returnFromBlock = 0;
-#pragma omp critical (tau_comp_pgi_1)
-      {
-	if (!p->isseen) {	
-	  void *handle=NULL;
-	  p->isseen ++;
-	  TAU_PROFILER_CREATE(handle, routine, "", TAU_DEFAULT);
-	  FunctionInfo *fi = (FunctionInfo*)handle;
-          if (!(fi->GetProfileGroup() & RtsLayer::TheProfileMask())) {
-	    Tau_ignore[tid].count++; // the rouent2 shouldn't call stop
-	    returnFromBlock = 1;
-            /* return; Not allowed inside an omp critical */
-          } else {
-	    Tau_start_timer(fi,0, tid);
-	    p->rid = Tau_get_function_index_in_DB(fi);
-	  }
-	}
-      }
-      if (returnFromBlock == 1) {
-	return;
-      }
-    } else {
-      void *handle=NULL;
-      p->isseen = -1;
-      TAU_PROFILER_CREATE(handle, routine, "", TAU_DEFAULT);
-      FunctionInfo *fi = (FunctionInfo*)handle;
-      if (!(fi->GetProfileGroup() & RtsLayer::TheProfileMask())) {
-	Tau_ignore[tid].count++; // the rouent2 shouldn't call stop
-	return;
-      }
-      Tau_start_timer(fi,0, tid);
-      p->rid = Tau_get_function_index_in_DB(fi);
-      p->isseen = isseen_local+1;
-    }
-#else
-    void *handle=NULL;
-    p->isseen = -1; // hold on, this is in the middle of creating a profiler
-    // if we re-enter this routine, just return so we can continue in the right
-    // place. 
-    TAU_PROFILER_CREATE(handle, routine, "", TAU_DEFAULT);
-    FunctionInfo *fi = (FunctionInfo*)handle;
-    if (!(fi->GetProfileGroup() & RtsLayer::TheProfileMask())) {
+		void *vp;
+		if (!p->isseen) {
+			Tau_create_top_level_timer_if_necessary_task(tid); 
+			TAU_PROFILER_CREATE(vp, routine, "", TAU_DEFAULT);
+			FunctionInfo *fi = (FunctionInfo *)vp;
+			p->rid = Tau_get_function_index_in_DB(fi);
+			Tau_start_timer(fi, 0, Tau_get_tid());
+			if (!(fi->GetProfileGroup() & RtsLayer::TheProfileMask())) {
+				Tau_ignore[tid].count++; // the rouent2 shouldn't call stop
+				p->isseen = ROUTINE_THROTTLED;	
+			}
+			else {
+				p->isseen = ROUTINE_CREATED;
+			}
+		}
+		RtsLayer::UnLockEnv();
+	} // seen this routine before, no need to create another FunctionInfo object.
+	else 
+	{
+    if (p->isseen == ROUTINE_THROTTLED) {
       Tau_ignore[tid].count++; // the rouent2 shouldn't call stop
       return;
     }
-    Tau_start_timer(fi,0, tid);
-    p->rid = Tau_get_function_index_in_DB(fi);
-    p->isseen = isseen_local+1;
-#endif
-  } else {
+		RtsLayer::LockDB();
     FunctionInfo *fi = (FunctionInfo*)(TheFunctionDB()[p->rid]);
-    if (!(fi->GetProfileGroup() & RtsLayer::TheProfileMask())) {
-      Tau_ignore[tid].count++; // the rouent2 shouldn't call stop
-      return;
-    }
-    Tau_start_timer(fi, 0, tid);
-  }
+		RtsLayer::UnLockDB();
+	  Tau_start_timer(fi, 0, Tau_get_tid());
+		if (!(fi->GetProfileGroup() & RtsLayer::TheProfileMask())) {
+			Tau_ignore[tid].count++; // the rouent2 shouldn't call stop
+			p->isseen = ROUTINE_THROTTLED;	
+		}
+	}
 }
+
 
 // called at the end of each profiled routine
 #pragma save_all_regs
 extern "C" void ___rouret2(void) {
-  int tid = Tau_get_tid();
-  if (Tau_ignore[tid].count == 0) { 
-    TAU_MAPPING_PROFILE_STOP(0);
-  }
-  else {
+  
+	int tid = Tau_get_local_tid();
+	
+	if (Tau_ignore[tid].count == 0)
+	{
+  	TAU_MAPPING_PROFILE_STOP(0);
+	} else {
     Tau_ignore[tid].count--;
-  }
+	}
 }
 
 #pragma save_used_gp_regs

src/Profile/OpenMPLayer.cpp
@@ -23,6 +23,9 @@
 #ifdef TAU_DOT_H_LESS_HEADERS
 #include <iostream>
 using namespace std;
+
+#include <math.h>
+
 #else /* TAU_DOT_H_LESS_HEADERS */
 #include <iostream.h>
 #endif /* TAU_DOT_H_LESS_HEADERS */
@@ -62,7 +65,7 @@ static int threadCount = 0;
 int OpenMPLayer::RegisterThread(void)
 {
 	int id = RtsLayer::createThread();
-	//printf("OpenMP: registering thread, id = %d.\n", id);
+//	printf("OpenMP: registering thread, id = %d.\n", id);
 	return id;
   // Not needed for OpenMP programs! 
   //return 0;
@@ -91,16 +94,27 @@ int OpenMPLayer::GetTauThreadId(void)
 	int omp_thread_id, tau_thread_id;
 #ifdef TAU_OPENMP
 #ifdef TAU_OPENMP_NESTED
-  if (threadId == -1) {
-#pragma omp critical(threadLock)
-    {
-      threadId = threadCount++;
-    }
-  }
+	threadId = omp_get_thread_num();
+	int level = omp_get_level() - 1;
+	int depth = 0;
+	int width = omp_get_team_size(level+1);
+	while (level >= 0)
+	{
+		depth++;
+		threadId += omp_get_ancestor_thread_num(level)*width;
+		width *= omp_get_team_size(level);
+		level--;
+	}
   omp_thread_id = threadId;
 #else
+	if (omp_get_nested())
+	{
+		//OPENMP thread identification not supported by compiler.
+		printf("ERROR: OpenMP nesting not supported. Please use a compiler that supports OMP specification >= 3.0 or rerun with OMP_NESTED=FALSE.\n");
+		exit(1);
+	}
   omp_thread_id = omp_get_thread_num();
-	//printf("omp thread id = %d.\n", omp_thread_id);
+#endif /* TAU_OPENMP_NESTED */
 
 	if (omp_thread_id == 0)
 	{
@@ -108,8 +122,7 @@ int OpenMPLayer::GetTauThreadId(void)
 	}
 	else
 	{
-		// ONLY INITIALIZE THE LOCK ONCE!
-        static int registerInitFlag = InitializeRegisterMutexData();
+		Initialize();
 		//fprintf(stderr, "Thread %d locking register\n", omp_thread_id);
         omp_set_lock(&OpenMPLayer::tauRegistermutex);
 
@@ -133,7 +146,6 @@ int OpenMPLayer::GetTauThreadId(void)
         omp_unset_lock(&OpenMPLayer::tauRegistermutex);
 		//fprintf(stderr, "Thread %d unlocking register\n", omp_thread_id);
 	}
-#endif /* TAU_OPENMP_NESTED */
 	return omp_thread_id;
 #endif /* TAU_OPENMP */
 }
@@ -142,14 +154,25 @@ int OpenMPLayer::GetThreadId(void)
 {
 #ifdef TAU_OPENMP
 #ifdef TAU_OPENMP_NESTED
-  if (threadId == -1) {
-#pragma omp critical(threadLock)
-    {
-      threadId = threadCount++;
-    }
-  }
+	threadId = omp_get_thread_num();
+	int level = omp_get_level() - 1;
+	int depth = 0;
+	int width = omp_get_team_size(level+1);
+	while (level >= 0)
+	{
+		depth++;
+		threadId += omp_get_ancestor_thread_num(level)*width;
+		width *= omp_get_team_size(level);
+		level--;
+	}
   return threadId;
 #else
+	if (omp_get_nested())
+	{
+		//OPENMP thread identification not supported by compiler.
+		printf("ERROR: OpenMP nesting not supported. Please use a compiler that supports OMP specification >= 3.0 or rerun with OMP_NESTED=FALSE.\n");
+		exit(1);
+	}
  	return omp_get_thread_num();
 #endif /* TAU_OPENMP_NESTED */
 #endif /* TAU_OPENMP */
@@ -177,6 +200,15 @@ int OpenMPLayer::TotalThreads(void)
 int OpenMPLayer::InitializeThreadData(void)
 {
   return 1;
+}
+
+void OpenMPLayer::Initialize(void)
+{
+		// ONLY INITIALIZE THE LOCK ONCE!
+    static int registerInitFlag = InitializeRegisterMutexData();
+    static int dbInitFlag = InitializeDBMutexData();
+    static int envInitFlag = InitializeEnvMutexData();
+
 }
 
 ////////////////////////////////////////////////////////////////////////
@@ -207,7 +239,7 @@ int OpenMPLayer::InitializeRegisterMutexData(void)
 ////////////////////////////////////////////////////////////////////////
 int OpenMPLayer::LockDB(void)
 {
-  static int initflag=InitializeDBMutexData();
+	Initialize();
   // Lock the functionDB mutex
   //fprintf(stderr, "Thread %d locking DB\n", omp_get_thread_num());
   omp_set_lock(&OpenMPLayer::tauDBmutex);
@@ -244,7 +276,7 @@ int OpenMPLayer::InitializeEnvMutexData(void)
 ////////////////////////////////////////////////////////////////////////
 int OpenMPLayer::LockEnv(void)
 {
-  static int initflag=InitializeEnvMutexData();
+	Initialize();
   // Lock the functionEnv mutex
   //fprintf(stderr, "Thread %d locking Env\n", omp_get_thread_num());
   omp_set_lock(&OpenMPLayer::tauEnvmutex);

commit dfb46468be0976a7d321b09e66de7dd673330b23

Bug fix: initialize RtsLayer before it is used
Bug introduced in d0f7c682f5f96b9c0df50f235940ab828cb4cf15 [formerly 52aee6aa2ff22acdbbb978c107f72aa3021713a5]


Former-commit-id: fbf51fdc5f1afb8b1af179f1471064c3da1643c3

src/Profile/TauInit.cpp
@@ -309,6 +309,9 @@ extern "C" int Tau_init_initializeTAU()
   if (initializing) return 0;
   initializing = 1;
 
+  //Initialize locks.
+  RtsLayer::Initialize();
+
   // Protect TAU from itself
   TauInternalFunctionGuard protects_this_function;
 
@@ -327,14 +330,12 @@ extern "C" int Tau_init_initializeTAU()
 
 #ifdef TAU_EPILOG
   /* no more initialization necessary if using epilog/scalasca */
-  initializing = 1;
   Tau_init_epilog();
   return 0;
 #endif
 
 #ifdef TAU_SCOREP
   /* no more initialization necessary if using SCOREP */
-  initializing = 1;
   SCOREP_Tau_InitMeasurement();
   SCOREP_Tau_RegisterExitCallback(Tau_profile_exit_all_threads);
   return 0;
@@ -342,7 +343,6 @@ extern "C" int Tau_init_initializeTAU()
 
 #ifdef TAU_VAMPIRTRACE
   /* no more initialization necessary if using vampirtrace */
-  initializing = 1;
   Tau_init_vampirTrace();
   return 0;
 #endif
@@ -371,6 +371,7 @@ extern "C" int Tau_init_initializeTAU()
   if (TauEnv_get_compensate()) {
     Tau_compensate_initialization();
   }
+
   /* initialize sampling if requested */
 #if !defined(TAU_MPI) && !defined(TAU_WINDOWS)
   if (TauEnv_get_ebs_enabled()) {
@@ -391,20 +392,16 @@ extern "C" int Tau_init_initializeTAU()
   Tau_initialize_collector_api();
 #endif
 
+  // Mark initialization complete so calls below can start timers
   tau_initialized = 1;
 
 #ifdef __MIC__
-  if (TauEnv_get_mic_offload())
-  {
+  if (TauEnv_get_mic_offload()) {
     TAU_PROFILE_SET_NODE(0);
     Tau_create_top_level_timer_if_necessary();
   }
 #endif
 
-  //Initialize locks.
-  RtsLayer::Initialize();
-
-  // FIXME: No so sure this is a good idea...
   Tau_memory_wrapper_enable();
 
   return 0;