UO-OACISS/tau2

commit 018bc24a7cd297a3764006743e8c97faf0b74436

Inital prototype (in C form) for the new threading model.
Former-commit-id: 76e06da3eee3923fee7f639961aaed66033ddb35

src/Profile/TauCAPI.cpp
@@ -503,6 +503,7 @@ extern "C" int Tau_stop_current_timer() {
 
 
 extern "C" int Tau_profile_exit_all_tasks() {
+	printf("stopping all tasks...\n");
 	int tid = 1;
 	while (tid < TAU_MAX_THREADS)
 	{
@@ -1162,6 +1163,48 @@ extern "C" void Tau_profile_c_timer(void **ptr, const char *name, const char *ty
 /* We need a routine that will create a top level parent profiler and give
  * it a dummy name for the application, if just the MPI wrapper interposition
  * library is used without any instrumentation in main */
+extern "C" void Tau_create_top_level_timer_if_necessary_task(int tid) {
+
+
+  int disabled = 0;
+#ifdef TAU_VAMPIRTRACE
+  disabled = 1;
+#endif
+#ifdef TAU_EPILOG
+  disabled = 1;
+#endif
+  if (disabled) {
+    return;
+  }
+
+  static bool initialized = false;
+  static bool initthread[TAU_MAX_THREADS];
+  if (!initialized) {
+    RtsLayer::LockDB();
+    if (!initialized) {
+      for (int i=0; i<TAU_MAX_THREADS; i++) {
+	initthread[i] = false;
+      }
+    }
+    RtsLayer::UnLockDB();
+    initialized = true;
+  }
+  if (initthread[tid] == true) {
+    return;
+  }
+  
+	FunctionInfo *ptr;
+  if (TauInternal_CurrentProfiler(tid) == NULL) {
+			printf("in create_top_level_timer.\n");
+    initthread[tid] = true;
+    ptr = (FunctionInfo *) Tau_get_profiler(".TAU application", " ", TAU_DEFAULT, "TAU_DEFAULT");
+    if (ptr) {
+      Tau_start_timer(ptr, 0, tid);
+    }
+  }
+  atexit(Tau_destructor_trigger);
+}
+
 extern "C" void Tau_create_top_level_timer_if_necessary(void) {
   int disabled = 0;
 #ifdef TAU_VAMPIRTRACE

include/Profile/RtsLayer.h
@@ -41,6 +41,11 @@ public:
   RtsLayer () { }  // defaults
   ~RtsLayer () { } 
 
+	static int createThread(void);
+	static void destroyThread(int id);
+	static void recycleThread(int id);
+
+
   static TauGroup_t & TheProfileMask(void);
   static bool& TheEnableInstrumentation(void);
   static bool& TheShutdown(void);

src/Profile/Profiler.cpp
@@ -21,9 +21,9 @@
 #include <Profile/TauSampling.h>
 #endif
 #include <Profile/TauSnapshot.h>
-#ifdef TAU_GPU
+//#ifdef TAU_GPU
 extern "C" int Tau_profile_exit_all_tasks();
-#endif
+//#endif
 //#include <tau_internal.h>
 
 #ifdef TAU_PERFSUITE
@@ -600,6 +600,9 @@ void Profiler::Stop(int tid, bool useLastTimeStamp) {
     if (strcmp(ThisFunction->GetName(), "_fini") == 0) {
       TheSafeToDumpData() = 0;
     }
+		if (tid == 0) {
+	  	Tau_profile_exit_all_tasks();
+		}
 #ifdef TAU_GPU
 		//Stop all other running tasks.
 		if (tid == 0) {

src/Profile/RtsThread.cpp
@@ -50,6 +50,75 @@ int RtsLayer::lockDBcount[TAU_MAX_THREADS];
 int RtsLayer::lockEnvCount[TAU_MAX_THREADS];
 
 //////////////////////////////////////////////////////////////////////
+// Thread struct 
+//////////////////////////////////////////////////////////////////////
+typedef struct TauThread_t
+{
+	int thread_rank;
+	bool recyclable;
+	bool active;
+	TauThread_t* next_available;
+} TauThread;
+
+TauThread TheThreadList[TAU_MAX_THREADS];
+
+static TauThread* nextThread;
+
+int initializeThreads()
+{
+	TauThread last = (TauThread){TAU_MAX_THREADS-1,false,false, NULL};
+	printf("index: %d \n\t rank: %d next_rank: n/a.\n", TAU_MAX_THREADS-1, TAU_MAX_THREADS-1);
+	TheThreadList[TAU_MAX_THREADS-1] = last;
+	for (int i=TAU_MAX_THREADS-2;i>0;i--)
+	{
+		printf("index: %d \n\t rank: %d next_rank %d.\n", i, i, last.thread_rank);
+		TheThreadList[i] = (TauThread){i,false,false, &TheThreadList[i+1]};
+		last = TheThreadList[i];
+	}
+	nextThread = &TheThreadList[1];
+	return 0;
+}
+
+int RtsLayer::createThread()
+{
+  LockEnv();
+
+	static int i = initializeThreads();
+
+	if (nextThread == NULL)
+	{
+		printf("error exceeded TAU_MAX_THREADS.\n");
+	}
+
+	TauThread newThread = *nextThread;
+
+	newThread.recyclable = false;
+	newThread.active = true;
+	nextThread = newThread.next_available;
+  
+	printf("creating thread: %d.\n", newThread.thread_rank);
+	UnLockEnv();
+
+	return newThread.thread_rank;
+}
+
+void RtsLayer::recycleThread(int id)
+{
+  LockEnv();
+	
+	TheThreadList[id].active = false;
+	TheThreadList[id].next_available = nextThread;
+	nextThread = &TheThreadList[id];	
+  
+	printf("recycling thread: %d.\n", TheThreadList[id].thread_rank);
+	UnLockEnv();
+}
+
+int RtsLayer::myThread() { return 0; }
+int RtsLayer::setMyThread(int i) { return 0; }
+int RtsLayer::getNumThreads() { return 1; } 
+int* RtsLayer::numThreads() { static int i = 1; return &i; } 
+//////////////////////////////////////////////////////////////////////
 // myNode() returns the current node id (0..N-1)
 //////////////////////////////////////////////////////////////////////
 int RtsLayer::myNode(void)
@@ -72,57 +141,6 @@ int RtsLayer::myNode(void)
 int RtsLayer::myContext(void)
 {
   return TheContext(); 
-}
-
-extern "C" int Tau_RtsLayer_myThread(void) {
-  return RtsLayer::myThread();
-}
-
-
-//////////////////////////////////////////////////////////////////////
-// myNode() returns the current node id (0..N-1)
-//////////////////////////////////////////////////////////////////////
-int RtsLayer::myThread(void)
-{
-#ifdef PTHREADS
-  return PthreadLayer::GetThreadId();
-#elif  TAU_SPROC
-  return SprocLayer::GetThreadId();
-#elif  TAU_WINDOWS
-  return WindowsThreadLayer::GetThreadId();
-#elif  TULIPTHREADS
-  return TulipThreadLayer::GetThreadId();
-#elif JAVA
-  return JavaThreadLayer::GetThreadId(); 
-	// C++ app shouldn't use this unless there's a VM
-#elif TAU_OPENMP
-  return OpenMPLayer::GetThreadId();
-#elif TAU_PAPI_THREADS
-  return PapiThreadLayer::GetThreadId();
-#else  // if no other thread package is available 
-  return 0;
-#endif // PTHREADS
-}
-
-int RtsLayer::setMyThread(int tid) {
-#ifdef PTHREADS
-  PthreadLayer::SetThreadId(tid);
-#endif // PTHREADS
-  return 0;
-}
-
-int RtsLayer::getNumThreads() {
-  return *(RtsLayer::numThreads());
-}
-
-int *RtsLayer::numThreads() {
-#ifdef TAU_OPENMP
-	static int numthreads = OpenMPLayer::numThreads();
-#else
-  static int numthreads = 1;
-#endif
-	return &numthreads;
-
 }
 
 

commit 01bb83a8da2c2b7391b42878eba7ee246edfba2f

Bug fix: Tau_destructor_trigger short-circuted
Bug introduced in 018bc24a7cd297a3764006743e8c97faf0b74436 [formerly 76e06da3eee3923fee7f639961aaed66033ddb35]


Former-commit-id: c29041c80634ebee705e4d44b87e3e7fb2a988b0

src/Profile/TauCAPI.cpp
@@ -1632,24 +1632,17 @@ extern void Tau_pure_start_task_string(const string name, int tid);
  * library is used without any instrumentation in main */
 extern "C" void Tau_create_top_level_timer_if_necessary_task(int tid)
 {
-#ifdef TAU_SCOREP
-//  printf("Returning from Tau_create_top_level_timer_if_necessary_task");
-  return;
-#endif /* TAU_SCOREP */
-#if ! (defined(TAU_VAMPIRTRACE) || defined(TAU_EPILOG))
+#if ! (defined(TAU_VAMPIRTRACE) || defined(TAU_EPILOG) || defined(TAU_SCOREP))
   TauInternalFunctionGuard protects_this_function;
 
   /* After creating the ".TAU application" timer, we start it. In the
    timer start code, it will call this function, so in that case,
    return right away. */
   static bool initialized = false;
-  static bool initthread[TAU_MAX_THREADS] = { false };
   static bool initializing[TAU_MAX_THREADS] = { false };
+  static bool initthread[TAU_MAX_THREADS] = { false };
 
-  if (!initialized) {
-    if (initializing[tid]) {
-      return;
-    }
+  if (!initialized && !initializing[tid]) {
     RtsLayer::LockDB();
     if (!initialized) {
       // whichever thread got here first, has the lock and will create the
@@ -1665,17 +1658,15 @@ extern "C" void Tau_create_top_level_timer_if_necessary_task(int tid)
     RtsLayer::UnLockDB();
   }
 
-  if (initthread[tid] == true) {
-    return;
-  }
-
-  // if there is no top-level timer, create one - But only create one FunctionInfo object.
-  // that should be handled by the Tau_pure_start_task call.
-  if (TauInternal_CurrentProfiler(tid) == NULL) {
-    initthread[tid] = true;
-    initializing[tid] = true;
-    Tau_pure_start_task_string(gTauApplication(), tid);
-    initializing[tid] = false;
+  if (!initthread[tid]) {
+    // if there is no top-level timer, create one - But only create one FunctionInfo object.
+    // that should be handled by the Tau_pure_start_task call.
+    if (!TauInternal_CurrentProfiler(tid)) {
+      initthread[tid] = true;
+      initializing[tid] = true;
+      Tau_pure_start_task_string(gTauApplication(), tid);
+      initializing[tid] = false;
+    }
   }
 
   atexit(Tau_destructor_trigger);