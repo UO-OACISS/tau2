39544a257bd8be6d8fb9b2bf7ed18d3fa6afdfa7
Ender Dai <xdai@uoregon.edu>
2014-05-09 18:59:49 -0700

    Android: fix several locking problems
    1. Every thread should possess its own tauDBMutex and tauEnvMutex
    2. The access to the profiles map in Profiler class should be
       synchronized accross threads
    3. GetThreadId() *CAN NOT* be called before the calling thread is
       registerd by DTM. We should take special care with what we can
       do before WaitForDTM() returns. Specifically, we should not use
       TAU_VERBOSE() because it will call GetThreadId() eventually
    4. WaitForDTM() should be called at the beginning of NativeStart().
       Because the FunctionInfo of injected method may has already been
       created in other threads, NativeStart() could be the entry point
       of TAU API in current thread in addition to NativeProfile()
    
    
    Former-commit-id: 1421eaff946d824ad1b249571912e7cfff9bf8ae

--- tools/src/android/dexInjector/edu/uoregon/TAU/Profiler.java
+++ tools/src/android/dexInjector/edu/uoregon/TAU/Profiler.java
@@ -4,13 +4,12 @@ import java.io.*;
 import java.util.*;
 
 public class Profiler {
-    static Map<String, Profile> profiles;
+    private static final String TAG = "TAU";
 
-    static HashMap<String,ArrayList<ArrayList<String>>> methodMap;
+    static Map<String, Profile> profiles;
 
     static {
 	profiles = new HashMap<String, Profile>();
-	System.out.println("TAU: init Java Profiler");
     }
 
     /*
@@ -19,15 +18,13 @@ public class Profiler {
     public static void start(String signature) {
 	Profile profile;
 
-	if (signature == null) {
-	    return;
-	}
-
-	if (profiles.containsKey(signature)) {
-	    profile = profiles.get(signature);
-	} else {
-	    profile = new Profile(signature, "myType", "myGroup", Profile.TAU_DEFAULT);
-	    profiles.put(signature, profile);
+	synchronized (profiles) {
+	    if (profiles.containsKey(signature)) {
+		profile = profiles.get(signature);
+	    } else {
+		profile = new Profile(signature, "myType", "myGroup", Profile.TAU_DEFAULT);
+		profiles.put(signature, profile);
+	    }
 	}
 
 	profile.Start();
@@ -38,7 +35,11 @@ public class Profiler {
      * Our injected code makes sure that the profile is always exist for stop()
      */
     public static void stop(String signature) {
-	Profile profile = profiles.get(signature);
+	Profile profile;
+
+	synchronized (profiles) {
+	    profile = profiles.get(signature);
+	}
 
 	profile.Stop();
     }
