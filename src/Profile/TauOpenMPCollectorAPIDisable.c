6168415ff524caa422f64b7ff01bd4424907162d
Kevin Huck <khuck@cs.uoregon.edu>
2014-06-23 16:41:12 -0700

    Finally fixed Tau_finalize_collector_api. I hope.
    Former-commit-id: 1ea8c226c243cac31ed38b7e1f8851b1501b127a

--- src/Profile/TauOpenMPCollectorAPIDisable.c
+++ src/Profile/TauOpenMPCollectorAPIDisable.c
@@ -1,4 +1,5 @@
 
 int Tau_initialize_collector_api(){return 0;};
+void Tau_finalize_collector_api(){return;};
 void Tau_disable_collector_api(){return;};
 int Tau_get_thread_omp_state(int tid){return 0;};
