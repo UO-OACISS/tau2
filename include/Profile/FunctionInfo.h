77185d4d22494a8ebcfffd133ce2253532d5a18b
John C. Linford <jlinford@paratools.com>
2014-08-28 12:38:33 -0400

    Tidy up some code.
    Former-commit-id: 73c932e042962001ced77a6e95eeac907bff2fac

--- include/Profile/FunctionInfo.h
+++ include/Profile/FunctionInfo.h
@@ -188,10 +188,15 @@ public:
   }
 
   // Cough up the information about this function.
-  void SetName(std::string& str) { Name = strdup(str.c_str()); }
+  void SetName(std::string & str) { Name = strdup(str.c_str()); }
   const char* GetName() const { return Name; }
-  void SetType(std::string& str) { Type = strdup(str.c_str()); }
-  const char* GetType() const { return Type; }
+
+  void SetType(char const * str) {
+    Type = strdup(str);
+  }
+  char const * GetType() const {
+    return Type;
+  }
 
   const char* GetPrimaryGroup() const { return GroupName; }
   const char* GetAllGroups() const { return AllGroups; }
