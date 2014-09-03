UO-OACISS/tau2

commit 866110beb8dc1d687171c69806cc857f33527f56

Added sync. point wrappers for StreamQuery and EventQuery. Added Tau_gpu_exit call to Profiler when main timer stops as well.
Former-commit-id: 37ce3994874ff078105f04c367254183c11635da

src/Profile/Profiler.cpp
@@ -21,7 +21,9 @@
 #include <Profile/TauSampling.h>
 #endif
 #include <Profile/TauSnapshot.h>
-
+#ifdef TAU_GPU
+#include <Profile/TauGpu.h>
+#endif
 //#include <tau_internal.h>
 
 #ifdef TAU_PERFSUITE
@@ -590,7 +592,12 @@ void Profiler::Stop(int tid, bool useLastTimeStamp) {
     if (strcmp(ThisFunction->GetName(), "_fini") == 0) {
       TheSafeToDumpData() = 0;
     }
-
+#ifdef TAU_GPU
+		//Stop all other running tasks.
+		if (tid == 0) {
+		  Tau_gpu_exit();
+		}
+#endif
 #ifndef TAU_WINDOWS
     if (tid == 0) {
       atexit(TauAppShutdown);

src/Profile/Makefile
@@ -340,6 +340,8 @@ CRAY_PGI_HACK=
 #CUPTI#TAU_CUPTI_OBJS=TauGpuAdapterCupti.o TauGpu.o #ENDIF#
 #CUPTI#TAU_CUPTI_DEF=-DTAU_GPU #ENDIF#
 
+CXXFLAGS+= $(TAU_CUDA_DEF) $(TAU_CUPTI_DEF)
+
 GPU_INSTALL   = $(TAU_SHAREDDIR)/$(TAU_CUDA_LIB) $(TAU_SHAREDDIR)/$(TAU_OPENCL_LIB) $(TAU_SHAREDDIR)/$(TAU_CUPTI_LIB) $(TAU_PYCUDA_LIB)#ENDIF#
 
 #$(TAU_SHAREDDIR)/$(TAU_CUDA_LIB): $(TAU_CUDA_LIB)
@@ -673,7 +675,7 @@ ProfileParam.o   : ProfileParam.cpp $(HDRS)
 	$(CXX) $(CXXFLAGS) -c $(@:.o=.cpp)
 
 Tracer.o   	: Tracer.cpp $(HDRS)
-	$(CXX) -DTAU_GPU $(CXXFLAGS) -c $(@:.o=.cpp)
+	$(CXX) $(CXXFLAGS) -c $(@:.o=.cpp)
 
 TauMetrics.o   	: TauMetrics.cpp $(HDRS)
 	$(CXX) $(CXXFLAGS) $(TAU_CUPTI_INCLUDE) $(TAU_CUDA_INCLUDE) -c $(@:.o=.cpp)

src/Profile/TauGpu.cpp
@@ -317,7 +317,7 @@ void Tau_gpu_register_gpu_event(eventId id, double startTime, double endTime)
 {
 	int task = get_task(id.device);
   
-	//printf("in TauGpu.cpp.\n");
+	//printf("in TauGpu.cpp, registering gpu event.\n");
 	//printf("Tau gpu name: %s.\n", name);
 	stage_gpu_event(id.name, task,
 		startTime + id.device->syncOffset(), id.callingSite);
@@ -459,7 +459,7 @@ void Tau_gpu_exit(void)
 		}
 #ifdef DEBUG_PROF
 		cerr << "stopping first gpu event.\n" << endl;
-		printf("stopping level 0.\n");
+		printf("stopping level %d tasks.\n", number_of_tasks);
 #endif
 		for (int i=0; i<number_of_tasks; i++)
 		{

src/Profile/TauGpuAdapterCUDA.cpp
@@ -25,6 +25,8 @@ double sync_offset = 0;
 static cudaEvent_t lastEvent;
 static double lastEventTime = 0;
 
+//call to cudaEventQuery that does not register a sync event.
+extern cudaError_t cudaEventQuery_nosync(cudaEvent_t a);
 
 cudaRuntimeGpuId *cudaRuntimeGpuId::getCopy() { 
 		//printf("in runtime, getCopy.\n");
@@ -346,12 +348,12 @@ void Tau_cuda_register_sync_event()
 	if (KernelBuffer.size() > 0 && KernelBuffer.front().stopEvent != NULL)
 	{
 		//printf("buffer front stop: %d.\n", KernelBuffer.front().stopEvent == NULL);
-		cudaError err = cudaEventQuery(KernelBuffer.front().stopEvent);
+		cudaError err = cudaEventQuery_nosync(KernelBuffer.front().stopEvent);
 		//printf("buffer front is: %d\n", err);
 	}
 	float start_sec, stop_sec;
 
-	while (!KernelBuffer.empty() && cudaEventQuery(KernelBuffer.front().stopEvent) == cudaSuccess)
+	while (!KernelBuffer.empty() && cudaEventQuery_nosync(KernelBuffer.front().stopEvent) == cudaSuccess)
 	{
 		KernelEvent kernel = KernelBuffer.front();
 		//printf("kernel buffer size = %d.\n", KernelBuffer.size());

src/wrappers/cuda/cudart_wrap.cpp
@@ -118,7 +118,7 @@ cudaError_t cudaThreadExit() {
     }
 	//printf("in cudaThreadExit(), check for kernel events.\n");
 #ifdef TRACK_KERNEL
-	Tau_cuda_register_sync_event();
+	//Tau_cuda_register_sync_event();
 #endif 
   TAU_PROFILE_START(t);
 #ifdef CUPTI
@@ -699,6 +699,9 @@ cudaError_t cudaStreamQuery(cudaStream_t a1) {
   TAU_PROFILE_START(t);
   retval  =  (*cudaStreamQuery_h)( a1);
   TAU_PROFILE_STOP(t);
+#ifdef TRACK_KERNEL
+	Tau_cuda_register_sync_event();
+#endif
   }
   return retval;
 
@@ -788,6 +791,34 @@ cudaError_t cudaEventRecord(cudaEvent_t a1, cudaStream_t a2) {
 
 }
 
+cudaError_t cudaEventQuery_nosync(cudaEvent_t a1) {
+
+  typedef cudaError_t (*cudaEventQuery_p) (cudaEvent_t);
+  static cudaEventQuery_p cudaEventQuery_h = NULL;
+  cudaError_t retval;
+  TAU_PROFILE_TIMER(t,"cudaError_t cudaEventQuery(cudaEvent_t) C", "", CUDART_API);
+  if (cudart_handle == NULL) 
+    cudart_handle = (void *) dlopen(cudart_orig_libname, RTLD_NOW); 
+
+  if (cudart_handle == NULL) { 
+    perror("Error opening library in dlopen call"); 
+    return retval;
+  } 
+  else { 
+    if (cudaEventQuery_h == NULL)
+	cudaEventQuery_h = (cudaEventQuery_p) dlsym(cudart_handle,"cudaEventQuery"); 
+    if (cudaEventQuery_h == NULL) {
+      perror("Error obtaining symbol info from dlopen'ed lib"); 
+      return retval;
+    }
+  TAU_PROFILE_START(t);
+  retval  =  (*cudaEventQuery_h)( a1);
+  TAU_PROFILE_STOP(t);
+  }
+  return retval;
+
+}
+
 cudaError_t cudaEventQuery(cudaEvent_t a1) {
 
   typedef cudaError_t (*cudaEventQuery_p) (cudaEvent_t);
@@ -811,6 +842,9 @@ cudaError_t cudaEventQuery(cudaEvent_t a1) {
   TAU_PROFILE_START(t);
   retval  =  (*cudaEventQuery_h)( a1);
   TAU_PROFILE_STOP(t);
+#ifdef TRACK_KERNEL
+	Tau_cuda_register_sync_event();
+#endif
   }
   return retval;
 

commit a6caf8019afd11a3a07bd44d96d279501c0ed75b

Fix infinite recursion in exit
Looks like a copy/paste error introduced in 866110beb8dc1 [formerly 37ce3994874ff].


Former-commit-id: f0d78ab69958cc74b679e47519f831304bbeed1a

src/Profile/Profiler.cpp
@@ -595,13 +595,9 @@ void Profiler::Stop(int tid, bool useLastTimeStamp)
     if (strcmp(ThisFunction->GetName(), "_fini") == 0) {
       TheSafeToDumpData() = 0;
     }
-    if (tid == 0) {
-      Tau_profile_exit_all_tasks();
-    }
 #ifdef TAU_GPU
     //Stop all other running tasks.
     if (tid == 0) {
-      //printf("exiting all tasks....\n");
       Tau_profile_exit_all_tasks();
     }
 #endif