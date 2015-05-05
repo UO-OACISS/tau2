a12d51230c608fd03dec54fd91f6d5150d906e85
Sameer Shende <sameer@cs.uoregon.edu>
2015-05-04 19:50:03 -0700

    Disable MPI_Attr_get. Enable with -useropt=-DTAU_ENABLE_MPI_ATTR_GET for now.
    Former-commit-id: 95cfeac29a88bc64332695d85600bc95e6fa6a0f

--- src/Profile/TauMpi.c
+++ src/Profile/TauMpi.c
@@ -774,6 +774,7 @@ int keyval;
   return returnVal;
 }
 
+#ifdef TAU_ENABLE_MPI_ATTR_GET
 int   MPI_Attr_get( comm, keyval, attr_value, flag )
 MPI_Comm comm;
 int keyval;
@@ -792,6 +793,8 @@ int * flag;
 
   return returnVal;
 }
+
+#endif /* TAU_ENABLE_MPI_ATTR_GET */
 
 int   MPI_Attr_put( comm, keyval, attr_value )
 MPI_Comm comm;
