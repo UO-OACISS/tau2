bf85eac5260b32c3e3986f0fc7d97fab7842e87a
Srinath Vadlamani <srinath.vadlamani@gmail.com>
2015-07-28 17:18:13 -0600

    runTest.sh: make it easier and quick
    Former-commit-id: be704e5e7a90be882a0817da4a60a3fd832817d0

--- examples/matmult_api/build.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/local/bin/bash -f
-source ~/gnu5Bin/linkGccs.source
-export TAU_MAKEFILE=/Users/srinathv/Repos/tau2/apple/lib/Makefile.tau-mpi-pdt
-export TAU_OPTIONS='-optLinkOnly'
-tau_f90.sh matmult.ss.f90 -o matmult.ss
