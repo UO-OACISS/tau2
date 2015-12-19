d4fcb71f567af149481a099dce86c222075dab4c
Kevin Huck <khuck@cs.uoregon.edu>
2015-12-18 16:36:23 -0800

    Adding support for the PGI compiler, which shouldn't be compiling the OMPT support anyway. This is a bigger problem than the lack of thread local storage in the PIG compiler.
    Former-commit-id: ae83b91ddba6565fb11b7b42e2c8aca8027721da

--- src/Profile/TauOpenMPCollectorAPI.cpp
+++ src/Profile/TauOpenMPCollectorAPI.cpp
@@ -832,7 +832,14 @@ extern "C" void Tau_omp_event_handler(OMP_COLLECTORAPI_EVENT event) {
 static bool initializing = false;
 static bool initialized = false;
 static bool ora_success = false;
+#if defined (TAU_USE_TLS)
 __thread bool is_master = false;
+#elif defined (TAU_USE_DTLS)
+__declspec(thread) bool is_master = false;
+#elif defined (TAU_USE_PGS)
+#include "pthread.h"
+pthread_key_t thr_id_key;
+#endif
 
 #if TAU_DISABLE_SHARED
 extern int __omp_collector_api(void *);
@@ -1237,7 +1244,13 @@ extern "C" void my_thread_begin() {
 extern "C" void my_thread_begin(my_ompt_thread_type_t thread_type, ompt_thread_id_t thread_id) {
   // special entry?
 #endif // version
-  if (is_master) return;
+#if defined (TAU_USE_TLS)
+  if (is_master) return; // master thread can't be a new worker.
+#elif defined (TAU_USE_DTLS)
+  if (is_master) return; // master thread can't be a new worker.
+#elif defined (TAU_USE_PGS)
+  if (pthread_getspecific(thr_id_key) == NULL) return; // master thread can't be a new worker.
+#endif
   TAU_OMPT_COMMON_ENTRY;
   //TAU_VERBOSE("OMPT Created thread: %d\n", tid); fflush(stdout);
   Tau_create_top_level_timer_if_necessary();
@@ -1534,7 +1547,14 @@ int __ompt_initialize() {
   if (!TauEnv_get_openmp_runtime_enabled()) return 0;
   TAU_VERBOSE("Registering OMPT events...\n"); fflush(stderr);
   initializing = true;
+#if defined (TAU_USE_TLS)
   is_master = true;
+#elif defined (TAU_USE_DTLS)
+  is_master = true;
+#elif defined (TAU_USE_PGS)
+  pthread_key_create(&thr_id_key, NULL);
+  pthread_setspecific(thr_id_key, 1);
+#endif
   TAU_OPENMP_INIT_LOCK;
 
   /* required events */
