547b378d75f407d70ca9e4247684bcc218635076
John C. Linford <jlinford@paratools.com>
2016-03-01 07:58:58 -0500

    Rewrite OpenCL wrapper.
    Memory copy intervals aren't timed correctly yet.
    
    
    Former-commit-id: 1c51e39038ac6e77001bcc2ac475a0556ef45e73

--- include/Profile/TauGpuAdapterOpenCL.h
+++ include/Profile/TauGpuAdapterOpenCL.h
@@ -7,10 +7,6 @@
 #endif
 #include <string.h>
 
-#ifndef CL_CALLBACK
-#define CL_CALLBACK
-#endif
-
 #define TAU_MAX_FUNCTIONNAME 200
 
 // Putting "using namespace" statements in header files can create ambiguity
@@ -28,148 +24,91 @@ double sync_offset = 0;
 
 class OpenCLGpuEvent : public GpuEvent {
 
-public:
-	x_uint64 commandId;
-	cl_device_id id;
-	cl_event *event;
-	const char *name;
-	FunctionInfo *callingSite;
-	GpuEventAttributes *gpu_event_attr;
-	int number_of_gpu_events;
-	int memcpy_type;
+  public:
+    x_uint64 commandId;
+    cl_device_id id;
+    cl_event event;
+    const char *name;
+    FunctionInfo *callingSite;
+    GpuEventAttributes *gpu_event_attr;
+    int number_of_gpu_events;
+    int memcpy_type;
 
-	//double sync_offset;
+    //double sync_offset;
 
-/*	cudaGpuEvent(const NvU64 cId, const NvU32 dId) :
-		contextId(cId), deviceId(dId) {} */
-/*
-	OpenCLGpuEvent(char* n, FunctionInfo* cs, cl_event* ev)
-	{
-		name  = n;
-		callingSite = cs;
-		event = ev;
-		memcpy_type = -1;
-		gpu_event_attr = NULL;
-	}
-	OpenCLGpuEvent(char* n, FunctionInfo* cs, cl_event* ev, GpuEventAttributes *m, int nAttr)
-	{
-		name  = n;
-		callingSite = cs;
-		event = ev;
-		memcpy_type = -1;
-		gpu_event_attr = m;
-		number_of_gpu_events = nAttr;
-	}
-	OpenCLGpuEvent(char* n, FunctionInfo* cs, cl_event* ev, int memtype)
-	{
-		name  = n;
-		callingSite = cs;
-		event = ev;
-		memcpy_type = memtype;
-		gpu_event_attr = NULL;
-	}
-	OpenCLGpuEvent(char* n, FunctionInfo* cs, cl_event* ev, int memtype,  GpuEventAttributes *m, int nAttr)
-	{
-		name  = n;
-		callingSite = cs;
-		event = ev;
-		memcpy_type = memtype;
-		gpu_event_attr = m;
-		number_of_gpu_events = nAttr;
-	}
-*/
-	~OpenCLGpuEvent()
-	{
-		free(gpu_event_attr);
-	}
-	
-	bool isMemcpy()
-	{
-		return memcpy_type != -1;
-	}
-
-	OpenCLGpuEvent(cl_device_id i, x_uint64 cId, double sync)
-	{
-		id = i;
-		commandId = cId;
-		sync_offset = sync;
-	}
-	OpenCLGpuEvent *getCopy() const { 
-			OpenCLGpuEvent *c = new OpenCLGpuEvent(*this);
-			return c;
-	}
-
-	bool less_than(const GpuEvent *other) const
-	{
-		if (this->id_p1() == other->id_p1())
-		{
-			return this->id_p2() < other->id_p2();
-		}
-		else
-		{
-			return this->id_p1() < other->id_p1();
-		}
-		//return strcmp(printId(), ((OpenCLGpuEvent *)o)->printId()) < 0;
-	}
-
-	const char *getName() const { return name; }
-
-	FunctionInfo *getCallingSite() const { 
-  
-    if (callingSite != NULL)
+    ~OpenCLGpuEvent()
     {
-      callingSite->SetPrimaryGroupName("TAU_REMOTE");
+      free(gpu_event_attr);
     }
-    
-    return callingSite; 
-  }
 
-	double syncOffset() const
-	{
-		return sync_offset;
-	}
-	// GPU attributes not implemented for OpenCL.
-	void getAttributes(GpuEventAttributes *&gA, int &num) const
-	{
-		num = number_of_gpu_events;
-		gA = gpu_event_attr;
-	}
+    bool isMemcpy()
+    {
+      return memcpy_type != -1;
+    }
 
-	void recordMetadata(int i) const {}
-	
-	/* CUDA Event are uniquely identified as the pair of two other ids:
-	 * context and call (API).
-	 */
-	const char* gpuIdentifier() const 
-	{	
-			//printf("in printId, id: %d.\n", id);
-			char r[40];
-			sprintf(r, "%d:%lld", id, commandId);
-			return r;
-	}
-	x_uint64 id_p1() const { return (x_uint64) id; }
-	x_uint64 id_p2() const { return (x_uint64) commandId; }
+    OpenCLGpuEvent(cl_device_id i, x_uint64 cId, double sync)
+    {
+      id = i;
+      commandId = cId;
+      sync_offset = sync;
+    }
+    OpenCLGpuEvent *getCopy() const { 
+      OpenCLGpuEvent *c = new OpenCLGpuEvent(*this);
+      return c;
+    }
+
+    bool less_than(const GpuEvent *other) const
+    {
+      if (this->id_p1() == other->id_p1())
+      {
+        return this->id_p2() < other->id_p2();
+      }
+      else
+      {
+        return this->id_p1() < other->id_p1();
+      }
+      //return strcmp(printId(), ((OpenCLGpuEvent *)o)->printId()) < 0;
+    }
+
+    const char *getName() const { return name; }
+
+    FunctionInfo *getCallingSite() const { 
+
+      if (callingSite != NULL)
+      {
+        callingSite->SetPrimaryGroupName("TAU_REMOTE");
+      }
+
+      return callingSite; 
+    }
+
+    double syncOffset() const
+    {
+      return sync_offset;
+    }
+    // GPU attributes not implemented for OpenCL.
+    void getAttributes(GpuEventAttributes *&gA, int &num) const
+    {
+      num = number_of_gpu_events;
+      gA = gpu_event_attr;
+    }
+
+    void recordMetadata(int i) const {}
+
+    /* CUDA Event are uniquely identified as the pair of two other ids:
+     * context and call (API).
+     */
+    const char* gpuIdentifier() const 
+    {	
+      char r[40];
+      sprintf(r, "%d:%lld", id, commandId);
+      return strdup(r);
+    }
+
+    x_uint64 id_p1() const { return (x_uint64) id; }
+    x_uint64 id_p2() const { return (x_uint64) commandId; }
 };
-/*
-class callback_data
-{
-	public:
 
-	char* name;
-	FunctionInfo* callingSite;
-	cl_event* event;
-	int memcpy_type;
-	OpenCLGpuEvent *id;
-	GpuEventAttributes *gpu_event_attr;
-
-	callback_data(char* n, OpenCLGpuEvent *id, FunctionInfo* cs, cl_event* ev);
-	callback_data(char* n, OpenCLGpuEvent *id, FunctionInfo* cs, cl_event* ev, int memtype);
-	callback_data(char* n, OpenCLGpuEvent *id, FunctionInfo* cs, cl_event* ev, GpuEventAttributes *m);
-	callback_data(char* n, OpenCLGpuEvent *id, FunctionInfo* cs, cl_event* ev, int memtype, GpuEventAttributes *m);
-	bool isMemcpy();
-	~callback_data();
-};
-*/
 int memcpy_data_size = sizeof(OpenCLGpuEvent);
 
 int kernel_data_size = sizeof(OpenCLGpuEvent);
@@ -186,7 +125,7 @@ void Tau_opencl_register_gpu_event(OpenCLGpuEvent *id, double start, double stop
 
 void Tau_opencl_register_memcpy_event(OpenCLGpuEvent *id, double start, double stop, int transferSize, int MemcpyType);
 
-OpenCLGpuEvent *Tau_opencl_enqueue_event(const char* name, cl_command_queue q, cl_event *e, FunctionInfo *fi, int memcpy_type);
+OpenCLGpuEvent *Tau_opencl_enqueue_event(const char* name, cl_command_queue q, cl_event * e, FunctionInfo *fi, int memcpy_type);
 
 void Tau_opencl_register_sync_event();
 
@@ -197,10 +136,10 @@ OpenCLGpuEvent *Tau_opencl_retrive_gpu(cl_command_queue cq);
 //Memcpy event callback
 
 void CL_CALLBACK Tau_opencl_memcpy_callback(cl_event event, cl_int command_stat, void
-*memcpy_type);
+    *memcpy_type);
 
 void CL_CALLBACK Tau_opencl_kernel_callback(cl_event event, cl_int command_stat, void
-*kernel_type);
+    *kernel_type);
 
 double Tau_opencl_sync_clocks(cl_command_queue commandQueue, cl_context context);
 
