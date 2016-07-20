e2c1a9e81b929fb36b4598a455e8047ac32e8869
John C. Linford <jlinford@paratools.com>
2016-07-20 09:48:56 -0700

    Fortran broadcast{4,8} prototypes.
    Former-commit-id: 7cd532469c4ef9ed01b7818378f2b2fc980d4d97

--- src/wrappers/shmem/openshmem/tau_shmem.h
+++ src/wrappers/shmem/openshmem/tau_shmem.h
@@ -1,2 +1,7 @@
 #include <shmem.h>
 #include <shmemx.h>
+
+void shmem_broadcast4(void *dest, const void *source, size_t nelems, int PE_root, int PE_start, int logPE_stride, int PE_size, long *pSync);
+
+void shmem_broadcast8(void *dest, const void *source, size_t nelems, int PE_root, int PE_start, int logPE_stride, int PE_size, long *pSync);
+
