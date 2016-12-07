5758cf15d646a120f75dcff4a1f3e786fa433b9d
Aurele MAHEO <amaheo@uoregon.edu>
2016-12-06 19:59:16 -0800

    declare function in a separate cpp file to generate profiling inside MPC wrapper of MPI_Finalize() function: still buggy when P (number of processes) <> N (number of tasks)
    Former-commit-id: afa025c11003e6b215c68a9ee5661d6accc8af29

--- src/Profile/TauMetaDataMerge.cpp
+++ src/Profile/TauMetaDataMerge.cpp
@@ -29,6 +29,81 @@ using namespace std;
 
 extern "C" int TAU_MPI_Finalized();
 
+extern "C" int Tau_metadataMerge_mergeMetaData_bis() {
+//extern int Tau_metadataMerge_mergeMetaData_bis() {
+
+  Tau_metadata_fillMetaData();
+
+  static int merged = 0;
+  if (merged == 1) {
+    return 0;
+  }
+  merged = 1;
+
+  int rank = 0;
+
+#ifdef TAU_MPI
+  int numRanks;
+  if (TAU_MPI_Finalized()) {
+    fprintf(stdout, "TAU_MPI_Finalized() called\n");
+    return 0;
+  }
+
+  PMPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  PMPI_Comm_size(MPI_COMM_WORLD, &numRanks);
+
+  TAU_VERBOSE("TAU: rank=%d, numRanks=%d\n", rank, numRanks);
+#endif /* TAU_MPI */
+
+  x_uint64 start, end;
+
+#if 1
+  if (rank == 0) {
+
+    TAU_VERBOSE("TAU: Merging MetaData...\n");
+    start = TauMetrics_getTimeOfDay();
+
+#if 1
+#ifdef TAU_MPI
+    Tau_util_outputDevice *out = Tau_metadata_generateMergeBuffer();
+    char *defBuf = Tau_util_getOutputBuffer(out);
+    int defBufSize = Tau_util_getOutputBufferLength(out);
+
+    //PMPI_Bcast(&defBufSize, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    //PMPI_Bcast(defBuf, defBufSize, MPI_CHAR, 0, MPI_COMM_WORLD);
+#endif /* TAU_MPI */
+#endif
+
+    end = TauMetrics_getTimeOfDay();
+    TAU_VERBOSE("TAU: MetaData Merging Complete, duration = %.4G seconds\n", ((double)(end-start))/1000000.0f);
+    char tmpstr[256];
+    sprintf(tmpstr, "%.4G seconds", ((double)(end-start))/1000000.0f);
+    TAU_METADATA("TAU MetaData Merge Time", tmpstr);
+
+#if 0
+#ifdef TAU_MPI
+	Tau_util_destroyOutputDevice(out);
+#endif /* TAU_MPI */
+#endif
+
+  } else {
+
+#if 0
+#ifdef TAU_MPI
+    TAU_VERBOSE("TAU: Metadata, rank different from 0\n");
+    int BufferSize;
+    PMPI_Bcast(&BufferSize, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    char *Buffer = (char*) TAU_UTIL_MALLOC(BufferSize);
+    PMPI_Bcast(Buffer, BufferSize, MPI_CHAR, 0, MPI_COMM_WORLD);
+    Tau_metadata_removeDuplicates(Buffer, BufferSize);
+	free(Buffer);
+#endif /* TAU_MPI */
+#endif
+  }
+#endif
+
+  return 0;
+}
 
 extern "C" int Tau_metadataMerge_mergeMetaData() {
 
@@ -45,20 +120,25 @@ extern "C" int Tau_metadataMerge_mergeMetaData() {
 #ifdef TAU_MPI
   int numRanks;
   if (TAU_MPI_Finalized()) {
+    fprintf(stdout, "TAU_MPI_Finalized() called\n");
     return 0;
   }
 
   PMPI_Comm_rank(MPI_COMM_WORLD, &rank);
   PMPI_Comm_size(MPI_COMM_WORLD, &numRanks);
+
+  TAU_VERBOSE("TAU: rank=%d, numRanks=%d\n", rank, numRanks);
 #endif /* TAU_MPI */
 
   x_uint64 start, end;
 
+#if 1
   if (rank == 0) {
 
     TAU_VERBOSE("TAU: Merging MetaData...\n");
     start = TauMetrics_getTimeOfDay();
 
+#if 1
 #ifdef TAU_MPI
     Tau_util_outputDevice *out = Tau_metadata_generateMergeBuffer();
     char *defBuf = Tau_util_getOutputBuffer(out);
@@ -67,18 +147,25 @@ extern "C" int Tau_metadataMerge_mergeMetaData() {
     PMPI_Bcast(&defBufSize, 1, MPI_INT, 0, MPI_COMM_WORLD);
     PMPI_Bcast(defBuf, defBufSize, MPI_CHAR, 0, MPI_COMM_WORLD);
 #endif /* TAU_MPI */
+#endif
 
     end = TauMetrics_getTimeOfDay();
     TAU_VERBOSE("TAU: MetaData Merging Complete, duration = %.4G seconds\n", ((double)(end-start))/1000000.0f);
     char tmpstr[256];
     sprintf(tmpstr, "%.4G seconds", ((double)(end-start))/1000000.0f);
     TAU_METADATA("TAU MetaData Merge Time", tmpstr);
+
+#if 1
 #ifdef TAU_MPI
 	Tau_util_destroyOutputDevice(out);
 #endif /* TAU_MPI */
+#endif
 
   } else {
+
+#if 1
 #ifdef TAU_MPI
+    TAU_VERBOSE("TAU: Metadata, rank different from 0\n");
     int BufferSize;
     PMPI_Bcast(&BufferSize, 1, MPI_INT, 0, MPI_COMM_WORLD);
     char *Buffer = (char*) TAU_UTIL_MALLOC(BufferSize);
@@ -86,7 +173,10 @@ extern "C" int Tau_metadataMerge_mergeMetaData() {
     Tau_metadata_removeDuplicates(Buffer, BufferSize);
 	free(Buffer);
 #endif /* TAU_MPI */
+#endif
   }
+#endif
+
   return 0;
 }
 
