6596c6d354caf45de8ecc077a0c1f710ab7c3386
Aurele MAHEO <amaheo@uoregon.edu>
2017-02-22 11:01:06 -0800

    Plugin manager: add num_args and args arguments to Tau_util_call_plugin_func()
    Former-commit-id: 2a4ed0f818062acc82c84c40aa33a4a97339d1bc

--- include/Profile/TauUtil.h
+++ include/Profile/TauUtil.h
@@ -63,8 +63,8 @@ void *Tau_util_malloc(size_t size, const char *file, int line);
 void *Tau_util_calloc(size_t size, const char *file, int line);
 #define TAU_UTIL_CALLOC(size) Tau_util_calloc(size, __FILE__, __LINE__);
 
-int Tau_util_load_plugin(char *name, char *path, void *args);
-int Tau_util_call_plugin_func(char *name, char *funcName);
+int Tau_util_load_plugin(char *name, char *path, int num_args, void **args);
+int Tau_util_call_plugin_func(char *name, char *funcName, int num_args, void **args);
 //int Tau_util_close_plugin();
 int Tau_util_cleanup_plugins();
 
