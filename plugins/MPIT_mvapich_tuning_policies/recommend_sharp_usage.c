2fc00625f951279d64ecf1155b734748769f0370
Srinivasan Ramesh <sramesh@godzilla.nic.uoregon.edu>
2017-04-19 12:37:33 -0700

    Add necessary header files
    Former-commit-id: 6ab028faca83f49e148fc5a2cf57f4945b4933e4

--- plugins/MPIT_mvapich_tuning_policies/recommend_sharp_usage.c
+++ plugins/MPIT_mvapich_tuning_policies/recommend_sharp_usage.c
@@ -1,5 +1,9 @@
 #include <mpi.h>
+
 #include <Profile/Profiler.h>
+#include <Profile/UserEvent.h>
+#include <Profile/TauMetrics.h>
+
 #include <Profile/TauEnv.h>
 #include <Profile/TauMpiTTypes.h>
 #include <stdio.h>
@@ -12,11 +16,13 @@
 #define TAU_NAME_LENGTH 1024
 
 int Tau_mpi_t_recommend_sharp_usage(int argc, void** argv) {
-  double *exclusiveTimeApp = (double*)malloc(sizeof(double));
+  double exclusiveTime;
+
   void* ptr = NULL;
   ptr = (void *)Tau_pure_search_for_function("MPI_Wait()");
-  Tau_get_exclusive_values(ptr, exclusiveTimeApp, Tau_get_thread());
 
+   
+  
   printf("Total time exclusive for application is %f\n", exclusiveTimeApp);
   return 0;
 }
