2e210886c471c5ed37f0929a84c9d264b45f2f17
Srinivasan Ramesh <sramesh@godzilla.nic.uoregon.edu>
2017-05-04 17:54:33 -0700

    Add initialization routine for plugin callback data structure
    Former-commit-id: e7209b5e451cac526732771a4b63c7c79eda81ba

--- include/Profile/TauPlugin.h
+++ include/Profile/TauPlugin.h
@@ -1,6 +1,10 @@
 #ifndef _TAU_PLUGIN_H_
 #define _TAU_PLUGIN_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
 #define TAU_PLUGIN_PATH "TAU_PLUGIN_PATH"
 #define TAU_PLUGINS "TAU_PLUGINS"
 #define TAU_PLUGIN_INIT_FUNC "Tau_plugin_init_func"
@@ -41,19 +45,10 @@ int Tau_util_load_and_register_plugins(PluginManager* plugin_manager);
 void* Tau_util_load_plugin(const char *name, const char *path, PluginManager* plugin_manager);
 void* Tau_util_register_plugin(const char *name, void* handle, PluginManager* plugin_manager);
 
-#ifdef __cplusplus
-extern "C" void Tau_util_plugin_manager_register_role_hook(PluginManager* plugin_manager, const char* role_name, PluginRoleHook role_hook);
-extern "C" void Tau_util_apply_role_hook(PluginManager* plugin_manager, const char* role_name, int argc, void** argv);
-#else
 void Tau_util_plugin_manager_register_role_hook(PluginManager* plugin_manager, const char* role_name, PluginRoleHook role_hook);
 void Tau_util_apply_role_hook(PluginManager* plugin_manager, const char* role_name, int argc, void** argv);
-#endif
 
 int Tau_util_cleanup_all_plugins(PluginManager* plugin_manager);
-
-#ifdef __cplusplus
-extern "C" {
-#endif /* __cplusplus */
 
 /*Define callbacks for specific events*/
 typedef int (*Tau_plugin_function_registration_complete)(void);
@@ -63,19 +58,24 @@ typedef int (*Tau_plugin_atomic_event_registration)(void);
 typedef int (*Tau_plugin_atomic_event_trigger)(void);
 typedef int (*Tau_plugin_end_of_execution)(void);
 
-typedef struct Tau_plugin_callbacks {
+struct Tau_plugin_callbacks{
    Tau_plugin_function_registration_complete FunctionRegistrationComplete;
    Tau_plugin_function_invocation FunctionInvocation;
    Tau_plugin_function_exit FunctionExit;
    Tau_plugin_atomic_event_registration AtomicEventRegistration;
    Tau_plugin_atomic_event_trigger AtomicEventTrigger;
    Tau_plugin_end_of_execution EndOfExecution;
-} Tau_plugin_callbacks;
+};
+
+typedef struct Tau_plugin_callbacks Tau_plugin_callbacks;
 
 PluginManager* Tau_util_get_plugin_manager();
+
+void Tau_util_init_tau_plugin_callbacks(Tau_plugin_callbacks* cb);
+void Tau_util_plugin_register_callbacks(Tau_plugin_callbacks cb);
+
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
-
 
 #endif /* _TAU_PLUGIN_H_ */
