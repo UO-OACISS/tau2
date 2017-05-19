180414f1a0f352220b3c2c750d6a64de37693e7c
Sameer Shende <sameer@cs.uoregon.edu>
2017-05-18 17:37:49 -0700

    Checking if plugins dir and path are null.
    Former-commit-id: f0d08a07ea29d08f2091026f3698567535b63502

--- src/Profile/UserEvent.cpp
+++ src/Profile/UserEvent.cpp
@@ -113,11 +113,11 @@ void TauUserEvent::AddEventToDB()
   DEBUGPROFMSG("Size of eventDB is " << TheEventDB().size() <<endl);
 
   /*Invoke plugins only if both plugin path and plugins are specified*/
-  //if((strcmp(TauEnv_get_plugin_path(), "") != 0) && (strcmp(TauEnv_get_plugins(), "") != 0)) {
+  if(TauEnv_get_plugin_path() && TauEnv_get_plugins()) {
     Tau_plugin_event_atomic_event_registration_data plugin_data;
     plugin_data.user_event_ptr = this;
     Tau_util_invoke_callbacks(TAU_PLUGIN_EVENT_ATOMIC_EVENT_REGISTRATION, &plugin_data);
-  //}
+  }
 
   /* Set user event id */
   eventId = RtsLayer::GenerateUniqueId();
@@ -274,11 +274,11 @@ void TauUserEvent::TriggerEvent(TAU_EVENT_DATATYPE data, int tid, double timesta
     }
 #endif /* PROFILING_ON */
   /*Invoke plugins only if both plugin path and plugins are specified*/
-  //  if((strcmp(TauEnv_get_plugin_path(), "") != 0) && (strcmp(TauEnv_get_plugins(), "") != 0)) {
+    if(TauEnv_get_plugin_path() && TauEnv_get_plugins()) {
       Tau_plugin_event_atomic_event_trigger_data plugin_data;
       plugin_data.user_event_ptr = this;
       Tau_util_invoke_callbacks(TAU_PLUGIN_EVENT_ATOMIC_EVENT_TRIGGER, &plugin_data);
-  //  }
+    }
   } // Tau_global_getLightsOut
 
 }
