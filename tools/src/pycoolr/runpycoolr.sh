5079c6d8f6491de363f8cd79fa5e3b22024a58bc
Aurele MAHEO <amaheo@uoregon.edu>
2017-08-18 13:49:59 -0700

    Add beaconcerberus.cfg configuration file
    Former-commit-id: a9ec2eb90b19d8c6ff10493a89f2be2303662237

--- tools/src/pycoolr/runpycoolr.sh
+++ tools/src/pycoolr/runpycoolr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if currdir=${PWD}
-
+currdir=${PWD}
 echo ${PWD}
+export PYCOOLR_NODE=cerberus.nic.uoregon.edu
 
-./pycoolr/pycoolr-plot/coolr.py configs/beaconcerberus.cfg
+./src/pycoolr/pycoolr-plot/coolr.py configs/beaconcerberus.cfg
