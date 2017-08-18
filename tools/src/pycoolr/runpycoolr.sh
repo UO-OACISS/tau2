f3f70283116351e4bfccaf595fcbc13686d9aa61
Aurele MAHEO <amaheo@uoregon.edu>
2017-08-18 14:05:32 -0700

    Add environment variables inside pycoolr script to run Pycoolr successfully
    Former-commit-id: ba05a4638fc3a00be660b8244db1075e62261f1c

--- tools/src/pycoolr/runpycoolr.sh
+++ tools/src/pycoolr/runpycoolr.sh
@@ -2,6 +2,18 @@
 
 currdir=${PWD}
 echo ${PWD}
-export PYCOOLR_NODE=cerberus.nic.uoregon.edu
 
-./src/pycoolr/pycoolr-plot/coolr.py configs/beaconcerberus.cfg
+export BEACON_TOPOLOGY_SERVER_ADDR=128.223.202.147
+export BEACON_ROOT_FS=/dev/shm/aurelem
+export LD_LIBRARY_PATH=/home/users/aurelem/beacon/mix/BEACON_inst/lib:$LD_LIBRARY_PATH
+export PATH=/home/users/aurelem/beacon/mix/BEACON_inst/bin:/home/users/aurelem/beacon/mix/BEACON_inst/sbin:$PATH
+export PYCOOLR_NODE=cerberus.nic.uoregon.edu
+export PYCOOLR_LIBPATH=/home/users/aurelem/beacon/mix/BEACON_inst/lib
+alias psbeacon="ps -aux | grep beacon"
+killall -9 beacon_topology_setup_server
+killall -9 global_beacon
+beacon_topology_setup_server > ./setup_server.log 2>&1 &
+sleep 1
+global_beacon > ./global_beacon.log 2>&1 &
+
+./src/pycoolr/pycoolr-plot/coolr.py src/pycoolr/pycoolr-plot/configs/beaconcerberus.cfg
