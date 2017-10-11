2db1b2a4cb299f220d80d14f52578073cc404854
Srinivasan Ramesh <sramesh@godzilla.nic.uoregon.edu>
2017-10-11 12:44:22 -0700

    [CALIPER] Make routines thread-safe
    Former-commit-id: 689b96dec4be346194a12c12e2457403a944fadc

--- src/Profile/TauCaliper.cpp
+++ src/Profile/TauCaliper.cpp
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <map>
 #include <stack>
-#include <mutex>
 #include <caliper/cali.h>
 #include <TAU.h>
+
 
 #include <Profile/TauCaliperTypes.h>
 
@@ -38,11 +38,11 @@ extern "C" void cali_init() {
 
   TAU_VERBOSE("TAU: CALIPER init invoked.\n");
 
-  
+ 
+  RtsLayer::LockEnv(); 
+
   //Initialize the ID field to 0. This is the dummy id generation mechanism that we 
-  RtsLayer::LockDB();
   current_id = 0;
-  RtsLayer::UnLockDB();
 
   if(Tau_init_initializeTAU()) {
 
@@ -50,6 +50,8 @@ extern "C" void cali_init() {
   }
 
   cali_tau_initialized = 1;
+
+  RtsLayer::UnLockEnv();
 }
 
 /**
@@ -62,8 +64,13 @@ extern "C" void cali_init() {
 cali_err cali_begin_double_byname(const char* attr_name, double val) {
 
   /*We do not supporting "stacking" semantics for UserEvents*/
+  RtsLayer::LockEnv();
+
   if(!attribute_stack[std::string(attr_name)].empty()) {
     printf("TAU: CALIPER operation not supported! TAU UserEvent has already been created for %s. Use cali_set_double_byname instead to update the value\n", attr_name);
+
+    RtsLayer::UnLockEnv();
+
     return CALI_EINV;
   }
 
@@ -74,12 +81,21 @@ cali_err cali_begin_double_byname(const char* attr_name, double val) {
   value.type = DOUBLE;
   value.data.as_double = val;
   attribute_stack[std::string(attr_name)].push(value);
+
+  RtsLayer::UnLockEnv();
+
   return CALI_SUCCESS;
 }
 
 cali_err cali_begin_int_byname(const char* attr_name, int val) {
+
+  RtsLayer::LockEnv();
+
   if(!attribute_stack[std::string(attr_name)].empty()) {
     printf("TAU: CALIPER operation not supported! TAU UserEvent has already been created for %s. Use cali_set_int_byname instead to update the value.\n", attr_name);
+
+    RtsLayer::UnLockEnv();
+
     return CALI_EINV;
   }
 
@@ -91,22 +107,31 @@ cali_err cali_begin_int_byname(const char* attr_name, int val) {
   value.data.as_integer = val;
   attribute_stack[std::string(attr_name)].push(value);
 
+  RtsLayer::UnLockEnv();
+
   return CALI_SUCCESS;
 }
 
 /* TAU Wrapper: Create and start a timer with a given name*/
 extern "C" cali_err cali_begin_byname(const char* attr_name) {
+
+  RtsLayer::LockEnv();
+
   if(!cali_tau_initialized)
     cali_init();
 
   TAU_VERBOSE("TAU: CALIPER create and start a TAU static timer with name: %s\n", attr_name);
   TAU_START(attr_name);
 
+  RtsLayer::UnLockEnv();
+
   return CALI_SUCCESS;
 }
 
 /* TAU Wrapper: Start a nested timer with \a val name*/
 cali_err cali_begin_string_byname(const char* attr_name, const char* val) {
+  RtsLayer::LockEnv();
+
   if(!cali_tau_initialized)
     cali_init();
   
@@ -124,6 +149,9 @@ cali_err cali_begin_string_byname(const char* attr_name, const char* val) {
 
   //Start timer with name \a val
   TAU_START(val);
+
+  RtsLayer::UnLockEnv(); 
+
   return CALI_SUCCESS;
 } 
 
@@ -133,6 +161,8 @@ cali_err cali_begin_string_byname(const char* attr_name, const char* val) {
  * 3. String types: Operation currently not supported
  */
 cali_err cali_set_double_byname(const char* attr_name, double val) {
+  RtsLayer::LockEnv();
+
   if(!cali_tau_initialized)
     cali_init();
 
@@ -145,12 +175,17 @@ cali_err cali_set_double_byname(const char* attr_name, double val) {
   StackValue value;
   value.type = DOUBLE;
   value.data.as_double = val;
+
   attribute_stack[std::string(attr_name)].push(value);
+
+  RtsLayer::UnLockEnv();
 
   return CALI_SUCCESS;
 }
 
 cali_err cali_set_int_byname(const char* attr_name, int val) {
+  RtsLayer::LockEnv();
+ 
   if(!cali_tau_initialized)
     cali_init();
 
@@ -163,7 +198,11 @@ cali_err cali_set_int_byname(const char* attr_name, int val) {
   StackValue value;
   value.type = INTEGER;
   value.data.as_integer = val;
+
   attribute_stack[std::string(attr_name)].push(value);
+
+  RtsLayer::UnLockEnv();
+
   return CALI_SUCCESS;
 }
 
@@ -180,12 +219,16 @@ cali_err cali_set_string_byname(const char* attr_name, const char* val) {
  *              If stack is empty, stop the top level timer with name \a attr_name
  */
 cali_err cali_end_byname(const char* attr_name) {
+  RtsLayer::LockEnv(); 
+
   if(!cali_tau_initialized)
     cali_init();
 
   if(!attribute_stack[std::string(attr_name)].empty()) {
+
     StackValue value = attribute_stack[std::string(attr_name)].top();
     attribute_stack[std::string(attr_name)].pop();
+   
 
     if(value.type == STRING) {
       TAU_VERBOSE("TAU: CALIPER stop timer with name: %s\n", attr_name);
@@ -197,6 +240,9 @@ cali_err cali_end_byname(const char* attr_name) {
     TAU_VERBOSE("TAU: CALIPER stop top level timer with name %s\n", attr_name);
     TAU_STOP(attr_name);
   }
+  
+  RtsLayer::UnLockEnv(); 
+  return CALI_SUCCESS;
 } 
 /*
  * --- Attributes ------------------------------------------------------
@@ -213,6 +259,11 @@ cali_err cali_end_byname(const char* attr_name) {
  * \param type Type of the attribute
  * \param properties Attribute properties
  * \return Attribute id
+ * TAU Wrapper: Maintain a local key:value mapping of name:id and id:name
+ * 		We generate our own ID (dumb counter), but we don't pass through
+ * 		any calls to CALIPER.
+ * 		As of the moment, we do not support properties while creating the caliper attributes.
+ * 		We may support some basic ones involving process / thread scope.
  */
 
 cali_id_t cali_create_attribute(const char*     name,
@@ -222,15 +273,17 @@ cali_id_t cali_create_attribute(const char*     name,
   if(!cali_tau_initialized)
     cali_init();
 
-  RtsLayer::LockDB();
-
-  //Critical section?    
+  RtsLayer::LockEnv(); 
+  //Critical section
   ++current_id;
   _attribute_name_map_[name] = current_id;
   _attribute_id_map_[current_id] = name;
+  RtsLayer::UnLockEnv();
 
-  RtsLayer::UnLockDB();
+  if(properties != CALI_ATTR_DEFAULT) {
+    fprintf(stdout, "TAU: CALIPER Warning: Property combination for attribute not supported. CALI_ATTR_SCOPE_PROCESS is assumed as default\n");
+  }
 
   return current_id;
 }  
-   
+ 
