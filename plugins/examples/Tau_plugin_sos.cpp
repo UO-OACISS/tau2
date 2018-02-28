fa49d4fb142af3282bfbf082c510f46b3f4e7d6e
Aurele MAHEO <amaheo@uoregon.edu>
2018-02-28 14:47:07 -0800

    [SOS] Added SOS plugin
    Former-commit-id: 96a8bda9cb450dd6619d5738f7529eaccf449636

---
+++ plugins/examples/Tau_plugin_sos.cpp
@@ -0,0 +1,36 @@
+/************************************************************************************************
+ * *   Plugin Testing
+ * *   Tests basic functionality of a plugin for function registration event
+ * *
+ * *********************************************************************************************/
+
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+
+#include <Profile/Profiler.h>
+#include <Profile/TauSampling.h>
+#include <Profile/TauMetrics.h>
+#include <Profile/TauAPI.h>
+#include <Profile/TauPlugin.h>
+
+int Tau_plugin_dump(Tau_plugin_event_function_dump_data data) {
+  printf("TAU PLUGIN: Function tid: %d\n", data.tid);
+  
+  return 0;
+}
+
+/*This is the init function that gets invoked by the plugin mechanism inside TAU.
+ * Every plugin MUST implement this function to register callbacks for various events 
+ * that the plugin is interested in listening to*/
+extern "C" int Tau_plugin_init_func(int argc, char **argv) {
+  Tau_plugin_callbacks * cb = (Tau_plugin_callbacks*)malloc(sizeof(Tau_plugin_callbacks));
+  TAU_UTIL_INIT_TAU_PLUGIN_CALLBACKS(cb);
+  cb->FunctionDump = Tau_plugin_dump;
+  TAU_UTIL_PLUGIN_REGISTER_CALLBACKS(cb);
+
+  return 0;
+}
+
