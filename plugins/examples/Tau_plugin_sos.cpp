a6e84ee7914630523a357d6bb6f51e9733ddc4fa
Aurele MAHEO <amaheo@uoregon.edu>
2018-02-28 18:35:29 -0800

    [SOS] Add a function to SOS plugin for finalizing
    Former-commit-id: 0062c0efc66626b842b27fff7fd34654859434ee

--- plugins/examples/Tau_plugin_sos.cpp
+++ plugins/examples/Tau_plugin_sos.cpp
@@ -25,13 +25,23 @@ int Tau_plugin_dump(Tau_plugin_event_function_dump_data data) {
   return 0;
 }
 
+int Tau_plugin_finalize(Tau_plugin_event_function_finalize_data) {
+
+  TAU_SOS_finalize();
+
+  return 0;
+}
+
 /*This is the init function that gets invoked by the plugin mechanism inside TAU.
  * Every plugin MUST implement this function to register callbacks for various events 
  * that the plugin is interested in listening to*/
 extern "C" int Tau_plugin_init_func(int argc, char **argv) {
   Tau_plugin_callbacks * cb = (Tau_plugin_callbacks*)malloc(sizeof(Tau_plugin_callbacks));
+
+  TAU_SOS_init_simple();
   TAU_UTIL_INIT_TAU_PLUGIN_CALLBACKS(cb);
   cb->FunctionDump = Tau_plugin_dump;
+  cb->FunctionFinalize = Tau_plugin_finalize;
   TAU_UTIL_PLUGIN_REGISTER_CALLBACKS(cb);
 
   return 0;
