4b6249a1ab9bda089e2e87196edaf0981ce4ffa9
Aurele MAHEO <amaheo@uoregon.edu>
2018-06-05 20:58:34 -0700

    [MPC] Implement TAU_MPI_Finalized() function in TauMpi.c only if using non MPC MPI support
    Former-commit-id: f49293ffb10c2d8473cb76c609d3594f59c5ca7a

--- src/Profile/TauMpi.c
+++ src/Profile/TauMpi.c
@@ -1793,11 +1793,13 @@ MPI_Errhandler errhandler;
   return returnVal;
 }
 
+#if !defined(TAU_MPC)
 int tau_mpi_finalized = 0;
 int TAU_MPI_Finalized() {
   //fprintf(stdout, "In TAU_MPI_Finalized(): tau_mpi_finalized=%d\n", tau_mpi_finalized);
   return tau_mpi_finalized;
 }
+#endif
 
 void finalizeCallSites_if_necessary();
 int  MPI_Finalize(  )
