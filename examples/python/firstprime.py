988d050f95a4c4dc6324cca5f9a870f5cd95c774
Sameer Shende <sameer@cs.uoregon.edu>
2018-08-05 20:53:11 -0700

    A better python example. Calculates first prime number beyond a given number. Contribution by Devin Shende.
    Former-commit-id: d8eafc58aaba7bc0c8027f881847a4dae04918c1

---
+++ examples/python/firstprime.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#Program written by Devin Shende on August 5th 2018
+
+def firstPrimeAfter(x):
+	import math
+	current = x + 1 
+	sqrtno = math.sqrt(current)
+	while True:
+		#search for primes starting at x until it finds one
+		#break once found a prime
+		for potentialfactor in range(2,current):
+			# start at 2 because 1 will always be a factor
+			# go all the way up to the sqrt of current looking for a factor
+			if current % potentialfactor == 0:
+				# Found factor. not prime
+				break # move on to next number
+			elif potentialfactor >= sqrtno: 
+				print("The first prime number after {} is {}".format(x,current))
+				return current
+		current += 1
+
+firstPrimeAfter(10000000)
+
