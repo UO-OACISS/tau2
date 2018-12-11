c62e22dc152c5206e59fed163ce06b4ef1705f42
Srinivasan Ramesh <sramesh@delphi.nic.uoregon.edu>
2018-12-10 17:05:45 -0800

    Add changes
    Former-commit-id: 4fc77f4e37b9a1ad0742dd41e2d8ad9c67ec0926

--- examples/sos/go.sh
+++ examples/sos/go.sh
@@ -1,29 +1,26 @@
 #!/bin/bash -x
 
-#In order to run this example, please change the lines below that say UPDATE
+# In the current form, this example expects TAU to be configured as follows:
+# ./configure -bfd=download -unwind=download -mpi -mpit -sos=download -prefix=<>
+# In order to run this example, please change the lines below that say UPDATE
 
 DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-TAU=/home/users/sramesh/MPI_T/TAU_INSTALLATION # UPDATE
-
-export PATH=$TAU/86_64/bin:$TAU/x86_64/sos/sos_flow/build-linux/bin:$PATH #UPDATE
+# Update 
+######################################
+TAU=/home/users/sramesh/MPI_T/TAU_INSTALLATION
+export PATH=$TAU/86_64/bin:$TAU/x86_64/sos/sos_flow/inst/bin:$PATH
+export TAU_PLUGINS_PATH=$TAU/x86_64/lib/shared-mpi-pthread-sos-mpit
+######################################
 
 export SOS_CMD_PORT=22501
 export SOS_WORK=`pwd`
 export SOS_EVPATH_MEETUP=${DIR}
-#export SOS_IN_MEMORY_DATABASE=1
-# to use periodic, enable this variable, and comment out the
-# TAU_SOS_send_data() call in matmult.c.
-#export TAU_SOS_PERIODIC=1
 
 export TAU_PLUGINS=libTAU-sos-plugin.so
-export TAU_PLUGINS_PATH=$TAU/x86_64/lib/shared-mpi-pthread-sos-mpit # UPDATE
-
 export TAU_VERBOSE=0
 export TAU_PROFILE=1
 export TAU_TRACK_MPI_T_PVARS=1
-
-PLATFORM=`hostname`'.nic.uoregon.edu' # UPDATE
 
 start_sos_daemon()
 {
@@ -57,9 +54,6 @@ ulimit -c unlimited
 
 mpirun -np 4 tau_exec -T mpi,mpit,sos ./matmult &
 sleep 1
-echo "Launch PyCOOLR"
 
-cd $TAU/x86_64/bin
-./pycoolr -tool=sos -platform=$PLATFORM
-#stop_sos_daemon
-#showdb
+echo "Launch PyCOOLR"
+pycoolr -tool=sos
