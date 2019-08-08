979891631ec9e312ba4216e880d2ea57205ff120
Kevin Huck <khuck@cs.uoregon.edu>
2019-08-08 08:28:35 -0700

    Updating null plugin example
    Former-commit-id: 3006de7ba4d673497f9c0c21cedcd9bd3713b0f6

--- examples/null_plugin/sourceme.sh
+++ examples/null_plugin/sourceme.sh
@@ -1,4 +1,8 @@
+TAUROOT=`tau-config | grep TAUROOT | sed 's/TAUROOT=//g'`
+TAUARCH=`tau-config | grep TAUARCH | sed 's/TAUARCH=//g'`
+TAU_BASEDIR=`tau-config | grep BASEDIR | sed 's/BASEDIR=//g'`
+
 export TAU_PLUGINS=libTAU-null-plugin.so
-export TAU_PLUGINS_PATH=../../x86_64/lib/shared-mpi-pthread
+export TAU_PLUGINS_PATH=${TAUROOT}/${TAUARCH}/lib/shared-mpi-pthread
 
 
