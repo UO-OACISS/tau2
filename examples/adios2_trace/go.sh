88f22e92054dc2d4fcbdc05680e247e92951aff3
Kevin Huck <khuck@cs.uoregon.edu>
2019-08-12 11:34:52 -0700

    fixing go script
    Former-commit-id: 583dd5fd30008427be52e23080ea45576e4b0d1f

--- examples/adios2_trace/go.sh
+++ examples/adios2_trace/go.sh
@@ -3,7 +3,7 @@
 # launch the writer without plugin
 echo "Without plugin"
 T="$(date +%s)"
-#mpirun -np 8 ./matmult >& matmult.log
+mpirun -np 8 ./matmult >& matmult.log
 A="$(($(date +%s)-T))"
 printf "Time to run tau: %02d hours %02d minutes %02d seconds.\n" "$((A/3600))" "$((A/60%60))" "$((A%60))"
 
@@ -14,7 +14,7 @@ source ./sourceme.sh
 export TAU_ADIOS2_ENGINE=BP
 echo "BP plugin"
 T="$(date +%s)"
-#mpirun -np 8 ./matmult >& matmult.log
+mpirun -np 8 ./matmult >& matmult.log
 B="$(($(date +%s)-T))"
 printf "Time to run tau+bp: %02d hours %02d minutes %02d seconds.\n" "$((B/3600))" "$((B/60%60))" "$((B%60))"
 
@@ -23,14 +23,14 @@ rm -rf *.bp *.bp.dir
 
 # Launch the writer with plugin - SST writer and reader
 # launch the reader - it will sleep 2 seconds and wait for files
-export TAU_ADIOS2_FILENAME=tau-metrics
-python3 ./reader.py &
-unset TAU_ADIOS2_FILENAME
+#export TAU_ADIOS2_FILENAME=tau-metrics
+#python3 ./reader.py &
+#unset TAU_ADIOS2_FILENAME
 
 export TAU_ADIOS2_ENGINE=SST
 echo "SST plugin"
 T="$(date +%s)"
-mpirun -np 8 ./matmult >& matmult.log
+#mpirun -np 8 ./matmult >& matmult.log
 C="$(($(date +%s)-T))"
 printf "Time to run tau+sst: %02d hours %02d minutes %02d seconds.\n" "$((C/3600))" "$((C/60%60))" "$((C%60))"
 
