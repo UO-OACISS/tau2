f4c526bd24aed29974f3244354e042c6e36ea0e0
Kevin Huck <khuck@cs.uoregon.edu>
2019-08-14 09:39:34 -0700

    Add -papi_components option to tau_exec
    Former-commit-id: 8592922f1d8517a288defbdacd7220f6c4ffd040

--- examples/papi_component_plugin/go.sh
+++ examples/papi_component_plugin/go.sh
@@ -4,10 +4,20 @@ export TAU_PLUGINS=libTAU-papi-components-plugin.so
 export TAU_PLUGINS_PATH=../../x86_64/lib/shared-papi-mpi-pthread
 
 # launch the program with the plugin
-echo "With PAPI component plugin"
+echo "With PAPI component plugin environment variables"
 T="$(date +%s)"
 mpirun -np 4 ./matmult
-#gdb --args ./matmult
+A="$(($(date +%s)-T))"
+printf "Time to run tau: %02d hours %02d minutes %02d seconds.\n" "$((A/3600))" "$((A/60%60))" "$((A%60))"
+
+
+unset TAU_PLUGINS
+unset TAU_PLUGINS_PATH
+
+# launch the program with tau_exec
+echo "With PAPI component plugin tau_exec"
+T="$(date +%s)"
+mpirun -np 4 tau_exec -T papi,mpi,pthread -papi_components ./matmult
 A="$(($(date +%s)-T))"
 printf "Time to run tau: %02d hours %02d minutes %02d seconds.\n" "$((A/3600))" "$((A/60%60))" "$((A%60))"
 
