89257b3b3ff7d2e2e908c11ef10abbc2396d3083
Kevin Huck <khuck@cs.uoregon.edu>
2019-08-28 16:46:54 -0700

    Do NOT call the exit plugin callback on non-zero threads
    Former-commit-id: 66c5f7dd17eeb3ac2afbc8a1bb013d2785f4a34f

--- src/Profile/Profiler.cpp
+++ src/Profile/Profiler.cpp
@@ -1659,7 +1659,7 @@ int TauProfiler_StoreData(int tid)
   TAU_VERBOSE("TAU<%d,%d>: TauProfiler_StoreData 5\n", RtsLayer::myNode(), tid);
   /*Invoke plugins only if both plugin path and plugins are specified
    *Do this first, because the plugin can write TAU_METADATA as recommendations to the user*/
-  if(Tau_plugins_enabled.end_of_execution) {
+  if(RtsLayer::myThread() == 0 && tid == 0 && Tau_plugins_enabled.end_of_execution) {
     Tau_plugin_event_end_of_execution_data_t plugin_data;
     plugin_data.tid = tid;
     Tau_util_invoke_callbacks(TAU_PLUGIN_EVENT_END_OF_EXECUTION, "*", &plugin_data);
