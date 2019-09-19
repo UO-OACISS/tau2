681362ef68b88b89e4787150bbea689d5b428bfc
Jordi Alcaraz <jalcaraz@uoregon.edu>
2019-09-19 10:43:11 -0700

    Updated report_compile.sh, only reads SOSDIR from ../../include/Makefile
    Former-commit-id: 0cfede8ecdd2ffacbb8d1daa8a52a7361f07abae

--- examples/mm_sos/report_compile.sh
+++ examples/mm_sos/report_compile.sh
@@ -1,5 +1,2 @@
-architecture="$(grep -oP '(?<=TAU_ARCH=).*' ../../include/Makefile)"
-#MY_TAU_PATH="$(dirname $(dirname  `pwd`))"
-MY_TAU_PATH="$(grep -oP '(?<=TAUROOT=).*' ../../include/Makefile)"
-SOS_PATH=$MY_TAU_PATH/$architecture/sos/sos_flow_master/inst
+SOS_PATH="$(grep -oP '(?<=SOSDIR=).*' ../../include/Makefile)"
 mpicc report.c -L$SOS_PATH/lib -lsos -Wl,-rpath,$SOS_PATH/lib -I$SOS_PATH/include/ -o report
