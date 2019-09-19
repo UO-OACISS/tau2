6ddf587bfda6d7ee32411813bab9c2ee1092eeb4
Jordi Alcaraz <jalcaraz@uoregon.edu>
2019-09-19 10:15:05 -0700

    Removed local paths, read them from ../../include/Makefile
    Former-commit-id: fcac0425bbb54e6da57813fda036aeb5435e0325

--- examples/mm_sos/report_compile.sh
+++ examples/mm_sos/report_compile.sh
@@ -1,2 +1,5 @@
-export SOS_PATH=/home/users/jalcaraz/tau2_merge1_cfgmodv2/x86_64/sos/sos_flow_master/inst
+architecture="$(grep -oP '(?<=TAU_ARCH=).*' ../../include/Makefile)"
+#MY_TAU_PATH="$(dirname $(dirname  `pwd`))"
+MY_TAU_PATH="$(grep -oP '(?<=TAUROOT=).*' ../../include/Makefile)"
+SOS_PATH=$MY_TAU_PATH/$architecture/sos/sos_flow_master/inst
 mpicc report.c -L$SOS_PATH/lib -lsos -Wl,-rpath,$SOS_PATH/lib -I$SOS_PATH/include/ -o report
