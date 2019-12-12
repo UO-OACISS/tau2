b93965aabb478e3f5e719873b58c99591523ea28
Kevin Huck <khuck@cs.uoregon.edu>
2019-12-12 14:20:36 -0800

    Updating monitoring component to use ADIOS2 streaming output, and adding a python post-processing script.
    Former-commit-id: 214231e7ab2318b393c24abdbec9943475632a2b

--- examples/papi_component_plugin/go.sh
+++ examples/papi_component_plugin/go.sh
@@ -1,29 +1,32 @@
 #!/bin/bash -e
 
-rm -f core.*
-rm -rf profile.* MULTI__*
+source sourceme_mpi.sh
 
-# launch the program with the plugin
-echo "With PAPI component plugin environment variables"
-T="$(date +%s)"
-export TAU_PLUGINS=libTAU-papi-components-plugin.so
-export TAU_PLUGINS_PATH=../../x86_64/lib/shared-papi-mpi-pthread
-mpirun -np 4 ./matmult
-A="$(($(date +%s)-T))"
-printf "Time to run tau: %02d hours %02d minutes %02d seconds.\n" "$((A/3600))" "$((A/60%60))" "$((A%60))"
-unset TAU_PLUGINS
-unset TAU_PLUGINS_PATH
+old_method() {
+    rm -f core.*
+    rm -rf profile.* MULTI__* tauprofile-matmult.bp tauprofile.xml
+    # launch the program with the plugin
+    echo "With PAPI component plugin environment variables"
+    T="$(date +%s)"
+    export TAU_PLUGINS=libTAU-papi-components-plugin.so
+    export TAU_PLUGINS_PATH=../../x86_64/lib/shared-papi-mpi-pthread
+    mpirun -np 4 ./matmult
+    A="$(($(date +%s)-T))"
+    printf "Time to run tau: %02d hours %02d minutes %02d seconds.\n" "$((A/3600))" "$((A/60%60))" "$((A%60))"
+    unset TAU_PLUGINS
+    unset TAU_PLUGINS_PATH
+}
 
-#kill -INT $$
+new_method() {
+    rm -f core.*
+    rm -rf profile.* MULTI__* tauprofile-matmult.bp tauprofile.xml
+    # launch the program with tau_exec
+    echo "With PAPI component plugin tau_exec"
+    T="$(date +%s)"
+    mpirun -np 2 tau_exec -T papi,mpi,pthread,adios2 -adios2 -papi_components ./matmult
+    A="$(($(date +%s)-T))"
+    printf "Time to run tau: %02d hours %02d minutes %02d seconds.\n" "$((A/3600))" "$((A/60%60))" "$((A%60))"
+}
 
-rm -f core.*
-rm -rf profile.* MULTI__*
-
-# launch the program with tau_exec
-echo "With PAPI component plugin tau_exec"
-T="$(date +%s)"
-mpirun -np 4 tau_exec -T papi,mpi,pthread -papi_components ./matmult
-A="$(($(date +%s)-T))"
-printf "Time to run tau: %02d hours %02d minutes %02d seconds.\n" "$((A/3600))" "$((A/60%60))" "$((A%60))"
-
+new_method
 
