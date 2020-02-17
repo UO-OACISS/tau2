3b608a996fcddfda07df4e4b93bad646cea4c14f
Kevin Huck <khuck@cs.uoregon.edu>
2020-02-17 12:40:16 -0800

    Updating the example to parse the callgraph and write a graphviz graph
    Former-commit-id: 1dbda7b9eb0a6f895c61059304f3a832a3fe004c

--- examples/plugin/sqlite3/go.sh
+++ examples/plugin/sqlite3/go.sh
@@ -3,8 +3,13 @@
 #make clean
 #make -j
 
+rm -f tauprofile.db
+export TAU_CALLPATH=1
+export TAU_CALLPATH_DEPTH=100
+
 tau_exec -T pthread,serial -sqlite3 ./matmult
 tau_exec -T pthread,serial -sqlite3 ./matmult
 tau_exec -T pthread,serial -sqlite3 ./matmult
 
 sqlite3 tauprofile.db < check.sql
+./parser.py
