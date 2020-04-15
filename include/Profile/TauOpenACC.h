281c46717b9267797ee03ae27d96001de0348486
Kevin Huck <khuck@cs.uoregon.edu>
2020-04-15 16:27:30 -0700

    Removing CUDA headers from OpenACC header
    Former-commit-id: 0fe9bc3d26c207aa876167db94ad4ff57f2aadf4

--- include/Profile/TauOpenACC.h
+++ include/Profile/TauOpenACC.h
@@ -2,9 +2,6 @@
 #define TAU_OPENACC_H
 
 #include <Profile/TauGpu.h>
-//#include <Profile/TauGpuAdapterOpenACC.h>
-#include <cuda.h>
-#include <cupti.h>
 
 extern "C" void Tau_openacc_register_gpu_event(
   const char* name,                                                                                                                      uint32_t device,                                                                                                                       uint32_t stream,                                                                                                                       uint32_t context,
