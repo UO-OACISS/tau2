d27772bacc1f3c957542700a88ed0331639f11b2
Kevin Huck <khuck@cs.uoregon.edu>
2021-04-21 22:03:24 -0700

    removing troublesome wildcard support
    Former-commit-id: 17e02d4c0ab1acb9891355b7eee8302830da9fc2

--- plugins/examples/Tau_plugin_pthread_stopper.cpp
+++ plugins/examples/Tau_plugin_pthread_stopper.cpp
@@ -30,7 +30,6 @@ class plugin_options {
     public:
         bool env_use_selection;
         std::set<std::string> excluded_timers;
-        std::set<std::string> excluded_timers_with_wildcards;
         static plugin_options& thePluginOptions() {
             static plugin_options tpo;
             return tpo;
@@ -78,11 +77,7 @@ void Tau_stopper_parse_selection_file(const char * filename) {
             excluding_timers = false;
         } else {
             if (excluding_timers) {
-                if (str.find("#") == std::string::npos && str.find("?") == std::string::npos) {
-                    thePluginOptions().excluded_timers.insert(str);
-                } else {
-                    thePluginOptions().excluded_timers_with_wildcards.insert(str);
-                }
+                thePluginOptions().excluded_timers.insert(str);
             }
         }
     }
@@ -141,78 +136,6 @@ extern "C" int Tau_plugin_init_func(int argc, char **argv, int id) {
     return 0;
 }
 
-// C++ program to implement wildcard
-// pattern matching algorithm
-// from: https://www.geeksforgeeks.org/wildcard-pattern-matching/
-#if defined(__APPLE__) && defined(__clang__)
-// do nothing
-#else
-#include <bits/stdc++.h>
-#endif
-using namespace std;
-
-// Function that matches input str with
-// given wildcard pattern
-bool strmatch(const char str[], const char pattern[],
-              int n, int m)
-{
-    // empty pattern can only match with
-    // empty string
-    if (m == 0)
-        return (n == 0);
-
-    // lookup table for storing results of
-    // subproblems
-    bool lookup[n + 1][m + 1]; // = {false};
-	// PGI compiler doesn't like initialization during declaration...
-    for (int i = 0; i <= n; i++) {
-        for (int j = 0; j <= m; j++) {
-            lookup[i][j] = false;
-		}
-	}
-
-    // initailze lookup table to false
-    //memset(lookup, false, sizeof(lookup));
-
-    // empty pattern can match with empty string
-    lookup[0][0] = true;
-
-    // Only '#' can match with empty string
-    for (int j = 1; j <= m; j++)
-        if (pattern[j - 1] == '#')
-            lookup[0][j] = lookup[0][j - 1];
-
-    // fill the table in bottom-up fashion
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= m; j++)
-        {
-            // Two cases if we see a '#'
-            // a) We ignore ‘#’ character and move
-            //    to next  character in the pattern,
-            //     i.e., ‘#’ indicates an empty sequence.
-            // b) '#' character matches with ith
-            //     character in input
-            if (pattern[j - 1] == '#')
-                lookup[i][j] = lookup[i][j - 1] ||
-                               lookup[i - 1][j];
-
-            // Current characters are considered as
-            // matching in two cases
-            // (a) current character of pattern is '?'
-            // (b) characters actually match
-            else if (pattern[j - 1] == '?' ||
-                    str[i - 1] == pattern[j - 1])
-                lookup[i][j] = lookup[i - 1][j - 1];
-
-            // If characters don't match
-            else lookup[i][j] = false;
-        }
-    }
-
-    return lookup[n][m];
-}
-
 } // end namespace
 
 using namespace tau_plugin;
@@ -225,17 +148,6 @@ bool skip_timer(const char * key) {
     // check to see if this label is excluded
     if (Tau_stopper_contains(thePluginOptions().excluded_timers, key, false)) {
         return true;
-    } else {
-        // check to see if it's in the excluded wildcards
-        for (std::set<std::string>::iterator
-                it=thePluginOptions().excluded_timers_with_wildcards.begin();
-             it!=thePluginOptions().excluded_timers_with_wildcards.end(); ++it) {
-            if (strmatch(key, it->c_str(), strlen(key), it->length())) {
-                // make the lookup faster next time
-                thePluginOptions().excluded_timers.insert(key);
-                return true;
-            }
-        }
     }
     // by default, don't skip it.
     return false;
