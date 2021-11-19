07a5956ef71bfa4193721772b0ef6ca958d5a253
Wyatt Spear <wspear@cs.uoregon.edu>
2021-11-19 07:14:12 -0800

    Reverted opari fix. Breaks other thread cases.
    Former-commit-id: ac25c93104d781877e560b90cac56ba6334b79cb

--- include/Profile/FunctionInfo.h
+++ include/Profile/FunctionInfo.h
@@ -192,7 +192,8 @@ std::mutex fInfoVectorMutex;
 //
 // This keeps a sequential ID number for each FunctionInfo instance.
 // This is used an an index into the static thread_local MetricThreadCache.
-static thread_local FMetricListVector MetricThreadCache; // One entry per instance
+static thread_local vector<FunctionMetrics*> MetricThreadCache; // One entry per instance
+//static thread_local FMetricListVector MetricThreadCache; // One entry per instance #Fixes opari bug, breaks pthreads
 static std::atomic<uint64_t> next_id; // The next available ID; incremented when function_info_id is set.
 uint64_t function_info_id; // This is set in FunctionInfo::FunctionInfoInit()
 static bool use_metric_tls; // This is set to false to disable the thread-local cache during shutdown.
