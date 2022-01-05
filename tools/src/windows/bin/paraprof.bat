09cf4a7aa75f05e3cae080d33bf63337fac21fa8
Wyatt Spear <wspear@cs.uoregon.edu>
2022-01-05 14:01:29 -0800

    Removed debug output
    Former-commit-id: 9cd0361fa3bff4db0724cef3083c421577705a8e

--- tools/src/windows/bin/paraprof.bat
+++ tools/src/windows/bin/paraprof.bat
@@ -20,11 +20,10 @@ if %ERRORLEVEL% EQU 0 goto SETJARS
 echo.
 echo Warning: Unsupported Java detected. Some features may fail. Please use Sun/Oracle Java.
 echo.
+
 :SETJARS
 set JARS=%JAR_ROOT%/paraprof.jar;%JAR_ROOT%/vis-jogl2.jar;%PERFDMF_JAR%;%JAR_ROOT%/jogl-all.jar;%JAR_ROOT%/gluegen-rt.jar;%JAR_ROOT%/jgraph.jar;%JDBC_JAR%;%JAR_ROOT%/jargs.jar;%JAR_ROOT%/epsgraphics.jar;%JAR_ROOT%/batik-combined.jar;%JAR_ROOT%/tau-common.jar;%JAR_ROOT%/jfreechart-1.0.12.jar;%JAR_ROOT%/jcommon-1.0.15.jar;%JAR_ROOT%/xerces.jar;%JAR_ROOT%/mesp.jar;%JAR_ROOT%/CubeReader.jar
-REM echo %JARS%
 echo.
-echo java -Xmx500m -Dsun.java2d.opengl=true -Djava.library.path=%JAR_ROOT% -Dderby.system.home="%HOMEPATH%/.ParaProf" -cp %JARS% edu/uoregon/tau/paraprof/ParaProf -j %JAR_ROOT% -c %ETC% %1 %2 %3 %4 %5
 
 java -Xmx500m -Dsun.java2d.opengl=true -Djava.library.path=%JAR_ROOT% -Dderby.system.home="%HOMEPATH%/.ParaProf" -cp %JARS% edu/uoregon/tau/paraprof/ParaProf -j %JAR_ROOT% -c %ETC% %1 %2 %3 %4 %5
 
