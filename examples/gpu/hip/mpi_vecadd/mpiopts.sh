92fcca070d1b9e1081bb5f853c56928feb05f2bd
Sameer Shende <sameer@cs.uoregon.edu>
2022-01-10 15:15:10 -0500

    If mpicc is not in your path (as is the case with PrgEnv-cray), it adds the right set of MPI libraries to the command line. If you are using MVAPICH2 on Cray, and mpicc is already is in your path, it doesn't add those libraries from Cray, but gets the right MVAPICH2 libs in the link line.
    Former-commit-id: 8ea27bdb81461057b54bc1137065ad3a8ff97921

--- examples/gpu/hip/mpi_vecadd/mpiopts.sh
+++ examples/gpu/hip/mpi_vecadd/mpiopts.sh
@@ -2,7 +2,12 @@
 
 if [ "x$PE_MPICH_GTL_DIR_amd_gfx908" != "x" ]; then 
   MPI_EXTRA_OPTS="${PE_MPICH_GTL_DIR_amd_gfx908} ${PE_MPICH_GTL_LIBS_amd_gfx908} "
-  export PATH=${CRAY_MPICH_PREFIX}/bin:$PATH
+  mpicc_path=`which mpicc 2>/dev/null`
+  if [ $? == 0  -a "x$mpicc_path" != "x" ]; then
+    mpicc_path=""
+  else
+    export PATH=${CRAY_MPICH_PREFIX}/bin:$PATH
+  fi
 fi
 
 MPIOPTS=`mpicc -show | awk '{$1=""}1'  `
