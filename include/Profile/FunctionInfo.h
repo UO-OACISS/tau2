239edc5f0cdaea020b7f77871a0ab96050f3716d
Wyatt Spear <wspear@cs.uoregon.edu>
2022-02-10 16:59:18 -0800

    Testing disabling local thread cache
    Former-commit-id: 97864454cf6ab9ef0c02ec06867a8451e3600b30

--- include/Profile/FunctionInfo.h
+++ include/Profile/FunctionInfo.h
@@ -224,7 +224,7 @@ FunctionMetrics* getFunctionMetric(unsigned int tid){
     // Also don't use the cache during shutdown -- it might have been destructed already,
     // but we can't put a destructor trigger on MetricThreadCache because they are *also*
     // destructed when a thread exits.
-    if(tid!=0 && use_metric_tls && (tid == local_tid) && !destructed && !destructed_local) {
+    if(false && tid!=0 && use_metric_tls && (tid == local_tid) && !destructed && !destructed_local) {
         if(MetricThreadCache.size() > function_info_id) {
             MOut = MetricThreadCache.operator[](function_info_id);
             if(MOut != NULL) {
