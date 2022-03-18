fca7a169899e3644e0b573916d479246421848a2
Sameer Shende <sameer@cs.uoregon.edu>
2022-03-17 22:28:34 -0400

    This does not add Cray MPI libraries when MVAPICH2 mpicc is in your path on a Cray.
    Former-commit-id: 9dbf9744448c548eec3929216f73b6776db35069

--- examples/gpu/hip/mpi_vecadd/mpiopts.sh
+++ examples/gpu/hip/mpi_vecadd/mpiopts.sh
@@ -5,6 +5,10 @@ if [ "x$PE_MPICH_GTL_DIR_amd_gfx908" != "x" ]; then
   mpicc_path=`which mpicc 2>/dev/null`
   if [ $? == 0  -a "x$mpicc_path" != "x" ]; then
     mpicc_path=""
+    is_cray_mpi=`which mpicc | grep lmpi_cray | wc -l`
+    if [ "x$is_cray_mpi" = "x0" ]; then
+      MPI_EXTRA_OPTS=""
+    fi
   else
     export PATH=${CRAY_MPICH_PREFIX}/bin:$PATH
   fi
