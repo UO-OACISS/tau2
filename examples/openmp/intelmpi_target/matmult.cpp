c13fdfb04d16a05fa5ead115df74fd76a0f5c9da
Sameer Shende <sameer@cs.uoregon.edu>
2022-04-19 16:36:19 -0700

    Added MPI_Comm_size to ensure that MPI is being used properly and it is not spawning just two serial programs.
    Former-commit-id: 6a076964d75d2a7cd3870e63a55bbfe3fd7af224

--- examples/openmp/intelmpi_target/matmult.cpp
+++ examples/openmp/intelmpi_target/matmult.cpp
@@ -47,14 +47,17 @@ int main(int argc, char **argv) {
   int rc; 
   rc = MPI_Init(&argc, &argv);
     
-  int rank = 0;
+  int size, rank = 0;
+
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
   if (rank == 0) {
       if (provided == MPI_THREAD_MULTIPLE) {
         printf("provided is MPI_THREAD_MULTIPLE\n");
       } else if (provided == MPI_THREAD_FUNNELED) {
         printf("provided is MPI_THREAD_FUNNELED\n");
       }
+      printf("MPI size = %d\n", size);
   }
 #endif /* TAU_MPI */
 
