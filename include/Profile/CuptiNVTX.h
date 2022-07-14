749d08743c1be7c67343beb1443025b8441f5fd8
Jordi Alcaraz <jalcaraz@uoregon.edu>
2022-07-14 12:55:07 -0700

    Wrappers working. Named domain with nvtxDoaminCreate, returning null domain handle. Trying to fix.
    Former-commit-id: 76fb34371009230ffa0ba5ec428503f72f485409

--- include/Profile/CuptiNVTX.h
+++ include/Profile/CuptiNVTX.h
@@ -3,19 +3,106 @@
 #include <cuda.h>
 #include <cupti.h>
 #include <map>
+#include <stack>
 #include <iostream>
 #include <sstream>
 #include <nvToolsExt.h>
 #include <nvToolsExtSync.h>
 #include <generated_nvtx_meta.h>
+/*
+https://gitlab.com/nvidia/headers/cuda-individual/nvtx/-/blob/0570d3f68e5bd2f3c97b0c928930534ca0230f9a/nvToolsExt.h
+https://nvidia.github.io/NVTX/doxygen/group___m_a_r_k_e_r_s___a_n_d___r_a_n_g_e_s.html
+https://github.com/UO-OACISS/apex/blob/eda271d93bd6871028cbcc037ce173e29bf9e934/src/apex/cupti_trace.cpp
+https://docs.nvidia.com/gameworks/content/gameworkslibrary/nvtx/nvtx_api_events.htm
+https://docs.nvidia.com/cuda/profiler-users-guide/index.html#nvtx
+
+https://github.com/UO-OACISS/apex/blob/eda271d93bd6871028cbcc037ce173e29bf9e934/src/apex/cupti_trace.cpp
+
+
+store_sync_counter_data(nullptr, tmp, 0); --> Tau_userevent
+https://github.com/UO-OACISS/tau2/blob/dd9689beb1360632576ac38deaa0dc5de62e077e/src/Profile/TauCAPI.cpp#L2036-L2040
+https://gitlab.com/nvidia/headers/cuda-individual/nvtx/-/blob/0570d3f68e5bd2f3c97b0c928930534ca0230f9a/nvToolsExt.h#L481-491
+*/
+
+//Apex CUpti_trace.cpp
+/* Fun!  CUPTI doesn't do callbacks for end or push events.  Wheeeeee
+ * So, what we'll do is wrap the functions instead of having callbacks. */
+
+#define TAU_BROKEN_CUPTI_NVTX_PUSH_POP 1
+
+
+// Ranges defined by Start/End can overlap. 
+// Therefore, they are not adapted for TAU and no measurement is performed.
+// Ranges defined by Push/Pop are safe.
+
+
+
+
+//Typedefs for wrappers
+#ifdef TAU_BROKEN_CUPTI_NVTX_PUSH_POP
+
+typedef NVTX_DECLSPEC int NVTX_API (*nvtxRangePushA_p)(const char * message);
+typedef NVTX_DECLSPEC int NVTX_API (*nvtxRangePushW_p)(const wchar_t * message);
+typedef NVTX_DECLSPEC int NVTX_API (*nvtxRangePushEx_p)(const nvtxEventAttributes_t *eventAttrib);
+typedef NVTX_DECLSPEC int NVTX_API (*nvtxRangePop_p)(void);
+typedef nvtxDomainHandle_t (*nvtxDomainCreateA_p)(const char * name);
+typedef nvtxDomainHandle_t (*nvtxDomainCreateW_p)(const wchar_t* name);
+typedef void (*nvtxDomainDestroy_p)(nvtxDomainHandle_t domain);
+typedef NVTX_DECLSPEC int NVTX_API (*nvtxDomainRangePushEx_p)(nvtxDomainHandle_t domain, const nvtxEventAttributes_t *eventAttrib);
+typedef NVTX_DECLSPEC int NVTX_API (*nvtxDomainRangePop_p)(nvtxDomainHandle_t domain);
+
+#endif //TAU_BROKEN_CUPTI_NVTX_PUSH_POP
+
+
+//Auxiliary 
 
 std::map<nvtxDomainHandle_t, std::string>& get_domain_map();
+//std::map<nvtxRangeId_t, std::string>& get_range_map();
+std::stack<std::string>& get_range_stack();
+
+static void * get_system_function_handle(char const * name, void * caller);
 std::string get_nvtx_message(const nvtxEventAttributes_t * eventAttrib);
+
+void tau_nvtxRangePush (const std::string name);
+void tau_nvtxRangePop ();
+
+
+
+
+//Wrappers & Interceptors
+
+
+#ifdef TAU_BROKEN_CUPTI_NVTX_PUSH_POP
+int tau_nvtxRangePushA_wrapper (nvtxRangePushA_p nvtxRangePushA_call, const char * message);
+int tau_nvtxRangePushW_wrapper (nvtxRangePushW_p nvtxRangePushW_call, const wchar_t *message);
+int tau_nvtxRangePushEx_wrapper (nvtxRangePushEx_p nvtxRangePushEx_call, const nvtxEventAttributes_t *eventAttrib);
+int tau_nvtxRangePop_wrapper (nvtxRangePop_p nvtxRangePop_call);
+
 NVTX_DECLSPEC int NVTX_API nvtxRangePushA (const char *message);
 NVTX_DECLSPEC int NVTX_API nvtxRangePushW (const wchar_t *message);
 NVTX_DECLSPEC int NVTX_API nvtxRangePushEx (const nvtxEventAttributes_t *eventAttrib);
 NVTX_DECLSPEC int NVTX_API nvtxRangePop (void);
-NVTX_DECLSPEC nvtxRangeId_t NVTX_API nvtxDomainRangeStartEx  (nvtxDomainHandle_t domain, const nvtxEventAttributes_t *eventAttrib);
-NVTX_DECLSPEC void NVTX_API 	nvtxDomainRangeEnd (nvtxDomainHandle_t domain, nvtxRangeId_t id);
+
+//TO TEST
+
+nvtxDomainHandle_t tau_nvtxDomainCreateA_wrapper (nvtxDomainCreateA_p nvtxDomainCreateA_call, const char* name);
+nvtxDomainHandle_t tau_nvtxDomainCreateW_wrapper (nvtxDomainCreateW_p nvtxDomainCreateW_call, const wchar_t* name);
+NVTX_DECLSPEC nvtxDomainHandle_t NVTX_API nvtxDomainCreateA(const char* name);
+NVTX_DECLSPEC nvtxDomainHandle_t NVTX_API nvtxDomainCreateW(const wchar_t* name);
+
+void tau_nvtxDomainDestroy_wrapper (nvtxDomainDestroy_p nvtxDomainDestroy_call, nvtxDomainHandle_t domain);
+NVTX_DECLSPEC void NVTX_API nvtxDomainDestroy(nvtxDomainHandle_t domain);
+
+
+int tau_nvtxDomainRangePushEx_wrapper (nvtxDomainRangePushEx_p nvtxDomainRangePushEx_call, nvtxDomainHandle_t domain, const nvtxEventAttributes_t* eventAttrib);
+int tau_nvtxDomainRangePop_wrapper (nvtxDomainRangePop_p nvtxDomainRangePop_call, const wchar_t *message);
+NVTX_DECLSPEC int NVTX_API nvtxDomainRangePushEx(nvtxDomainHandle_t domain, const nvtxEventAttributes_t *eventAttrib);
+NVTX_DECLSPEC int NVTX_API nvtxDomainRangePop(nvtxDomainHandle_t domain);
+
+#endif //TAU_BROKEN_CUPTI_NVTX_PUSH_POP
+
+
+//Handle for CUPTI_CB_DOMAIN_NVTX
+void handle_nvtx_callback(CUpti_CallbackId id, const void *cbdata);
 
 #endif //CUPTI_NVTX
--- src/Profile/CuptiNVTX.cpp
+++ src/Profile/CuptiNVTX.cpp
@@ -1,7 +1,8 @@
 //#include <Profile/CuptiActivity.h>
 //#include <Profile/CuptiLayer.h>
-//#include <Profile/TauMetaData.h>
-//#include <Profile/TauBfd.h>
+#include <TAU.h>
+#include <Profile/TauMetaData.h>
+#include <Profile/TauBfd.h>
 //#include <Profile/TauPluginInternals.h>
 //#include <Profile/TauPluginCPPTypes.h>
 #include <iostream>
@@ -10,18 +11,97 @@
 #include <assert.h>
 #define __STDC_FORMAT_MACROS
 #include <inttypes.h>
-
+#include <dlfcn.h>
 #include <Profile/CuptiNVTX.h>
 
 
 #define NVTX_DEBUG_ENV
 
-//-----------------------------------------------------------------------
+
+
+
+
+//--------------------------
+
+ostream & operator<<(ostream & os, stack<std::string> my_stack) //function header
+{
+    while(!my_stack.empty()) //body
+    {
+        os << my_stack.top() << " ";
+        my_stack.pop();
+    }
+    return os; // end of function
+}
+
+//--------------------------
+
+
+
+
+#define RESET_DLERROR() dlerror()
+#define CHECK_DLERROR() { \
+  char const * err = dlerror(); \
+  if (err) { \
+    printf("Error getting %s handle: %s\n", name, err); \
+    fflush(stdout); \
+    exit(1); \
+  } \
+}
+
+
+
+
 
 
 std::map<nvtxDomainHandle_t, std::string>& get_domain_map() {
     static std::map<nvtxDomainHandle_t, std::string> the_map;
     return the_map;
+}
+
+/*std::map<nvtxRangeId_t, std::string>& get_range_map() {
+    static std::map<nvtxRangeId_t, std::string> the_map;
+    return the_map;
+}*/
+
+std::stack<std::string>& get_range_stack() {
+    static thread_local std::stack<std::string> the_stack;
+    return the_stack;
+}
+
+
+//-----------------------
+
+
+
+
+static void * get_system_function_handle(char const * name, void * caller)
+{
+  void * handle;
+
+  // Reset error pointer
+  RESET_DLERROR();
+
+  // Attempt to get the function handle
+  handle = dlsym(RTLD_NEXT, name);
+
+  // Detect errors
+  CHECK_DLERROR();
+
+  // Prevent recursion if more than one wrapping approach has been loaded.
+  // This happens because we support wrapping pthreads three ways at once:
+  // #defines in Profiler.h, -Wl,-wrap on the link line, and LD_PRELOAD.
+  if (handle == caller) {
+    RESET_DLERROR();
+    void * syms = dlopen(NULL, RTLD_NOW);
+    CHECK_DLERROR();
+    do {
+      RESET_DLERROR();
+      handle = dlsym(syms, name);
+      CHECK_DLERROR();
+    } while (handle == caller);
+  }
+
+  return handle;
 }
 
 
@@ -38,76 +118,215 @@ std::string get_nvtx_message(const nvtxEventAttributes_t * eventAttrib) {
 }
 
 
+void tau_nvtxRangePush (const std::string name) {
+    TAU_START(name.c_str());
 
+    #ifdef NVTX_DEBUG_ENV
+        std::cout << "TAU-NVTX " << "STACK content before push : " << get_range_stack() << std::endl;
+    #endif
 
+    get_range_stack().push(name);
 
+    #ifdef NVTX_DEBUG_ENV
+        std::cout << "TAU-NVTX " << "STACK content after push: " << get_range_stack() << std::endl;
+    #endif
+}
 
-
-
+void tau_nvtxRangePop () {
+    if (!get_range_stack().empty()) {
+        auto timer = get_range_stack().top();
+        #ifdef NVTX_DEBUG_ENV
+            std::cout << "TAU-NVTX " << "nvtxRangePop  (" << timer << ")!!!!!!!!!!!!!!!!!!!" << std::endl;
+        #endif
+        TAU_STOP(timer.c_str());
+        #ifdef NVTX_DEBUG_ENV
+            std::cout << "TAU-NVTX " << "STACK content before pop: " << get_range_stack() << std::endl;
+        #endif
+        get_range_stack().pop();
+        #ifdef NVTX_DEBUG_ENV
+            std::cout << "TAU-NVTX " << "STACK content after pop: " << get_range_stack() << std::endl;
+        #endif
+    }
+}
 
 //---------
-//Mover a wrappers/nvtx.h
-  /* Define the interceptor for nvtxRangePushA */
-  NVTX_DECLSPEC int NVTX_API nvtxRangePushA (const char *message) {
-	  #ifdef NVTX_DEBUG_ENV
-	  std::cout << "TAU-NVTX " << "nvtxRangePushA ( " << message << ") !!!!!!!!!!!!!!!!!!!!!!!!!" << std::endl;
-	  #endif
-		
-  }
-  
-  
-  /* Define the interceptor for nvtxRangePushW */
-  NVTX_DECLSPEC int NVTX_API nvtxRangePushW (const wchar_t *message) {
-		
-		std::wstring wtmp(message);
-		std::string tmp = std::string(wtmp.begin(), wtmp.end());
-		#ifdef NVTX_DEBUG_ENV
-		std::cout << "TAU-NVTX " << "nvtxRangePushW ( " << tmp << ") !!!!!!!!!!!!!!!!!!!!!!!!!" << std::endl;
-		#endif
-  }
-  
-    /* Define the interceptor for nvtxRangePushEx */
-  NVTX_DECLSPEC int NVTX_API nvtxRangePushEx (const nvtxEventAttributes_t *eventAttrib) {
-		
-		std::string tmp{get_nvtx_message(eventAttrib)};
-		#ifdef NVTX_DEBUG_ENV
-		std::cout << "TAU-NVTX " << "nvtxRangePushW ( " << tmp << ") !!!!!!!!!!!!!!!!!!!!!!!!!" << std::endl;
-		#endif
-  }
+//Mover a wrappers/nvtx.h?
 
+
+#ifdef TAU_BROKEN_CUPTI_NVTX_PUSH_POP
+
+/* Define the wrapper for nvtxRangePushA */
+int tau_nvtxRangePushA_wrapper (nvtxRangePushA_p nvtxRangePushA_call, const char * message) {
+    auto handle = nvtxRangePushA_call(message);
+    #ifdef NVTX_DEBUG_ENV
+    std::cout << "TAU-NVTX " << "nvtxRangePushA ( " << message << ") !!!!!!!!!!!!!!!!!!!!!!!!!" << std::endl;
+    #endif
+    std::string tmp{message};
+    tau_nvtxRangePush(tmp);
+    return handle;
+}
+
+/* Define the interceptor for nvtxRangePushA */
+NVTX_DECLSPEC int NVTX_API nvtxRangePushA (const char *message) {
+    static nvtxRangePushA_p _nvtxRangePushA = 
+        (nvtxRangePushA_p)(get_system_function_handle("nvtxRangePushA", (void*)(nvtxRangePushA)));
+    return tau_nvtxRangePushA_wrapper(_nvtxRangePushA, message);
+}
+
+/* Define the wrapper for nvtxRangePushW */
+int tau_nvtxRangePushW_wrapper (nvtxRangePushW_p nvtxRangePushW_call, const wchar_t *message) {
+    auto handle = nvtxRangePushW_call(message);
+    std::wstring wtmp(message);
+    std::string tmp = std::string(wtmp.begin(), wtmp.end());
+    #ifdef NVTX_DEBUG_ENV
+    std::cout << "TAU-NVTX " << "nvtxRangePushW ( " << tmp << ") !!!!!!!!!!!!!!!!!!!!!!!!!" << std::endl;
+    #endif
+    tau_nvtxRangePush(tmp);
+    return handle;
+}
+  
+/* Define the interceptor for nvtxRangePushW */
+// Not currentyle supported https://docs.nvidia.com/gameworks/content/gameworkslibrary/nvtx/nvtx_api_events.htm
+// Treated as nvtxRangePushA
+NVTX_DECLSPEC int NVTX_API nvtxRangePushW (const wchar_t *message) {
+	static nvtxRangePushW_p _nvtxRangePushW = 
+        (nvtxRangePushW_p)(get_system_function_handle("nvtxRangePushW", (void*)(nvtxRangePushW)));
+    return tau_nvtxRangePushW_wrapper(_nvtxRangePushW, message);
+
+}
+
+/* Define the wrapper for nvtxRangePushEx */
+int tau_nvtxRangePushEx_wrapper (nvtxRangePushEx_p nvtxRangePushEx_call, const nvtxEventAttributes_t *eventAttrib) {
+    auto handle = nvtxRangePushEx_call(eventAttrib);
+    std::string tmp{get_nvtx_message(eventAttrib)};
+    #ifdef NVTX_DEBUG_ENV
+        std::cout << "TAU-NVTX " << "nvtxRangePushEx ( " << tmp << ") !!!!!!!!!!!!!!!!!!!!!!!!!" << std::endl;
+    #endif
+    tau_nvtxRangePush(tmp);
+    return handle;
+}
+  
+/* Define the interceptor for nvtxRangePushEx */
+NVTX_DECLSPEC int NVTX_API nvtxRangePushEx (const nvtxEventAttributes_t *eventAttrib) {
+	static nvtxRangePushEx_p _nvtxRangePushEx = 
+        (nvtxRangePushEx_p)(get_system_function_handle("nvtxRangePushEx", (void*)(nvtxRangePushEx)));
+    return tau_nvtxRangePushEx_wrapper(_nvtxRangePushEx, eventAttrib);
+
+	
+
+}
+
+/* Define the wrapper for nvtxRangePop */
+int tau_nvtxRangePop_wrapper (nvtxRangePop_p nvtxRangePop_call) {
+    auto handle = nvtxRangePop_call();
+    tau_nvtxRangePop();
+    return handle;
+}
 
 /* Define the interceptor for nvtxRangePop */
-  NVTX_DECLSPEC int NVTX_API nvtxRangePop (void) {
-		#ifdef NVTX_DEBUG_ENV
-		std::cout << "TAU-NVTX " << "nvtxRangePop " << std::endl;
-		#endif
-  }
-  
+NVTX_DECLSPEC int NVTX_API nvtxRangePop (void) {
+    static nvtxRangePop_p _nvtxRangePop =
+        (nvtxRangePop_p)(get_system_function_handle("nvtxRangePop", (void*)(nvtxRangePop)));
+    return tau_nvtxRangePop_wrapper(_nvtxRangePop);
+}  
 
-/* Define the interceptor for nvtxDomainRangeStartEx */
-  NVTX_DECLSPEC nvtxRangeId_t NVTX_API nvtxDomainRangeStartEx  (nvtxDomainHandle_t domain, const nvtxEventAttributes_t *eventAttrib) {
-		std::string tmp;
-		if (domain != NULL) {
-            std::string domain_name(get_domain_map()[domain]);
-            std::stringstream ss;
-            ss <<  domain_name << ": " << get_nvtx_message(eventAttrib);
-            tmp = ss.str();
-        } else {
-            tmp = get_nvtx_message(eventAttrib);
-        }
-		#ifdef NVTX_DEBUG_ENV
-		std::cout << "TAU-NVTX " << "nvtxDomainRangeStartEx " << tmp << std::endl;
-		#endif
-  }
-  
-/* Define the interceptor for nvtxDomainRangeEnd */
-  NVTX_DECLSPEC void NVTX_API 	nvtxDomainRangeEnd (nvtxDomainHandle_t domain, nvtxRangeId_t id) {
-		#ifdef NVTX_DEBUG_ENV
-		std::cout << "TAU-NVTX " << "nvtxDomainRangeEnd " << std::endl;
-		#endif
-  }
+//To Test -------------------------------------------------------------------------------------------
+
+nvtxDomainHandle_t tau_nvtxDomainCreateA_wrapper (nvtxDomainCreateA_p nvtxDomainCreateA_call, const char* name){
+    auto handle = nvtxDomainCreateA_call(name);
+    std::string tmp{name};
+    get_domain_map().insert(std::pair<nvtxDomainHandle_t, std::string>(handle, tmp));
+	#ifdef NVTX_DEBUG_ENV
+		if (handle == NULL)
+			std:cout << "TAU-NVTX DomainCreateA wrapper obtained NULL domain handle!!!"<< std:endl;
+        std::cout << "TAU-NVTX " << "nvtxDomainCreateA ( " << tmp << ") !!!!!!!!!!!!!!!!!!!!!!!!!" << std::endl;
+    #endif
+    return handle;
+}
+NVTX_DECLSPEC nvtxDomainHandle_t NVTX_API nvtxDomainCreateA(const char* name){
+	static nvtxDomainCreateA_p _nvtxDomainCreateA =
+        (nvtxDomainCreateA_p)(get_system_function_handle("nvtxDomainCreateA", (void*)(nvtxDomainCreateA)));
+    return tau_nvtxDomainCreateA_wrapper(_nvtxDomainCreateA, name);
+}
+
+nvtxDomainHandle_t tau_nvtxDomainCreateW_wrapper (nvtxDomainCreateW_p nvtxDomainCreateW_call, const wchar_t* name){
+	auto handle = nvtxDomainCreateW_call(name);
+	std::wstring wtmp(name);
+    std::string tmp = std::string(wtmp.begin(), wtmp.end());
+    get_domain_map().insert(std::pair<nvtxDomainHandle_t, std::string>(handle, tmp));
+	#ifdef NVTX_DEBUG_ENV
+        std::cout << "TAU-NVTX " << "nvtxDomainCreateW ( " << tmp << ") !!!!!!!!!!!!!!!!!!!!!!!!!" << std::endl;
+    #endif
+    return handle;
+}
+
+NVTX_DECLSPEC nvtxDomainHandle_t NVTX_API nvtxDomainCreateW(const wchar_t* name){
+	static nvtxDomainCreateW_p _nvtxDomainCreateW =
+        (nvtxDomainCreateW_p)(get_system_function_handle("nvtxDomainCreateW", (void*)(nvtxDomainCreateW)));
+    return tau_nvtxDomainCreateW_wrapper(_nvtxDomainCreateW, name);
+}
 
 
+//---------!!!!!!!!!!!!!!!!!!!Should not implement??????????????????!!!!!!!!!!
+void tau_nvtxDomainDestroy_wrapper (nvtxDomainDestroy_p nvtxDomainDestroy_call, nvtxDomainHandle_t domain){
+	nvtxDomainDestroy_call(domain);
+	std::string domain_name(get_domain_map()[domain]);	
+    get_domain_map().erase(domain);
+	#ifdef NVTX_DEBUG_ENV
+        std::cout << "TAU-NVTX " << "nvtxDomainDestroy ( " << domain_name << ") !!!!!!!!!!!!!!!!!!!!!!!!!" << std::endl;
+    #endif
+    return;
+}
+
+NVTX_DECLSPEC void NVTX_API nvtxDomainDestroy(nvtxDomainHandle_t domain){
+	static nvtxDomainDestroy_p _nvtxDomainDestroy =
+        (nvtxDomainDestroy_p)(get_system_function_handle("nvtxDomainDestroy", (void*)(nvtxDomainDestroy)));
+    return tau_nvtxDomainDestroy_wrapper(_nvtxDomainDestroy, domain);
+}
+//----------------------------------------------
+
+int tau_nvtxDomainRangePushEx_wrapper (nvtxDomainRangePushEx_p nvtxDomainRangePushEx_call, 
+				nvtxDomainHandle_t domain, const nvtxEventAttributes_t* eventAttrib){
+	
+    auto handle = nvtxDomainRangePushEx_call(domain, eventAttrib);
+	std::string tmp;
+	if (domain != NULL) {
+		std::cout << "TAU-NVTX " << "nvtxDomainRangePushEx domain != NULL !!!!!!!!!!!!!!!!!!!!!!!!!" << std::endl;
+		std::string domain_name(get_domain_map()[domain]);
+		std::stringstream ss;
+		ss << domain_name << ": " << get_nvtx_message(eventAttrib);
+		tmp = ss.str();
+	} else {
+		std::cout << "TAU-NVTX " << "nvtxDomainRangePushEx domain == NULL !!!!!!!!!!!!!!!!!!!!!!!!!" << std::endl;
+		tmp = get_nvtx_message(eventAttrib);
+	}
+	#ifdef NVTX_DEBUG_ENV
+		std::cout << "TAU-NVTX " << "nvtxDomainRangePushEx ( " << tmp << ") !!!!!!!!!!!!!!!!!!!!!!!!!" << std::endl;
+	#endif
+	tau_nvtxRangePush(tmp);
+    return handle;
+}
+
+NVTX_DECLSPEC int NVTX_API nvtxDomainRangePushEx(nvtxDomainHandle_t domain, const nvtxEventAttributes_t *eventAttrib){
+	static nvtxDomainRangePushEx_p _nvtxDomainRangePushEx =
+        (nvtxDomainRangePushEx_p)(get_system_function_handle("nvtxDomainRangePushEx", (void*)(nvtxDomainRangePushEx)));
+    return tau_nvtxDomainRangePushEx_wrapper(_nvtxDomainRangePushEx, domain, eventAttrib);
+}
+
+int tau_nvtxDomainRangePop_wrapper (nvtxDomainRangePop_p nvtxDomainRangePop_call, nvtxDomainHandle_t domain){
+	auto handle = nvtxDomainRangePop_call(domain);
+    tau_nvtxRangePop();
+    return handle;
+	
+}
+
+NVTX_DECLSPEC int NVTX_API nvtxDomainRangePop(nvtxDomainHandle_t domain){
+	static nvtxDomainRangePop_p _nvtxDomainRangePop =
+        (nvtxDomainRangePop_p)(get_system_function_handle("nvtxDomainRangePop", (void*)(nvtxDomainRangePop)));
+    return tau_nvtxDomainRangePop_wrapper(_nvtxDomainRangePop, domain);
+}
+
+#endif //TAU_BROKEN_CUPTI_NVTX_PUSH_POP
 
 
 
