7c4c369035a1a0678c67af9b29517e2e7e212499
Kevin Huck <khuck@cs.uoregon.edu>
2023-05-30 17:46:59 -0400

    Adding ompt option
    Former-commit-id: b3dfb5ea34fbb7e3d7e07f76c52c20992153be5a

--- include/Profile/TauEnv.h
+++ include/Profile/TauEnv.h
@@ -94,6 +94,7 @@ extern "C" {
   int  TAUDECL TauEnv_set_ompt_resolve_address_eagerly(int value);
   int  TAUDECL TauEnv_get_ompt_support_level();
   int  TAUDECL TauEnv_set_ompt_support_level(int value);
+  int  TAUDECL TauEnv_get_ompt_force_finalize(void);
   int  TAUDECL TauEnv_get_signals_gdb();
   int  TAUDECL TauEnv_get_echo_backtrace();
   int  TAUDECL TauEnv_get_openmp_runtime_enabled();
