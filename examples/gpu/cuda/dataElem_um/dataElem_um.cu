681865adfd23e4db59d9817cd184f654ade9065b
Jordi Alcaraz <jalcaraz@uoregon.edu>
2024-02-14 13:14:17 -0800

    Small fix in example
    Former-commit-id: 258d7ec67dc859b3d8bd1de7e56da1b04353d488

--- examples/gpu/cuda/dataElem_um/dataElem_um.cu
+++ examples/gpu/cuda/dataElem_um/dataElem_um.cu
@@ -27,7 +27,7 @@ int main(void)
 
   e->value = 10;
   cudaMallocManaged((void**)&(e->name), sizeof(char) * (strlen("hello") + 1) );
-  strncpy(e->name,  "hello", sizeof(char) * (strlen("hello") + 1); 
+  strncpy(e->name,  "hello", sizeof(char) * (strlen("hello") + 1)); 
 
   launch(e);
 
