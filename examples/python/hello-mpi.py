6297054e583c5f2c341c13270cd2e912635c6dc6
Sameer Shende <sameer@cs.uoregon.edu>
2025-03-11 18:56:01 -0700

    Added processor name.
    Former-commit-id: 3d6059c551396801daf14ea75acb5a79b24b7116

--- examples/python/hello-mpi.py
+++ examples/python/hello-mpi.py
@@ -5,6 +5,7 @@ from mpi4py import MPI
 
 comm = MPI.COMM_WORLD
 
-print ("Hello! I'm rank %d from %d running in total..." % (comm.rank, comm.size))
+processor_name = MPI.Get_processor_name()
+print ("Hello! I'm rank %d from %d running on %s ..." % (comm.rank, comm.size, processor_name))
 
 comm.Barrier()   # wait for everybody to synchronize _here_
