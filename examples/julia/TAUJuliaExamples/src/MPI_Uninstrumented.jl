1bf784217f51764546965495665dbfe3abefc541
Nicholas Chaimov <nchaimov@paratools.com>
2026-02-17 15:28:20 -0800

    Add Julia MPI+Rewriter example
    Former-commit-id: 6cb5493acdabbf7634ccaa6d1d0c0a57e7308f83

---
+++ examples/julia/TAUJuliaExamples/src/MPI_Uninstrumented.jl
@@ -0,0 +1,31 @@
+# MPI.jl send/recv example
+# Adapted from https://juliaparallel.org/MPI.jl/stable/examples/04-sendrecv/
+
+using MPI
+MPI.Init()
+
+comm = MPI.COMM_WORLD
+rank = MPI.Comm_rank(comm)
+size = MPI.Comm_size(comm)
+
+dst = mod(rank+1, size)
+src = mod(rank-1, size)
+
+N = 4
+
+send_mesg = Array{Float64}(undef, N)
+recv_mesg = Array{Float64}(undef, N)
+
+fill!(send_mesg, Float64(rank))
+
+rreq = MPI.Irecv!(recv_mesg, comm; source=src, tag=src+32)
+
+print("$rank: Sending   $rank -> $dst = $send_mesg\n")
+sreq = MPI.Isend(send_mesg, comm; dest=dst, tag=rank+32)
+
+stats = MPI.Waitall([rreq, sreq])
+
+print("$rank: Received $src -> $rank = $recv_mesg\n")
+
+MPI.Barrier(comm)
+
