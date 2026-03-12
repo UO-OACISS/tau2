a9b874fc425a0e413853947a8a55ac46b591fd35
Jordi Alcaraz <jalcaraz@uoregon.edu>
2026-03-12 11:45:36 -0700

    Undo last commit
    Former-commit-id: e6a92e72a1ff79cb7b97c5182dd64883795cee67

--- tools/src/tau_compiler.sh
+++ tools/src/tau_compiler.sh
@@ -1297,7 +1297,8 @@ for arg in "$@" ; do
 done
 
 if [ $useNVCC == $TRUE ]; then
-  optLinking=`echo "$optLinking" | sed -e 's/-fopenmp/-Xcompiler -fopenmp/g' -e 's/-qsmp=omp/-Xcompiler -qsmp=omp/g' -e 's/-qopenmp/-Xcompiler -qopenmp/g' -e 's/-pthread/-Xcompiler -pthread/g'`
+  echo "$optLinking"
+  optLinking=`echo "$optLinking" | sed -e 's/-fopenmp/-Xcompiler -fopenmp/g' -e 's/-qsmp=omp/-Xcompiler -qsmp=omp/g' -e 's/-qopenmp/-Xcompiler -qopenmp/g' -e 's/\b-pthread\b/-Xcompiler -pthread/g'`
   echoIfDebug "Modified (after -Xcompiler substitution) optLinking = $optLinking"
 fi
 
